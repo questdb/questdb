c811f94f2de2473ba480c4355eeb41d64269488b
Vlad Ilyushchenko <bluestreak@gmail.com>
2015-06-13 14:33:27 +0100

    improved quote removal from string contants

--- nfsdb-core/src/main/java/com/nfsdb/ql/parser/IntrinsicExtractor.java
+++ nfsdb-core/src/main/java/com/nfsdb/ql/parser/IntrinsicExtractor.java
@@ -1,22 +1,22 @@
 /*******************************************************************************
- *   _  _ ___ ___     _ _
- *  | \| | __/ __| __| | |__
- *  | .` | _|\__ \/ _` | '_ \
- *  |_|\_|_| |___/\__,_|_.__/
+ *  _  _ ___ ___     _ _
+ * | \| | __/ __| __| | |__
+ * | .` | _|\__ \/ _` | '_ \
+ * |_|\_|_| |___/\__,_|_.__/
  *
- *  Copyright (c) 2014-2015. The NFSdb project and its contributors.
+ * Copyright (c) 2014-2015. The NFSdb project and its contributors.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
 package com.nfsdb.ql.parser;
 
@@ -97,7 +97,7 @@ public class IntrinsicExtractor {
 
         if (a.type == ExprNode.NodeType.LITERAL && b.type == ExprNode.NodeType.CONSTANT) {
             if (timestamp != null && timestamp.getName().equals(a.token)) {
-                boolean reversible = parseInterval(Chars.stripQuotes(b.token), b.position);
+                boolean reversible = parseInterval(quoteEraser.of(b.token), b.position);
                 node.intrinsicValue = IntrinsicValue.TRUE;
                 // exact timestamp matches will be returning FALSE
                 // which means that they are irreversible and won't be added to timestampNodes.
