1d64515297289cc6615c1f308f162f80d8af1831
Vlad Ilyushchenko <bluestreak@gmail.com>
2017-11-20 01:56:51 +0000

    OS.currentTimeMicros() was returning incorrect time.

--- core/src/main/c/share/os.c
+++ core/src/main/c/share/os.c
@@ -38,7 +38,7 @@ JNIEXPORT jlong JNICALL Java_com_questdb_std_Os_currentTimeMicros
         (JNIEnv *e, jclass cl) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000 + tv.tv_usec;
+    return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
 JNIEXPORT jint JNICALL Java_com_questdb_std_Os_errno
