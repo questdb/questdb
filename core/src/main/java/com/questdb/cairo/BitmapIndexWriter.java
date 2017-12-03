4b030b5a19eb54e80da4df09b5b7f7da43c0673e
Vlad Ilyushchenko <bluestreak@gmail.com>
2017-12-03 11:14:16 +0000

    CAIRO: transaction rollback implementation for bitmap index.

--- core/src/main/java/com/questdb/cairo/BitmapIndexWriter.java
+++ core/src/main/java/com/questdb/cairo/BitmapIndexWriter.java
@@ -39,12 +39,15 @@ public class BitmapIndexWriter implements Closeable {
     private final int blockValueCountMod;
     private long valueMemSize;
     private long keyCount;
+    private long seekValueCount;
+    private long seekValueBlockOffset;
+    private final BitmapIndexUtils.ValueBlockSeeker SEEKER = this::seek;
 
     public BitmapIndexWriter(CairoConfiguration configuration, CharSequence name, int blockCapacity) {
         long pageSize = TableUtils.getMapPageSize(configuration.getFilesFacade());
 
         try (Path path = new Path()) {
-            BitmapIndexConstants.keyFileName(path, configuration.getRoot(), name);
+            BitmapIndexUtils.keyFileName(path, configuration.getRoot(), name);
 
             boolean exists = configuration.getFilesFacade().exists(path);
             this.keyMem = new ReadWriteMemory(configuration.getFilesFacade(), path, pageSize);
@@ -54,33 +57,33 @@ public class BitmapIndexWriter implements Closeable {
 
             long keyMemSize = this.keyMem.size();
             // check if key file header is present
-            if (keyMemSize < BitmapIndexConstants.KEY_FILE_RESERVED) {
+            if (keyMemSize < BitmapIndexUtils.KEY_FILE_RESERVED) {
                 LOG.error().$("file too short [corrupt] ").$(path).$();
                 throw CairoException.instance(0).put("Index file too short: ").put(path);
             }
 
             // verify header signature
-            if (this.keyMem.getByte(BitmapIndexConstants.KEY_RESERVED_OFFSET_SIGNATURE) != BitmapIndexConstants.SIGNATURE) {
+            if (this.keyMem.getByte(BitmapIndexUtils.KEY_RESERVED_OFFSET_SIGNATURE) != BitmapIndexUtils.SIGNATURE) {
                 LOG.error().$("unknown format [corrupt] ").$(path).$();
                 throw CairoException.instance(0).put("Unknown format: ").put(path);
             }
 
             // verify key count
-            this.keyCount = this.keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_OFFSET_KEY_COUNT);
+            this.keyCount = this.keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_OFFSET_KEY_COUNT);
             if (keyMemSize != keyMemSize()) {
                 LOG.error().$("key count does not match file length [corrupt] of ").$(path).$(" [keyCount=").$(this.keyCount).$(']').$();
                 throw CairoException.instance(0).put("Key count does not match file length of ").put(path);
             }
 
             // check if sequence is intact
-            if (this.keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_SEQUENCE_CHECK) != this.keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_SEQUENCE)) {
+            if (this.keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_SEQUENCE_CHECK) != this.keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_SEQUENCE)) {
                 LOG.error().$("sequence mismatch [corrupt] at ").$(path).$();
                 throw CairoException.instance(0).put("Sequence mismatch on ").put(path);
             }
 
-            this.valueMemSize = this.keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_OFFSET_VALUE_MEM_SIZE);
+            this.valueMemSize = this.keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_OFFSET_VALUE_MEM_SIZE);
 
-            BitmapIndexConstants.valueFileName(path, configuration.getRoot(), name);
+            BitmapIndexUtils.valueFileName(path, configuration.getRoot(), name);
 
             this.valueMem = new ReadWriteMemory(configuration.getFilesFacade(), path, pageSize);
 
@@ -91,8 +94,8 @@ public class BitmapIndexWriter implements Closeable {
 
             // block value count is always a power of two
             // to calculate remainder we use faster 'x & (count-1)', which is equivalent to (x % count)
-            this.blockValueCountMod = this.keyMem.getInt(BitmapIndexConstants.KEY_RESERVED_OFFSET_BLOCK_VALUE_COUNT) - 1;
-            this.blockCapacity = (this.blockValueCountMod + 1) * 8 + BitmapIndexConstants.VALUE_BLOCK_FILE_RESERVED;
+            this.blockValueCountMod = this.keyMem.getInt(BitmapIndexUtils.KEY_RESERVED_OFFSET_BLOCK_VALUE_COUNT) - 1;
+            this.blockCapacity = (this.blockValueCountMod + 1) * 8 + BitmapIndexUtils.VALUE_BLOCK_FILE_RESERVED;
         } catch (CairoException e) {
             this.close();
             throw e;
@@ -110,7 +113,7 @@ public class BitmapIndexWriter implements Closeable {
      * @param value long value
      */
     public void add(int key, long value) {
-        final long offset = BitmapIndexConstants.getKeyEntryOffset(key);
+        final long offset = BitmapIndexUtils.getKeyEntryOffset(key);
         if (key < keyCount) {
             // when key exists we have possible outcomes with regards to values
             // 1. last value block has space if value cell index is not the last in block
@@ -118,8 +121,8 @@ public class BitmapIndexWriter implements Closeable {
             // 3. value count is 0. This means key was created as byproduct of adding sparse key value
             // second option is supposed to be less likely because we attempt to
             // configure block capacity to accommodate as many values as possible
-            long valueBlockOffset = keyMem.getLong(offset + BitmapIndexConstants.KEY_ENTRY_OFFSET_LAST_VALUE_BLOCK_OFFSET);
-            long valueCount = keyMem.getLong(offset + BitmapIndexConstants.KEY_ENTRY_OFFSET_VALUE_COUNT);
+            long valueBlockOffset = keyMem.getLong(offset + BitmapIndexUtils.KEY_ENTRY_OFFSET_LAST_VALUE_BLOCK_OFFSET);
+            long valueCount = keyMem.getLong(offset + BitmapIndexUtils.KEY_ENTRY_OFFSET_VALUE_COUNT);
             int valueCellIndex = (int) (valueCount & blockValueCountMod);
             if (valueCellIndex > 0) {
                 // this is scenario #1: key exists and there is space in last block to add value
@@ -159,6 +162,50 @@ public class BitmapIndexWriter implements Closeable {
         }
     }
 
+    /**
+     * Rolls values back. Removes values that are strictly greater than given maximum. Empty value blocks
+     * will also be removed as well as blank space at end of value memory.
+     *
+     * @param maxValue maximum value allowed in index.
+     */
+    public void rollbackValues(long maxValue) {
+
+        long maxValueBlockOffset = 0;
+        for (int k = 0; k < keyCount; k++) {
+            long offset = BitmapIndexUtils.getKeyEntryOffset(k);
+            long valueCount = keyMem.getLong(offset + BitmapIndexUtils.KEY_ENTRY_OFFSET_VALUE_COUNT);
+
+            // do we have anything for the key?
+            if (valueCount > 0) {
+                long blockOffset = keyMem.getLong(offset + BitmapIndexUtils.KEY_ENTRY_OFFSET_LAST_VALUE_BLOCK_OFFSET);
+                BitmapIndexUtils.seekValueBlock(valueCount, blockOffset, valueMem, maxValue, blockValueCountMod, SEEKER);
+
+                if (valueCount != seekValueCount || blockOffset != seekValueBlockOffset) {
+                    // set new value count
+                    keyMem.jumpTo(offset + BitmapIndexUtils.KEY_ENTRY_OFFSET_VALUE_COUNT);
+                    keyMem.putLong(seekValueCount);
+
+                    if (blockOffset != seekValueBlockOffset) {
+                        Unsafe.getUnsafe().storeFence();
+                        keyMem.skip(8);
+                        keyMem.putLong(seekValueBlockOffset);
+                        Unsafe.getUnsafe().storeFence();
+                    } else {
+                        // skip first and last offset
+                        keyMem.skip(16);
+                    }
+                    keyMem.putLong(seekValueCount);
+                }
+
+                if (seekValueBlockOffset > maxValueBlockOffset) {
+                    maxValueBlockOffset = seekValueBlockOffset;
+                }
+            }
+        }
+        valueMemSize = maxValueBlockOffset + blockCapacity;
+        updateValueMemSize();
+    }
+
     private void addValueBlockAndStoreValue(long offset, long valueBlockOffset, long valueCount, long value) {
         long newValueBlockOffset = allocateValueBlockAndStore(value);
 
@@ -167,11 +214,11 @@ public class BitmapIndexWriter implements Closeable {
 
         // we subtract 8 because we just written long value
         // update this block reference to previous block
-        valueMem.skip(blockCapacity - 8 - BitmapIndexConstants.VALUE_BLOCK_FILE_RESERVED);
+        valueMem.skip(blockCapacity - 8 - BitmapIndexUtils.VALUE_BLOCK_FILE_RESERVED);
         valueMem.putLong(valueBlockOffset);
 
         // update previous block' "next" block reference to this block
-        valueMem.jumpTo(valueBlockOffset + blockCapacity - BitmapIndexConstants.VALUE_BLOCK_FILE_RESERVED + 8);
+        valueMem.jumpTo(valueBlockOffset + blockCapacity - BitmapIndexUtils.VALUE_BLOCK_FILE_RESERVED + 8);
         valueMem.putLong(newValueBlockOffset);
 
         // update count and last value block offset for the key
@@ -197,25 +244,29 @@ public class BitmapIndexWriter implements Closeable {
 
     private long allocateValueBlockAndStore(long value) {
         long newValueBlockOffset = valueMemSize;
-        valueMemSize += blockCapacity;
-
-        // must update value mem size in key memory header
-        // so that index can be opened correctly next time it loads
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_SEQUENCE);
-        long seq = keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_SEQUENCE) + 1;
-        keyMem.putLong(seq);
-        Unsafe.getUnsafe().storeFence();
-
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_OFFSET_VALUE_MEM_SIZE);
-        keyMem.putLong(valueMemSize);
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_SEQUENCE_CHECK);
-        Unsafe.getUnsafe().storeFence();
-        keyMem.putLong(seq);
 
         // store our value
         valueMem.jumpTo(newValueBlockOffset);
         valueMem.putLong(value);
+
+        valueMemSize += blockCapacity;
+
+        // must update value mem size in key memory header
+        // so that index can be opened correctly next time it loads
+        updateValueMemSize();
         return newValueBlockOffset;
+    }
+
+    private void initKeyMemory(int blockValueCount) {
+        keyMem.putByte(BitmapIndexUtils.SIGNATURE);
+        keyMem.putLong(1); // SEQUENCE
+        Unsafe.getUnsafe().storeFence();
+        keyMem.putLong(0); // VALUE MEM SIZE
+        keyMem.putInt(blockValueCount); // BLOCK VALUE COUNT
+        keyMem.putLong(0); // KEY COUNT
+        Unsafe.getUnsafe().storeFence();
+        keyMem.putLong(1); // SEQUENCE CHECK
+        keyMem.skip(BitmapIndexUtils.KEY_FILE_RESERVED - keyMem.getAppendOffset());
     }
 
     private void appendValue(long offset, long valueBlockOffset, long valueCount, int valueCellIndex, long value) {
@@ -237,16 +288,8 @@ public class BitmapIndexWriter implements Closeable {
         keyMem.putLong(valueCount + 1);
     }
 
-    private void initKeyMemory(int blockValueCount) {
-        keyMem.putByte(BitmapIndexConstants.SIGNATURE);
-        keyMem.putLong(1); // SEQUENCE
-        Unsafe.getUnsafe().storeFence();
-        keyMem.putLong(0); // VALUE MEM SIZE
-        keyMem.putInt(blockValueCount); // BLOCK VALUE COUNT
-        keyMem.putLong(0); // KEY COUNT
-        Unsafe.getUnsafe().storeFence();
-        keyMem.putLong(1); // SEQUENCE CHECK
-        keyMem.skip(BitmapIndexConstants.KEY_FILE_RESERVED - keyMem.getAppendOffset());
+    private long keyMemSize() {
+        return this.keyCount * BitmapIndexUtils.KEY_ENTRY_SIZE + BitmapIndexUtils.KEY_FILE_RESERVED;
     }
 
     private void initValueBlockAndStoreValue(long offset, long value) {
@@ -272,24 +315,38 @@ public class BitmapIndexWriter implements Closeable {
         Unsafe.getUnsafe().storeFence();
     }
 
-    private long keyMemSize() {
-        return this.keyCount * BitmapIndexConstants.KEY_ENTRY_SIZE + BitmapIndexConstants.KEY_FILE_RESERVED;
+    private void seek(long count, long offset) {
+        this.seekValueCount = count;
+        this.seekValueBlockOffset = offset;
     }
 
     private void updateKeyCount(int key) {
         keyCount = key + 1;
 
         // also write key count to header of key memory
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_SEQUENCE);
-        long seq = keyMem.getLong(BitmapIndexConstants.KEY_RESERVED_SEQUENCE) + 1;
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_SEQUENCE);
+        long seq = keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_SEQUENCE) + 1;
         keyMem.putLong(seq);
         Unsafe.getUnsafe().storeFence();
 
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_OFFSET_KEY_COUNT);
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_OFFSET_KEY_COUNT);
         keyMem.putLong(keyCount);
         Unsafe.getUnsafe().storeFence();
 
-        keyMem.jumpTo(BitmapIndexConstants.KEY_RESERVED_SEQUENCE_CHECK);
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_SEQUENCE_CHECK);
+        keyMem.putLong(seq);
+    }
+
+    private void updateValueMemSize() {
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_SEQUENCE);
+        long seq = keyMem.getLong(BitmapIndexUtils.KEY_RESERVED_SEQUENCE) + 1;
+        keyMem.putLong(seq);
+        Unsafe.getUnsafe().storeFence();
+
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_OFFSET_VALUE_MEM_SIZE);
+        keyMem.putLong(valueMemSize);
+        keyMem.jumpTo(BitmapIndexUtils.KEY_RESERVED_SEQUENCE_CHECK);
+        Unsafe.getUnsafe().storeFence();
         keyMem.putLong(seq);
     }
 }
