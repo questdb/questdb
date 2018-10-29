c7d34051917fc3b3d7a80c73285adb3036c4c6b7
Vlad Ilyushchenko <bluestreak@gmail.com>
2018-10-29 06:35:53 +0000

    CAIRO: compiler for copying Record into MapValue

---
+++ core/src/main/java/com/questdb/cairo/map/RecordValueSinkFactory.java
@@ -0,0 +1,149 @@
+/*******************************************************************************
+ *    ___                  _   ____  ____
+ *   / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *  | | | | | | |/ _ \/ __| __| | | |  _ \
+ *  | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *   \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ * Copyright (C) 2014-2018 Appsicle
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package com.questdb.cairo.map;
+
+import com.questdb.cairo.ColumnFilter;
+import com.questdb.cairo.ColumnType;
+import com.questdb.cairo.ColumnTypes;
+import com.questdb.cairo.RecordSink;
+import com.questdb.cairo.sql.Record;
+import com.questdb.std.BytecodeAssembler;
+import com.questdb.std.Transient;
+
+public class RecordValueSinkFactory {
+
+    public static RecordValueSink getInstance(BytecodeAssembler asm, ColumnTypes columnTypes, @Transient ColumnFilter columnFilter) {
+        asm.init(RecordSink.class);
+        asm.setupPool();
+        int thisClassIndex = asm.poolClass(asm.poolUtf8("questdbasm"));
+        int interfaceClassIndex = asm.poolClass(RecordValueSink.class);
+
+        int rGetInt = asm.poolInterfaceMethod(Record.class, "getInt", "(I)I");
+        int rGetLong = asm.poolInterfaceMethod(Record.class, "getLong", "(I)J");
+        int rGetDate = asm.poolInterfaceMethod(Record.class, "getDate", "(I)J");
+        int rGetTimestamp = asm.poolInterfaceMethod(Record.class, "getTimestamp", "(I)J");
+        int rGetByte = asm.poolInterfaceMethod(Record.class, "getByte", "(I)B");
+        int rGetShort = asm.poolInterfaceMethod(Record.class, "getShort", "(I)S");
+        int rGetBool = asm.poolInterfaceMethod(Record.class, "getBool", "(I)Z");
+        int rGetFloat = asm.poolInterfaceMethod(Record.class, "getFloat", "(I)F");
+        int rGetDouble = asm.poolInterfaceMethod(Record.class, "getDouble", "(I)D");
+        //
+        int wPutInt = asm.poolInterfaceMethod(MapValue.class, "putInt", "(II)V");
+        int wPutLong = asm.poolInterfaceMethod(MapValue.class, "putLong", "(IJ)V");
+        int wPutByte = asm.poolInterfaceMethod(MapValue.class, "putByte", "(IB)V");
+        int wPutShort = asm.poolInterfaceMethod(MapValue.class, "putShort", "(IS)V");
+        int wPutBool = asm.poolInterfaceMethod(MapValue.class, "putBool", "(IZ)V");
+        int wPutFloat = asm.poolInterfaceMethod(MapValue.class, "putFloat", "(IF)V");
+        int wPutDouble = asm.poolInterfaceMethod(MapValue.class, "putDouble", "(ID)V");
+        int wPutDate = asm.poolInterfaceMethod(MapValue.class, "putDate", "(IJ)V");
+        int wPutTimestamp = asm.poolInterfaceMethod(MapValue.class, "putTimestamp", "(IJ)V");
+
+        int copyNameIndex = asm.poolUtf8("copy");
+        int copySigIndex = asm.poolUtf8("(Lcom/questdb/cairo/sql/Record;Lcom/questdb/cairo/map/MapValue;)V");
+
+        asm.finishPool();
+        asm.defineClass(thisClassIndex);
+        asm.interfaceCount(1);
+        asm.putShort(interfaceClassIndex);
+        asm.fieldCount(0);
+        asm.methodCount(2);
+        asm.defineDefaultConstructor();
+
+        asm.startMethod(copyNameIndex, copySigIndex, 4, 3);
+
+        int n = columnFilter.getColumnCount();
+        for (int i = 0; i < n; i++) {
+
+            int index = columnFilter.getColumnIndex(i);
+            asm.aload(2);
+            asm.iconst(index);
+            asm.aload(1);
+            asm.iconst(index);
+
+            switch (columnTypes.getColumnType(index)) {
+                case ColumnType.INT:
+                    asm.invokeInterface(rGetInt, 1);
+                    asm.invokeInterface(wPutInt, 2);
+                    break;
+                case ColumnType.SYMBOL:
+                    asm.invokeInterface(rGetInt, 1);
+                    asm.invokeInterface(wPutInt, 2);
+                    break;
+                case ColumnType.LONG:
+                    asm.invokeInterface(rGetLong, 1);
+                    asm.invokeInterface(wPutLong, 3);
+                    break;
+                case ColumnType.DATE:
+                    asm.invokeInterface(rGetDate, 1);
+                    asm.invokeInterface(wPutDate, 3);
+                    break;
+                case ColumnType.TIMESTAMP:
+                    asm.invokeInterface(rGetTimestamp, 1);
+                    asm.invokeInterface(wPutTimestamp, 3);
+                    break;
+                case ColumnType.BYTE:
+                    asm.invokeInterface(rGetByte, 1);
+                    asm.invokeInterface(wPutByte, 2);
+                    break;
+                case ColumnType.SHORT:
+                    asm.invokeInterface(rGetShort, 1);
+                    asm.invokeInterface(wPutShort, 2);
+                    break;
+                case ColumnType.BOOLEAN:
+                    asm.invokeInterface(rGetBool, 1);
+                    asm.invokeInterface(wPutBool, 2);
+                    break;
+                case ColumnType.FLOAT:
+                    asm.invokeInterface(rGetFloat, 1);
+                    asm.invokeInterface(wPutFloat, 2);
+                    break;
+                case ColumnType.DOUBLE:
+                    asm.invokeInterface(rGetDouble, 1);
+                    asm.invokeInterface(wPutDouble, 3);
+                    break;
+                default:
+                    throw new UnsupportedOperationException();
+            }
+        }
+
+        asm.return_();
+        asm.endMethodCode();
+
+        // exceptions
+        asm.putShort(0);
+
+        // we have to add stack map table as branch target
+        // jvm requires it
+
+        // attributes: 0 (void, no stack verification)
+        asm.putShort(0);
+
+        asm.endMethod();
+
+        // class attribute count
+        asm.putShort(0);
+
+        return asm.newInstance();
+    }
+}
---
+++ core/src/test/java/com/questdb/cairo/map/RecordValueSinkFactoryTest.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ *    ___                  _   ____  ____
+ *   / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *  | | | | | | |/ _ \/ __| __| | | |  _ \
+ *  | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *   \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ * Copyright (C) 2014-2018 Appsicle
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package com.questdb.cairo.map;
+
+import com.questdb.cairo.*;
+import com.questdb.cairo.sql.Record;
+import com.questdb.cairo.sql.RecordCursor;
+import com.questdb.cairo.sql.SymbolTable;
+import com.questdb.std.BytecodeAssembler;
+import com.questdb.std.Numbers;
+import com.questdb.std.Rnd;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RecordValueSinkFactoryTest extends AbstractCairoTest {
+    @Test
+    public void testAllSupportedTypes() {
+        SingleColumnType keyTypes = new SingleColumnType(ColumnType.INT);
+        try (TableModel model = new TableModel(configuration, "all", PartitionBy.NONE)
+                .col("int", ColumnType.INT)
+                .col("short", ColumnType.SHORT)
+                .col("byte", ColumnType.BYTE)
+                .col("double", ColumnType.DOUBLE)
+                .col("float", ColumnType.FLOAT)
+                .col("long", ColumnType.LONG)
+                .col("sym", ColumnType.SYMBOL).symbolCapacity(64)
+                .col("bool", ColumnType.BOOLEAN)
+                .col("date", ColumnType.DATE)
+                .col("ts", ColumnType.TIMESTAMP)
+        ) {
+            CairoTestUtils.create(model);
+        }
+
+        final int N = 1024;
+        final Rnd rnd = new Rnd();
+        try (TableWriter writer = new TableWriter(configuration, "all")) {
+
+            for (int i = 0; i < N; i++) {
+                TableWriter.Row row = writer.newRow(0);
+                row.putInt(0, rnd.nextInt());
+                row.putShort(1, rnd.nextShort());
+                row.putByte(2, rnd.nextByte());
+                row.putDouble(3, rnd.nextDouble2());
+                row.putFloat(4, rnd.nextFloat2());
+                row.putLong(5, rnd.nextLong());
+                row.putSym(6, rnd.nextChars(10));
+                row.putBool(7, rnd.nextBoolean());
+                row.putDate(8, rnd.nextLong());
+                row.putTimestamp(9, rnd.nextLong());
+                row.append();
+            }
+            writer.commit();
+        }
+
+        try (TableReader reader = new TableReader(configuration, "all")) {
+            final SymbolAsIntTypes valueTypes = new SymbolAsIntTypes(reader.getMetadata());
+            try (final Map map = new FastMap(Numbers.SIZE_1MB, keyTypes, valueTypes, N, 0.5)) {
+
+                EntityColumnFilter columnFilter = new EntityColumnFilter();
+                columnFilter.of(reader.getMetadata().getColumnCount());
+                RecordValueSink sink = RecordValueSinkFactory.getInstance(new BytecodeAssembler(), reader.getMetadata(), columnFilter);
+                RecordCursor cursor = reader.getCursor();
+                final Record record = cursor.getRecord();
+
+                int index = 0;
+                while (cursor.hasNext()) {
+                    MapKey key = map.withKey();
+                    key.putInt(index++);
+                    MapValue value = key.createValue();
+                    sink.copy(record, value);
+                }
+
+                Assert.assertEquals(N, index);
+
+                rnd.reset();
+
+                SymbolTable symbolTable = reader.getSymbolMapReader(6);
+
+                for (int i = 0; i < N; i++) {
+                    MapKey key = map.withKey();
+                    key.putInt(i);
+                    MapValue value = key.findValue();
+
+                    Assert.assertNotNull(value);
+                    Assert.assertEquals(rnd.nextInt(), value.getInt(0));
+                    Assert.assertEquals(rnd.nextShort(), value.getShort(1));
+                    Assert.assertEquals(rnd.nextByte(), value.getByte(2));
+                    Assert.assertEquals(rnd.nextDouble2(), value.getDouble(3), 0.000001);
+                    Assert.assertEquals(rnd.nextFloat2(), value.getFloat(4), 0.000001f);
+                    Assert.assertEquals(rnd.nextLong(), value.getLong(5));
+                    Assert.assertEquals(symbolTable.getQuick(rnd.nextChars(10)), value.getInt(6));
+                    Assert.assertEquals(rnd.nextBoolean(), value.getBool(7));
+                    Assert.assertEquals(rnd.nextLong(), value.getDate(8));
+                    Assert.assertEquals(rnd.nextLong(), value.getTimestamp(9));
+                }
+            }
+        }
+    }
+}
