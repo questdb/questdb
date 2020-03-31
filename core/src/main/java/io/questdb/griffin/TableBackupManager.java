cfa9db8c42a88d945aefca0caf728eef14caff26
Patrick Mackinlay <patrick@spacesurfer.com>
2020-03-31 16:17:37 +0100

    feat(griffin): add table backup 	Avoid allocating new TableReaderMetadata needlessly

--- core/src/main/java/io/questdb/griffin/TableBackupManager.java
+++ core/src/main/java/io/questdb/griffin/TableBackupManager.java
@@ -34,7 +34,7 @@ public class TableBackupManager {
 		}
 
 		try (TableReader reader = engine.getReader(securityContext, tableName)) {
-			cloneMetaData(tableName, configuration.getFilesFacade(), path, configuration.getRoot(), configuration.getBackupRoot(), configuration.getMkDirMode());
+			cloneMetaData(tableName, configuration.getFilesFacade(), path, configuration.getRoot(), configuration.getBackupRoot(), configuration.getMkDirMode(), (TableReaderMetadata) reader.getMetadata());
 			try (TableWriter backupWriter = engine.getBackupWriter(securityContext, tableName); RecordCursor cursor = reader.getCursor()) {
 				compiler.copyTableData(cursor, reader.getMetadata(), backupWriter);
 				backupWriter.commit();
@@ -42,37 +42,33 @@ public class TableBackupManager {
 		}
 	}
 
-	private static void cloneMetaData(CharSequence tableName, FilesFacade ff, Path path, CharSequence root, CharSequence backupRoot, int mkDirMode) {
-		path.of(root).concat(tableName).concat(TableUtils.META_FILE_NAME).$();
-		try (TableReaderMetadata sourceMetaData = new TableReaderMetadata(ff, path)) {
-			path.of(backupRoot).concat(tableName).put(Files.SEPARATOR).$();
+	private static void cloneMetaData(CharSequence tableName, FilesFacade ff, Path path, CharSequence root, CharSequence backupRoot, int mkDirMode, TableReaderMetadata sourceMetaData) {
+		path.of(backupRoot).concat(tableName).put(Files.SEPARATOR).$();
 
-			if (ff.exists(path)) {
-				throw CairoException.instance(0).put("Backup dir for table \"" + tableName + "\" already exists [dir=").put(path).put(']');
-			}
+		if (ff.exists(path)) {
+			throw CairoException.instance(0).put("Backup dir for table \"" + tableName + "\" already exists [dir=").put(path).put(']');
+		}
 
-			if (ff.mkdirs(path, mkDirMode) != 0) {
-				throw CairoException.instance(ff.errno()).put("Could not create [dir=").put(path).put(']');
-			}
+		if (ff.mkdirs(path, mkDirMode) != 0) {
+			throw CairoException.instance(ff.errno()).put("Could not create [dir=").put(path).put(']');
+		}
 
-			final int rootLen = path.length();
-			try (AppendMemory backupMem = new AppendMemory()) {
-				backupMem.of(ff, path.trimTo(rootLen).concat(TableUtils.META_FILE_NAME).$(), ff.getPageSize());
-				sourceMetaData.cloneTo(backupMem);
+		final int rootLen = path.length();
+		try (AppendMemory backupMem = new AppendMemory()) {
+			backupMem.of(ff, path.trimTo(rootLen).concat(TableUtils.META_FILE_NAME).$(), ff.getPageSize());
+			sourceMetaData.cloneTo(backupMem);
 
-				// create symbol maps
-				path.trimTo(rootLen).$();
-				int symbolMapCount = 0;
-				for (int i = 0; i < sourceMetaData.getColumnCount(); i++) {
-					if (sourceMetaData.getColumnType(i) == ColumnType.SYMBOL) {
-						SymbolMapWriter.createSymbolMapFiles(ff, backupMem, path, sourceMetaData.getColumnName(i), 128, true);
-						symbolMapCount++;
-					}
+			// create symbol maps
+			path.trimTo(rootLen).$();
+			int symbolMapCount = 0;
+			for (int i = 0; i < sourceMetaData.getColumnCount(); i++) {
+				if (sourceMetaData.getColumnType(i) == ColumnType.SYMBOL) {
+					SymbolMapWriter.createSymbolMapFiles(ff, backupMem, path, sourceMetaData.getColumnName(i), 128, true);
+					symbolMapCount++;
 				}
-				backupMem.of(ff, path.trimTo(rootLen).concat(TableUtils.TXN_FILE_NAME).$(), ff.getPageSize());
-				TableUtils.resetTxn(backupMem, symbolMapCount, 0L, TableUtils.INITIAL_TXN);
-
 			}
+			backupMem.of(ff, path.trimTo(rootLen).concat(TableUtils.TXN_FILE_NAME).$(), ff.getPageSize());
+			TableUtils.resetTxn(backupMem, symbolMapCount, 0L, TableUtils.INITIAL_TXN);
 		}
 	}
 
