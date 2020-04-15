15d33379c1b46f34f6ed78916aa369f38d43ec4e
Patrick Mackinlay <github.9fadd@nospam.spacesurfer.com>
2020-04-15 19:31:12 +0100

    chore(griffin): more backup tests

--- core/src/test/java/io/questdb/griffin/TableBackupTest.java
+++ core/src/test/java/io/questdb/griffin/TableBackupTest.java
@@ -24,29 +24,42 @@
 
 package io.questdb.griffin;
 
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
 import io.questdb.MessageBus;
 import io.questdb.MessageBusImpl;
-import io.questdb.cairo.*;
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.CairoEngine;
+import io.questdb.cairo.CairoException;
+import io.questdb.cairo.DefaultCairoConfiguration;
+import io.questdb.cairo.RecordCursorPrinter;
 import io.questdb.cairo.security.AllowAllCairoSecurityContext;
 import io.questdb.cairo.sql.RecordCursor;
 import io.questdb.cairo.sql.RecordCursorFactory;
 import io.questdb.griffin.engine.functions.bind.BindVariableService;
 import io.questdb.std.Files;
+import io.questdb.std.FilesFacade;
+import io.questdb.std.FilesFacadeImpl;
 import io.questdb.std.Misc;
 import io.questdb.std.microtime.DateFormatCompiler;
 import io.questdb.std.microtime.TimestampFormat;
+import io.questdb.std.str.LPSZ;
 import io.questdb.std.str.Path;
 import io.questdb.std.str.StringSink;
 import io.questdb.test.tools.TestUtils;
-import org.junit.*;
-import org.junit.rules.TemporaryFolder;
-
-import java.io.IOException;
 
 public class TableBackupTest {
     // todo: rename test methods to fall inline with other test method names, e.g. testSomething
     private static final StringSink sink = new StringSink();
     private static final RecordCursorPrinter printer = new RecordCursorPrinter(sink);
+    private static final int ERRNO_EIO = 5;
     @Rule
     public TemporaryFolder temp = new TemporaryFolder();
 
@@ -57,9 +70,10 @@ public class TableBackupTest {
     private CairoEngine mainEngine;
     private SqlCompiler mainCompiler;
     private SqlExecutionContext mainSqlExecutionContext;
+    private int renameErrno;
 
     @Test
-    public void allTypesPartitionedTableTest1() throws Exception {
+    public void testAllTypesPartitionedTable() throws Exception {
         assertMemoryLeak(() -> {
             String tableName = "testTable2";
             // @formatter:off
@@ -96,7 +110,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void backupDatabaseTest() throws Exception {
+    public void testBackupDatabase() throws Exception {
         assertMemoryLeak(() -> {
             // @formatter:off
             mainCompiler.compile("create table tb1 as (select" +
@@ -125,7 +139,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void compromisedTableNameTest() throws Exception {
+    public void testCompromisedTableName() throws Exception {
         assertMemoryLeak(() -> {
             try {
                 // @formatter:off
@@ -145,7 +159,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void incorrectConfigTest() throws Exception {
+    public void testIncorrectConfig() throws Exception {
         backupRoot = null;
         assertMemoryLeak(() -> {
             try {
@@ -167,7 +181,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void invalidSqlTest1() throws Exception {
+    public void testInvalidSql1() throws Exception {
         assertMemoryLeak(() -> {
             try {
                 mainCompiler.compile("backup something", mainSqlExecutionContext);
@@ -180,7 +194,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void invalidSqlTest2() throws Exception {
+    public void testInvalidSql2() throws Exception {
         assertMemoryLeak(() -> {
             try {
                 mainCompiler.compile("backup table", mainSqlExecutionContext);
@@ -193,7 +207,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void invalidSqlTest3() throws Exception {
+    public void testInvalidSql3() throws Exception {
         assertMemoryLeak(() -> {
             try {
                 // @formatter:off
@@ -214,7 +228,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void missingTableTest() throws Exception {
+    public void testMissingTable() throws Exception {
         assertMemoryLeak(() -> {
             try {
                 // @formatter:off
@@ -235,7 +249,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void multipleTableTest() throws Exception {
+    public void testMultipleTable() throws Exception {
         assertMemoryLeak(() -> {
             // @formatter:off
             mainCompiler.compile("create table tb1 as (select" +
@@ -269,6 +283,30 @@ public class TableBackupTest {
         CharSequence root = temp.newFolder("dbRoot").getAbsolutePath();
         backupRoot = temp.newFolder("dbBackupRoot").getAbsolutePath();
 
+        renameErrno = -1;
+        FilesFacade ff = new FilesFacadeImpl() {
+            private int nextErrno = -1;
+
+            @Override
+            public boolean rename(LPSZ from, LPSZ to) {
+                if (renameErrno != -1) {
+                    nextErrno = renameErrno;
+                    renameErrno = -1;
+                    return false;
+                }
+                return super.rename(from, to);
+            }
+
+            @Override
+            public int errno() {
+                if (nextErrno != -1) {
+                    int errno = nextErrno;
+                    nextErrno = -1;
+                    return errno;
+                }
+                return super.errno();
+            }
+        };
         mainConfiguration = new DefaultCairoConfiguration(root) {
             @Override
             public CharSequence getBackupRoot() {
@@ -279,6 +317,11 @@ public class TableBackupTest {
             public TimestampFormat getBackupDirTimestampFormat() {
                 return new DateFormatCompiler().compile("ddMMMyyyy");
             }
+
+            @Override
+            public FilesFacade getFilesFacade() {
+                return ff;
+            }
         };
         MessageBus mainMessageBus = new MessageBusImpl();
         mainEngine = new CairoEngine(mainConfiguration, mainMessageBus);
@@ -287,7 +330,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void simpleTableTest1() throws Exception {
+    public void testSimpleTable1() throws Exception {
         assertMemoryLeak(() -> {
             String tableName = "testTable1";
             // @formatter:off
@@ -298,7 +341,7 @@ public class TableBackupTest {
                     " from long_sequence(10000)) timestamp(ts)", mainSqlExecutionContext);
             // @formatter:on
 
-            mainCompiler.compile("backup table " + tableName, mainSqlExecutionContext);
+            mainCompiler.compile("backup table " + tableName + ";", mainSqlExecutionContext);
             setFinalBackupPath();
             String sourceSelectAll = selectAll(tableName, false);
             String backupSelectAll = selectAll(tableName, true);
@@ -307,7 +350,7 @@ public class TableBackupTest {
     }
 
     @Test
-    public void successiveBackupsTest() throws Exception {
+    public void testSuccessiveBackups() throws Exception {
         assertMemoryLeak(() -> {
             String tableName = "testTable1";
             // @formatter:off
@@ -342,6 +385,86 @@ public class TableBackupTest {
             setFinalBackupPath();
             String backupSelectAllOriginal = selectAll(tableName, true);
             Assert.assertEquals(backupSelectAll1, backupSelectAllOriginal);
+        });
+    }
+
+    @Test
+    public void testRenameFailure() throws Exception {
+        assertMemoryLeak(() -> {
+            String tableName = "testTable1";
+            // @formatter:off
+            mainCompiler.compile("create table " + tableName + " as (select" +
+                    " rnd_symbol(4,4,4,2) sym," +
+                    " rnd_double(2) d," +
+                    " timestamp_sequence(0, 1000000000) ts" +
+                    " from long_sequence(10000)) timestamp(ts)", mainSqlExecutionContext);
+            // @formatter:on
+
+            renameErrno = ERRNO_EIO;
+            try {
+                mainCompiler.compile("backup table " + tableName + ";", mainSqlExecutionContext);
+                Assert.fail();
+            } catch (CairoException ex) {
+                Assert.assertTrue(ex.getMessage().startsWith("[5] Could not rename "));
+            }
+
+            mainCompiler.compile("backup table " + tableName + ";", mainSqlExecutionContext);
+            setFinalBackupPath(1);
+            String sourceSelectAll = selectAll(tableName, false);
+            String backupSelectAll = selectAll(tableName, true);
+            Assert.assertEquals(sourceSelectAll, backupSelectAll);
+        });
+    }
+
+    @Test
+    public void testTableBackupDirExists() throws Exception {
+        assertMemoryLeak(() -> {
+            String tableName = "testTable1";
+            // @formatter:off
+            mainCompiler.compile("create table " + tableName + " as (select" +
+                    " rnd_symbol(4,4,4,2) sym," +
+                    " rnd_double(2) d," +
+                    " timestamp_sequence(0, 1000000000) ts" +
+                    " from long_sequence(10000)) timestamp(ts)", mainSqlExecutionContext);
+            // @formatter:on
+
+            try (Path path = new Path()) {
+                path.of(mainConfiguration.getBackupRoot()).concat("tmp").concat(tableName).put(Files.SEPARATOR).$();
+                int rc = FilesFacadeImpl.INSTANCE.mkdirs(path, mainConfiguration.getBackupMkDirMode());
+                Assert.assertEquals(0, rc);
+            }
+            try {
+                mainCompiler.compile("backup table " + tableName + ";", mainSqlExecutionContext);
+                Assert.fail();
+            } catch (CairoException ex) {
+                Assert.assertTrue(ex.getMessage().startsWith("[0] Backup dir for table \"testTable1\" already exists"));
+            }
+        });
+    }
+
+    @Test
+    public void testTableBackupDirUnwritable() throws Exception {
+        assertMemoryLeak(() -> {
+            String tableName = "testTable1";
+            // @formatter:off
+            mainCompiler.compile("create table " + tableName + " as (select" +
+                    " rnd_symbol(4,4,4,2) sym," +
+                    " rnd_double(2) d," +
+                    " timestamp_sequence(0, 1000000000) ts" +
+                    " from long_sequence(10000)) timestamp(ts)", mainSqlExecutionContext);
+            // @formatter:on
+
+            try (Path path = new Path()) {
+                path.of(mainConfiguration.getBackupRoot()).concat("tmp").put(Files.SEPARATOR).$();
+                int rc = FilesFacadeImpl.INSTANCE.mkdirs(path, 5);
+                Assert.assertEquals(0, rc);
+            }
+            try {
+                mainCompiler.compile("backup table " + tableName + ";", mainSqlExecutionContext);
+                Assert.fail();
+            } catch (CairoException ex) {
+                Assert.assertTrue(ex.getMessage().startsWith("[13] Could not create "));
+            }
         });
     }
 
