f485f0ea12e7439f5f45b4faca2144ea15da321f
TheTanc <52114895+TheTanc@users.noreply.github.com>
2020-04-16 11:25:16 +0100

    feat(griffin): timestamp functions

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/date/AddHoursToTimestampFunctionFactory.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2020 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.date;
+
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.sql.Function;
+import io.questdb.cairo.sql.Record;
+import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.engine.functions.BinaryFunction;
+import io.questdb.griffin.engine.functions.TimestampFunction;
+import io.questdb.std.Numbers;
+import io.questdb.std.ObjList;
+import io.questdb.std.microtime.Timestamps;
+
+public class AddHoursToTimestampFunctionFactory implements FunctionFactory {
+    @Override
+    public String getSignature() {
+        return "add_hours(NL)";
+    }
+
+    @Override
+    public Function newInstance(ObjList<Function> args, int position, CairoConfiguration configuration) {
+        return new AddLongFunc(position, args.getQuick(0), args.getQuick(1));
+    }
+
+    private static class AddLongFunc extends TimestampFunction implements BinaryFunction {
+        final Function left;
+        final Function right;
+
+        public AddLongFunc(int position, Function left, Function right) {
+            super(position);
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public Function getLeft() {
+            return left;
+        }
+
+        @Override
+        public Function getRight() {
+            return right;
+        }
+
+        @Override
+        public long getTimestamp(Record rec) {
+            final long l = left.getTimestamp(rec);
+            final int r = right.getInt(rec);
+            if (l == Numbers.LONG_NaN || r == Numbers.INT_NaN) {
+                return Numbers.LONG_NaN;
+            }
+            return Timestamps.addHours(l,r);
+        }
+    }
+}
