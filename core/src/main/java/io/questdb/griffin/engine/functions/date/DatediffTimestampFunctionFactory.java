aef75de27186f6d8cd015043bc373fb577cfc8bc
TheTanc <tanc@questdb.io>
2020-04-20 08:34:18 +0100

    feat(griffin): datediff - constant second implementation + test

--- core/src/main/java/io/questdb/griffin/engine/functions/date/DatediffTimestampFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/date/DatediffTimestampFunctionFactory.java
@@ -29,7 +29,10 @@ import io.questdb.cairo.sql.Function;
 import io.questdb.cairo.sql.Record;
 import io.questdb.griffin.FunctionFactory;
 import io.questdb.griffin.engine.functions.LongFunction;
+import io.questdb.griffin.engine.functions.UnaryFunction;
+import io.questdb.griffin.engine.functions.BinaryFunction;
 import io.questdb.griffin.engine.functions.TernaryFunction;
+import io.questdb.griffin.engine.functions.constants.TimestampConstant;
 import io.questdb.std.Numbers;
 import io.questdb.std.ObjList;
 import io.questdb.std.microtime.Timestamps;
@@ -42,18 +45,36 @@ public class DatediffTimestampFunctionFactory implements FunctionFactory {
 
     @Override
     public Function newInstance(ObjList<Function> args, int position, CairoConfiguration configuration) {
-        return new DateDiffFunc(position, args.getQuick(0), args.getQuick(1), args.getQuick(2));
-    }
 
-    private static class DateDiffFunc extends LongFunction implements TernaryFunction {
+        Function period = args.getQuick(0);
+        if (period.isConstant()) {
+            Function start = args.getQuick(1);
+            Function end = args.getQuick(2);
+            char periodValue = period.getChar(null);
+            if (periodValue == 's') {
+                if (start.isConstant() && start.getTimestamp(null) != Numbers.LONG_NaN) {
+                    long startValue = start.getLong(null);
+                    return new DateDiffFuncSecondConstantTimestampConstant(position, args.getQuick(2), startValue);
+                }
+                if (end.isConstant() && end.getTimestamp(null) != Numbers.LONG_NaN) {
+                    long endValue = end.getLong(null);
+                    return new DateDiffFuncSecondConstantTimestampConstant(position, args.getQuick(1), endValue);
+                }
+                return new DateDiffFuncSecondConstant(position, args.getQuick(1), args.getQuick(2));
+            }
+            return new TimestampConstant(position, Numbers.LONG_NaN);
+        }
+        return new DateDiffFunc(position, args.getQuick(0), args.getQuick(1), args.getQuick(2));
+}
+
+
+    private static class DateDiffFuncSecondConstant extends LongFunction implements BinaryFunction {
         final Function left;
-        final Function center;
         final Function right;
 
-        public DateDiffFunc(int position, Function left, Function center, Function right) {
+        public DateDiffFuncSecondConstant(int position, Function left, Function right) {
             super(position);
             this.left = left;
-            this.center = center;
             this.right = right;
         }
 
@@ -63,24 +84,83 @@ public class DatediffTimestampFunctionFactory implements FunctionFactory {
         }
 
         @Override
-        public Function getCenter() {
-            return center;
-        }
-
-        @Override
         public Function getRight() {
             return right;
         }
 
         @Override
         public long getLong(Record rec) {
-            final char l = left.getChar(rec);
-            final long c = center.getTimestamp(rec);
+            final long l = left.getTimestamp(rec);
             final long r = right.getTimestamp(rec);
-            if (c == Numbers.LONG_NaN || r == Numbers.LONG_NaN) {
+            if (l == Numbers.LONG_NaN || r == Numbers.LONG_NaN) {
                 return Numbers.LONG_NaN;
             }
-            return Timestamps.getPeriodBetween(l, c, r);
+            return Timestamps.getPeriodBetween('s', l, r);
         }
     }
+
+    private static class DateDiffFuncSecondConstantTimestampConstant extends LongFunction implements UnaryFunction {
+        private final Function arg;
+        private final long constantTime;
+
+        public DateDiffFuncSecondConstantTimestampConstant(int position, Function left, long right) {
+            super(position);
+            this.arg = left;
+            this.constantTime = right;
+        }
+
+        @Override
+        public Function getArg() {
+            return arg;
+        }
+
+        @Override
+        public long getLong(Record rec) {
+            final long l = arg.getTimestamp(rec);
+            if (l == Numbers.LONG_NaN) {
+                return Numbers.LONG_NaN;
+            }
+            return Timestamps.getPeriodBetween('s', l, constantTime);
+        }
+    }
+
+
+private static class DateDiffFunc extends LongFunction implements TernaryFunction {
+    final Function left;
+    final Function center;
+    final Function right;
+
+    public DateDiffFunc(int position, Function left, Function center, Function right) {
+        super(position);
+        this.left = left;
+        this.center = center;
+        this.right = right;
+    }
+
+    @Override
+    public Function getLeft() {
+        return left;
+    }
+
+    @Override
+    public Function getCenter() {
+        return center;
+    }
+
+    @Override
+    public Function getRight() {
+        return right;
+    }
+
+    @Override
+    public long getLong(Record rec) {
+        final char l = left.getChar(rec);
+        final long c = center.getTimestamp(rec);
+        final long r = right.getTimestamp(rec);
+        if (c == Numbers.LONG_NaN || r == Numbers.LONG_NaN) {
+            return Numbers.LONG_NaN;
+        }
+        return Timestamps.getPeriodBetween(l, c, r);
+    }
+}
 }
