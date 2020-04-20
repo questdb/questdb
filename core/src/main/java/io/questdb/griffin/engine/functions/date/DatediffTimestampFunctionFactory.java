bb62fea65d8b11c46e09f3776ea0425ed552c6fe
TheTanc <tanc@questdb.io>
2020-04-20 06:27:03 +0100

    feat(griffin): datediff - naive implementation

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/date/DatediffTimestampFunctionFactory.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2020 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.date;
+
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.sql.Function;
+import io.questdb.cairo.sql.Record;
+import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.engine.functions.LongFunction;
+import io.questdb.griffin.engine.functions.TernaryFunction;
+import io.questdb.std.Numbers;
+import io.questdb.std.ObjList;
+import io.questdb.std.microtime.Timestamps;
+
+public class DatediffTimestampFunctionFactory implements FunctionFactory {
+    @Override
+    public String getSignature() {
+        return "datediff(ANN)";
+    }
+
+    @Override
+    public Function newInstance(ObjList<Function> args, int position, CairoConfiguration configuration) {
+        return new DateDiffFunc(position, args.getQuick(0), args.getQuick(1), args.getQuick(2));
+    }
+
+    private static class DateDiffFunc extends LongFunction implements TernaryFunction {
+        final Function left;
+        final Function center;
+        final Function right;
+
+        public DateDiffFunc(int position, Function left, Function center, Function right) {
+            super(position);
+            this.left = left;
+            this.center = center;
+            this.right = right;
+        }
+
+        @Override
+        public Function getLeft() {
+            return left;
+        }
+
+        @Override
+        public Function getCenter() {
+            return center;
+        }
+
+        @Override
+        public Function getRight() {
+            return right;
+        }
+
+        @Override
+        public long getLong(Record rec) {
+            final char l = left.getChar(rec);
+            final long c = center.getTimestamp(rec);
+            final long r = right.getTimestamp(rec);
+            if (c == Numbers.LONG_NaN || r == Numbers.LONG_NaN) {
+                return Numbers.LONG_NaN;
+            }
+            return Timestamps.getPeriodBetween(l, c, r);
+        }
+    }
+}
