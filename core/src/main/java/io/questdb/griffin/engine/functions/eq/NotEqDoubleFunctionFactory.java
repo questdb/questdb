801d80fe8ba7b1025a5c0059d51cd1342324eb2b
clicking buttons <clickingbuttons@pm.me>
2020-05-02 17:16:17 -0400

    feat(griffin): implement !=(DD)

--- core/src/main/java/io/questdb/griffin/engine/functions/eq/NotEqDoubleFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/eq/NotEqDoubleFunctionFactory.java
@@ -28,27 +28,16 @@ import io.questdb.cairo.CairoConfiguration;
 import io.questdb.cairo.ColumnType;
 import io.questdb.cairo.sql.Function;
 import io.questdb.cairo.sql.Record;
-import io.questdb.griffin.FunctionFactory;
-import io.questdb.griffin.engine.functions.BinaryFunction;
-import io.questdb.griffin.engine.functions.BooleanFunction;
-import io.questdb.griffin.engine.functions.UnaryFunction;
-import io.questdb.std.Numbers;
 import io.questdb.std.ObjList;
 
-public class EqDoubleFunctionFactory implements FunctionFactory {
+public class NotEqDoubleFunctionFactory extends EqDoubleFunctionFactory {
     @Override
     public String getSignature() {
-        return "=(DD)";
+        return "!=(DD)";
     }
 
     @Override
     public Function newInstance(ObjList<Function> args, int position, CairoConfiguration configuration) {
-        // this is probably a special case factory
-        // NaN is always a double, so this could lead comparisons of all primitive types
-        // to NaN route to this factory. Obviously comparing naively will not work
-        // We have to check arg types and when NaN is present we would generate special case
-        // functions for NaN checks.
-
         Function left = args.getQuick(0);
         Function right = args.getQuick(1);
 
@@ -88,145 +77,68 @@ public class EqDoubleFunctionFactory implements FunctionFactory {
         return new Func(position, args.getQuick(0), args.getQuick(1));
     }
 
-    protected static class Func extends BooleanFunction implements BinaryFunction {
-        protected final Function left;
-        protected final Function right;
-
+    private static class Func extends EqDoubleFunctionFactory.Func {
         public Func(int position, Function left, Function right) {
-            super(position);
-            this.left = left;
-            this.right = right;
+            super(position, left, right);
         }
 
         @Override
         public boolean getBool(Record rec) {
-            final double l = left.getDouble(rec);
-            final double r = right.getDouble(rec);
-            return l != l && r != r || Math.abs(l - r) < 0.0000000001;
-        }
-
-        @Override
-        public Function getLeft() {
-            return left;
-        }
-
-        @Override
-        public Function getRight() {
-            return right;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncIntIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncIntIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncIntIsNaN extends EqDoubleFunctionFactory.FuncIntIsNaN {
+        public FuncIntIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return arg.getInt(rec) == Numbers.INT_NaN;
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncLongIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncLongIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncLongIsNaN extends EqDoubleFunctionFactory.FuncLongIsNaN {
+        public FuncLongIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return arg.getLong(rec) == Numbers.LONG_NaN;
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncDateIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncDateIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncDateIsNaN extends EqDoubleFunctionFactory.FuncDateIsNaN {
+        public FuncDateIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return arg.getDate(rec) == Numbers.LONG_NaN;
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncTimestampIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncTimestampIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncTimestampIsNaN extends EqDoubleFunctionFactory.FuncTimestampIsNaN {
+        public FuncTimestampIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return arg.getTimestamp(rec) == Numbers.LONG_NaN;
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncFloatIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncFloatIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncFloatIsNaN extends EqDoubleFunctionFactory.FuncFloatIsNaN {
+        public FuncFloatIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return Float.isNaN(arg.getFloat(rec));
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 
-    protected static class FuncDoubleIsNaN extends BooleanFunction implements UnaryFunction {
-        protected final Function arg;
-
-        public FuncDoubleIsNaN(int position, Function arg) {
-            super(position);
-            this.arg = arg;
-        }
+    private static class FuncDoubleIsNaN extends EqDoubleFunctionFactory.FuncDoubleIsNaN {
+        public FuncDoubleIsNaN(int position, Function arg) { super(position, arg); }
 
         @Override
         public boolean getBool(Record rec) {
-            return Double.isNaN(arg.getDouble(rec));
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
+            return !super.getBool(rec);
         }
     }
 }
