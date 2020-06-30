questdb/questdb

commit fc7eeba61144ef9a89618c5516be2b9fbd37a4bd

fix(server): do not copy config when starting with '-f' (#445)

core/src/main/java/io/questdb/ServerMain.java
@@ -250,9 +250,7 @@ public class ServerMain {
                 log.error().$("could not find site [resource=").$(publicZip).$(']').$();
             }
         }
-        copyConfResource(dir, force, buffer, "conf/date.formats", log);
         copyConfResource(dir, force, buffer, "conf/mime.types", log);
-        copyConfResource(dir, force, buffer, "conf/server.conf", log);
     }
 
     private static void copyConfResource(String dir, boolean force, byte[] buffer, String res, Log log) throws IOException {

commit 8035ed5ccef7e9dd9e8057691789611865ae6887

fix(server): always copy conf/date.formats when not present, fix regression introduced in #445

core/src/main/java/io/questdb/ServerMain.java
@@ -277,7 +277,9 @@ public class ServerMain {
                 log.error().$("could not find site [resource=").$(publicZip).$(']').$();
             }
         }
+        copyConfResource(dir, false, buffer, "conf/date.formats", log);
         copyConfResource(dir, force, buffer, "conf/mime.types", log);
+        copyConfResource(dir, false, buffer, "conf/server.conf", log);
     }
 
     private static void copyConfResource(String dir, boolean force, byte[] buffer, String res, Log log) throws IOException {