97ab670c358f098cfc4a2f3845182f664dd5e720
Levi Bowman <lbowman@trace3.com>
2020-07-03 06:03:09 -0600

    feat(griffin): add LastDoubleGroupByFunction

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -392,6 +392,7 @@ open module io.questdb {
             io.questdb.griffin.engine.functions.groupby.KSumDoubleGroupByFunctionFactory,
             io.questdb.griffin.engine.functions.groupby.NSumDoubleGroupByFunctionFactory,
 //                  'last' group by function
+            io.questdb.griffin.engine.functions.groupby.LastDoubleGroupByFunctionFactory,
             io.questdb.griffin.engine.functions.groupby.LastIntGroupByFunctionFactory,
             io.questdb.griffin.engine.functions.groupby.LastTimestampGroupByFunctionFactory,
 //                  'max' group
---
+++ core/src/test/java/io/questdb/griffin/engine/functions/groupby/LastDoubleGroupByFunctionFactoryTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2020 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.groupby;
+
+import io.questdb.cairo.TableWriter;
+import io.questdb.cairo.sql.Record;
+import io.questdb.cairo.sql.RecordCursor;
+import io.questdb.cairo.sql.RecordCursorFactory;
+import io.questdb.griffin.AbstractGriffinTest;
+import io.questdb.griffin.SqlException;
+import io.questdb.griffin.engine.functions.rnd.SharedRandom;
+import io.questdb.std.Rnd;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LastDoubleGroupByFunctionFactoryTest extends AbstractGriffinTest {
+
+    @Before
+    public void setUp3() {
+        SharedRandom.RANDOM.set(new Rnd());
+    }
+
+    @Test
+    public void testLastSequentialInput() throws SqlException {
+
+        compiler.compile("create table tab (d double)", sqlExecutionContext);
+
+        try (TableWriter w = engine.getWriter(sqlExecutionContext.getCairoSecurityContext(), "tab")) {
+            for (double i = 1.0; i < 100.0; i++) {
+                TableWriter.Row r = w.newRow();
+                r.putDouble(0, i);
+                r.append();
+            }
+            w.commit();
+        }
+
+        try (RecordCursorFactory factory = compiler.compile("select last(d) from tab", sqlExecutionContext).getRecordCursorFactory()) {
+            try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
+                Record record = cursor.getRecord();
+                Assert.assertEquals(1, cursor.size());
+                Assert.assertTrue(cursor.hasNext());
+                Assert.assertEquals(99.0, record.getDouble(0), 0.0001);
+            }
+        }
+    }
+}
