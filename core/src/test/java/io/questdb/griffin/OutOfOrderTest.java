3c3fcdfa61f1758645c9cbbc8035b1ce35a8e54f
Vlad Ilyushchenko <bluestreak@gmail.com>
2020-09-23 11:51:43 +0100

    binary search test

--- core/src/test/java/io/questdb/griffin/OutOfOrderTest.java
+++ core/src/test/java/io/questdb/griffin/OutOfOrderTest.java
@@ -606,4 +606,202 @@ public class OutOfOrderTest extends AbstractGriffinTest {
                 }
         );
     }
+
+    @Test
+    public void testPartitionedOOTopAndBottom() throws Exception {
+        assertMemoryLeak(() -> {
+                    // create table with roughly 2AM data
+                    compiler.compile(
+                            "create table x as (" +
+                                    "select" +
+                                    " cast(x as int) i," +
+                                    " rnd_symbol('msft','ibm', 'googl') sym," +
+                                    " round(rnd_double(0)*100, 3) amt," +
+                                    " to_timestamp('2018-01', 'yyyy-MM') + x * 720000000 timestamp," +
+                                    " rnd_boolean() b," +
+                                    " rnd_str('ABC', 'CDE', null, 'XYZ') c," +
+                                    " rnd_double(2) d," +
+                                    " rnd_float(2) e," +
+                                    " rnd_short(10,1024) f," +
+                                    " rnd_date(to_date('2015', 'yyyy'), to_date('2016', 'yyyy'), 2) g," +
+                                    " rnd_symbol(4,4,4,2) ik," +
+                                    " rnd_long() j," +
+                                    " timestamp_sequence(500000000000L,1000000L) ts," +
+                                    " rnd_byte(2,50) l," +
+                                    " rnd_bin(10, 20, 2) m," +
+                                    " rnd_str(5,16,2) n," +
+                                    " rnd_char() t" +
+                                    " from long_sequence(500)" +
+                                    ") timestamp (ts) partition by DAY",
+                            sqlExecutionContext
+                    );
+
+                    // create table with 1AM data
+
+                    compiler.compile(
+                            "create table top as (" +
+                                    "select" +
+                                    " cast(x as int) i," +
+                                    " rnd_symbol('msft','ibm', 'googl') sym," +
+                                    " round(rnd_double(0)*100, 3) amt," +
+                                    " to_timestamp('2018-01', 'yyyy-MM') + x * 720000000 timestamp," +
+                                    " rnd_boolean() b," +
+                                    " rnd_str('ABC', 'CDE', null, 'XYZ') c," +
+                                    " rnd_double(2) d," +
+                                    " rnd_float(2) e," +
+                                    " rnd_short(10,1024) f," +
+                                    " rnd_date(to_date('2015', 'yyyy'), to_date('2016', 'yyyy'), 2) g," +
+                                    " rnd_symbol(4,4,4,2) ik," +
+                                    " rnd_long() j," +
+                                    " timestamp_sequence(150000000000L,1000000L) ts," +
+                                    " rnd_byte(2,50) l," +
+                                    " rnd_bin(10, 20, 2) m," +
+                                    " rnd_str(5,16,2) n," +
+                                    " rnd_char() t" +
+                                    " from long_sequence(500)" +
+                                    ") timestamp (ts) partition by DAY",
+                            sqlExecutionContext
+                    );
+
+                    compiler.compile(
+                            "create table bottom as (" +
+                                    "select" +
+                                    " cast(x as int) i," +
+                                    " rnd_symbol('msft','ibm', 'googl') sym," +
+                                    " round(rnd_double(0)*100, 3) amt," +
+                                    " to_timestamp('2018-01', 'yyyy-MM') + x * 720000000 timestamp," +
+                                    " rnd_boolean() b," +
+                                    " rnd_str('ABC', 'CDE', null, 'XYZ') c," +
+                                    " rnd_double(2) d," +
+                                    " rnd_float(2) e," +
+                                    " rnd_short(10,1024) f," +
+                                    " rnd_date(to_date('2015', 'yyyy'), to_date('2016', 'yyyy'), 2) g," +
+                                    " rnd_symbol(4,4,4,2) ik," +
+                                    " rnd_long() j," +
+                                    " timestamp_sequence(500500000000L,100000000L) ts," +
+                                    " rnd_byte(2,50) l," +
+                                    " rnd_bin(10, 20, 2) m," +
+                                    " rnd_str(5,16,2) n," +
+                                    " rnd_char() t" +
+                                    " from long_sequence(500)" +
+                                    ") timestamp (ts) partition by DAY",
+                            sqlExecutionContext
+                    );
+
+                    // create third table, which will contain both X and 1AM
+                    compiler.compile("create table y as (select * from x union all select * from top union all select * from bottom)", sqlExecutionContext);
+
+                    // expected outcome
+                    sink.clear();
+                    try (RecordCursorFactory factory = compiler.compile("y order by ts", sqlExecutionContext).getRecordCursorFactory()) {
+                        try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
+                            printer.print(cursor, factory.getMetadata(), true);
+                        }
+                    }
+
+                    String expected = Chars.toString(sink);
+
+                    compiler.compile("insert into x select * from (top union all bottom)", sqlExecutionContext);
+
+                    // release reader
+                    engine.releaseAllReaders();
+
+                    sink.clear();
+                    try (RecordCursorFactory factory = compiler.compile("x", sqlExecutionContext).getRecordCursorFactory()) {
+                        try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
+                            printer.print(cursor, factory.getMetadata(), true);
+                        }
+                    }
+
+                    TestUtils.assertEquals(expected, sink);
+                }
+        );
+    }
+
+    @Test
+    public void testPartitionedDataOOData() throws Exception {
+        assertMemoryLeak(() -> {
+                    // create table with roughly 2AM data
+                    compiler.compile(
+                            "create table x as (" +
+                                    "select" +
+                                    " cast(x as int) i," +
+                                    " rnd_symbol('msft','ibm', 'googl') sym," +
+                                    " round(rnd_double(0)*100, 3) amt," +
+                                    " to_timestamp('2018-01', 'yyyy-MM') + x * 720000000 timestamp," +
+                                    " rnd_boolean() b," +
+                                    " rnd_str('ABC', 'CDE', null, 'XYZ') c," +
+                                    " rnd_double(2) d," +
+                                    " rnd_float(2) e," +
+                                    " rnd_short(10,1024) f," +
+                                    " rnd_date(to_date('2015', 'yyyy'), to_date('2016', 'yyyy'), 2) g," +
+                                    " rnd_symbol(4,4,4,2) ik," +
+                                    " rnd_long() j," +
+                                    " timestamp_sequence(500000000000L,1000000L) ts," +
+                                    " rnd_byte(2,50) l," +
+                                    " rnd_bin(10, 20, 2) m," +
+                                    " rnd_str(5,16,2) n," +
+                                    " rnd_char() t" +
+                                    " from long_sequence(500)" +
+                                    ") timestamp (ts) partition by DAY",
+                            sqlExecutionContext
+                    );
+
+                    // create table with 1AM data
+
+                    compiler.compile(
+                            "create table middle as (" +
+                                    "select" +
+                                    " cast(x as int) i," +
+                                    " rnd_symbol('msft','ibm', 'googl') sym," +
+                                    " round(rnd_double(0)*100, 3) amt," +
+                                    " to_timestamp('2018-01', 'yyyy-MM') + x * 720000000 timestamp," +
+                                    " rnd_boolean() b," +
+                                    " rnd_str('ABC', 'CDE', null, 'XYZ') c," +
+                                    " rnd_double(2) d," +
+                                    " rnd_float(2) e," +
+                                    " rnd_short(10,1024) f," +
+                                    " rnd_date(to_date('2015', 'yyyy'), to_date('2016', 'yyyy'), 2) g," +
+                                    " rnd_symbol(4,4,4,2) ik," +
+                                    " rnd_long() j," +
+                                    " timestamp_sequence(500288000000L,10L) ts," +
+                                    " rnd_byte(2,50) l," +
+                                    " rnd_bin(10, 20, 2) m," +
+                                    " rnd_str(5,16,2) n," +
+                                    " rnd_char() t" +
+                                    " from long_sequence(500)" +
+                                    ") timestamp (ts) partition by DAY",
+                            sqlExecutionContext
+                    );
+
+
+                    // create third table, which will contain both X and 1AM
+                    compiler.compile("create table y as (x union all middle)", sqlExecutionContext);
+
+                    // expected outcome
+                    sink.clear();
+                    try (RecordCursorFactory factory = compiler.compile("y order by ts", sqlExecutionContext).getRecordCursorFactory()) {
+                        try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
+                            printer.print(cursor, factory.getMetadata(), true);
+                        }
+                    }
+
+                    String expected = Chars.toString(sink);
+
+                    compiler.compile("insert into x select * from middle", sqlExecutionContext);
+
+                    // release reader
+                    engine.releaseAllReaders();
+
+                    sink.clear();
+                    try (RecordCursorFactory factory = compiler.compile("x", sqlExecutionContext).getRecordCursorFactory()) {
+                        try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
+                            printer.print(cursor, factory.getMetadata(), true);
+                        }
+                    }
+
+                    TestUtils.assertEquals(expected, sink);
+                }
+        );
+    }
 }
