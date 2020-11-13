5ea6d6eda3f212a86a50fb636f7f32741a6382f2
Vlad Ilyushchenko <bluestreak@gmail.com>
2020-11-13 11:35:57 +0000

    chore(sql): small tidy up

--- core/src/main/java/io/questdb/griffin/SqlOptimiser.java
+++ core/src/main/java/io/questdb/griffin/SqlOptimiser.java
@@ -1222,7 +1222,7 @@ class SqlOptimiser {
         for (int i = 0; i < len; i++) {
             QueryColumn queryColumn = map.get(orderBy.getQuick(i).token);
             if (queryColumn.getAst().type == ExpressionNode.LITERAL) {
-                orderByAdvice.add(nextLiteral(queryColumn.getAst().token));
+                orderByAdvice.add(queryColumn.getAst());
             } else {
                 orderByAdvice.clear();
                 break;
