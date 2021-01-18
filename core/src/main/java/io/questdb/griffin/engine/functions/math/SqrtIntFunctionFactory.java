75e3962afc2f231b1d23b5a4dee590e74387861d
Marc Recht <marc.recht@outlook.com>
2021-01-18 21:05:18 +0100

    implement sqrt

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/math/SqrtIntFunctionFactory.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2020 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.math;
+
+import io.questdb.cairo.CairoConfiguration;
+import io.questdb.cairo.sql.Function;
+import io.questdb.cairo.sql.Record;
+import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.SqlExecutionContext;
+import io.questdb.griffin.engine.functions.IntFunction;
+import io.questdb.griffin.engine.functions.UnaryFunction;
+import io.questdb.std.ObjList;
+
+public class SqrtIntFunctionFactory implements FunctionFactory {
+    @Override
+    public String getSignature() {
+        return "sqrt(I)";
+    }
+
+    @Override
+    public Function newInstance(
+            ObjList<Function> args,
+            int position,
+            CairoConfiguration configuration,
+            SqlExecutionContext sqlExecutionContext) {
+        return new SqrtIntFunction(position, args.getQuick(0));
+    }
+
+    private static class SqrtIntFunction extends IntFunction implements UnaryFunction {
+        private final Function arg;
+
+        public SqrtIntFunction(int position, Function arg) {
+            super(position);
+            this.arg = arg;
+        }
+
+        @Override
+        public Function getArg() {
+            return arg;
+        }
+
+
+        @Override
+        public int getInt(Record rec) {
+            int value = arg.getInt(rec);
+            return (int) Math.sqrt(value);
+        }
+    }
+}
