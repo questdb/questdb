a3d3f7eb64dfddb29b7a68861a623ca86884d06a
Alexander Pelagenko <ideoma@gmail.com>
2021-05-09 22:00:37 +0100

    chore: ci code coverage on PR diff

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-echo "##vso[task.setvariable variable=COVERAGE_DIFF;]$(
+#!/bin/bash
 
 curl https://api.github.com/repos/questdb/questdb/pulls/977/files -s \
       | grep -oP 'filename": "core/src/main/java/io/questdb.*\/\K[^.]+' \
-      | tr '\n' ',' | sed -e 's/,/,+:*./g' \
+      | tr '\n' ',' | sed -e 's/,/,+:*./g' | sed 's/,+:\*\.$//' \
       | awk '{print "##vso[task.setvariable variable=COVERAGE_DIFF;]+:*." $0}'
