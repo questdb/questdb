2421dcc41a36b942297977d15b4c045439ab4f49
alpel <2159629+ideoma@users.noreply.github.com>
2021-05-11 14:43:15 +0100

    chore: ci coverage diff

--- ci/coverage.sh
+++ ci/coverage.sh
@@ -7,6 +7,8 @@ COV_CLASSES=$(curl https://api.github.com/repos/questdb/questdb/pulls/$1/files -
 if [ -z "$COV_CLASSES" ]
 then
     echo "##vso[task.setvariable variable=COVERAGE_DIFF;]-:*"
+    echo "##vso[task.setvariable variable=CODE_COVERAGE_TOOL_OPTION;]None"
 else
     echo "##vso[task.setvariable variable=COVERAGE_DIFF;]+:*.$COV_CLASSES"
+    echo "##vso[task.setvariable variable=CODE_COVERAGE_TOOL_OPTION;]JaCoCo"
 fi
