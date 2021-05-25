072bdf5d0bf9f8f870eec403d0f8cb6101150933
Vlad Ilyushchenko <bluestreak01@users.noreply.github.com>
2021-05-25 21:11:15 +0100

    feat(sql): to_timezone() and to_utc() timestamp functions (#1062)

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/date/OffsetTimestampFunctionFromOffset.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2020 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.date;
+
+import io.questdb.cairo.sql.Function;
+import io.questdb.cairo.sql.Record;
+import io.questdb.griffin.engine.functions.TimestampFunction;
+import io.questdb.griffin.engine.functions.UnaryFunction;
+
+class OffsetTimestampFunctionFromOffset extends TimestampFunction implements UnaryFunction {
+    private final Function timestamp;
+    private final long offset;
+
+    public OffsetTimestampFunctionFromOffset(Function timestamp, long offset) {
+        this.timestamp = timestamp;
+        this.offset = offset;
+    }
+
+    @Override
+    public Function getArg() {
+        return timestamp;
+    }
+
+    @Override
+    public long getTimestamp(Record rec) {
+        return timestamp.getTimestamp(rec) + offset;
+    }
+}
