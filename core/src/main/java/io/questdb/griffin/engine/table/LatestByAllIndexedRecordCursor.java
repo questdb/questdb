5cddae1c3cdfbe6633d2b40321fab018d68ca0db
Eugene Lysiuchenko <ls.eugene@gmail.com>
2021-06-02 19:00:54 +0300

    feat(griffin): implement index scanning for latest by on cpp side

--- core/src/main/java/io/questdb/griffin/engine/table/LatestByAllIndexedRecordCursor.java
+++ core/src/main/java/io/questdb/griffin/engine/table/LatestByAllIndexedRecordCursor.java
@@ -26,18 +26,16 @@ package io.questdb.griffin.engine.table;
 
 import io.questdb.cairo.BitmapIndexReader;
 import io.questdb.cairo.sql.DataFrame;
-import io.questdb.cairo.sql.RowCursor;
 import io.questdb.griffin.SqlExecutionContext;
-import io.questdb.std.DirectLongList;
-import io.questdb.std.IntHashSet;
-import io.questdb.std.IntList;
-import io.questdb.std.Rows;
+import io.questdb.std.*;
 import org.jetbrains.annotations.NotNull;
 
 class LatestByAllIndexedRecordCursor extends AbstractRecordListCursor {
-
     private final int columnIndex;
-    private final IntHashSet found = new IntHashSet();
+
+    private long bias = 0;
+    private long aIndex;
+    private long aLimit;
 
     public LatestByAllIndexedRecordCursor(int columnIndex, DirectLongList rows, @NotNull IntList columnIndexes) {
         super(rows, columnIndexes);
@@ -45,53 +43,129 @@ class LatestByAllIndexedRecordCursor extends AbstractRecordListCursor {
     }
 
     @Override
-    protected void buildTreeMap(SqlExecutionContext executionContext) {
-        found.clear();
+    public boolean hasNext() {
+        if (aIndex < aLimit) {
+            long row = rows.get(aIndex++) - 1; // we added 1 on cpp side
+            recordA.jumpTo(Rows.toPartitionIndex(row), Rows.toLocalRowID(row));
+            return true;
+        }
+        return false;
+    }
 
+    @Override
+    public void toTop() {
+        aIndex = bias;
+    }
+
+    @Override
+    public long size() {
+        return aLimit - bias;
+    }
+
+    @Override
+    protected void buildTreeMap(SqlExecutionContext executionContext) {
         int keyCount = getSymbolTable(columnIndex).size() + 1;
         int keyLo = 0;
         int keyHi = keyCount;
 
-        int localLo = Integer.MAX_VALUE;
-        int localHi = Integer.MIN_VALUE;
+        rows.extend(keyCount);
+        rows.setPos(rows.getCapacity());
+        rows.zero(0);
+
+        long rowCount = 0;
+        long argsAddress = ArgumentsWrapper.allocateMemory();
+        ArgumentsWrapper.setRowsAddress(argsAddress, rows.getAddress());
+        ArgumentsWrapper.setRowsCapacity(argsAddress, rows.getCapacity());
 
         DataFrame frame;
         // frame metadata is based on TableReader, which is "full" metadata
         // this cursor works with subset of columns, which warrants column index remap
         int frameColumnIndex = columnIndexes.getQuick(columnIndex);
-        while ((frame = this.dataFrameCursor.next()) != null && found.size() < keyCount) {
+        while ((frame = this.dataFrameCursor.next()) != null && rowCount < keyCount) {
             final BitmapIndexReader indexReader = frame.getBitmapIndexReader(frameColumnIndex, BitmapIndexReader.DIR_BACKWARD);
             final long rowLo = frame.getRowLo();
             final long rowHi = frame.getRowHi() - 1;
 
-            for (int i = keyLo; i < keyHi; i++) {
-                int index = found.keyIndex(i);
-                if (index > -1) {
-                    RowCursor cursor = indexReader.getCursor(false, i, rowLo, rowHi);
-                    if (cursor.hasNext()) {
-                        rows.add(Rows.toRowID(frame.getPartitionIndex(), cursor.next()));
-                        found.addAt(index, i);
-                    } else {
-                        // adjust range
-                        if (i < localLo) {
-                            localLo = i;
-                        }
+            ArgumentsWrapper.setKeyLo(argsAddress, keyLo);
+            ArgumentsWrapper.setKeyHi(argsAddress, keyHi);
+            ArgumentsWrapper.setRowsSize(argsAddress, rows.size());
 
-                        if (i > localHi) {
-                            localHi = i;
-                        }
-                    }
-                }
-            }
+            BitmapIndexUtilsNative.latestScanBackward(
+                    indexReader.getKeyBaseAddress(),
+                    indexReader.getKeyMemorySize(),
+                    indexReader.getValueBaseAddress(),
+                    indexReader.getValueMemorySize(),
+                    argsAddress,
+                    indexReader.getUnIndexedNullCount(),
+                    rowHi, rowLo,
+                    frame.getPartitionIndex(), indexReader.getValueBlockCapacity()
+            );
 
-            // we have to sort rows because multiple symbols
-            // are liable to be looked up out of order
-            rows.sortAsUnsigned();
+            rowCount += ArgumentsWrapper.getRowsSize(argsAddress);
+            keyLo = (int)ArgumentsWrapper.getKeyLo(argsAddress);
+            keyHi = (int)ArgumentsWrapper.getKeyHi(argsAddress) + 1;
+        }
+        ArgumentsWrapper.releaseMemory(argsAddress);
 
-            keyLo = localLo;
-            keyHi = localHi + 1;
-            localLo = Integer.MAX_VALUE;
-            localHi = Integer.MIN_VALUE;
+        // we have to sort rows because multiple symbols
+        // are liable to be looked up out of order
+        rows.setPos(rows.getCapacity());
+        rows.sortAsUnsigned();
+
+        //skip "holes"
+        while(rows.get(bias) <= 0) {
+            bias++;
+        }
+
+        aLimit = rows.size();
+        aIndex = bias;
+    }
+
+    static final class ArgumentsWrapper {
+        private static final long KEY_LO_OFFSET = 0*8;
+        private static final long KEY_HI_OFFSET = 1*8;
+        private static final long ROWS_ADDRESS_OFFSET = 2*8;
+        private static final long ROWS_CAPACITY_OFFSET = 3*8;
+        private static final long ROWS_SIZE_OFFSET = 4*8;
+        private static final long MEMORY_SIZE = 5*8;
+
+        public static long allocateMemory() {
+            return Unsafe.calloc(MEMORY_SIZE);
+        }
+
+        public static void releaseMemory(long address) {
+            Unsafe.free(address, MEMORY_SIZE);
+        }
+
+        public static long getKeyLo(long address) {
+            return Unsafe.getUnsafe().getLong(address + KEY_LO_OFFSET);
+        }
+        public static long getKeyHi(long address) {
+            return Unsafe.getUnsafe().getLong(address + KEY_HI_OFFSET);
+        }
+        public static long getRowsAddress(long address) {
+            return Unsafe.getUnsafe().getLong(address + ROWS_ADDRESS_OFFSET);
+        }
+        public static long getRowsCapacity(long address) {
+            return Unsafe.getUnsafe().getLong(address + ROWS_CAPACITY_OFFSET);
+        }
+        public static long getRowsSize(long address) {
+            return Unsafe.getUnsafe().getLong(address + ROWS_SIZE_OFFSET);
+        }
+        public static void setKeyLo(long address, long lo) {
+            Unsafe.getUnsafe().putLong(address + KEY_LO_OFFSET, lo);
+        }
+        public static void setKeyHi(long address, long up) {
+            Unsafe.getUnsafe().putLong(address + KEY_HI_OFFSET, up);
+        }
+        public static void setRowsAddress(long address, long addr) {
+            Unsafe.getUnsafe().putLong(address + ROWS_ADDRESS_OFFSET, addr);
+        }
+        public static void setRowsCapacity(long address, long cap) {
+            Unsafe.getUnsafe().putLong(address + ROWS_CAPACITY_OFFSET, cap);
+        }
+        public static void setRowsSize(long address, long size) {
+            Unsafe.getUnsafe().getLong(address + ROWS_SIZE_OFFSET, size);
         }
     }
 }
