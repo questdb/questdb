76590593eb73b01d3a81b79cfadfc985f9ddfb8a
Maciej Bodek <maciej.bodek@gmail.com>
2021-09-30 10:12:13 +0200

    Disable notifications for copying to clipboard

--- ui/src/scenes/Schema/Table/ContextualMenu/index.tsx
+++ ui/src/scenes/Schema/Table/ContextualMenu/index.tsx
@@ -1,11 +1,7 @@
 import React, { useCallback, useContext } from "react"
-import { useDispatch } from "react-redux"
 import { ContextMenu, MenuItem } from "components/ContextMenu"
 import { QuestContext } from "providers"
 import * as QuestDB from "utils/questdb"
-import { actions } from "store"
-import { Text } from "components"
-import { NotificationType } from "types"
 import { formatTableSchemaQueryResult } from "./services"
 
 type Props = {
@@ -14,38 +10,15 @@ type Props = {
 
 const ContextualMenu = ({ name }: Props) => {
   const { quest } = useContext(QuestContext)
-  const dispatch = useDispatch()
 
   const handleCopySchemaToClipboard = useCallback(() => {
-    void quest
-      .queryRaw(`table_columns('${name}')`)
-      .then((result) => {
-        if (result.type === QuestDB.Type.DQL && result.count > 0) {
-          const formattedResult = formatTableSchemaQueryResult(name, result)
-          void navigator.clipboard.writeText(formattedResult).then(() => {
-            dispatch(
-              actions.query.addNotification({
-                title: (
-                  <Text color="draculaForeground">Table Schema copied !</Text>
-                ),
-                line1: <Text color="draculaForeground">{formattedResult}</Text>,
-                type: NotificationType.SUCCESS,
-              }),
-            )
-          })
-        }
-      })
-      .catch(() => {
-        dispatch(
-          actions.query.addNotification({
-            title: (
-              <Text color="draculaForeground">Table Schema copy error</Text>
-            ),
-            type: NotificationType.ERROR,
-          }),
-        )
-      })
-  }, [quest, dispatch, name])
+    void quest.queryRaw(`table_columns('${name}')`).then((result) => {
+      if (result.type === QuestDB.Type.DQL && result.count > 0) {
+        const formattedResult = formatTableSchemaQueryResult(name, result)
+        void navigator.clipboard.writeText(formattedResult)
+      }
+    })
+  }, [quest, name])
 
   return (
     <ContextMenu id={name}>
