956c02b9fd51e9d13126c55ada91be5da9aabd10
Maciej Bodek <maciej.bodek@gmail.com>
2021-11-03 15:23:50 +0100

    get prop types from the library interface

--- ui/src/components/VirtualList/index.tsx
+++ ui/src/components/VirtualList/index.tsx
@@ -1,25 +1,6 @@
-import React, { ReactNode } from "react"
-import { Virtuoso } from "react-virtuoso"
+import React from "react"
+import { Virtuoso, VirtuosoProps } from "react-virtuoso"
 
-type Props = Readonly<{
-  height?: number
-  isScrolling?: (isScrolling: boolean) => void
-  itemContent: (index: number, data: any) => ReactNode
-  totalCount: number | undefined
-}>
-
-export const VirtualList = ({
-  height,
-  isScrolling,
-  itemContent,
-  totalCount,
-}: Props) => {
-  return (
-    <Virtuoso
-      isScrolling={isScrolling}
-      itemContent={itemContent}
-      style={height ? { height } : {}}
-      totalCount={totalCount}
-    />
-  )
+export const VirtualList = ({ height, ...rest }: VirtuosoProps<any>) => {
+  return <Virtuoso style={height ? { height } : {}} {...rest} />
 }
