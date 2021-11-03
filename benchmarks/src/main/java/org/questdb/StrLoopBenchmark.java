ab2066a6d80f5adcb86f3ff42bf0ec42447f069a
Vlad Ilyushchenko <bluestreak@gmail.com>
2021-11-03 15:03:43 +0000

    chore: string loop benchmark

---
+++ benchmarks/src/main/java/org/questdb/StrLoopBenchmark.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.questdb;
+
+import io.questdb.std.Rnd;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class StrLoopBenchmark {
+
+    static Rnd rnd = new Rnd();
+    static int N = 256;
+    private static final String[] array = new String[N];
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(StrLoopBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public String testBaseline() {
+        return array[rnd.nextInt(N)];
+    }
+
+    @Benchmark
+    public int testConventional() {
+        int sum = 0;
+        String s = array[rnd.nextInt(N)];
+        for (int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i);
+        }
+        return sum;
+    }
+
+    @Benchmark
+    public int testOptimised() {
+        int sum = 0;
+        String s = array[rnd.nextInt(N)];
+        for (int i = 0, n = s.length(); i < n; i++) {
+            sum += s.charAt(i);
+        }
+        return sum;
+    }
+
+    static {
+        for (int i = 0; i < N; i++) {
+            array[i] = rnd.nextString(64);
+        }
+    }
+}
