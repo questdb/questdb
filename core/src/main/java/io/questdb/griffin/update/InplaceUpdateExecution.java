b9dbb6c5556dfa7fb98bb773d78b9fc1515d3ed0
Alex Pelagenko <2159629+ideoma@users.noreply.github.com>
2021-12-17 13:12:14 +0000

    fix java17 build

--- core/src/main/java/io/questdb/griffin/update/InplaceUpdateExecution.java
+++ core/src/main/java/io/questdb/griffin/update/InplaceUpdateExecution.java
@@ -26,6 +26,7 @@ package io.questdb.griffin.update;
 
 import io.questdb.cairo.*;
 import io.questdb.cairo.sql.*;
+import io.questdb.cairo.sql.Record;
 import io.questdb.cairo.vm.Vm;
 import io.questdb.cairo.vm.api.MemoryCMARW;
 import io.questdb.griffin.SqlException;
