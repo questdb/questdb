7a4f97f6806bdfc488613941fe0caf5f03c2778b
Maciej Bodek <maciej.bodek@gmail.com>
2021-12-29 15:01:55 +0100

    feat(ui): table autocomplete in sql editor

---
+++ ui/src/scenes/Editor/Monaco/questdb-sql/operators.ts
@@ -0,0 +1,36 @@
+export const operators = [
+  // Logical
+  "ALL",
+  "AND",
+  "ANY",
+  "BETWEEN",
+  "EXISTS",
+  "IN",
+  "LIKE",
+  "NOT",
+  "OR",
+  "SOME",
+  // Set
+  "EXCEPT",
+  "INTERSECT",
+  "UNION",
+  // Join
+  "APPLY",
+  "CROSS",
+  "FULL",
+  "INNER",
+  "JOIN",
+  "LEFT",
+  "OUTER",
+  "RIGHT",
+  // Predicates
+  "CONTAINS",
+  "FREETEXT",
+  "IS",
+  "NULL",
+  // Pivoting
+  "PIVOT",
+  "UNPIVOT",
+  // Merging
+  "MATCHED",
+]
