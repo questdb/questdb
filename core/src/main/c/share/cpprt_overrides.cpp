48af0213d2d71cef707a7e11480575354c45b81e
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2022-01-05 14:38:34 +0200

    fix(cpp): use "Zone" allocated stack. use no-trowing error handler.

---
+++ core/src/main/c/share/cpprt_overrides.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+#include "cpprt_overrides.h"
+#include <stdlib.h>
+
+extern "C" void __cxa_pure_virtual() {
+    abort();
+}
+
+//void* operator new(size_t size) { return malloc(size); }
+//void  operator delete(void * p) noexcept { free(p); }
+//
+//void* operator new(size_t, void* p) noexcept { return p; }
+//void  operator delete(void*, void*) noexcept {}
