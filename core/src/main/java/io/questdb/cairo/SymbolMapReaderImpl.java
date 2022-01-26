a3125279d5ceedd6d5259f2a421787daec6f4055
Vlad Ilyushchenko <bluestreak@gmail.com>
2022-01-26 22:12:57 +0000

    refactor(core): removed unused code

--- core/src/main/java/io/questdb/cairo/SymbolMapReaderImpl.java
+++ core/src/main/java/io/questdb/cairo/SymbolMapReaderImpl.java
@@ -96,17 +96,6 @@ public class SymbolMapReaderImpl implements Closeable, SymbolMapReader {
         }
     }
 
-    @Override
-    public long symbolCharsAddressOf(int symbolIndex) {
-        if (symbolIndex < symbolCount) {
-            long offset = offsetMem.getLong(SymbolMapWriter.keyToOffset(symbolIndex));
-            return charMem.addressOf(offset);
-        } else if (symbolIndex == symbolCount) {
-            return charMem.addressOf(charMem.getGrownLength());
-        }
-        return -1;
-    }
-
     public void of(CairoConfiguration configuration, Path path, CharSequence columnName, int symbolCount) {
         FilesFacade ff = configuration.getFilesFacade();
         this.symbolCount = symbolCount;
