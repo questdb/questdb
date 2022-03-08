75b9eb0b90a5de3c0846de6b22c3fc92495b2675
Vlad Ilyushchenko <bluestreak@gmail.com>
2022-03-08 10:01:09 +0000

    fix(ilp): zero terminated file path in error message

--- core/src/main/java/io/questdb/cairo/mig/Mig620.java
+++ core/src/main/java/io/questdb/cairo/mig/Mig620.java
@@ -189,11 +189,11 @@ public class Mig620 {
         long txSize = txMemory.size() - 4 * 8;
         for (int partitionIndex = 0; partitionIndex < partitionCount; partitionIndex++) {
             if (offset > txSize) {
-                throw CairoException.instance(0).put("corrupt _txn file ").put(path.trimTo(pathLen)).put(", file is too small to read offset ").put(offset);
+                throw CairoException.instance(0).put("corrupt _txn file ").put(path.trimTo(pathLen).$()).put(", file is too small to read offset ").put(offset);
             }
             long partitionTs = txMemory.getLong(offset);
             if (partitionTs <= prevPartition) {
-                throw CairoException.instance(0).put("corrupt _txn file, partitions are not ordered at ").put(path.trimTo(pathLen));
+                throw CairoException.instance(0).put("corrupt _txn file, partitions are not ordered at ").put(path.trimTo(pathLen).$());
             }
             long partitionNameTxn = txMemory.getLong(offset + PARTITION_NAME_TX_OFFSET_MIG * 8);
             readColumnTopsForPartition(result, columnNames, columnCount, partitionBy, partitionTs, partitionNameTxn, ff, path, pathLen);
