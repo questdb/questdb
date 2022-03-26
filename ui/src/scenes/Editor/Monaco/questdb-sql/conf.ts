3657bdcfd534041c430321d36d74eee02a368eca
Maciej Bodek <maciej.bodek@gmail.com>
2022-03-26 22:41:55 +0100

    fix(ui): adjust regex to allow brackets in editor tokens (#1985)

--- ui/src/scenes/Editor/Monaco/questdb-sql/conf.ts
+++ ui/src/scenes/Editor/Monaco/questdb-sql/conf.ts
@@ -4,8 +4,9 @@ export const conf: monaco.languages.LanguageConfiguration = {
   /**
    * Override the default word definition regex to also allow single quotes and dots.
    * This way we can highlight table names escaped with quotes and the ones created from CSV files.
+   * An additional example is a "bad integer" error, i.e. (20000) - needs brackets to be allowed as well.
    */
-  wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\$\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\"\,\<\>\/\?\s]+)/g,
+  wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\$\%\^\&\*\-\=\+\[\{\]\}\\\|\;\:\"\,\<\>\/\?\s]+)/g,
   comments: {
     lineComment: "--",
     blockComment: ["/*", "*/"],
