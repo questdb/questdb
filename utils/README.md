84fae1ad54922c7085d3a479b0e8eb7858895fc5
Alex Pelagenko <2159629+ideoma@users.noreply.github.com>
2022-03-28 14:37:29 +0100

    feat(util): index rebuild, string col index recover utils (#1978)

--- utils/README.md
+++ utils/README.md
@@ -5,7 +5,7 @@
 Serializes binary `_txn` file to / from readable JSON format. Primary usage to
 investigate storage issues
 
-Usage
+#### Usage
 
 ```
 io.questdb.cliutil.TxSerializer -d <json_path> | -s <json_path> <txn_path>
@@ -13,3 +13,73 @@ io.questdb.cliutil.TxSerializer -d <json_path> | -s <json_path> <txn_path>
 
 - `-d` option prints contents of `_txn` file to std output in JSON format
 - `-s` option transforms existing JSON file into binary \_txn format
+
+#### Examples
+
+```bash
+java -cp utils.jar io.questdb.cliutil.TxSerializer -d /questdb-root/db/trades-COINBASE/_txn > /questdb-root/db/trades-COINBASE/txn.json
+
+java -cp utils.jar io.questdb.cliutil.TxSerializer -s /questdb-root/db/trades-COINBASE/txn.json /questdb-root/db/trades-COINBASE/_txnCopy
+```
+
+### Rebuild index
+
+Rebuilds indexes for a table
+
+#### Usage
+
+```
+io.questdb.cliutil.RebuildIndex <table_path> [-p <partition_name>] [-c <column_name>]
+```
+
+- `<table_path>` full path to the table
+- `-c` column name, optional. If omitted, all indexed columns will have indexes
+  rebuilt
+- `-p` optional partition name. If omitted, all partitions will be affected
+
+#### Examples
+
+```bash
+java -cp utils.jar io.questdb.cliutil.RebuildIndex /questdb-root/db/trades-COINBASE
+
+java -cp utils.jar io.questdb.cliutil.RebuildIndex /questdb-root/db/trades-COINBASE -c symbol
+
+java -cp utils.jar io.questdb.cliutil.RebuildIndex /questdb-root/db/trades-COINBASE -p 2022-03-21
+
+java -cp utils.jar io.questdb.cliutil.RebuildIndex /questdb-root/db/trades-COINBASE -p 2022-03-21 -c symbol
+```
+
+### Rebuild String column index `.i` file
+
+Rebuilds indexes for a table
+
+#### Usage
+
+```
+io.questdb.cliutil.RecoverVarIndex <table_path> [-p <partition_name>] [-c <column_name>]
+```
+
+- `<table_path>` full path to the table
+- `-c` column name, optional. If omitted, all string columns will have `.i` file
+  rebuild
+- `-p` optional partition name. If omitted, all partitions will be affected
+
+#### Examples
+
+```bash
+java -cp utils.jar io.questdb.cliutil.RecoverVarIndex /questdb-root/db/trades-COINBASE
+
+java -cp utils.jar io.questdb.cliutil.RecoverVarIndex /questdb-root/db/trades-COINBASE -c stringColumn
+
+java -cp utils.jar io.questdb.cliutil.RecoverVarIndex /questdb-root/db/trades-COINBASE -p 2022-03-21
+
+java -cp utils.jar io.questdb.cliutil.RecoverVarIndex /questdb-root/db/trades-COINBASE -p 2022-03-21 -c stringColumn
+```
+
+## Build Utils project
+
+To build single jar with dependencies run
+
+```bash
+mvn clean package
+```
---
+++ utils/src/test/java/io/questdb/cliutil/RebuildIndexTest.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.cliutil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.questdb.cliutil.RebuildColumnCommandArgs.parseCommandArgs;
+
+public class RebuildIndexTest {
+    @Test
+    public void testColumnAndPartition() {
+        RebuildColumnCommandArgs params = parseCommandArgs(
+                new String[]{"tablePath", "-c", "abc", "-p", "2020"},
+                RebuildIndex.class.getName()
+        );
+        Assert.assertNotNull(params);
+        Assert.assertEquals("tablePath", params.tablePath);
+        Assert.assertEquals("abc", params.column);
+        Assert.assertEquals("2020", params.partition);
+
+        params = parseCommandArgs(
+                new String[]{"tablePath", "-p", "2020", "-c", "abc"},
+                RebuildIndex.class.getName()
+        );
+        Assert.assertNotNull(params);
+        Assert.assertEquals("tablePath", params.tablePath);
+        Assert.assertEquals("abc", params.column);
+        Assert.assertEquals("2020", params.partition);
+    }
+
+    @Test
+    public void testColumnOnly() {
+        RebuildColumnCommandArgs params = parseCommandArgs(
+                new String[]{"tablePath", "-c", "9393"},
+                RebuildIndex.class.getName()
+        );
+        Assert.assertNotNull(params);
+        Assert.assertEquals("tablePath", params.tablePath);
+        Assert.assertEquals("9393", params.column);
+    }
+
+    @Test
+    public void testNoArgsFails() {
+        Assert.assertNull(parseCommandArgs(
+                new String[]{}, ""
+        ));
+    }
+
+    @Test
+    public void testNoTableFails() {
+        Assert.assertNull(parseCommandArgs(
+                new String[]{"-p", "2222"}, ""
+        ));
+    }
+
+    @Test
+    public void testPartitionOnly() {
+        RebuildColumnCommandArgs params = parseCommandArgs(
+                new String[]{"tablePath", "-p", "9393"}, ""
+        );
+        Assert.assertNotNull(params);
+        Assert.assertEquals("tablePath", params.tablePath);
+        Assert.assertEquals("9393", params.partition);
+    }
+
+    @Test
+    public void testTableNameOnly() {
+        RebuildColumnCommandArgs params = parseCommandArgs(
+                new String[]{"tablePath"}, ""
+        );
+        Assert.assertNotNull(params);
+        Assert.assertEquals("tablePath", params.tablePath);
+    }
+
+    @Test
+    public void testTooColumnManyArgsFails() {
+        Assert.assertNull(parseCommandArgs(
+                new String[]{"tablePath", "-c", "2222", "-c", "2223"}, ""
+        ));
+        Assert.assertNull(parseCommandArgs(
+                new String[]{"tablePath", "-p", "2222", "-c", "dafda", "-c", "asb"}, ""
+        ));
+    }
+
+    @Test
+    public void testTooManyPartitionArgsFails() {
+        Assert.assertNull(parseCommandArgs(
+                new String[]{"tablePath", "-p", "2222", "-p", "2223"}, ""
+        ));
+    }
+}
