224163e047c234979aeb46276e06201caba7c1da
glasstiger <imre@questdb.io>
2022-05-12 18:21:00 +0100

    chore(ilp): fix flapping test

--- core/src/test/java/io/questdb/cutlass/line/tcp/LineTcpReceiverTest.java
+++ core/src/test/java/io/questdb/cutlass/line/tcp/LineTcpReceiverTest.java
@@ -981,21 +981,17 @@ public class LineTcpReceiverTest extends AbstractLineTcpReceiverTest {
                 }
             }).start();
 
-            // this will wait until the writer is returned into the pool or times out after 20 seconds
-            finished.await(20_000_000_000L);
+            // this will wait until the writer is returned into the pool
+            finished.await();
             engine.setPoolListener((factoryType, thread, name, event, segment, position) -> {
             });
 
             try (TableReader reader = engine.getReader(AllowAllCairoSecurityContext.INSTANCE, tableName)) {
                 final int expectedNumOfRows = numOfRows / 2;
                 // usually the data will be in the table by the time we get here but
-                // if it is not we will wait for it in a loop for a maximum of 20 seconds
-                for (int i = 0; i < 200; i++) {
-                    if (reader.getTransientRowCount() < expectedNumOfRows) {
-                        Os.sleep(100);
-                    } else {
-                        break;
-                    }
+                // if it is not we will wait for it in a loop
+                while (reader.getTransientRowCount() < expectedNumOfRows) {
+                    Os.sleep(100);
                 }
                 Assert.assertEquals(expectedNumOfRows, reader.getTransientRowCount());
             } catch (Exception e) {
