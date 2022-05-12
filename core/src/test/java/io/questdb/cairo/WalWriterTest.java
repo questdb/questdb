5e328f01a684f8e909564279a3c60531b5ea6235
Jaromir Hamala <jaromir.hamala@gmail.com>
2022-05-12 14:56:25 +0200

    bootstapping test

---
+++ core/src/test/java/io/questdb/cairo/WalWriterTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.cairo;
+
+import io.questdb.cairo.vm.Vm;
+import io.questdb.griffin.AbstractGriffinTest;
+import io.questdb.std.str.Path;
+import org.junit.Test;
+
+public class WalWriterTest extends AbstractGriffinTest {
+
+    @Test
+    public void bootstrapWal() {
+        String tableName = "testtable";
+        try (Path path = new Path().of(configuration.getRoot());
+             TableModel model = new TableModel(configuration, tableName, PartitionBy.NONE).col("a", ColumnType.INT)
+        ) {
+
+            TableUtils.createTable(configuration, Vm.getMARWInstance(), path, model, 0);
+            try (WalWriter walWriter = new WalWriter(configuration, tableName, metrics)) {
+                WalWriter.Row row = walWriter.newRow();
+                row.putLong(0, 1);
+                row.append();
+            }
+        }
+    }
+
+}
