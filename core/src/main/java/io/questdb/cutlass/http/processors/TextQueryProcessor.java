6cea9b0b26b345df5a7ff3102dfe67d52c2ecd29
Matthew <2575976183@qq.com>
2022-05-23 20:46:02 +0800

    fix(http): make /exp endpoint only allow SELECT (#2145)

--- core/src/main/java/io/questdb/cutlass/http/processors/TextQueryProcessor.java
+++ core/src/main/java/io/questdb/cutlass/http/processors/TextQueryProcessor.java
@@ -102,11 +102,25 @@ public class TextQueryProcessor implements HttpRequestProcessor, Closeable {
         Misc.free(circuitBreaker);
     }
 
+    private static boolean isExpUrl(CharSequence tok) {
+        if (tok.length() != 4) {
+            return false;
+        }
+
+        int i = 0;
+        return (tok.charAt(i++) | 32) == '/'
+                && (tok.charAt(i++) | 32) == 'e'
+                && (tok.charAt(i++) | 32) == 'x'
+                && (tok.charAt(i)) == 'p';
+    }
+
     public void execute(
             HttpConnectionContext context,
             TextQueryProcessorState state
     ) throws PeerDisconnectedException, PeerIsSlowToReadException {
         try {
+            boolean isExpRequest = isExpUrl(context.getRequestHeader().getUrl());
+
             state.recordCursorFactory = QueryCache.getInstance().poll(state.query);
             state.setQueryCacheable(true);
             sqlExecutionContext.with(
@@ -120,6 +134,8 @@ public class TextQueryProcessor implements HttpRequestProcessor, Closeable {
                 final CompiledQuery cc = compiler.compile(state.query, sqlExecutionContext);
                 if (cc.getType() == CompiledQuery.SELECT) {
                     state.recordCursorFactory = cc.getRecordCursorFactory();
+                } else if (isExpRequest) {
+                    throw SqlException.$(0, "/exp endpoint only accepts SELECT");
                 }
                 info(state).$("execute-new [q=`").utf8(state.query).
                         $("`, skip: ").$(state.skip).
@@ -145,6 +161,10 @@ public class TextQueryProcessor implements HttpRequestProcessor, Closeable {
                             info(state).$(e.getFlyweightMessage()).$();
                             state.recordCursorFactory = Misc.free(state.recordCursorFactory);
                             final CompiledQuery cc = compiler.compile(state.query, sqlExecutionContext);
+                            if (cc.getType() != CompiledQuery.SELECT && isExpRequest) {
+                                throw SqlException.$(0, "/exp endpoint only accepts SELECT");
+                            }
+
                             state.recordCursorFactory = cc.getRecordCursorFactory();
                         }
                     } while (runQuery);
