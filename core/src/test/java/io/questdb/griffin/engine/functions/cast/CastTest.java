635fc84e05ae178a7cc61cb7efbf1630cfe9280e
KevinChen16 <55957648+KevinChen16@users.noreply.github.com>
2022-05-29 02:38:33 +0800

    fix(sql): fix the comparison refer to 'infinity' (#2160)

--- core/src/test/java/io/questdb/griffin/engine/functions/cast/CastTest.java
+++ core/src/test/java/io/questdb/griffin/engine/functions/cast/CastTest.java
@@ -80,6 +80,53 @@ public class CastTest extends AbstractGriffinTest {
     }
 
     @Test
+    public void testInfinity() throws Exception {
+        assertQuery(
+                "a\n",
+                "select a from tab",
+                "create table tab (a boolean)",
+                null,
+                "insert into tab select cast('Infinity' as double) = cast('Infinity' as double)," +
+                        "cast('Infinity' as float) = cast('Infinity' as double)," +
+                        "cast('Infinity' as float) = cast('Infinity' as float)," +
+                        "cast('-Infinity' as double) = cast('-Infinity' as double)," +
+                        "cast('-Infinity' as double) = cast('-Infinity' as float)," +
+                        "cast('-Infinity' as float) = cast('-Infinity' as float)," +
+                        "cast('Infinity' as double) != cast('-Infinity' as double)," +
+                        "cast('Infinity' as float) > 1 " +
+                        "from long_sequence(8)",
+                "a\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n" +
+                        "true\n",
+                true,
+                true,
+                true
+        );
+    }
+
+    @Test
+    public void testInfinityNonConstant() throws Exception {
+        assertQuery(
+                "column\n",
+                "select a = b from tab ",
+                "create table tab (a double, b float)",
+                null,
+                "insert into tab values (cast('Infinity' as double), cast('Infinity' as float))",
+                "column\n" +
+                        "true\n",
+                true,
+                true,
+                true
+        );
+    }
+
+    @Test
     public void testBooleanToByte() throws Exception {
         assertQuery(
                 "a\n",
