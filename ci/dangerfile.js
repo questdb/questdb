ad3eb4effd3d53a55c9b2a907c579e4ed53361e9
Nikki Ng <ng.niccc@gmail.com>
2022-05-30 14:25:40 +0800

    chore: try restricting from the workflow instead

--- ci/dangerfile.js
+++ ci/dangerfile.js
@@ -1,15 +1,5 @@
 import { danger, fail } from "danger"
 
-const headRepoName = danger.github.pr.head.repo.full_name
-const baseRepoName = danger.github.pr.base.repo.full_name
-// Ignore forks for now until we find a good solution
-if (headRepoName != baseRepoName) {
-  // This is shown inline in the output and also integrates with the GitHub
-  // Action reporting UI and produces a warning
-  console.log("##[warning]Running from a forked repo. Danger won't be able to post comments on the main repo unless GitHub Actions are enabled on the fork, too.\033[0m")
-  return
-}
-
 const supportedTypes = [
   "feat",
   "fix",
