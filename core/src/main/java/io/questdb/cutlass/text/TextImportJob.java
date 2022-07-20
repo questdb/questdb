4b4eeab6f083121737af048a789ea0b3e3e79d09
Andrey Pechkurov <apechkurov@gmail.com>
2022-07-20 15:16:02 +0300

    Introduce separate memory tag for native memory

--- core/src/main/java/io/questdb/cutlass/text/TextImportJob.java
+++ core/src/main/java/io/questdb/cutlass/text/TextImportJob.java
@@ -52,7 +52,7 @@ public class TextImportJob extends AbstractQueueConsumerJob<TextImportTask> impl
         super(messageBus.getTextImportQueue(), messageBus.getTextImportSubSeq());
         this.textLexer = new TextLexer(messageBus.getConfiguration().getTextConfiguration());
         this.fileBufSize = messageBus.getConfiguration().getSqlCopyBufferSize();
-        this.fileBufAddr = Unsafe.malloc(fileBufSize, MemoryTag.MMAP_PARALLEL_IMPORT);
+        this.fileBufAddr = Unsafe.malloc(fileBufSize, MemoryTag.NATIVE_PARALLEL_IMPORT);
         this.indexer = new CsvFileIndexer(messageBus.getConfiguration());
         this.utf8Sink = new DirectCharSink(messageBus.getConfiguration().getTextConfiguration().getUtf8SinkSize());
         this.mergeIndexes = new DirectLongList(INDEX_MERGE_LIST_CAPACITY, MemoryTag.NATIVE_DEFAULT);
@@ -73,7 +73,7 @@ public class TextImportJob extends AbstractQueueConsumerJob<TextImportTask> impl
         this.textLexer = Misc.free(textLexer);
         this.indexer = Misc.free(indexer);
         if (fileBufSize > 0) {
-            Unsafe.free(fileBufAddr, fileBufSize, MemoryTag.MMAP_PARALLEL_IMPORT);
+            Unsafe.free(fileBufAddr, fileBufSize, MemoryTag.NATIVE_PARALLEL_IMPORT);
             fileBufSize = 0;
         }
         this.mergeIndexes = Misc.free(this.mergeIndexes);
