8262602b34fce7dfb53127c35e12b15ab3df3f9d
Jaromir Hamala <jaromir.hamala@gmail.com>
2022-07-22 14:46:42 +0200

    test with a double alias added

--- core/src/test/java/io/questdb/griffin/OrderByAdviceTest.java
+++ core/src/test/java/io/questdb/griffin/OrderByAdviceTest.java
@@ -1293,4 +1293,27 @@ public class OrderByAdviceTest extends AbstractGriffinTest {
                 null,
                 true, true, true);
     }
+
+    @Test
+    public void testOrderByPriority_columnDoubleAliased() throws Exception {
+        assertQuery("a1\ta2\n" +
+                        "1\t1\n" +
+                        "2\t2\n" +
+                        "3\t3\n" +
+                        "4\t4\n" +
+                        "5\t5\n" +
+                        "6\t6\n" +
+                        "7\t7\n" +
+                        "8\t8\n" +
+                        "9\t9",
+                "select a as a1, a as a2 from x order by a1 asc, a2 desc;",
+                "create table x as (" +
+                        "select" +
+                        " x a," +
+                        " x % 3 b" +
+                        " from long_sequence(9)" +
+                        ")",
+                null,
+                true, true, true);
+    }
 }
