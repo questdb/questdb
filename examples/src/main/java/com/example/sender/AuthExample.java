85df4ff2bce109b9d6c3be0e682e141343b31a45
Jaromir Hamala <jaromir.hamala@gmail.com>
2022-08-24 16:26:48 +0200

    docs(ilp): add an example with auth, but without TLS (#2455)

---
+++ examples/src/main/java/com/example/sender/AuthExample.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package com.example.sender;
+
+import io.questdb.client.Sender;
+
+public class AuthExample {
+    public static void main(String[] args) {
+        // Replace:
+        // 1. "localhost:9000" with a host and port of your QuestDB server
+        // 2. "testUser1" with KID portion from your JSON Web Key
+        // 3. token with the D portion of your JSON Web Key
+        try (Sender sender = Sender.builder()
+                .address("localhost:9009")
+                .enableAuth("testUser1").authToken("GwBXoGG5c6NoUTLXnzMxw_uNiVa8PKobzx5EiuylMW0")
+                .build()) {
+            sender.table("inventors")
+                    .symbol("born", "Austrian Empire")
+                    .longColumn("id", 0)
+                    .stringColumn("name", "Nicola Tesla")
+                    .atNow();
+            sender.table("inventors")
+                    .symbol("born", "USA")
+                    .longColumn("id", 1)
+                    .stringColumn("name", "Thomas Alva Edison")
+                    .atNow();
+        }
+    }
+}
