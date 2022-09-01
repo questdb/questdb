f8022d255ec815f98350b81416b5b59d7bb7bf05
Andrey Pechkurov <apechkurov@gmail.com>
2022-09-01 17:04:46 +0300

    Address review comments

--- core/src/main/java/io/questdb/griffin/engine/functions/regex/RegexpReplaceStrFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/regex/RegexpReplaceStrFunctionFactory.java
@@ -170,7 +170,7 @@ public class RegexpReplaceStrFunctionFactory implements FunctionFactory {
 
     // TODO(puzpuzpuz):
     //  Get rid of this class in favor of StringSink once we drop support for Java 8 where j.u.r.Matcher
-    //  has method overloads for StringBuilder.
+    //  has no method overloads for StringBuilder.
     private static class StringBufferSink implements CharSequence {
         private final StringBuffer buffer = new StringBuffer();
 
