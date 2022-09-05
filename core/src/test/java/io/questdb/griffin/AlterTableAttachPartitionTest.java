91d338aeffc7efe59bd712743d329295b498f8d9
Andrey Pechkurov <apechkurov@gmail.com>
2022-09-05 12:44:45 +0300

    Enable failing test

--- core/src/test/java/io/questdb/griffin/AlterTableAttachPartitionTest.java
+++ core/src/test/java/io/questdb/griffin/AlterTableAttachPartitionTest.java
@@ -33,7 +33,10 @@ import io.questdb.std.str.LPSZ;
 import io.questdb.std.str.Path;
 import io.questdb.std.str.StringSink;
 import io.questdb.test.tools.TestUtils;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -743,7 +746,6 @@ public class AlterTableAttachPartitionTest extends AbstractGriffinTest {
         });
     }
 
-    @Ignore
     @Test
     public void testAttachPartitionsWithSymbolsValueDoesNotMatch() throws Exception {
         assertMemoryLeak(() -> {
