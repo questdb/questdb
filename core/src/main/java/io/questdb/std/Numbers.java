376d72f0c81028b70185ed2266cae2d9ffbda335
Vlad Ilyushchenko <bluestreak@gmail.com>
2022-09-28 07:29:00 +0100

    fix: fix jdk8 build

--- core/src/main/java/io/questdb/std/Numbers.java
+++ core/src/main/java/io/questdb/std/Numbers.java
@@ -27,7 +27,11 @@ package io.questdb.std;
 import io.questdb.std.fastdouble.FastDoubleParser;
 import io.questdb.std.fastdouble.FastFloatParser;
 import io.questdb.std.str.CharSink;
+//#if jdk.version==8
+//$import sun.misc.FDBigInteger;
+//#else
 import jdk.internal.math.FDBigInteger;
+//#endif
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
