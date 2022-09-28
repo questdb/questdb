204428623556a6cf2cb0e8d9229e5ea52791d0b2
Vlad Ilyushchenko <bluestreak@gmail.com>
2022-09-28 15:49:49 +0100

    chore: JDK8 build fix

--- core/src/test/java/io/questdb/AbstractBootstrapTest.java
+++ core/src/test/java/io/questdb/AbstractBootstrapTest.java
@@ -101,7 +101,7 @@ public abstract class AbstractBootstrapTest {
         final String confPath = root.toString() + Files.SEPARATOR + "conf";
         TestUtils.createTestPath(confPath);
         String file = confPath + Files.SEPARATOR + "server.conf";
-        try (PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8)) {
+        try (PrintWriter writer = new PrintWriter(file, "UTF8")) {
 
             // enable services
             writer.println("http.enabled=true");
@@ -138,14 +138,14 @@ public abstract class AbstractBootstrapTest {
 
         // mime types
         file = confPath + Files.SEPARATOR + "mime.types";
-        try (PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8)) {
+        try (PrintWriter writer = new PrintWriter(file, "UTF8")) {
             writer.println("");
         }
 
         // logs
         file = confPath + Files.SEPARATOR + "log.conf";
         System.setProperty("out", file);
-        try (PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8)) {
+        try (PrintWriter writer = new PrintWriter(file, "UTF8")) {
             writer.println("writers=stdout");
             writer.println("w.stdout.class=io.questdb.log.LogConsoleWriter");
             writer.println("w.stdout.level=INFO");
