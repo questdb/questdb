01b3bac0d74588062d36b79994de06a63dd2b7de
Andrey Pechkurov <apechkurov@gmail.com>
2022-10-04 22:20:34 +0300

    Call madvise in WAL writer

--- core/src/main/java/io/questdb/cairo/WalWriter.java
+++ core/src/main/java/io/questdb/cairo/WalWriter.java
@@ -511,7 +511,8 @@ public class WalWriter implements Closeable {
                     configuration.getDataAppendPageSize(),
                     -1,
                     MemoryTag.MMAP_TABLE_WRITER,
-                    configuration.getWriterFileOpenOpts()
+                    configuration.getWriterFileOpenOpts(),
+                    Files.POSIX_MADV_SEQUENTIAL
             );
 
             final MemoryMA mem2 = getSecondaryColumn(columnIndex);
@@ -521,7 +522,8 @@ public class WalWriter implements Closeable {
                         configuration.getDataAppendPageSize(),
                         -1,
                         MemoryTag.MMAP_TABLE_WRITER,
-                        configuration.getWriterFileOpenOpts()
+                        configuration.getWriterFileOpenOpts(),
+                        Files.POSIX_MADV_SEQUENTIAL
                 );
                 mem2.putLong(0L);
             }
