2c69bfb375a1f2ece46d1183446297c5f1e65eea
blackstar-baba <535650957@qq.com>
2022-10-11 21:50:47 +0800

    change test cases

--- core/src/test/java/io/questdb/griffin/SqlCodeGeneratorTest.java
+++ core/src/test/java/io/questdb/griffin/SqlCodeGeneratorTest.java
@@ -60,16 +60,16 @@ public class SqlCodeGeneratorTest extends AbstractGriffinTest {
     @Test
     public void testAnalyticFunctionWithPartitionAndOrderByNonSymbol() throws Exception {
         assertQuery("row_number\tprice\tts\n" +
-                        "0\t42\t1970-01-01T00:00:00.000000Z\n" +
-                        "1\t42\t1970-01-02T03:46:40.000000Z\n" +
-                        "2\t42\t1970-01-03T07:33:20.000000Z\n" +
-                        "3\t42\t1970-01-04T11:20:00.000000Z\n" +
-                        "4\t42\t1970-01-05T15:06:40.000000Z\n" +
-                        "5\t42\t1970-01-06T18:53:20.000000Z\n" +
-                        "6\t42\t1970-01-07T22:40:00.000000Z\n" +
-                        "7\t42\t1970-01-09T02:26:40.000000Z\n" +
-                        "8\t42\t1970-01-10T06:13:20.000000Z\n" +
-                        "9\t42\t1970-01-11T10:00:00.000000Z\n",
+                        "1\t42\t1970-01-01T00:00:00.000000Z\n" +
+                        "2\t42\t1970-01-02T03:46:40.000000Z\n" +
+                        "3\t42\t1970-01-03T07:33:20.000000Z\n" +
+                        "4\t42\t1970-01-04T11:20:00.000000Z\n" +
+                        "5\t42\t1970-01-05T15:06:40.000000Z\n" +
+                        "6\t42\t1970-01-06T18:53:20.000000Z\n" +
+                        "7\t42\t1970-01-07T22:40:00.000000Z\n" +
+                        "8\t42\t1970-01-09T02:26:40.000000Z\n" +
+                        "9\t42\t1970-01-10T06:13:20.000000Z\n" +
+                        "10\t42\t1970-01-11T10:00:00.000000Z\n",
                 "select row_number() over (partition by price order by ts), price, ts from trades",
                 "create table trades as " +
                         "(" +
@@ -89,16 +89,16 @@ public class SqlCodeGeneratorTest extends AbstractGriffinTest {
     @Test
     public void testAnalyticFunctionWithPartitionAndOrderBySymbolNoWildcard() throws Exception {
         assertQuery("row_number\n" +
+                        "3\n" +
+                        "6\n" +
                         "2\n" +
+                        "1\n" +
                         "5\n" +
                         "1\n" +
-                        "0\n" +
                         "4\n" +
-                        "0\n" +
                         "3\n" +
                         "2\n" +
-                        "1\n" +
-                        "0\n",
+                        "1\n",
                 "select row_number() over (partition by symbol order by symbol) from trades",
                 "create table trades as " +
                         "(" +
@@ -118,16 +118,16 @@ public class SqlCodeGeneratorTest extends AbstractGriffinTest {
     @Test
     public void testAnalyticFunctionWithPartitionAndOrderBySymbolWildcardFirst() throws Exception {
         assertQuery("price\tsymbol\tts\trow_number\n" +
-                        "0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\t2\n" +
-                        "0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\t5\n" +
-                        "0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\t1\n" +
-                        "0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\t0\n" +
-                        "0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\t4\n" +
-                        "0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\t0\n" +
-                        "0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\t3\n" +
-                        "0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\t2\n" +
-                        "0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\t1\n" +
-                        "0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\t0\n",
+                        "0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\t3\n" +
+                        "0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\t6\n" +
+                        "0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\t2\n" +
+                        "0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\t1\n" +
+                        "0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\t5\n" +
+                        "0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\t1\n" +
+                        "0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\t4\n" +
+                        "0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\t3\n" +
+                        "0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\t2\n" +
+                        "0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\t1\n",
                 "select *, row_number() over (partition by symbol order by symbol) from trades",
                 "create table trades as " +
                         "(" +
@@ -147,16 +147,16 @@ public class SqlCodeGeneratorTest extends AbstractGriffinTest {
     @Test
     public void testAnalyticFunctionWithPartitionAndOrderBySymbolWildcardLast() throws Exception {
         assertQuery("row_number\tprice\tsymbol\tts\n" +
-                        "2\t0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\n" +
-                        "5\t0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\n" +
-                        "1\t0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\n" +
-                        "0\t0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\n" +
-                        "4\t0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\n" +
-                        "0\t0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\n" +
-                        "3\t0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\n" +
-                        "2\t0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\n" +
-                        "1\t0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\n" +
-                        "0\t0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\n",
+                        "3\t0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\n" +
+                        "6\t0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\n" +
+                        "2\t0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\n" +
+                        "5\t0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\n" +
+                        "4\t0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "3\t0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\n" +
+                        "2\t0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\n",
                 "select row_number() over (partition by symbol order by symbol), * from trades",
                 "create table trades as " +
                         "(" +
