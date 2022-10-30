684cebb62f7cfa88bed2a8994113bbffad6652fc
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-30 18:59:07 -0400

    spelling: traversal
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- core/src/test/java/io/questdb/cutlass/line/tcp/LineTcpConnectionContextTest.java
+++ core/src/test/java/io/questdb/cutlass/line/tcp/LineTcpConnectionContextTest.java
@@ -1726,7 +1726,7 @@ public class LineTcpConnectionContextTest extends BaseLineTcpContextTest {
     public void testNewTableNullType() throws Exception {
         runInContext(() -> {
             recvBuffer =
-                    "vbw water_speed_longitudinal=0.07,water_speed_transveral=,water_speed_status=\"A\",ground_speed_longitudinal=0,ground_speed_transveral=0,ground_speed_status=\"A\",water_speed_stern_transversal=,water_speed_stern_transversal_status=\"V\",ground_speed_stern_transversal=0,ground_speed_stern_transversal_status=\"V\" 1627046637414969856\n";
+                    "vbw water_speed_longitudinal=0.07,water_speed_traversal=,water_speed_status=\"A\",ground_speed_longitudinal=0,ground_speed_traversal=0,ground_speed_status=\"A\",water_speed_stern_traversal=,water_speed_stern_traversal_status=\"V\",ground_speed_stern_traversal=0,ground_speed_stern_traversal_status=\"V\" 1627046637414969856\n";
             do {
                 handleContextIO();
                 Assert.assertFalse(disconnected);
