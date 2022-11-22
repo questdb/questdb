625dd98d9d685e267ffd3a24c2b99c3ac2cfd04e
ideoma <2159629+ideoma@users.noreply.github.com>
2022-11-22 18:53:00 +0000

    fix test assertion

--- core/src/test/java/io/questdb/griffin/O3PartitionPurgeTest.java
+++ core/src/test/java/io/questdb/griffin/O3PartitionPurgeTest.java
@@ -406,7 +406,9 @@ public class O3PartitionPurgeTest extends AbstractGriffinTest {
                         rdr2.openPartition(0);
                     }
                     runPartitionPurgeJobs();
-                    Assert.assertFalse(Chars.toString(path), Files.exists(path));
+
+                    path.of(engine.getConfiguration().getRoot()).concat("tbl").concat("1970-01-10T00").concat("x.d").$();
+                    Assert.assertTrue(Chars.toString(path), Files.exists(path));
 
                     // This should not fail
                     rdr.openPartition(0);
