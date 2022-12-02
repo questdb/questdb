38448d7a651fd69f3a7edf0ce3d6c47b8df04a74
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2022-12-02 14:13:19 +0200

    fix failed test

--- core/src/test/java/io/questdb/griffin/wal/WalTableFailureTest.java
+++ core/src/test/java/io/questdb/griffin/wal/WalTableFailureTest.java
@@ -929,7 +929,7 @@ public class WalTableFailureTest extends AbstractGriffinTest {
 
             drainWalQueue();
             assertSql(tableName, "x\tsym\tts\tsym2\n1\tAB\t2022-02-24T00:00:00.000000Z\tYYY\n");
-            assertSql("wal_tables()", "name\tsuspended\twriterTxn\tsequencerTxn\n" + tableName + "\ttrue\t2\t4\n");
+            assertSql("wal_tables()", "name\tsuspended\twriterTxn\tsequencerTxn\n" + tableName + "\tfalse\t2\t4\n");
         });
     }
 
