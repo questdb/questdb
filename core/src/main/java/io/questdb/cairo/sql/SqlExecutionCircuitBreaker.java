d40d14ce4d15cc20e258fdbfac85aa5ddfe527ef
Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2022-12-14 19:55:49 +0200

    fix(http): fix unexpected query timeout errors in /exp endpoint (#2866)

--- core/src/main/java/io/questdb/cairo/sql/SqlExecutionCircuitBreaker.java
+++ core/src/main/java/io/questdb/cairo/sql/SqlExecutionCircuitBreaker.java
@@ -86,7 +86,9 @@ public interface SqlExecutionCircuitBreaker extends ExecutionCircuitBreaker {
 
     int getFd();
 
-    /* Returns true if time was reset/powered up (for current sql command) and false otherwise . */
+    /**
+     * Returns true if time was reset/powered up (for current sql command) and false otherwise.
+     */
     boolean isTimerSet();
 
     void resetTimer();
