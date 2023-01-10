530d20ec9094d54d173d0aff8849a2beba2be973
Adam Cimarosti <cimarosti@gmail.com>
2023-01-10 16:22:51 +0000

    Added a small JNI lib that reverses a string in Rust - just as a starting point.

---
+++ core/src/main/rust/questdb-r/src/lib.rs
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2022 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+use jni::JNIEnv;
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+
+#[no_mangle]
+pub extern "system" fn Java_io_questdb_rust_Hello_reverseString(
+        env: JNIEnv,
+        _class: JClass,
+        input: JString) -> jstring {
+    let input: String =
+        env.get_string(input).expect("Couldn't get java string!").into();
+    let reversed: String = input.chars().rev().collect();
+
+    let output = env.new_string(reversed)
+        .expect("Couldn't create java string!");
+
+    output.into_raw()
+}
