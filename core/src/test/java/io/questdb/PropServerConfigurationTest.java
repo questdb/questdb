aacfa461888154dca12d42eca21bf83c858efa37
ideoma <2159629+ideoma@users.noreply.github.com>
2023-01-26 17:03:38 +0000

    fix prop tests with new defaults

--- core/src/test/java/io/questdb/PropServerConfigurationTest.java
+++ core/src/test/java/io/questdb/PropServerConfigurationTest.java
@@ -380,7 +380,7 @@ public class PropServerConfigurationTest {
         Assert.assertTrue(configuration.getWalApplyPoolConfiguration().isEnabled());
         Assert.assertFalse(configuration.getWalApplyPoolConfiguration().haltOnError());
         Assert.assertEquals("wal-apply", configuration.getWalApplyPoolConfiguration().getPoolName());
-        Assert.assertEquals(0, configuration.getWalApplyPoolConfiguration().getWorkerCount());
+        Assert.assertTrue(configuration.getWalApplyPoolConfiguration().getWorkerCount() > 0);
         Assert.assertEquals(10, configuration.getWalApplyPoolConfiguration().getSleepTimeout());
         Assert.assertEquals(10_000, configuration.getWalApplyPoolConfiguration().getSleepThreshold());
         Assert.assertEquals(10, configuration.getWalApplyPoolConfiguration().getYieldThreshold());
