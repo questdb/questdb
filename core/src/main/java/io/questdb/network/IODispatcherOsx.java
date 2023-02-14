91eabfd4056922af982582b91aad0476eba37bc2
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2023-02-14 13:06:07 +0200

    remove unnecessary check

--- core/src/main/java/io/questdb/network/IODispatcherOsx.java
+++ core/src/main/java/io/questdb/network/IODispatcherOsx.java
@@ -89,22 +89,17 @@ public class IODispatcherOsx<C extends IOContext> extends AbstractIODispatcher<C
             kqueue.setWriteOffset(offset);
 
             final int fd = (int) pending.get(i, OPM_FD);
-            long disable = pending.get(i, OPM_DISABLE);
-            if (disable == 0) { //todo: remove this if ???
-                final int operation = initialBias == IODispatcherConfiguration.BIAS_READ ? IOOperation.READ : IOOperation.WRITE;
-
-                if (operation == IOOperation.READ) {
-                    kqueue.readFD(fd, pending.get(i, OPM_ID));
-                    pending.set(i, OPM_OPERATION, IOOperation.READ);
-                } else {
-                    kqueue.writeFD(fd, pending.get(i, OPM_ID));
-                    pending.set(i, OPM_OPERATION, IOOperation.WRITE);
-                }
-                if (++index > capacity - 1) {
-                    registerWithKQueue(index);
-                    index = 0;
-                    offset = 0;
-                }
+            final int operation = initialBias == IODispatcherConfiguration.BIAS_READ ? IOOperation.READ : IOOperation.WRITE;
+            pending.set(i, OPM_OPERATION, operation);
+            if (operation == IOOperation.READ) {
+                kqueue.readFD(fd, pending.get(i, OPM_ID));
+            } else {
+                kqueue.writeFD(fd, pending.get(i, OPM_ID));
+            }
+            if (++index > capacity - 1) {
+                registerWithKQueue(index);
+                index = 0;
+                offset = 0;
             }
         }
         if (index > 0) {
