ae5f380f3bd68be0ff9f174275f88b9742a26786
glasstiger <imre@questdb.io>
2023-03-06 16:25:25 +0000

    chore(wal): ilp drop table fuzz test - debug

--- core/src/test/java/io/questdb/cutlass/line/tcp/AbstractLineTcpReceiverFuzzTest.java
+++ core/src/test/java/io/questdb/cutlass/line/tcp/AbstractLineTcpReceiverFuzzTest.java
@@ -336,7 +336,10 @@ abstract class AbstractLineTcpReceiverFuzzTest extends AbstractLineTcpReceiverTe
                 try (RecordCursor cursor = factory.getCursor(sqlExecutionContext)) {
                     getLog().info().$("table.getName(): ").$(table.getName()).$(", tableName: ").$(tableName)
                             .$(", table.size(): ").$(table.size()).$(", cursor.size(): ").$(cursor.size()).$();
-                    return table.size() <= cursor.size();
+                    if (table.size() > cursor.size()) {
+                        getLog().info().$("checkTable timestampMark=").$(timestampMark).$();
+                    }
+                    return table.size() <= cursor.size() + 1;
                 }
             } catch (SqlException ex) {
                 if (ex.getFlyweightMessage().toString().contains("table does not exist")) {
