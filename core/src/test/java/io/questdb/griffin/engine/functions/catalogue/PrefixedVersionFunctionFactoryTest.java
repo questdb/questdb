08a8332e97ee15788cbd68447ea852ffb9fe32f5
javier <javier@formatinternet.com>
2023-03-21 16:53:58 +0100

    bug(sql): had forgotten to update the test for pg_catalog.version
    Fixed text for pg_catalog.version() after bumping the version number

--- core/src/test/java/io/questdb/griffin/engine/functions/catalogue/PrefixedVersionFunctionFactoryTest.java
+++ core/src/test/java/io/questdb/griffin/engine/functions/catalogue/PrefixedVersionFunctionFactoryTest.java
@@ -32,7 +32,7 @@ public class PrefixedVersionFunctionFactoryTest extends AbstractGriffinTest {
     @Test
     public void testPrefixedCurrentSchemaFunc() throws Exception {
         assertQuery("version\n" +
-                        "PostgreSQL 15.x (QuestDB)\n",
+                        "PostgreSQL 15.3 (QuestDB)\n",
                 "select pg_catalog.version();",
                 true);
     }
