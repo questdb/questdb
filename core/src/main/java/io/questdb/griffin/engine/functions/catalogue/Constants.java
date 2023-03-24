7bbd64e6bdea3c2997b45f8b9a53923b24828837
javier <javier@formatinternet.com>
2023-03-24 16:56:38 +0100

    bug(sql) Reverting back to former version literal to avoid side-effects
    The version literal was used at more places, including as a binary string for dot net.
    Reverting it back to its former version to avoid breaking test and functionality

--- core/src/main/java/io/questdb/griffin/engine/functions/catalogue/Constants.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/catalogue/Constants.java
@@ -124,7 +124,7 @@ public class Constants {
     };
     static final String[] NAMESPACES = {"pg_catalog", PUBLIC};
     static final int[] NAMESPACE_OIDS = {PGOids.PG_CATALOG_OID, PGOids.PG_PUBLIC_OID};
-    static final StrConstant PG_CATALOG_VERSION_CONSTANT = new StrConstant("PostgreSQL 15.2 (QuestDB), compiled by Visual C++ build 1914, 64-bit");
+    static final StrConstant PG_CATALOG_VERSION_CONSTANT = new StrConstant("PostgreSQL 12.3, compiled by Visual C++ build 1914, 64-bit");
     static final StrFunction PUBLIC_CONSTANT = new StrConstant(PUBLIC);
     static final StrFunction USER_CONSTANT = new StrConstant("admin");
 }
