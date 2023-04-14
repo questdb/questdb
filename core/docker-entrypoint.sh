d0ad2badecf2dc7c9e96b342b722e42cf207288d
Marius <marius.gheorghies@protonmail.com>
2023-04-14 11:32:42 +0200

    add localdirs as variable so users can change it

--- core/docker-entrypoint.sh
+++ core/docker-entrypoint.sh
@@ -6,12 +6,14 @@ QUESTDB_DATA_DIR=${QUESTDB_DATA_DIR:-"/var/lib/questdb"}
 IGNORE_DATA_ROOT_MOUNT_CHECK=${IGNORE_DATA_ROOT_MOUNT_CHECK:-"false"}
 FIND_AND_OWN_DIR=${FIND_AND_OWN_DIR:-"false"}
 RUN_AS_ROOT=${RUN_AS_ROOT:-"false"}
+LOCALDIRS=${LOCALDIRS:-"/conf /public /db"}
 
 find_and_own_dir() {
     if [ "$FIND_AND_OWN_DIR" = "true" ]; then
-        [ -d "${1}/conf" ] && find "${1}/conf" \( ! -user questdb -o ! -group questdb \) -exec chown questdb:questdb '{}' \;
-        [ -d "${1}/public" ] && find "${1}/public" \( ! -user questdb -o ! -group questdb \) -exec chown questdb:questdb '{}' \;
-        [ -d "${1}/db" ] && find "${1}/db" \( ! -user questdb -o ! -group questdb \) -exec chown questdb:questdb '{}' \;
+        for localdir in $LOCALDIRS
+        do
+            [ -d "${1}${localdir}" ] && find "${1}${localdir}" \( ! -user questdb -o ! -group questdb \) -exec chown questdb:questdb '{}' \;
+        done
     fi
 }
 
