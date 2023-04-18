63a0241b448e9f87df4a1588ff7d83c51e68fe98
Abir Kumar Nandi <116013922+Abir-0007@users.noreply.github.com>
2023-04-18 12:02:41 +0530

    updated  module-info

--- benchmarks/src/main/java/module-info.java
+++ benchmarks/src/main/java/module-info.java
@@ -1,11 +1,8 @@
-module io.questdb.benchmarks {
-    requires jdk.unsupported;
-    requires java.base;
-    requires io.questdb;
-    requires jmh.core;
-    requires org.apache.logging.log4j;
-    requires simpleclient;
-    requires simpleclient.common;
+import sun.misc.BASE64Encoder;
 
-    exports org.questdb.jmh_generated;
+public class TestDecoder {
+
+  public static void main(String[] args) {
+      System.out.println(new BASE64Encoder().encode(new byte[0]));
+  }
 }
