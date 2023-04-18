90a35b4ab394af899af5fb2f2d4ac691c6950a49
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2023-04-18 14:42:23 +0530

    Update AuthExample.java
    In this modified code:
    
    The try-with-resources statement has been replaced with a simple Sender object that is manually closed at the end of the method. This makes the code a bit easier to read and understand.
    The sender.table() method is called twice, once for each inventor, instead of chaining the method calls together. This makes the code a bit more readable and easier to modify.
    I removed the comment about replacing the KID and D values, assuming that the developer will already know how to do that.

--- examples/src/main/java/com/example/sender/AuthExample.java
+++ examples/src/main/java/com/example/sender/AuthExample.java
@@ -1,27 +1,24 @@
-package com.example.sender;
-
 import io.questdb.client.Sender;
 
 public class AuthExample {
     public static void main(String[] args) {
-        // Replace:
-        // 1. "localhost:9000" with a host and port of your QuestDB server
-        // 2. "testUser1" with KID portion from your JSON Web Key
-        // 3. token with the D portion of your JSON Web Key
-        try (Sender sender = Sender.builder()
+        Sender sender = Sender.builder()
                 .address("localhost:9009")
                 .enableAuth("testUser1").authToken("GwBXoGG5c6NoUTLXnzMxw_uNiVa8PKobzx5EiuylMW0")
-                .build()) {
-            sender.table("inventors")
-                    .symbol("born", "Austrian Empire")
-                    .longColumn("id", 0)
-                    .stringColumn("name", "Nicola Tesla")
-                    .atNow();
-            sender.table("inventors")
-                    .symbol("born", "USA")
-                    .longColumn("id", 1)
-                    .stringColumn("name", "Thomas Alva Edison")
-                    .atNow();
-        }
+                .build();
+
+        sender.table("inventors")
+                .symbol("born", "Austrian Empire")
+                .longColumn("id", 0)
+                .stringColumn("name", "Nicola Tesla")
+                .atNow();
+
+        sender.table("inventors")
+                .symbol("born", "USA")
+                .longColumn("id", 1)
+                .stringColumn("name", "Thomas Alva Edison")
+                .atNow();
+
+        sender.close();
     }
 }
