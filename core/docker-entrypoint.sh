e5fd84f10aef4803ca9dabe80d2be59cbc99df96
Marius <marius.gheorghies@protonmail.com>
2023-04-21 21:42:13 +0200

    improve a bit the logic and variable names

--- core/docker-entrypoint.sh
+++ core/docker-entrypoint.sh
@@ -4,18 +4,33 @@ export QDB_PACKAGE=${QDB_PACKAGE:-docker}
 
 QUESTDB_DATA_DIR=${QUESTDB_DATA_DIR:-"/var/lib/questdb"}
 IGNORE_DATA_ROOT_MOUNT_CHECK=${IGNORE_DATA_ROOT_MOUNT_CHECK:-"false"}
-FIND_AND_OWN_DIR=${FIND_AND_OWN_DIR:-"false"}
 RUN_AS_ROOT=${RUN_AS_ROOT:-"false"}
-DEFAULT_LOCAL_DIRS=${DEFAULT_LOCAL_DIRS:-"/conf /public /db"}
+DO_CHOWN=${DO_CHOWN:-"true"}
+QUESTDB_UID="${QUESTDB_UID:-"$(id -u questdb)"}"
+QUESTDB_GID="${QUESTDB_GID:-"$(id -g questdb)"}"
+TEMPFILE="$(mktemp)"
+HELP_TEMPFILE="$(mktemp)"
 
-cat << EOF > /tmp/help_found_files_not_owned
 
+# directories inside QUESTDB_DATA_DIR that we will chown
+DEFAULT_LOCAL_DIRS=${DEFAULT_LOCAL_DIRS:-"/conf /public /db /snapshot"}
+array=( ${DEFAULT_LOCAL_DIRS} )
+read -ra LOCALDIRS < <( echo -n "( "; printf -- "-ipath ${QUESTDB_DATA_DIR}%s* -o " "${array[@]:0:$((${#array[@]} - 1))}"; echo -n "-ipath ${QUESTDB_DATA_DIR}${array[@]: -1}*"; echo " )";)
+
+
+# backwards compatibility with previous versions
+if [ ${IGNORE_FIND_AND_OWN_DIR+x} ]
+then
+    DO_CHOWN=$IGNORE_FIND_AND_OWN_DIR
+fi
+
+cat << EOF > $HELP_TEMPFILE
 !!! The following files/directories have been found not to be owned by the questdb user/group, which may cause QuestDB to fail to start. !!!
 
-To allow the questdb container image to change the file ownership for you, please run the container with the -e FIND_AND_OWN_DIR=true option.
+To allow the questdb container image to change the file ownership for you, please run the container with the -e DO_CHOWN=true option.
 This will only work if you don't use --user argument when running the container image
 For example, you can use the command:
-  docker run -it -v /local-questdb-path:/var/lib/questdb -e FIND_AND_OWN_DIR=true questdb/questdb
+  docker run -it -v /local-questdb-path:/var/lib/questdb -e DO_CHOWN=true questdb/questdb
 
 
 Alternatively, you can change the file ownership yourself.
@@ -27,7 +42,8 @@ If you are running QuestDB on your local machine under a normal user (e.g., user
 you can run the questdb container image with the --user $(id -u):$(id -g) option. 
 For example, you can use the command:
   docker run -it -v /local-questdb-path:/var/lib/questdb --user $(id -u):$(id -g) questdb/questdb
-
+Or use environment variables QUESTDB_UID and QUESTDB_GID that should have the same effect:
+  docker run -it -v /local-questdb-path:/var/lib/questdb -e QUESTDB_UID=$(id -u) -e QUESTDB_GID=$(id -u) questdb/questdb
 
 If you want to run QuestDB as root, you can add the RUN_AS_ROOT environment variable when running questdb. 
 For example, you can use the command:
@@ -36,31 +52,21 @@ For example, you can use the command:
 EOF
 
 
-if [ -z "${LOCALDIRS:-}" ]
-then
-    array=( ${DEFAULT_LOCAL_DIRS} )
-    read -ra LOCALDIRS < <( echo -n "( "; printf -- "-ipath ${QUESTDB_DATA_DIR}%s* -o " "${array[@]:0:$((${#array[@]} - 1))}"; echo -n "-ipath ${QUESTDB_DATA_DIR}${array[@]: -1}*"; echo " )";)
-fi
-
-TEMPFILE="$(mktemp)"
-
 find_files_and_dirs_not_owned_by_user() {
-    USER=$1
-    GROUP=$2
+    local USER=$1
+    local GROUP=$2
+    # check if QUESTDB_DATA_DIR directory is owned by $USER:$GROUP
     [ $(stat --format '%u:%g' ${QUESTDB_DATA_DIR}) == "$USER:$GROUP" ] || echo ${QUESTDB_DATA_DIR} > $TEMPFILE
     find ${QUESTDB_DATA_DIR} "${LOCALDIRS[@]}" \( ! -user $USER -o ! -group $GROUP \) >> $TEMPFILE
-
-    grep . $TEMPFILE > /dev/null 2>&1 && cat /tmp/help_found_files_not_owned && cat $TEMPFILE || echo -n
+    grep . $TEMPFILE > /dev/null 2>&1 && cat $HELP_TEMPFILE && cat $TEMPFILE || echo -n
     echo
 }
 
 find_and_own_dir() {
-    USER=$1
-    GROUP=$2
+    local USER=$1
+    local GROUP=$2
     find_files_and_dirs_not_owned_by_user $USER $GROUP
-    if [ "$FIND_AND_OWN_DIR" = "true" ]; then
-        while read -r line; do chown $USER:$GROUP $line; done < $TEMPFILE
-    fi
+    while read -r line; do chown $USER:$GROUP $line; done < $TEMPFILE
 }
 
 
@@ -87,8 +93,10 @@ fi
 
 if [ "$(id -u)" = '0' ] && [ "${QUESTDB_DATA_DIR%/}" != "/root/.questdb" ] && [ "$RUN_AS_ROOT" = "false" ] ; then
     echo "Running as questdb user"
-    find_and_own_dir $(id -u questdb) $(id -g questdb)
-    exec gosu questdb "$@"
+    if [ "$DO_CHOWN" = "true" ]; then
+        find_and_own_dir $QUESTDB_UID $QUESTDB_GID
+    fi
+    exec gosu $QUESTDB_UID:$QUESTDB_GID "$@"
 fi
 
 if [ "$(id -u)" != '0' ] ; then
