2d5ff3025310a2bbef6ceacf1b58798e3d9c42e8
marregui <miguel.arregui@gmail.com>
2023-05-08 15:37:38 +0200

    merge master, remove old code

--- core/src/main/java/io/questdb/griffin/ExpressionParser.java
+++ core/src/main/java/io/questdb/griffin/ExpressionParser.java
@@ -193,11 +193,6 @@ public class ExpressionParser {
     }
 
     void parseExpr(GenericLexer lexer, ExpressionParserListener listener) throws SqlException {
-        parseExpr(lexer, listener, false);
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    void parseExpr(GenericLexer lexer, ExpressionParserListener listener, boolean unquoteStringConstants) throws SqlException {
         try {
             int paramCount = 0;
             int braceCount = 0;
@@ -777,7 +772,7 @@ public class ExpressionParser {
                                 }
 
                                 ExpressionNode constNode = expressionNodePool.next().of(ExpressionNode.CONSTANT,
-                                        unquoteStringConstants ? GenericLexer.unquote(tok) : GenericLexer.immutableOf(tok),
+                                        GenericLexer.immutableOf(tok),
                                         0,
                                         lastPos
                                 );
