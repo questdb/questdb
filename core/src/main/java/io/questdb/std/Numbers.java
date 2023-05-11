671ffffa6227073e1385d8413a8717d7176c38d6
Andrey Pechkurov <apechkurov@gmail.com>
2023-05-11 11:07:23 +0300

    Fix Numbers

--- core/src/main/java/io/questdb/std/Numbers.java
+++ core/src/main/java/io/questdb/std/Numbers.java
@@ -28,13 +28,13 @@ package io.questdb.std;
 import io.questdb.cairo.ImplicitCastException;
 import io.questdb.std.datetime.microtime.Timestamps;
 import io.questdb.std.fastdouble.FastDoubleParser;
+import io.questdb.std.fastdouble.FastFloatParser;
 import io.questdb.std.str.CharSink;
 //#if jdk.version==8
 //$import sun.misc.FDBigInteger;
 //#else
 import jdk.internal.math.FDBigInteger;
 //#endif
-import jdk.internal.math.FDBigInteger;
 import org.jetbrains.annotations.NotNull;
 import io.questdb.std.str.StringSink;
 
