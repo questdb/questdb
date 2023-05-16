a75483289ea4e9367ac933188240251318206db3
glasstiger <imre@questdb.io>
2023-05-16 18:08:40 +0100

    chore: revert breaking changes

--- core/src/main/java/io/questdb/cairo/TableWriter.java
+++ core/src/main/java/io/questdb/cairo/TableWriter.java
@@ -937,6 +937,10 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
             metrics.tableWriter().addCommittedRows(rowsAdded);
 
             shrinkO3Mem();
+
+            if (commitListener != null) {
+                commitListener.onCommit(txWriter.getTxn(), rowsAdded);
+            }
             return rowsAdded;
         }
 
