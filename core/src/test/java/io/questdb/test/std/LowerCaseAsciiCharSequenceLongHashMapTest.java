af97e750601da69a78c3499eb31d9c40d1e06448
glasstiger <94906625+glasstiger@users.noreply.github.com>
2023-05-23 15:58:59 +0100

    chore(core): add LowerCaseAsciiCharSequenceLongHashMap (#3385)

--- core/src/test/java/io/questdb/test/std/LowerCaseAsciiCharSequenceLongHashMapTest.java
+++ core/src/test/java/io/questdb/test/std/LowerCaseAsciiCharSequenceLongHashMapTest.java
@@ -24,7 +24,7 @@
 
 package io.questdb.test.std;
 
-import io.questdb.std.LowerCaseAsciiCharSequenceIntHashMap;
+import io.questdb.std.LowerCaseAsciiCharSequenceLongHashMap;
 import io.questdb.std.Rnd;
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,17 +32,45 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LowerCaseAsciiCharSequenceIntHashMapTest {
+public class LowerCaseAsciiCharSequenceLongHashMapTest {
+    @Test
+    public void testInvalidLoadFactor() {
+        try {
+            new LowerCaseAsciiCharSequenceLongHashMap(8, 0.0, -1L);
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("0 < loadFactor < 1", e.getMessage());
+        }
+        new LowerCaseAsciiCharSequenceLongHashMap(8, 0.0001, -1L);
+        new LowerCaseAsciiCharSequenceLongHashMap(8, 0.9999, -1L);
+        try {
+            new LowerCaseAsciiCharSequenceLongHashMap(8, 1.0, -1L);
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("0 < loadFactor < 1", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPutIfAbsent() {
+        final LowerCaseAsciiCharSequenceLongHashMap lowerCaseMap = new LowerCaseAsciiCharSequenceLongHashMap();
+        final CharSequence key = "testKey";
+        lowerCaseMap.putIfAbsent(key, 100L);
+        Assert.assertEquals(100L, lowerCaseMap.get(key));
+        lowerCaseMap.putIfAbsent(key, 102L);
+        Assert.assertEquals(100L, lowerCaseMap.get(key));
+        lowerCaseMap.put(key, 102L);
+        Assert.assertEquals(102L, lowerCaseMap.get(key));
+    }
+
     @Test
     public void testSaturation() {
 
         final int N = 10_000;
         final Rnd rnd = new Rnd();
-        final LowerCaseAsciiCharSequenceIntHashMap lowerCaseMap = new LowerCaseAsciiCharSequenceIntHashMap();
-        final HashMap<String, Integer> referenceMap = new HashMap<>();
+        final LowerCaseAsciiCharSequenceLongHashMap lowerCaseMap = new LowerCaseAsciiCharSequenceLongHashMap();
+        final HashMap<String, Long> referenceMap = new HashMap<>();
         for (int i = 0; i < N; i++) {
             String str = rnd.nextString(4);
-            int value = rnd.nextInt();
+            long value = rnd.nextLong();
 
             int keyIndex = lowerCaseMap.keyIndex(str, 0, str.length());
             if (lowerCaseMap.put(str, value)) {
@@ -57,17 +85,17 @@ public class LowerCaseAsciiCharSequenceIntHashMapTest {
         }
 
         // verify
-        for (Map.Entry<String, Integer> e : referenceMap.entrySet()) {
+        for (Map.Entry<String, Long> e : referenceMap.entrySet()) {
             Assert.assertTrue(lowerCaseMap.contains(e.getKey()));
             Assert.assertFalse(lowerCaseMap.excludes(e.getKey()));
             Assert.assertFalse(lowerCaseMap.excludes(e.getKey(), 0, e.getKey().length()));
 
             int keyIndex = lowerCaseMap.keyIndex(e.getKey());
             Assert.assertTrue(keyIndex < 0);
-            Assert.assertEquals(e.getKey(), (int) e.getValue(), lowerCaseMap.valueAt(keyIndex));
+            Assert.assertEquals(e.getKey(), (long) e.getValue(), lowerCaseMap.valueAt(keyIndex));
 
             keyIndex = lowerCaseMap.keyIndex(e.getKey(), 0, e.getKey().length());
-            Assert.assertEquals((int) e.getValue(), lowerCaseMap.valueAt(keyIndex));
+            Assert.assertEquals((long) e.getValue(), lowerCaseMap.valueAt(keyIndex));
         }
 
         for (int i = 0, n = lowerCaseMap.getKeyCount(); i < n; i++) {
@@ -90,7 +118,7 @@ public class LowerCaseAsciiCharSequenceIntHashMapTest {
         }
 
         // verify
-        for (Map.Entry<String, Integer> e : referenceMap.entrySet()) {
+        for (Map.Entry<String, Long> e : referenceMap.entrySet()) {
             Assert.assertTrue(e.getKey(), lowerCaseMap.contains(e.getKey()));
         }
 
