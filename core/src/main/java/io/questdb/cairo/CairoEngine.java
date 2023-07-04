699718ad5da1aacd82ad33be6e4b51533d31aa58
ideoma <2159629+ideoma@users.noreply.github.com>
2023-07-04 11:42:45 +0100

    chore(core): restore renaming method accidentally removed

--- core/src/main/java/io/questdb/cairo/CairoEngine.java
+++ core/src/main/java/io/questdb/cairo/CairoEngine.java
@@ -160,6 +160,11 @@ public class CairoEngine implements Closeable, WriterSource {
         }
     }
 
+    @SuppressWarnings("unused")
+    public void applyTableRename(TableToken token, TableToken updatedTableToken) {
+        tableNameRegistry.rename(token.getTableName(), updatedTableToken.getTableName(), token);
+    }
+
     @TestOnly
     public boolean clear() {
         boolean b1 = readerPool.releaseAll();
