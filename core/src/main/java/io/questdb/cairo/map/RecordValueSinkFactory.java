c81e78adf9c7a1cfd17cc956fd05f4cff344da16
allegraharris <aih19@georgetown.edu>
2023-07-17 17:19:42 +0100

    fixing formatting issue

--- core/src/main/java/io/questdb/cairo/map/RecordValueSinkFactory.java
+++ core/src/main/java/io/questdb/cairo/map/RecordValueSinkFactory.java
@@ -93,7 +93,6 @@ public class RecordValueSinkFactory {
             switch (ColumnType.tagOf(columnType)) {
                 case ColumnType.INT:
                 case ColumnType.SYMBOL:
-               // case ColumnType.IPv4:
                     asm.invokeInterface(rGetInt, 1);
                     asm.invokeInterface(wPutInt, 2);
                     break;
