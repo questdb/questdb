f46260b61a59e578c05041ed3571072c76bd78a3
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2023-07-17 21:34:11 +0300

    chore(wal): update WalListener API to pass  txn timestamp (#3573)
    Co-authored-by: Alex Pelagenko <2159629+ideoma@users.noreply.github.com>

--- core/src/main/java/io/questdb/cairo/wal/seq/TableSequencerImpl.java
+++ core/src/main/java/io/questdb/cairo/wal/seq/TableSequencerImpl.java
@@ -107,10 +107,11 @@ public class TableSequencerImpl implements TableSequencer {
     @Override
     public void dropTable() {
         checkDropped();
-        final long txn = tableTransactionLog.addEntry(getStructureVersion(), WalUtils.DROP_TABLE_WALID, 0, 0, microClock.getTicks());
+        final long timestamp = microClock.getTicks();
+        final long txn = tableTransactionLog.addEntry(getStructureVersion(), WalUtils.DROP_TABLE_WALID, 0, 0, timestamp);
         metadata.dropTable();
         engine.notifyWalTxnCommitted(tableToken, Long.MAX_VALUE);
-        engine.getWalListener().tableDropped(tableToken, txn);
+        engine.getWalListener().tableDropped(tableToken, txn, timestamp);
     }
 
     @Override
@@ -218,7 +219,8 @@ public class TableSequencerImpl implements TableSequencer {
         try {
             // From sequencer perspective metadata version is the same as column structure version
             if (metadata.getMetadataVersion() == expectedStructureVersion) {
-                tableTransactionLog.beginMetadataChangeEntry(expectedStructureVersion + 1, alterCommandWalFormatter, change, microClock.getTicks());
+                final long timestamp = microClock.getTicks();
+                tableTransactionLog.beginMetadataChangeEntry(expectedStructureVersion + 1, alterCommandWalFormatter, change, timestamp);
 
                 final TableToken oldTableToken = tableToken;
 
@@ -241,9 +243,9 @@ public class TableSequencerImpl implements TableSequencer {
                 if (!metadata.isSuspended()) {
                     engine.notifyWalTxnCommitted(tableToken, txn);
                     if (!tableToken.equals(oldTableToken)) {
-                        engine.getWalListener().tableRenamed(tableToken, txn, oldTableToken);
+                        engine.getWalListener().tableRenamed(tableToken, txn, timestamp, oldTableToken);
                     } else {
-                        engine.getWalListener().nonDataTxnCommitted(tableToken, txn);
+                        engine.getWalListener().nonDataTxnCommitted(tableToken, txn, timestamp);
                     }
                 }
             } else {
@@ -265,10 +267,11 @@ public class TableSequencerImpl implements TableSequencer {
         assert !closed;
         checkDropped();
         long txn;
+        final long timestamp = microClock.getTicks();
         try {
             // From sequencer perspective metadata version is the same as column structure version
             if (metadata.getMetadataVersion() == expectedStructureVersion) {
-                txn = nextTxn(walId, segmentId, segmentTxn);
+                txn = nextTxn(walId, segmentId, segmentTxn, timestamp);
             } else {
                 return NO_TXN;
             }
@@ -282,7 +285,7 @@ public class TableSequencerImpl implements TableSequencer {
 
         if (!metadata.isSuspended()) {
             engine.notifyWalTxnCommitted(tableToken, txn);
-            engine.getWalListener().dataTxnCommitted(tableToken, txn, walId, segmentId, segmentTxn);
+            engine.getWalListener().dataTxnCommitted(tableToken, txn, timestamp, walId, segmentId, segmentTxn);
         }
         return txn;
     }
@@ -384,8 +387,8 @@ public class TableSequencerImpl implements TableSequencer {
         path.trimTo(rootLen);
     }
 
-    private long nextTxn(int walId, int segmentId, int segmentTxn) {
-        return tableTransactionLog.addEntry(getStructureVersion(), walId, segmentId, segmentTxn, microClock.getTicks());
+    private long nextTxn(int walId, int segmentId, int segmentTxn, long timestamp) {
+        return tableTransactionLog.addEntry(getStructureVersion(), walId, segmentId, segmentTxn, timestamp);
     }
 
     void create(int tableId, TableStructure tableStruct) {
