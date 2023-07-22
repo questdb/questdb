f08fa51dd009378dc791b3f3d7195e895c97225f
MohanaSrinivas <mohanasrinivas08@gmail.com>
2023-07-22 17:50:53 +0530

    Added some more requriment modules in java

---
+++ win64svc/src/java/module io questdb benchmarks.java
@@ -0,0 +1,9 @@
+module io.questdb.benchmarks {
+    requires some.other.module;
+    requires another.library;
+    requires com.example.custommodule;
+
+    exports org.questdb.jmh_generated;
+}
+    
+
