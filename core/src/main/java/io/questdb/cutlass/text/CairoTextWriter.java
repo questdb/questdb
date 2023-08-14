29c2f4d42cb3e97c70ace40378fcbcebb9e9eb1d
ideoma <2159629+ideoma@users.noreply.github.com>
2023-08-14 17:05:56 +0100

    fix merge conflict lossy resolution

--- core/src/main/java/io/questdb/cutlass/text/CairoTextWriter.java
+++ core/src/main/java/io/questdb/cutlass/text/CairoTextWriter.java
@@ -233,6 +233,14 @@ public class CairoTextWriter implements Closeable, Mutable {
         return metadata.getTimestampIndex() > -1 ? metadata.getColumnName(metadata.getTimestampIndex()) : null;
     }
 
+    private long getO3MaxLag(TableWriterAPI writer) {
+        if (!writer.supportsMultipleWriters()) {
+            return ((TableWriter) writer).getMetaO3MaxLag();
+        }
+        // For WAL tables this parameter doesn't matter
+        return 0;
+    }
+
     private int getTablePartitionBy(TableWriterAPI writer) {
         // This is a bit of a hack. We need to know if the table is partitioned or not, mostly for compatibility with the non-WAL test cases.
         if (!writer.supportsMultipleWriters()) {
@@ -397,6 +405,18 @@ public class CairoTextWriter implements Closeable, Mutable {
         }
         if (!tableReCreated && (o3MaxLag > -1 || maxUncommittedRows > -1)) {
             LOG.info().$("cannot update metadata attributes o3MaxLag and maxUncommittedRows when the table exists and parameter overwrite is false").$();
+        }
+        if (PartitionBy.isPartitioned(partitionBy)) {
+            // We want to limit memory consumption during the import, so make sure
+            // to use table's maxUncommittedRows and o3MaxLag if they're not set.
+            if (o3MaxLag == -1) {
+                o3MaxLag = getO3MaxLag(writer);
+                LOG.info().$("using table's o3MaxLag ").$(o3MaxLag).$(", table=").utf8(tableName).$();
+            }
+            if (maxUncommittedRows == -1) {
+                maxUncommittedRows = writer.getMetadata().getMaxUncommittedRows();
+                LOG.info().$("using table's maxUncommittedRows ").$(maxUncommittedRows).$(", table=").utf8(tableName).$();
+            }
         }
         columnErrorCounts.seed(writer.getMetadata().getColumnCount(), 0);
 
