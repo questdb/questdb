854b89b8b34a2c7583b06965c556aed381e228cb
Allegra Harris <89586969+allegraharris@users.noreply.github.com>
2023-09-13 09:21:32 +0100

    feat(sql): allow string args for ipv4 operators (#3665)

--- core/src/main/java/io/questdb/griffin/engine/functions/eq/NegContainsEqIPv4FunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/eq/NegContainsEqIPv4FunctionFactory.java
@@ -34,10 +34,12 @@ import io.questdb.griffin.SqlExecutionContext;
 import io.questdb.griffin.engine.functions.BooleanFunction;
 import io.questdb.griffin.engine.functions.UnaryFunction;
 import io.questdb.std.IntList;
+import io.questdb.std.NumericException;
 import io.questdb.std.ObjList;
 
 import static io.questdb.std.Numbers.*;
 
+//second arg is contained within or equals first arg
 public class NegContainsEqIPv4FunctionFactory implements FunctionFactory {
 
     @Override
@@ -58,36 +60,24 @@ public class NegContainsEqIPv4FunctionFactory implements FunctionFactory {
             CairoConfiguration configuration,
             SqlExecutionContext sqlExecutionContext
     ) throws SqlException {
-        return createHalfConstantFunc(args.getQuick(1), args.getQuick(0));
+        return createHalfConstantFunc(args.getQuick(1), args.getQuick(0), argPositions.getQuick(0));
     }
 
-    private Function createHalfConstantFunc(Function varFunc, Function constFunc) throws SqlException {
+    private Function createHalfConstantFunc(Function varFunc, Function constFunc, int constFuncPos) throws SqlException {
         CharSequence constValue = constFunc.getStr(null);
 
         if (constValue == null) {
             return new NegContainsEqIPv4FunctionFactory.NullCheckFunc(varFunc);
         }
 
-        int subnet = getIPv4Subnet(constValue);
-        int netmask = getIPv4Netmask(constValue);
-
-        //catches negative netmask
-        if (subnet == -2 && netmask == -2) {
-            throw SqlException.$(18, "invalid argument: ").put(constValue);
+        try {
+            long subnetAndNetmask = getIPv4Subnet(constValue);
+            int subnet = (int) (subnetAndNetmask >> 32);
+            int netmask = (int) (subnetAndNetmask);
+            return new NegContainsEqIPv4FunctionFactory.ConstCheckFunc(varFunc, subnet & netmask, netmask);
+        } catch (NumericException ne) {
+            throw SqlException.$(constFuncPos, "invalid argument: ").put(constValue);
         }
-        //arg is invalid OR a subnet (-2 used as sentinel because -1 is valid (0xffffffff))
-        else if (subnet == -2) {
-            // check if arg is subnet
-            subnet = parseSubnet(constValue);
-
-            // arg is not a valid subnet/ip address
-            if (subnet == -2) {
-                throw SqlException.$(18, "invalid argument: ").put(constValue);
-            } else {
-                return new NegContainsEqIPv4FunctionFactory.ConstCheckFunc(varFunc, subnet, netmask);
-            }
-        }
-        return new NegContainsEqIPv4FunctionFactory.ConstCheckFunc(varFunc, subnet, netmask);
     }
 
     private static class ConstCheckFunc extends BooleanFunction implements UnaryFunction {
--- core/src/main/java/io/questdb/griffin/engine/functions/math/IntPlusIPv4StrFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/math/IntPlusIPv4StrFunctionFactory.java
@@ -26,20 +26,15 @@ package io.questdb.griffin.engine.functions.math;
 
 import io.questdb.cairo.CairoConfiguration;
 import io.questdb.cairo.sql.Function;
-import io.questdb.cairo.sql.Record;
 import io.questdb.griffin.FunctionFactory;
-import io.questdb.griffin.PlanSink;
 import io.questdb.griffin.SqlExecutionContext;
-import io.questdb.griffin.engine.functions.IPv4Function;
-import io.questdb.griffin.engine.functions.UnaryFunction;
 import io.questdb.std.IntList;
-import io.questdb.std.Numbers;
 import io.questdb.std.ObjList;
 
-public class IPv4StrNetmaskFunctionFactory implements FunctionFactory {
+public class IntPlusIPv4StrFunctionFactory implements FunctionFactory {
     @Override
     public String getSignature() {
-        return "netmask(S)";
+        return "+(Is)";
     }
 
     @Override
@@ -50,34 +45,6 @@ public class IPv4StrNetmaskFunctionFactory implements FunctionFactory {
             CairoConfiguration configuration,
             SqlExecutionContext sqlExecutionContext
     ) {
-        return new IPv4StrNetmaskFunctionFactory.IPv4StrNetmaskFunction(args.getQuick(0));
-    }
-
-    public static final class IPv4StrNetmaskFunction extends IPv4Function implements UnaryFunction {
-        private final Function arg;
-
-        public IPv4StrNetmaskFunction(Function arg) {
-            this.arg = arg;
-        }
-
-        @Override
-        public Function getArg() {
-            return arg;
-        }
-
-        @Override
-        public int getIPv4(Record rec) {
-            if (arg.getStr(rec) == null) {
-                return Numbers.IPv4_NULL;
-            }
-            final int val = Numbers.getIPv4Netmask(arg.getStr(rec));
-            return val == Numbers.BAD_NETMASK ? Numbers.IPv4_NULL : val;
-        }
-
-        @Override
-        public void toPlan(PlanSink sink) {
-            sink.val("netmask(").val(arg).val(')');
-        }
+        return new IntPlusIPv4FunctionFactory.IntPlusIPv4Function(args.getQuick(0), args.getQuick(1));
     }
 }
-
