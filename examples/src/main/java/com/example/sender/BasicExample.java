c2cd2243b3c8379097543265ec9261b19f343bd1
Andrey Pechkurov <apechkurov@gmail.com>
2023-09-18 14:01:24 +0300

    Fix client snippet

--- examples/src/main/java/com/example/sender/BasicExample.java
+++ examples/src/main/java/com/example/sender/BasicExample.java
@@ -7,15 +7,17 @@ import java.time.Instant;
 public class BasicExample {
     public static void main(String[] args) {
         try (Sender sender = Sender.builder().address("localhost:9009").build()) {
+            Instant bday = Instant.parse("1856-07-10T00:00:00.00Z");
             sender.table("inventors")
                     .symbol("born", "Austrian Empire")
-                    .timestampColumn("birthdate", Instant.parse("1856-07-10").toEpochMilli() * 1000) // epoch in micros
+                    .timestampColumn("birthday", bday.toEpochMilli() * 1000) // epoch in micros
                     .longColumn("id", 0)
                     .stringColumn("name", "Nicola Tesla")
                     .at(System.nanoTime()); // epoch in nanos
+            bday = Instant.parse("1847-02-11T00:00:00.00Z");
             sender.table("inventors")
                     .symbol("born", "USA")
-                    .timestampColumn("birthdate", Instant.parse("1847-02-11").toEpochMilli() * 1000)
+                    .timestampColumn("birthday", bday.toEpochMilli() * 1000)
                     .longColumn("id", 1)
                     .stringColumn("name", "Thomas Alva Edison")
                     .atNow();
