39ccbd9687482b58b8a372802ceae6b03f5aca6a
Andrey Pechkurov <apechkurov@gmail.com>
2023-09-18 13:39:56 +0300

    Add timestamps to Java client sample snippet

--- examples/src/main/java/com/example/sender/BasicExample.java
+++ examples/src/main/java/com/example/sender/BasicExample.java
@@ -2,16 +2,20 @@ package com.example.sender;
 
 import io.questdb.client.Sender;
 
+import java.time.Instant;
+
 public class BasicExample {
     public static void main(String[] args) {
         try (Sender sender = Sender.builder().address("localhost:9009").build()) {
             sender.table("inventors")
                     .symbol("born", "Austrian Empire")
+                    .timestampColumn("birthdate", Instant.parse("1856-07-10").toEpochMilli() * 1000) // epoch in microseconds
                     .longColumn("id", 0)
                     .stringColumn("name", "Nicola Tesla")
-                    .atNow();
+                    .at(System.nanoTime()); // epoch in nanoseconds
             sender.table("inventors")
                     .symbol("born", "USA")
+                    .timestampColumn("birthdate", Instant.parse("1847-02-11").toEpochMilli() * 1000)
                     .longColumn("id", 1)
                     .stringColumn("name", "Thomas Alva Edison")
                     .atNow();
