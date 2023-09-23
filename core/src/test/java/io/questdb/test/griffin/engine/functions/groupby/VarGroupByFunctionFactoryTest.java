773de705337528c8d5897b3ab734cb1b59b5ea0d
Charles <phucnh402@gmail.com>
2023-09-22 23:08:12 -0400

    add tests

--- core/src/test/java/io/questdb/test/griffin/engine/functions/groupby/VarGroupByFunctionFactoryTest.java
+++ core/src/test/java/io/questdb/test/griffin/engine/functions/groupby/VarGroupByFunctionFactoryTest.java
@@ -29,6 +29,115 @@ import org.junit.Test;
 
 public class VarGroupByFunctionFactoryTest extends AbstractCairoTest {
 
+    @Test
+    public void testVarAllNull() throws Exception {
+        assertMemoryLeak(() -> assertSql(
+                "variance\nNaN\n", "select variance(x) from (select cast(null as double) x from long_sequence(100))"
+        ));
+    }
 
+    @Test
+    public void testVarAllSameValues() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select 17.2151921 x from long_sequence(100))");
+            assertSql(
+                    "variance\n0.0\n", "select variance(x) from tbl1"
+            );
+        });
+    }
 
+    @Test
+    public void testVarDoubleValues() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select cast(x as double) x from long_sequence(100))");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarFirstNull() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1(x double)");
+            insert("insert into 'tbl1' VALUES (null)");
+            insert("insert into 'tbl1' select x from long_sequence(100)");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarFloatValues() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select cast(x as float) x from long_sequence(100))");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarIntValues() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select cast(x as int) x from long_sequence(100))");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarLong256Values() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select x cast(x as long256) from long_sequence(100))");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarNoValues() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1(x int)");
+            assertSql(
+                    "variance\nNaN\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarOneValue() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1(x int)");
+            insert("insert into 'tbl1' VALUES " +
+                    "(17.2151920)");
+            assertSql(
+                    "variance\nNaN\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarOverflow() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select 100000000 x from long_sequence(1000000))");
+            assertSql(
+                    "variance\n0.0\n", "select variance(x) from tbl1"
+            );
+        });
+    }
+
+    @Test
+    public void testVarSomeNull() throws Exception {
+        assertMemoryLeak(() -> {
+            ddl("create table tbl1 as (select cast(x as double) x from long_sequence(100))");
+            insert("insert into 'tbl1' VALUES (null)");
+            assertSql(
+                    "variance\n841.6666666666666\n", "select variance(x) from tbl1"
+            );
+        });
+    }
 }
