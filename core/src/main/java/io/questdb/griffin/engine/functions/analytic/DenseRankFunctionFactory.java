c6ce052788e0f1a21fa9c5aeca10fa9f6c8780f8
Dylan John Hennessy <hennessydylan@outlook.com>
2023-10-08 21:49:22 -0700

    feat(sql) supoort of dense_rank() function
    beginning of the creation of the `dense_rank()` sql function.
    
    Found on list: _Hacktoberfest 2022: list of SQL functions for contributors #2578_
    
    Co-Authored-By: hennyboiszz <126116395+hennyboiszz@users.noreply.github.com>

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/analytic/DenseRankFunctionFactory.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2023 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.analytic;
+
+import io.questdb.cairo.*;
+import io.questdb.cairo.map.Map;
+import io.questdb.cairo.map.MapFactory;
+import io.questdb.cairo.map.MapKey;
+import io.questdb.cairo.map.MapValue;
+import io.questdb.cairo.sql.Record;
+import io.questdb.cairo.sql.*;
+import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.PlanSink;
+import io.questdb.griffin.SqlException;
+import io.questdb.griffinSqlExecutionContext;
+import io.questdb.griffin.engine.RecordComparator;
+import io.questdb.griffin.engine.analytic.AnalyticContext;
+import io.questdb.griffin.engine.analytic.AnalyticFunction;
+import io.questdb.griffin.engine.functions.LongFunction;
+import io.questdb.griffin,engine.orderby.RecordComparatorCompiler;
+import io.questdb.std.IntList;
+import io.questdb.std.Misc;
+import io.questdb.std.ObjList;
+import io.questdb.std.Unsafe;
+
