2dc2e75bb9f1d0e82a6979a646f1b6c4695dd85f
glasstiger <imre@questdb.io>
2023-10-27 01:02:53 +0100

    comments

--- core/src/main/java/io/questdb/QueryLogger.java
+++ core/src/main/java/io/questdb/QueryLogger.java
@@ -4,14 +4,17 @@ import io.questdb.cairo.SecurityContext;
 import io.questdb.log.Log;
 
 public interface QueryLogger {
+    // called when an empty query received
     default void logEmptyQuery(Log logger, boolean doLog, int fd, CharSequence query, SecurityContext securityContext) {
         logQuery(logger, doLog, fd, query, securityContext, "empty query");
     }
 
+    // called when a cached query executed
     default void logExecQuery(Log logger, boolean doLog, int fd, CharSequence query, SecurityContext securityContext) {
         logQuery(logger, doLog, fd, query, securityContext, "exec");
     }
 
+    // called when a new query parsed
     default void logParseQuery(Log logger, boolean doLog, int fd, CharSequence query, SecurityContext securityContext) {
         logQuery(logger, doLog, fd, query, securityContext, "parse");
     }
