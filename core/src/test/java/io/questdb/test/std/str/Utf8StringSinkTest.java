8c1c55497e0d052a115a752a8de34f06bfdb3f59
Adam Cimarosti <cimarosti@gmail.com>
2023-10-30 13:45:52 +0000

    yet another string test

--- core/src/test/java/io/questdb/test/std/str/Utf8StringSinkTest.java
+++ core/src/test/java/io/questdb/test/std/str/Utf8StringSinkTest.java
@@ -24,6 +24,7 @@
 
 package io.questdb.test.std.str;
 
+import io.questdb.std.str.GcUtf8String;
 import io.questdb.std.str.Utf8String;
 import io.questdb.std.str.Utf8StringSink;
 import io.questdb.test.tools.TestUtils;
@@ -138,6 +139,17 @@ public class Utf8StringSinkTest {
         sink.put(str);
         byte[] expectedBytes = str.getBytes(StandardCharsets.UTF_8);
         TestUtils.assertEquals(expectedBytes, sink);
+    }
+
+    @Test
+    public void testDirectUtf8Sequence() {
+        final String str = "Здравей свят";
+        final GcUtf8String src = new GcUtf8String(str);
+        final Utf8StringSink sink = new Utf8StringSink(4);
+        sink.put(src);
+        final byte[] expectedBytes = str.getBytes(StandardCharsets.UTF_8);
+        TestUtils.assertEquals(expectedBytes, sink);
+        TestUtils.assertEquals(src, sink);
     }
 
     private static void assertUtf8Encoding(Utf8StringSink sink, String s) {
