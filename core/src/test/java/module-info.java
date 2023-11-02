ea0d7ba5f4ff9c6eb098011905b53930efcb7739
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-11-03 04:00:31 +0800

    revert wrong change from old pr

--- core/src/test/java/module-info.java
+++ core/src/test/java/module-info.java
@@ -12,7 +12,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *  <a href="http://www.apache.org/licenses/LICENSE-2.0">...</a>
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
