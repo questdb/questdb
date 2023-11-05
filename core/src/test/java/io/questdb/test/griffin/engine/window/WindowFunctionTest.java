4703efa7bf8cba23fdddf67f5b347d2378240d50
Dylan John Hennessy <108561929+hennessydylan@users.noreply.github.com>
2023-11-04 22:17:12 -0700

    add units tests for dense_rank()

--- core/src/test/java/io/questdb/test/griffin/engine/window/WindowFunctionTest.java
+++ core/src/test/java/io/questdb/test/griffin/engine/window/WindowFunctionTest.java
@@ -1776,25 +1776,387 @@ public class WindowFunctionTest extends AbstractCairoTest {
         );
     }
 
-    /* rank() function tests 
-     *
-     *   testRankFailsIsNowWindowContext
-     *   testRankWithNoPartitionByAndNoOrderByWildcardLast
-     *   testRankWithNoPartitionByAndOrderBySymbolWildcardLast
-     *   testRankWithPartitionAndOrderByNonSymbol
-     *   testRankWithPartitionAndOrderBySymbolNoWildcard
-     *   testRankWithPartitionAndOrdereBySymbolWildcardFirst
-     *   testRankWithPartitionAndOrderBySymbolWildcardLast
-     *   testRankWithPartitionBySymbolAndMultiOrderWildcardLast
-     *   testRankWithPartitionBySymbolAndNoOrderWildcardLast
-     *   testRankWithPartitionBySymbolAndOrderByIntPriceDescWildcardLast
-     *   testRankWithPartitionBySymbolAndOrderByIntPriceWildcardLast
-     *   testRankWithPartitionBySymbolAndOrderByPriceWildcardLast
-     *   testRowNumberFailsInNonWindowContext
-     * 
-     */
+/* dense_rank() function tests
+ * 
+ * 1.  testDenseRankFailsIsNoWindowContext
+ * 2.  testDenseRankNoPartitionByAndNoOrderByWildcardLast
+ * 3.  testDenseRankWithNoPartitionByAndOrderBySymbolWildcardLast
+ * 4.  testDenseRankWithPartitionAndOrderByNonSymbol
+ * 5.  testDenseRankWithPartitionAndOrderBySymbolNoWildcard
+ * TODO:
+ * 6.  testDenseRankWithPartitionAndOrderBySymbolWildcardFirst
+ * 7.  testDenseRankWithPartitionAndOrderBySymbolWildcardLast
+ * 8.  testDenseRankWithPartitionBySymbolAndMultiOrderWildcardLast
+ * 9.  testDenseRankWithPartitionBySymbolAndNoOrderWildcardLast
+ * 10. testDenseRankWithPartitionBySymbolAndOrderByIntPriceDescWildcardLast
+ * 11. testDenseRankWithPartitionBySymbolAndOrderByIntPriceWildcardLast
+ * 12. testDenseRankWithPartitionBySymbolAndOrderByPriceWildcardLast
+ */
+
+/* 1. 
+ * 
+ */
     @Test
-    public void 
+    public void testDenseRankFailsIsNoWindowContext() throws Exception {
+        assertException(
+                "select dense_rank(), * from trades",
+                "create table trades as " + 
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB', 'CC')" +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequennce(10)" +
+                        " ) timestapmp(ts) partition by day",
+                7,
+                "window functinnn called in now-window, make sure to add the OVER clause" 
+        );
+        
+    }
+
+/* 2. 
+ * 
+ */
+    @Test 
+    public void testDenseRankNoPartitionByAndNoOrderByWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "1\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "1\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "1\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "1\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "1\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "1\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 3. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithNoPartitionByAndOrderBySymbolWildcardLast() {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "2\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "3\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "3\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "2\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "2\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "2\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "3\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "3\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (order by symbol), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 4. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionAndOrderByNonSymbol() throws Exception {
+        assertQuery("rank\tprice\tts\n" +
+                        "1\t42\t1970-01-01T00:00:00.000000Z\n" +
+                        "2\t42\t1970-01-02T03:46:40.000000Z\n" +
+                        "3\t42\t1970-01-03T07:33:20.000000Z\n" +
+                        "4\t42\t1970-01-04T11:20:00.000000Z\n" +
+                        "5\t42\t1970-01-05T15:06:40.000000Z\n" +
+                        "6\t42\t1970-01-06T18:53:20.000000Z\n" +
+                        "7\t42\t1970-01-07T22:40:00.000000Z\n" +
+                        "8\t42\t1970-01-09T02:26:40.000000Z\n" +
+                        "9\t42\t1970-01-10T06:13:20.000000Z\n" +
+                        "10\t42\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by price order by ts), price, ts from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " 42 price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                false,
+                true
+        );
+    }
+
+/* 5. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionAndOrderBySymbolNoWildcard() throws Exception {
+        assertQuery("rank\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n" +
+                        "1\n",
+                "select dennse_rank() over (partition by symbol order by symbol) from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_double(42) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 6. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionAndOrderBySymbolWildcardFirst() throws Exception {
+        assertQuery("price\tsymbol\tts\trank\n" +
+                        "0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\t1\n" +
+                        "0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\t1\n" +
+                        "0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\t1\n" +
+                        "0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\t1\n" +
+                        "0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\t1\n" +
+                        "0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\t1\n" +
+                        "0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\t1\n" +
+                        "0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\t1\n" +
+                        "0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\t1\n" +
+                        "0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\t1\n",
+                "select *, dense_rank() over (partition by symbol order by symbol) from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_double(42) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 7. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionAndOrderBySymbolWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "1\t0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\n" +
+                        "1\t0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\n" +
+                        "1\t0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\n" +
+                        "1\t0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "1\t0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\n" +
+                        "1\t0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol order by symbol), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_double(42) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 8. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionBySymbolAndMultiOrderWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "1\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "2\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "2\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "1\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "1\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "1\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol order by symbol, price), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
+
+/* 9. stopped @ 21:27
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionBySymbolAndNoOrderWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "1\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "1\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "2\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "2\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "3\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "4\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "3\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "4\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "2\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                false,
+                true
+        );
+
+    }
+
+/* 10. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionBySymbolAndOrderByIntPriceDescWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "2\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "1\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "2\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "1\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "2\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "2\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "2\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "2\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol order by price desc), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+
+    }
+    
+/* 11. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionBySymbolAndOrderByIntPriceWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "1\t1\tBB\t1970-01-01T00:00:00.000000Z\n" +
+                        "4\t2\tCC\t1970-01-02T03:46:40.000000Z\n" +
+                        "1\t2\tAA\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t1\tCC\t1970-01-04T11:20:00.000000Z\n" +
+                        "4\t2\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t1\tBB\t1970-01-06T18:53:20.000000Z\n" +
+                        "1\t1\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "1\t1\tCC\t1970-01-09T02:26:40.000000Z\n" +
+                        "1\t1\tCC\t1970-01-10T06:13:20.000000Z\n" +
+                        "1\t2\tAA\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol order by price), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_int(1,2,3) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+
+    }
+
+/* 12. 
+ * 
+ */
+    @Test
+    public void testDenseRankWithPartitionBySymbolAndOrderByPriceWildcardLast() throws Exception {
+        assertQuery("rank\tprice\tsymbol\tts\n" +
+                        "2\t0.8043224099968393\tCC\t1970-01-01T00:00:00.000000Z\n" +
+                        "1\t0.2845577791213847\tBB\t1970-01-02T03:46:40.000000Z\n" +
+                        "3\t0.9344604857394011\tCC\t1970-01-03T07:33:20.000000Z\n" +
+                        "1\t0.7905675319675964\tAA\t1970-01-04T11:20:00.000000Z\n" +
+                        "6\t0.8899286912289663\tBB\t1970-01-05T15:06:40.000000Z\n" +
+                        "1\t0.11427984775756228\tCC\t1970-01-06T18:53:20.000000Z\n" +
+                        "2\t0.4217768841969397\tBB\t1970-01-07T22:40:00.000000Z\n" +
+                        "4\t0.7261136209823622\tBB\t1970-01-09T02:26:40.000000Z\n" +
+                        "3\t0.6693837147631712\tBB\t1970-01-10T06:13:20.000000Z\n" +
+                        "5\t0.8756771741121929\tBB\t1970-01-11T10:00:00.000000Z\n",
+                "select dense_rank() over (partition by symbol order by price), * from trades",
+                "create table trades as " +
+                        "(" +
+                        "select" +
+                        " rnd_double(42) price," +
+                        " rnd_symbol('AA','BB','CC') symbol," +
+                        " timestamp_sequence(0, 100000000000) ts" +
+                        " from long_sequence(10)" +
+                        ") timestamp(ts) partition by day",
+                null,
+                true,
+                false
+        );
+    }
 
     @Test
     public void testWindowBufferExceedsLimit() throws Exception {
