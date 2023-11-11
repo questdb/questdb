94cb229fa4cd2d6526858c9c798adadef8c63665
Dylan John Hennessy <108561929+hennessydylan@users.noreply.github.com>
2023-11-10 18:14:52 -0800

    reinstated increments

--- core/src/main/java/io/questdb/griffin/engine/functions/window/DenseRankFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/window/DenseRankFunctionFactory.java
@@ -130,7 +130,6 @@ public class DenseRankFunctionFactory implements FunctionFactory {
             if (recordComparator == null) {
                 // order dismiss 
                 Unsafe.getUnsafe().putLong(spi.getAddress(recordOffset, columnIndex), rankIndex + 1);
-                rankIndex++;
             } else {
                 if (currentIndex == 0) {
                     currentIndex = 1;
@@ -141,13 +140,12 @@ public class DenseRankFunctionFactory implements FunctionFactory {
                     if (recordComparator.compare(spi.getRecordAt(offset)) != 0) {
                         currentIndex = rankIndex + 1;
                         offset = recordOffset;
-                        rankIndex++;
                     }
                 }
                 Unsafe.getUnsafe().putLong(spi.getAddress(recordOffset, columnIndex), currentIndex);
             }
+            rankIndex++;
         }
-        // rankIndex++;
 
         @Override
         public void reopen() {
