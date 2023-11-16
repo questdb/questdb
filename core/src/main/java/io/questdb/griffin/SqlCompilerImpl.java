2bd0af26efd052679a52d4e55a1d8404bb63c3d7
Vlad Ilyushchenko <bluestreak@gmail.com>
2023-11-16 21:20:27 +0000

    fix jdk17 compilation error

--- core/src/main/java/io/questdb/griffin/SqlCompilerImpl.java
+++ core/src/main/java/io/questdb/griffin/SqlCompilerImpl.java
@@ -27,6 +27,7 @@ package io.questdb.griffin;
 import io.questdb.*;
 import io.questdb.cairo.*;
 import io.questdb.cairo.sql.*;
+import io.questdb.cairo.sql.Record;
 import io.questdb.cairo.vm.Vm;
 import io.questdb.cairo.vm.api.MemoryMARW;
 import io.questdb.cairo.wal.WalUtils;
