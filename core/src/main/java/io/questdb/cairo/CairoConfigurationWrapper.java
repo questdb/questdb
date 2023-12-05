011b43c421bb0de704eea94082e35dfe6e3ffdd2
Andrey Pechkurov <apechkurov@gmail.com>
2023-12-05 09:33:12 +0200

    perf(sql): get rid of offset packing in FastMap

--- core/src/main/java/io/questdb/cairo/CairoConfigurationWrapper.java
+++ core/src/main/java/io/questdb/cairo/CairoConfigurationWrapper.java
@@ -346,11 +346,6 @@ public class CairoConfigurationWrapper implements CairoConfiguration {
     }
 
     @Override
-    public int getWalMaxSegmentFileDescriptorsCache() {
-        return delegate.getWalMaxSegmentFileDescriptorsCache();
-    }
-
-    @Override
     public int getMetadataPoolCapacity() {
         return delegate.getMetadataPoolCapacity();
     }
@@ -646,11 +641,6 @@ public class CairoConfigurationWrapper implements CairoConfiguration {
     }
 
     @Override
-    public int getSqlJitRowsThreshold() {
-        return delegate.getSqlJitRowsThreshold();
-    }
-
-    @Override
     public int getSqlJoinContextPoolCapacity() {
         return delegate.getSqlJoinContextPoolCapacity();
     }
@@ -878,6 +868,11 @@ public class CairoConfigurationWrapper implements CairoConfiguration {
     @Override
     public int getWalMaxLagTxnCount() {
         return delegate.getWalMaxLagTxnCount();
+    }
+
+    @Override
+    public int getWalMaxSegmentFileDescriptorsCache() {
+        return delegate.getWalMaxSegmentFileDescriptorsCache();
     }
 
     @Override
