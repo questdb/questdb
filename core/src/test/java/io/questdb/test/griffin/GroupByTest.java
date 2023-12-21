36f5572c5e344fc700186f2c2cf315443a381f9c
Andrey Pechkurov <apechkurov@gmail.com>
2023-12-21 15:58:29 +0200

    Fix more tests

--- core/src/test/java/io/questdb/test/griffin/GroupByTest.java
+++ core/src/test/java/io/questdb/test/griffin/GroupByTest.java
@@ -946,7 +946,7 @@ public class GroupByTest extends AbstractCairoTest {
                     "    union all " +
                     "    select 1, 'a' )");
 
-            String query = "select l,s, l+1 from t group by l+1,s, l, l+2";
+            String query = "select l, s, l+1 from t group by l+1,s, l, l+2";
             assertPlan(
                     query,
                     "VirtualRecord\n" +
@@ -994,14 +994,14 @@ public class GroupByTest extends AbstractCairoTest {
     public void testGroupByNonPartitioned() throws Exception {
         assertQuery(
                 "k\tsum\n" +
-                        "TJWC\t0.2845577791213847\n" +
-                        "HYRX\t0.8423410920883345\n" +
-                        "NRXG\t0.3491070363730514\n",
-                "SELECT k, sum(val) FROM tab LIMIT 3;",
+                        "BBBE\t0.7453598685393461\n" +
+                        "BBBI\t0.7394866029725212\n" +
+                        "BBBK\t1.370328208214273\n",
+                "SELECT k, sum(val) FROM tab ORDER BY k LIMIT 3;",
                 "CREATE TABLE tab AS (SELECT rnd_str(4, 4, 0) k, rnd_double() val FROM long_sequence(100000));",
                 null,
                 true,
-                false
+                true
         );
     }
 
