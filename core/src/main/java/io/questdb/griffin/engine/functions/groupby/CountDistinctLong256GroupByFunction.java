ed4f9ddd5af2fa189b3e6b4b6181839826c5e432
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-01-17 12:20:40 +0000

    Addition of CountDistinctLong256GroupByFunction
    Currently debugging issues with the tests. Added to_long256 to help to align the tests across the long/long128/long256 count distinct groupby functions. Inner expression aren't parsing as expected.

--- core/src/main/java/io/questdb/griffin/engine/functions/groupby/CountDistinctLong256GroupByFunction.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/groupby/CountDistinctLong256GroupByFunction.java
@@ -32,59 +32,65 @@ import io.questdb.cairo.sql.Record;
 import io.questdb.griffin.engine.functions.GroupByFunction;
 import io.questdb.griffin.engine.functions.LongFunction;
 import io.questdb.griffin.engine.functions.UnaryFunction;
+import io.questdb.griffin.engine.groupby.GroupByAllocator;
+import io.questdb.griffin.engine.groupby.GroupByLong128HashSet;
+import io.questdb.griffin.engine.groupby.GroupByLong256HashSet;
 import io.questdb.std.*;
 
 public class CountDistinctLong256GroupByFunction extends LongFunction implements UnaryFunction, GroupByFunction {
     private final Function arg;
-    private final int setInitialCapacity;
-    private final double setLoadFactor;
-    private final ObjList<Long256HashSet> sets = new ObjList<>();
-    private int setIndex;
+    private final GroupByLong256HashSet setA;
+    private final GroupByLong256HashSet setB;
     private int valueIndex;
 
     public CountDistinctLong256GroupByFunction(Function arg, int setInitialCapacity, double setLoadFactor) {
         this.arg = arg;
-        this.setInitialCapacity = setInitialCapacity;
-        this.setLoadFactor = setLoadFactor;
+        // We use zero as the default value to speed up zeroing on rehash.
+        setA = new GroupByLong256HashSet(setInitialCapacity, setLoadFactor, 0);
+        setB = new GroupByLong256HashSet(setInitialCapacity, setLoadFactor, 0);
     }
 
     @Override
     public void clear() {
-        sets.clear();
-        setIndex = 0;
+        setA.resetPtr();
+        setB.resetPtr();
     }
 
     @Override
     public void computeFirst(MapValue mapValue, Record record) {
-        final Long256HashSet set;
-        if (sets.size() <= setIndex) {
-            sets.extendAndSet(setIndex, set = new Long256HashSet(setInitialCapacity, setLoadFactor));
-        } else {
-            set = sets.getQuick(setIndex);
-            set.clear();
-        }
+        final Long256 l256 = arg.getLong256A(record);
 
-        Long256 val = arg.getLong256A(record);
-        if (isNotNull(val)) {
-            set.add(val.getLong0(), val.getLong1(), val.getLong2(), val.getLong3());
+        if (isNotNull(l256)) {
             mapValue.putLong(valueIndex, 1L);
+            long l0 = l256.getLong0() == 0 ? Numbers.LONG_NaN : l256.getLong0();
+            long l1 = l256.getLong1() == 0 ? Numbers.LONG_NaN : l256.getLong1();
+            long l2 = l256.getLong2() == 0 ? Numbers.LONG_NaN : l256.getLong2();
+            long l3 = l256.getLong3() == 0 ? Numbers.LONG_NaN : l256.getLong3();
+            setA.of(0).add(l0, l1, l2, l3);
+            mapValue.putLong(valueIndex + 1, setA.ptr());
         } else {
-            mapValue.putLong(valueIndex, 0L);
+            mapValue.putLong(valueIndex, 0);
+            mapValue.putLong(valueIndex + 1, 0);;
         }
-        mapValue.putInt(valueIndex + 1, setIndex++);
     }
 
     @Override
     public void computeNext(MapValue mapValue, Record record) {
-        final Long256HashSet set = sets.getQuick(mapValue.getInt(valueIndex + 1));
-        final Long256 val = arg.getLong256A(record);
-        if (isNotNull(val)) {
-            final int index = set.keyIndex(val.getLong0(), val.getLong1(), val.getLong2(), val.getLong3());
-            if (index < 0) {
-                return;
+        final Long256 l256 = arg.getLong256A(record);
+
+        if (isNotNull(l256)) {
+            long ptr = mapValue.getLong(valueIndex + 1);
+            mapValue.putLong(valueIndex, 1L);
+            long l0 = l256.getLong0() == 0 ? Numbers.LONG_NaN : l256.getLong0();
+            long l1 = l256.getLong1() == 0 ? Numbers.LONG_NaN : l256.getLong1();
+            long l2 = l256.getLong2() == 0 ? Numbers.LONG_NaN : l256.getLong2();
+            long l3 = l256.getLong3() == 0 ? Numbers.LONG_NaN : l256.getLong3();
+            final int index = setA.of(ptr).keyIndex(l0, l1, l2, l3);
+            if (index >= 0) {
+                setA.addAt(index, l0, l1, l2, l3);
+                mapValue.addLong(valueIndex, 1);
+                mapValue.putLong(valueIndex + 1, setA.ptr());
             }
-            set.addAt(index, val.getLong0(), val.getLong1(), val.getLong2(), val.getLong3());
-            mapValue.addLong(valueIndex, 1);
         }
     }
 
@@ -115,12 +121,43 @@ public class CountDistinctLong256GroupByFunction extends LongFunction implements
 
     @Override
     public boolean isParallelismSupported() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isReadThreadSafe() {
         return false;
+    }
+
+    @Override
+    public void merge(MapValue destValue, MapValue srcValue) {
+        long srcCount = srcValue.getLong(valueIndex);
+        if (srcCount == 0 || srcCount == Numbers.LONG_NaN) {
+            return;
+        }
+        long srcPtr = srcValue.getLong(valueIndex + 1);
+
+        long destCount = destValue.getLong(valueIndex);
+        if (destCount == 0 || destCount == Numbers.LONG_NaN) {
+            destValue.putLong(valueIndex, srcCount);
+            destValue.putLong(valueIndex + 1, srcPtr);
+            return;
+        }
+        long destPtr = destValue.getLong(valueIndex + 1);
+
+        setA.of(destPtr);
+        setB.of(srcPtr);
+
+        if (setA.size() > (setB.size() >> 1)) {
+            setA.merge(setB);
+            destValue.putLong(valueIndex, setA.size());
+            destValue.putLong(valueIndex + 1, setA.ptr());
+        } else {
+            // Set A is significantly smaller than set B, so we merge it into set B.
+            setB.merge(setA);
+            destValue.putLong(valueIndex, setB.size());
+            destValue.putLong(valueIndex + 1, setB.ptr());
+        }
     }
 
     @Override
@@ -131,18 +168,27 @@ public class CountDistinctLong256GroupByFunction extends LongFunction implements
     }
 
     @Override
+    public void setAllocator(GroupByAllocator allocator) {
+        setA.setAllocator(allocator);
+        setB.setAllocator(allocator);
+    }
+
+    @Override
     public void setEmpty(MapValue mapValue) {
         mapValue.putLong(valueIndex, 0L);
+        mapValue.putLong(valueIndex + 1, 0);
     }
 
     @Override
     public void setLong(MapValue mapValue, long value) {
         mapValue.putLong(valueIndex, value);
+        mapValue.putLong(valueIndex + 1, 0);
     }
 
     @Override
     public void setNull(MapValue mapValue) {
         mapValue.putLong(valueIndex, Numbers.LONG_NaN);
+        mapValue.putLong(valueIndex + 1, 0);
     }
 
     @Override
@@ -153,7 +199,6 @@ public class CountDistinctLong256GroupByFunction extends LongFunction implements
     @Override
     public void toTop() {
         UnaryFunction.super.toTop();
-        setIndex = 0;
     }
 
     private static boolean isNotNull(Long256 value) {
