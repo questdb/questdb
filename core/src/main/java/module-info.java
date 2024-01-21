00894ad518f0980ab3f36bc34176c7fb83eb0d5f
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-20 23:54:15 -0800

    Update module-info.java

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -25,7 +25,6 @@
 import io.questdb.griffin.FunctionFactory;
 
 open module io.questdb {
-    requires transitive jdk.unsupported;
     requires static org.jetbrains.annotations;
     requires static java.management;
 
@@ -35,64 +34,33 @@ open module io.questdb {
     exports io.questdb.cairo.vm;
     exports io.questdb.cairo.map;
     exports io.questdb.cairo.sql;
-    exports io.questdb.cairo.pool;
-    exports io.questdb.cairo.pool.ex;
     exports io.questdb.cairo.security;
 
     exports io.questdb.cutlass;
-    exports io.questdb.cutlass.http;
-    exports io.questdb.cutlass.http.processors;
-    exports io.questdb.cutlass.http.ex;
-    exports io.questdb.cutlass.json;
     exports io.questdb.cutlass.line;
-    exports io.questdb.cutlass.line.udp;
     exports io.questdb.cutlass.line.tcp;
-    exports io.questdb.cutlass.pgwire;
-    exports io.questdb.cutlass.text;
-    exports io.questdb.cutlass.text.types;
 
     exports io.questdb.griffin;
-    exports io.questdb.griffin.engine;
-    exports io.questdb.griffin.model;
     exports io.questdb.griffin.engine.functions;
-    exports io.questdb.griffin.engine.functions.rnd;
-    exports io.questdb.griffin.engine.functions.bind;
-    exports io.questdb.griffin.engine.functions.bool;
-    exports io.questdb.griffin.engine.functions.cast;
-    exports io.questdb.griffin.engine.functions.catalogue;
-    exports io.questdb.griffin.engine.functions.columns;
     exports io.questdb.griffin.engine.functions.conditional;
     exports io.questdb.griffin.engine.functions.constants;
-    exports io.questdb.griffin.engine.functions.date;
-    exports io.questdb.griffin.engine.functions.eq;
-    exports io.questdb.griffin.engine.functions.groupby;
-    exports io.questdb.griffin.engine.functions.lt;
-    exports io.questdb.griffin.engine.functions.math;
-    exports io.questdb.griffin.engine.functions.regex;
-    exports io.questdb.griffin.engine.functions.str;
     exports io.questdb.griffin.engine.functions.test;
     exports io.questdb.griffin.engine.functions.geohash;
     exports io.questdb.griffin.engine.functions.bin;
     exports io.questdb.griffin.engine.functions.lock;
     exports io.questdb.griffin.engine.functions.window;
     exports io.questdb.griffin.engine.functions.table;
-    exports io.questdb.griffin.engine.groupby;
-    exports io.questdb.griffin.engine.groupby.vect;
     exports io.questdb.griffin.engine.orderby;
     exports io.questdb.griffin.engine.window;
     exports io.questdb.griffin.engine.table;
     exports io.questdb.jit;
     exports io.questdb.std;
-    exports io.questdb.std.datetime;
     exports io.questdb.std.datetime.microtime;
-    exports io.questdb.std.datetime.millitime;
     exports io.questdb.std.str;
-    exports io.questdb.std.ex;
     exports io.questdb.std.fastdouble;
     exports io.questdb.network;
     exports io.questdb.log;
     exports io.questdb.mp;
-    exports io.questdb.tasks;
     exports io.questdb.metrics;
     exports io.questdb.cairo.vm.api;
     exports io.questdb.cairo.mig;
