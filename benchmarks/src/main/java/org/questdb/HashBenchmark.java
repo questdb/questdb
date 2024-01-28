9adb3e7e056f9c05f3232fb2f7d924c739178470
Andrey Pechkurov <apechkurov@gmail.com>
2024-01-28 13:04:01 +0200

    Add hash function benchmark

---
+++ benchmarks/src/main/java/org/questdb/HashBenchmark.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2023 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.questdb;
+
+import io.questdb.std.Hash;
+import io.questdb.std.MemoryTag;
+import io.questdb.std.Unsafe;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.CRC32C;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class HashBenchmark {
+    private final CRC32C crc = new CRC32C();
+    @Param({"16", "22", "64", "72", "128", "134", "1024", "1034"})
+    public long len;
+    private ByteBuffer buf = ByteBuffer.allocateDirect((int) len);
+    private long mem = Unsafe.malloc(len, MemoryTag.NATIVE_DEFAULT);
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(HashBenchmark.class.getSimpleName())
+                .warmupIterations(3)
+                .measurementIterations(3)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Setup(Level.Trial)
+    public void setup() {
+        mem = Unsafe.malloc(len, MemoryTag.NATIVE_DEFAULT);
+        buf = ByteBuffer.allocateDirect((int) len);
+        for (int i = 0; i < len; i++) {
+            Unsafe.getUnsafe().putByte(mem + i, (byte) i);
+            buf.put((byte) i);
+        }
+    }
+
+    @Benchmark
+    public int testCrc32c() {
+        buf.flip();
+        crc.reset();
+        crc.update(buf);
+        return (int) crc.getValue();
+    }
+
+    @Benchmark
+    public int testHashMem32() {
+        return Hash.hashMem32(mem, len);
+    }
+}
