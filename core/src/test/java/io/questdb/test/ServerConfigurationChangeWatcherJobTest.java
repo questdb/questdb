bc753ab46ae3c50f27ad5a21a8e1256b1d30ab6b
Steven Sklar <steven@questdb.io>
2024-02-05 17:58:38 -0500

    job test works

--- core/src/test/java/io/questdb/test/ServerConfigurationChangeWatcherJobTest.java
+++ core/src/test/java/io/questdb/test/ServerConfigurationChangeWatcherJobTest.java
@@ -25,20 +25,29 @@
 package io.questdb.test;
 
 import io.questdb.BuildInformationHolder;
+import io.questdb.PropertyKey;
 import io.questdb.ReloadingPropServerConfiguration;
 import io.questdb.ServerConfigurationChangeWatcherJob;
 import io.questdb.log.Log;
 import io.questdb.log.LogFactory;
+import io.questdb.mp.SOCountDownLatch;
 import io.questdb.mp.SynchronizedJob;
 import io.questdb.mp.WorkerPool;
+import io.questdb.std.Os;
 import io.questdb.test.mp.TestWorkerPool;
+import io.questdb.test.tools.TestUtils;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerConfigurationChangeWatcherJobTest {
     @Rule
@@ -52,9 +61,70 @@ public class ServerConfigurationChangeWatcherJobTest {
             writer.println("");
         }
 
+        try (PrintWriter writer = new PrintWriter(temp.newFile("conf/server.conf"), Charset.defaultCharset())) {
+            writer.print(PropertyKey.HTTP_CONNECTION_POOL_INITIAL_CAPACITY);
+            writer.print("=");
+            writer.print(99);
+        }
+
         ReloadingPropServerConfiguration config = new ReloadingPropServerConfiguration(
             temp.getRoot().getAbsolutePath(), new Properties(), null, ServerConfigurationChangeWatcherJobTest.LOG, new BuildInformationHolder()
         );
+
+        try (ServerConfigurationChangeWatcherJob job = new ServerConfigurationChangeWatcherJob(config)) {
+            int concurrencyLevel = 4;
+            AtomicInteger values = new AtomicInteger(10);
+
+            CyclicBarrier startBarrier = new CyclicBarrier(concurrencyLevel + 1);
+            CyclicBarrier valueBarrier = new CyclicBarrier(concurrencyLevel + 1);
+            SOCountDownLatch endLatch = new SOCountDownLatch(concurrencyLevel);
+
+            new Thread(() -> {
+                TestUtils.await(startBarrier);
+                while (values.get() >= 0) {
+                    try (PrintWriter writer = new PrintWriter(Paths.get(temp.getRoot().getAbsolutePath(), "conf/server.conf").toString())) {
+                        writer.print(PropertyKey.HTTP_CONNECTION_POOL_INITIAL_CAPACITY);
+                        writer.print("=");
+                        writer.print(values.decrementAndGet());
+                    } catch (IOException e) {
+                        // If there is an error in writing the file, end the test
+                        endLatch.setCount(0);
+                       Assert.fail(e.getMessage());
+                    }
+
+                    Assert.assertTrue(job.run(1));
+                    TestUtils.await(valueBarrier);
+                }
+            }).start();
+
+            for (int i = 0; i < concurrencyLevel; i++) {
+                new Thread(() -> {
+                    TestUtils.await(startBarrier);
+                    try {
+                        while (true) {
+                            if (config.getHttpServerConfiguration().getHttpContextConfiguration().getConnectionPoolInitialCapacity() == values.get()) {
+                                TestUtils.await(valueBarrier);
+                            }
+                            Os.pause();
+                            if (values.get() == -1) {
+                                break;
+                            }
+                        }
+                    }
+                    finally {
+                        endLatch.countDown();
+                    }
+                }).start();
+            }
+            endLatch.await();
+
+        }
+
+
+
+
+
+
 
 
 
