39e422e75c4b62f6d30398571afb8269d77007ee
Steven Sklar <steven@questdb.io>
2024-02-05 12:45:36 -0500

    adds more filewatcher tests

--- core/src/test/java/io/questdb/test/std/FilewatcherTest.java
+++ core/src/test/java/io/questdb/test/std/FilewatcherTest.java
@@ -8,7 +8,10 @@ import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.junit.Test;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class FilewatcherTest {
     @Rule
@@ -24,6 +27,38 @@ public class FilewatcherTest {
 
             Assert.assertNotEquals(0, addr);
             Filewatcher.teardown(addr);
+        }
+    }
+
+    @Test
+    public void testFilewatcherChanged() throws Exception {
+
+        final File targetFile = temp.newFile();
+
+        try (Path path = new Path()) {
+            path.of(targetFile.getAbsolutePath()).$();
+            final long addr = Filewatcher.setup(path.ptr());
+
+            Assert.assertNotEquals(0, addr);
+            Assert.assertFalse(Filewatcher.changed(addr));
+
+            try (PrintWriter writer = new PrintWriter(path.toString(), StandardCharsets.UTF_8)) {
+                writer.println("hello");
+            }
+
+            Assert.assertTrue(Filewatcher.changed(addr));
+            Assert.assertFalse(Filewatcher.changed(addr));
+            Assert.assertFalse(Filewatcher.changed(addr));
+
+            try (PrintWriter writer = new PrintWriter(path.toString(), StandardCharsets.UTF_8)) {
+                writer.println("hello again ");
+            }
+
+            Assert.assertTrue(Filewatcher.changed(addr));
+
+
+            Filewatcher.teardown(addr);
+
         }
     }
 
