5a8c3c98c1eedc069c037a610c6c125853116568
Andrey Pechkurov <apechkurov@gmail.com>
2024-02-20 14:15:49 +0200

    chore(core): remove buffering for non-chunked responses in HttpClient

--- core/src/test/java/io/questdb/test/cutlass/http/SettingsEndpointTest.java
+++ core/src/test/java/io/questdb/test/cutlass/http/SettingsEndpointTest.java
@@ -2,11 +2,11 @@ package io.questdb.test.cutlass.http;
 
 import io.questdb.DefaultHttpClientConfiguration;
 import io.questdb.ServerMain;
-import io.questdb.cutlass.http.client.Chunk;
-import io.questdb.cutlass.http.client.ChunkedResponse;
+import io.questdb.cutlass.http.client.Fragment;
 import io.questdb.cutlass.http.client.HttpClient;
 import io.questdb.cutlass.http.client.HttpClientFactory;
-import io.questdb.std.str.StringSink;
+import io.questdb.cutlass.http.client.Response;
+import io.questdb.std.str.Utf8StringSink;
 import io.questdb.std.str.Utf8s;
 import io.questdb.test.AbstractBootstrapTest;
 import io.questdb.test.tools.TestUtils;
@@ -41,20 +41,20 @@ public class SettingsEndpointTest extends AbstractBootstrapTest {
     ) {
         final HttpClient.Request request = httpClient.newRequest("localhost", HTTP_PORT);
         request.GET().url("/settings");
-        try (HttpClient.ResponseHeaders response = request.send()) {
-            response.await();
+        try (HttpClient.ResponseHeaders responseHeaders = request.send()) {
+            responseHeaders.await();
 
-            TestUtils.assertEquals(String.valueOf(200), response.getStatusCode());
+            TestUtils.assertEquals(String.valueOf(200), responseHeaders.getStatusCode());
 
-            final StringSink sink = new StringSink();
+            final Utf8StringSink sink = new Utf8StringSink();
 
-            Chunk chunk;
-            final ChunkedResponse chunkedResponse = response.getChunkedResponse();
-            while ((chunk = chunkedResponse.recv()) != null) {
-                Utf8s.utf8ToUtf16(chunk.lo(), chunk.hi(), sink);
+            Fragment fragment;
+            final Response response = responseHeaders.getResponse();
+            while ((fragment = response.recv()) != null) {
+                Utf8s.strCpy(fragment.lo(), fragment.hi(), sink);
             }
 
-            TestUtils.assertEquals(expectedHttpResponse, sink);
+            TestUtils.assertEquals(expectedHttpResponse, sink.toString());
             sink.clear();
         }
     }
