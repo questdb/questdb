81cddeba6c40151fcd2910b30c3aa740b106912a
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-03-15 12:01:53 +0100

    superflovous config option removed

--- examples/src/main/java/com/example/sender/AuthTlsExample.java
+++ examples/src/main/java/com/example/sender/AuthTlsExample.java
@@ -4,7 +4,7 @@ import io.questdb.client.Sender;
 
 public class AuthTlsExample {
     public static void main(String[] args) {
-        try (Sender sender = Sender.fromConfig("tcps::addr=clever-black-363-c1213c97.ilp.b04c.questdb.net:32074;tls=true;user=admin;token=GwBXoGG5c6NoUTLXnzMxw_uNiVa8PKobzx5EiuylMW0;")) {
+        try (Sender sender = Sender.fromConfig("tcps::addr=clever-black-363-c1213c97.ilp.b04c.questdb.net:32074;user=admin;token=GwBXoGG5c6NoUTLXnzMxw_uNiVa8PKobzx5EiuylMW0;")) {
             sender.table("inventors")
                     .symbol("born", "Austrian Empire")
                     .longColumn("id", 0)
