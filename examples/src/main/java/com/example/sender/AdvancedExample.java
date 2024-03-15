554071329ecb32c1d7cc4a1586c1270b03de3984
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-03-15 10:55:35 +0100

    replace Examples with HTTP-only examples

--- examples/src/main/java/com/example/sender/AdvancedExample.java
+++ examples/src/main/java/com/example/sender/AdvancedExample.java
@@ -29,9 +29,14 @@ import io.questdb.client.Sender;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
-public class HttpsExample {
+public class AdvancedExample {
     public static void main(String[] args) {
-        try (Sender sender = Sender.fromConfig("https::addr=localhost:9000;")) {
+        // https::addr=localhost:9000 - Sends data over HTTP transport to the server at localhost:9000 with TLS enabled.
+        // tls_verify=unsafe_off - Disables SSL verification. Do not use this in production!
+        // retry_timeout=20000 - Sets timeout for retries to 20 seconds. This means recoverable errors will be retried for 20 seconds.
+        // auto_flush_rows=100000 - Sets maximum number of rows to be sent in a single request to 100,000.
+        // auto_flush_interval=5000 - Sets maximum interval between requests to 5 seconds - to avoid long lags in low-throughput scenarios.
+        try (Sender sender = Sender.fromConfig("https::addr=localhost:9000;tls_verify=unsafe_off;retry_timeout=20000;auto_flush_rows=100000;auto_flush_interval=5000;")) {
             sender.table("inventors")
                     .symbol("born", "Austrian Empire")
                     .timestampColumn("birthday", Instant.parse("1856-07-10T00:00:00.00Z"))
