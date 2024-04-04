00aab5b3d10e88a20871df7c9991051b1bbb23c8
Steve Sklar <steven@questdb.io>
2024-04-04 12:42:09 -0400

    its alive!

--- core/src/main/c/osx/filewatcher.c
+++ core/src/main/c/osx/filewatcher.c
@@ -24,6 +24,14 @@
 
 #include <CoreServices/CoreServices.h>
 #include <libgen.h>
+#include <sys/stat.h>
+
+struct file_watcher_context
+{
+    char *filePath;
+    time_t lastChangedSec;
+    long lastChangedNsec;
+};
 
 void myCallback(
     ConstFSEventStreamRef streamRef,
@@ -37,13 +45,33 @@ void myCallback(
     char **paths = eventPaths;
     printf("Callback called\n");
 
-    char *fileName = (char *)context;
-    printf("fileName %s", fileName);
+    struct file_watcher_context *ctx = (struct file_watcher_context *)context;
+
+    printf("filePath %s\n", ctx->filePath);
     for (i = 0; i < numEvents; i++)
     {
         int count;
         /* flags are unsigned long, IDs are uint64_t */
         printf("Change %llu in %s, flags %lu\n", eventIds[i], paths[i], eventFlags[i]);
+
+        struct stat confStat;
+        printf("stat %d\n", stat(ctx->filePath, &confStat));
+        printf("%ld\n", confStat.st_mtimespec.tv_sec);
+
+        if (confStat.st_mtimespec.tv_sec > ctx->lastChangedSec)
+        {
+            printf("changed!\n");
+            ctx->lastChangedSec = confStat.st_mtimespec.tv_sec;
+            ctx->lastChangedNsec = confStat.st_mtimespec.tv_nsec;
+        }
+
+        if (confStat.st_mtimespec.tv_sec == ctx->lastChangedSec &&
+            confStat.st_mtimespec.tv_nsec > ctx->lastChangedNsec)
+        {
+            printf("changed!\n");
+            ctx->lastChangedSec = confStat.st_mtimespec.tv_sec;
+            ctx->lastChangedNsec = confStat.st_mtimespec.tv_nsec;
+        }
     }
 }
 
@@ -56,8 +84,18 @@ int main(int argc, char *argv[])
     CFStringRef confDir = CFStringCreateWithCString(NULL, dirname(confPath), kCFStringEncodingUTF8);
     CFArrayRef pathsToWatch = CFArrayCreate(NULL, (const void **)&confDir, 1, NULL);
 
+    /* Init context and data */
+    uintptr_t ctxPtr = malloc(sizeof(struct file_watcher_context));
+    struct file_watcher_context *ctx = (struct file_watcher_context *)ctxPtr;
+    ctx->filePath = confPath;
+    printf("confPath %s\n", ctx->filePath);
+    struct stat confStat;
+    printf("stat %d\n", stat(ctx->filePath, &confStat));
+    ctx->lastChangedSec = confStat.st_mtimespec.tv_sec;
+    ctx->lastChangedNsec = confStat.st_mtimespec.tv_nsec;
+
     struct FSEventStreamContext context;
-    context.info = basename(confPath);
+    context.info = &ctx;
 
     FSEventStreamRef stream;
     CFAbsoluteTime latency = 3.0; // latency in seconds
