3170c94b100dd89e74f2240c124daad82efe7988
Marko Topolnik <marko.topolnik@gmail.com>
2024-04-18 13:36:17 +0200

    Fix wrong overload invocation

--- core/src/main/java/io/questdb/cutlass/line/tcp/LineTcpEventBuffer.java
+++ core/src/main/java/io/questdb/cutlass/line/tcp/LineTcpEventBuffer.java
@@ -218,7 +218,7 @@ public class LineTcpEventBuffer {
             if (!hasNonAsciiChars) {
                 tempSink.put(value);
             } else {
-                Utf8s.directUtf8ToUtf16(value, tempSink);
+                Utf8s.utf8ToUtf16(value, tempSink);
             }
             final int length = tempSink.length();
             Unsafe.getUnsafe().putByte(address, LineTcpParser.ENTITY_TYPE_TAG);
