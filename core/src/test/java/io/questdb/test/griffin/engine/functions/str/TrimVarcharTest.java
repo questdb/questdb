a454f886c2169b55543447198b7a50740c71a30b
Marko Topolnik <marko.topolnik@gmail.com>
2024-04-19 13:39:26 +0200

    Add integration trim varchar test

---
+++ core/src/test/java/io/questdb/test/griffin/engine/functions/str/TrimVarcharTest.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2024 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.test.griffin.engine.functions.str;
+
+import io.questdb.test.AbstractCairoTest;
+import org.junit.Test;
+
+public class TrimVarcharTest extends AbstractCairoTest {
+
+    @Test
+    public void testLtrim() throws Exception {
+        assertQuery(
+                "k\tltrim\n" +
+                        "  abc\tabc\n" +
+                        "  abc\tabc\n" +
+                        "abc  \tabc  \n" +
+                        "   \t\n" +
+                        "   \t\n",
+                "select k, ltrim(k) from x",
+                "create table x as (select rnd_varchar('  abc', 'abc  ', '   ') k from long_sequence(5))",
+                null,
+                true,
+                true
+        );
+    }
+
+    @Test
+    public void testRtrim() throws Exception {
+        assertQuery(
+                "k\trtrim\n" +
+                        "  abc\t  abc\n" +
+                        "  abc\t  abc\n" +
+                        "abc  \tabc\n" +
+                        "   \t\n" +
+                        "   \t\n",
+                "select k, rtrim(k) from x",
+                "create table x as (select rnd_varchar('  abc', 'abc  ', '   ') k from long_sequence(5))",
+                null,
+                true,
+                true
+        );
+    }
+
+    @Test
+    public void testTrim() throws Exception {
+        assertQuery(
+                "k\ttrim\n" +
+                        "  abc\tabc\n" +
+                        "  abc  \tabc\n" +
+                        "abc  \tabc\n" +
+                        "   \t\n" +
+                        "abc  \tabc\n",
+                "select k, trim(k) from x",
+                "create table x as (select rnd_varchar('  abc', 'abc  ', '  abc  ', '   ') k from long_sequence(5))",
+                null,
+                true,
+                true
+        );
+    }
+}
