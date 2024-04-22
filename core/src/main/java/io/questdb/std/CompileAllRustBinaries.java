ff74b0db099554c613b8e7f18bfc2c3bab91bd5d
Adam Cimarosti <cimarosti@gmail.com>
2024-04-22 17:34:08 +0200

    updated compile all rust binaries script

--- core/src/main/java/io/questdb/std/CompileAllRustBinaries.java
+++ core/src/main/java/io/questdb/std/CompileAllRustBinaries.java
@@ -31,58 +31,14 @@ import java.nio.file.Files;
 
 public class CompileAllRustBinaries {
     public static void main(String[] args) {
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        // Print the current working directory to stderr
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> CWD >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(System.getProperty("user.dir"));
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> CWD >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        // Print all system properties to stderr
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> PROPS >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.getProperties().forEach((k, v) -> System.err.println(k + ": " + v));
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> PROPS >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        // Print all args to stderr
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> ARGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        for (String arg : args) {
-            System.err.println(arg);
-        }
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> ARGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        Path path = Paths.get(args[0]);
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> PATH >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(path);
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> PATH >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        gatherAllRustBinaries(path);
-
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> AAAA >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-    }
-
-    private static void gatherAllRustBinaries(Path path) {
-        // Create a "hello.txt" file with contents "world" as a child of the given path
-        Path hello = path.resolve("hello.txt");
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> HELLO >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.err.println(hello);
-        System.err.println(">>>>>>>>>>>>>>>>>>>>>>>> HELLO >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        System.err.println(">>>> compiling all rust binaries >>>>");
+        Path destPath = Paths.get(args[0]);
+        Path hello = destPath.resolve("hello.txt");
         try {
             Files.writeString(hello, "world\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
+        System.err.println("<<<< compiling all rust binaries <<<<");
     }
 }
