9d4b2c0b8e7c2ca836a0e9d9a71b736038618a4b
Steven Sklar <steven@questdb.io>
2024-04-30 17:51:18 -0400

    starting to rework inotify

--- core/src/main/java/io/questdb/InotifyFileEventNotifier.java
+++ core/src/main/java/io/questdb/InotifyFileEventNotifier.java
@@ -1,56 +1,72 @@
 package io.questdb;
 
+import io.questdb.std.Os;
 import io.questdb.std.str.Path;
+
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public final class InotifyFileEventNotifier implements FileEventNotifier {
 
-    private final long filewatcherPtr;
-    private boolean closed;
+    private final AtomicBoolean closed = new AtomicBoolean();
+    private final Path dirPath;
+    private final long fd;
+    private final CharSequence filePath;
+    private final long wd;
 
 
-    public InotifyFileEventNotifier(CharSequence dirPath) throws FileEventNotifierException {
-        try (Path p = new Path()) {
-            p.of(dirPath).$();
-            this.filewatcherPtr = setup(p.ptr());
-            if (this.filewatcherPtr < 0) {
-                throw new FileEventNotifierException("error setting up event notifier");
-            }
-
-            assert this.filewatcherPtr != 0;
+    public InotifyFileEventNotifier(CharSequence filePath) throws FileEventNotifierException {
+        Os.init();
+        this.fd = InotifyAccessor.inotifyInit();
+        if (this.fd < 0) {
+            throw new FileEventNotifierException("inotify_init");
         }
+
+        this.filePath = filePath;
+        this.dirPath = new Path();
+        this.dirPath.of(Paths.get(filePath.toString()).getParent().toString()).$();
+
+        this.wd = InotifyAccessor.inotifyAddWatch(
+                this.fd,
+                this.dirPath.ptr(),
+                InotifyAccessor.IN_CREATE | InotifyAccessor.IN_MODIFY |
+                        InotifyAccessor.IN_MOVED_TO | InotifyAccessor.IN_CLOSE_WRITE
+        );
+
+        if (this.wd < 0) {
+            throw new FileEventNotifierException("inotify_add_watch");
+        }
+    }
+
+    public static void main(String[] args) {
+        try (InotifyFileEventNotifier n = new InotifyFileEventNotifier("/home/steven/qdbtmp")) {
+            n.waitForChange(() -> System.out.println("hi"));
+        } catch (FileEventNotifierException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 
     @Override
     public void close() {
-        if (!closed) {
-            closed = true;
-            if (filewatcherPtr > 0) {
-                teardown(filewatcherPtr);
-            }
+        if (closed.compareAndSet(false, true)) {
+            this.dirPath.close();
+            InotifyAccessor.inotifyRmWatch(this.fd, this.wd);
+            InotifyAccessor.closeFd(this.fd);
         }
+
+
     }
 
     @Override
-    public void waitForChange(FileEventCallback callback) throws FileEventNotifierException {
-        long result;
-
-        if (filewatcherPtr < 0) {
-            throw new FileEventNotifierException("filewatcher was not successfully set up");
-        }
-
-        result = waitForChange(filewatcherPtr);
-        if (closed) {
+    public void waitForChange(FileEventCallback callback) {
+        if (closed.get()) {
             return;
         }
-        if (result < 0) {
-            throw new FileEventNotifierException("inotify read", result);
-        }
-        callback.onFileEvent();
+        // do epoll stuff
+        // then callback.onFileEvent();
     }
 
-    static native long setup(long path);
-
-    static native void teardown(long address);
-
-    static native long waitForChange(long address);
 }
+
