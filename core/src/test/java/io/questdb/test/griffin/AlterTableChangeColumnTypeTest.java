9dd3c7016091f8442e5192e697f003eee56bbc9d
ideoma <2159629+ideoma@users.noreply.github.com>
2024-05-20 09:22:54 +0100

    cleanup

--- core/src/test/java/io/questdb/test/griffin/AlterTableChangeColumnTypeTest.java
+++ core/src/test/java/io/questdb/test/griffin/AlterTableChangeColumnTypeTest.java
@@ -790,23 +790,25 @@ public class AlterTableChangeColumnTypeTest extends AbstractCairoTest {
     }
 
     @Test
-    public void testWalConversionFromVarToFixedDoesNotLeaveAuxFiles() throws SqlException {
+    public void testWalConversionFromVarToFixedDoesNotLeaveAuxFiles() throws Exception {
         assumeWal();
-        ddl("create table x (s string, timestamp timestamp) timestamp (timestamp) PARTITION BY HOUR WAL;");
-        ddl("alter table x alter column s type int;");
+        assertMemoryLeak(() -> {
+            ddl("create table x (s string, timestamp timestamp) timestamp (timestamp) PARTITION BY HOUR WAL;");
+            ddl("alter table x alter column s type int;");
 
-        TableToken xTbl = engine.verifyTableName("x");
+            TableToken xTbl = engine.verifyTableName("x");
 
-        Path path = Path.getThreadLocal(engine.getConfiguration().getRoot()).concat(xTbl).concat("wal1").concat("0").concat("s.d");
-        Assert.assertTrue(Files.exists(path));
+            Path path = Path.getThreadLocal(engine.getConfiguration().getRoot()).concat(xTbl).concat("wal1").concat("0").concat("s.d");
+            Assert.assertTrue(Files.exists(path));
 
-        path = Path.getThreadLocal(engine.getConfiguration().getRoot()).concat(xTbl).concat("wal1").concat("0").concat("s.i");
-        Assert.assertFalse(Files.exists(path));
+            path = Path.getThreadLocal(engine.getConfiguration().getRoot()).concat(xTbl).concat("wal1").concat("0").concat("s.i");
+            Assert.assertFalse(Files.exists(path));
 
-        insert("insert into x(s, timestamp) values(1, '2024-02-04T00:00:00.000Z')", sqlExecutionContext);
-        drainWalQueue();
+            insert("insert into x(s, timestamp) values(1, '2024-02-04T00:00:00.000Z')", sqlExecutionContext);
+            drainWalQueue();
 
-        assertSql("s\n1\n", "select s from x limit -1");
+            assertSql("s\n1\n", "select s from x limit -1");
+        });
     }
 
     @Test
