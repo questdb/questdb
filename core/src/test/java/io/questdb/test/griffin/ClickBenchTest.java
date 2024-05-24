6eb577f2f617ac3f08fd54e214bcdb201c99a487
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-05-24 11:45:53 +0100

    Fix test

--- core/src/test/java/io/questdb/test/griffin/ClickBenchTest.java
+++ core/src/test/java/io/questdb/test/griffin/ClickBenchTest.java
@@ -574,14 +574,13 @@ public class ClickBenchTest extends AbstractCairoTest {
             new TestCase(
                     "Q35",
                     "SELECT ClientIP, ClientIP - 1, ClientIP - 2, ClientIP - 3, COUNT(*) AS c FROM hits GROUP BY ClientIP, ClientIP - 1, ClientIP - 2, ClientIP - 3 ORDER BY c DESC LIMIT 10;",
-                    "Sort light lo: 10\n" +
-                            "  keys: [c desc]\n" +
-                            "    VirtualRecord\n" +
-                            "      functions: [ClientIP,column,column1,column2,c]\n" +
-                            "        Async Group By workers: 1\n" +
-                            "          keys: [ClientIP,column,column1,column2]\n" +
+                    "VirtualRecord\n" +
+                            "  functions: [ClientIP,ClientIP-1,ClientIP-2,ClientIP-3,c]\n" +
+                            "    Sort light lo: 10\n" +
+                            "      keys: [c desc]\n" +
+                            "        GroupBy vectorized: true workers: 1\n" +
+                            "          keys: [ClientIP]\n" +
                             "          values: [count(*)]\n" +
-                            "          filter: null\n" +
                             "            DataFrame\n" +
                             "                Row forward scan\n" +
                             "                Frame forward scan on: hits\n"
