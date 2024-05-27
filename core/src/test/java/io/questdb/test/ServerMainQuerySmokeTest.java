738437b6287247bc76f03f57b731834840913ce6
Andrey Pechkurov <apechkurov@gmail.com>
2024-05-27 10:27:04 +0300

    Add tests

--- core/src/test/java/io/questdb/test/ServerMainQuerySmokeTest.java
+++ core/src/test/java/io/questdb/test/ServerMainQuerySmokeTest.java
@@ -56,74 +56,11 @@ public class ServerMainQuerySmokeTest extends AbstractBootstrapTest {
                 PropertyKey.SHARED_WORKER_COUNT + "=4",
                 PropertyKey.PG_WORKER_COUNT + "=4",
                 PropertyKey.PG_SELECT_CACHE_ENABLED + "=true",
+                PropertyKey.CAIRO_SQL_PARALLEL_WORK_STEALING_THRESHOLD + "=1",
+                PropertyKey.CAIRO_SQL_PARALLEL_GROUPBY_SHARDING_THRESHOLD + "=100",
                 PropertyKey.DEBUG_ENABLE_TEST_FACTORIES + "=true"
         ));
         dbPath.parent().$();
-    }
-
-    @Test
-    public void testServerMainAsyncFilterSmokeTest() throws Exception {
-        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
-        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
-            serverMain.start();
-            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
-                try (Statement statement = conn.createStatement()) {
-                    statement.execute("create table x as (select x % 10000 l from long_sequence(1000000));");
-                }
-
-                String query = "select count() from (select * from x where l = 42);";
-                String expected = "count[BIGINT]\n" +
-                        "100\n";
-                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
-                    sink.clear();
-                    assertResultSet(expected, sink, rs);
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testServerMainAsyncGroupBySmokeTest1() throws Exception {
-        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
-        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
-            serverMain.start();
-            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
-                try (Statement statement = conn.createStatement()) {
-                    statement.execute("create table x as (select x % 10000 l1, x % 1000 l2 from long_sequence(1000000));");
-                }
-
-                String query = "select count_distinct(l1), count_distinct(l2) from x;";
-                String expected = "count_distinct[BIGINT],count_distinct1[BIGINT]\n" +
-                        "10000,1000\n";
-                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
-                    sink.clear();
-                    assertResultSet(expected, sink, rs);
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testServerMainAsyncGroupBySmokeTest2() throws Exception {
-        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
-        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
-            serverMain.start();
-            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
-                try (Statement statement = conn.createStatement()) {
-                    statement.execute("create table x as (select 'k' || (x % 3) k, x % 10000 l from long_sequence(1000000));");
-                }
-
-                String query = "select k, count_distinct(l) from x order by k;";
-                String expected = "k[VARCHAR],count_distinct[BIGINT]\n" +
-                        "k0,10000\n" +
-                        "k1,10000\n" +
-                        "k2,10000\n";
-                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
-                    sink.clear();
-                    assertResultSet(expected, sink, rs);
-                }
-            }
-        }
     }
 
     @Test
@@ -172,6 +109,226 @@ public class ServerMainQuerySmokeTest extends AbstractBootstrapTest {
                                 try (ResultSet rs = conn.prepareStatement(testCase.query).executeQuery()) {
                                     sink.clear();
                                     assertResultSet(testCase.expectedResult, sink, rs);
+                                }
+                            }
+                        }
+                    } catch (Throwable th) {
+                        errors.incrementAndGet();
+                        th.printStackTrace();
+                    } finally {
+                        doneLatch.countDown();
+                    }
+                }).start();
+            }
+
+            doneLatch.await();
+
+            Assert.assertEquals(0, errors.get());
+        }
+    }
+
+    @Test
+    public void testServerMainParallelFilterLoadTest() throws Exception {
+        testServerMainParallelQueryLoadTest(
+                "CREATE TABLE tab as (" +
+                        "  select (x * 864000000)::timestamp ts, ('k' || (x % 5))::symbol key, x:: double price, x::long quantity from long_sequence(10000)" +
+                        ") timestamp (ts) PARTITION BY DAY",
+                "SELECT * FROM tab WHERE key = 'k3' LIMIT 10",
+                "QUERY PLAN[VARCHAR]\n" +
+                        "Async JIT Filter workers: 4\n" +
+                        "  limit: 10\n" +
+                        "  filter: key='k3'\n" +
+                        "    DataFrame\n" +
+                        "        Row forward scan\n" +
+                        "        Frame forward scan on: tab\n",
+                "ts[TIMESTAMP],key[VARCHAR],price[DOUBLE],quantity[BIGINT]\n" +
+                        "1970-01-01 00:43:12.0,k3,3.0,3\n" +
+                        "1970-01-01 01:55:12.0,k3,8.0,8\n" +
+                        "1970-01-01 03:07:12.0,k3,13.0,13\n" +
+                        "1970-01-01 04:19:12.0,k3,18.0,18\n" +
+                        "1970-01-01 05:31:12.0,k3,23.0,23\n" +
+                        "1970-01-01 06:43:12.0,k3,28.0,28\n" +
+                        "1970-01-01 07:55:12.0,k3,33.0,33\n" +
+                        "1970-01-01 09:07:12.0,k3,38.0,38\n" +
+                        "1970-01-01 10:19:12.0,k3,43.0,43\n" +
+                        "1970-01-01 11:31:12.0,k3,48.0,48\n"
+        );
+    }
+
+    @Test
+    public void testServerMainParallelFilterSmokeTest() throws Exception {
+        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
+        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
+            serverMain.start();
+            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
+                try (Statement statement = conn.createStatement()) {
+                    statement.execute("create table x as (select x % 10000 l from long_sequence(1000000));");
+                }
+
+                String query = "select count() from (select * from x where l = 42);";
+                String expected = "count[BIGINT]\n" +
+                        "100\n";
+                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
+                    sink.clear();
+                    assertResultSet(expected, sink, rs);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testServerMainParallelGroupBySmokeTest1() throws Exception {
+        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
+        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
+            serverMain.start();
+            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
+                try (Statement statement = conn.createStatement()) {
+                    statement.execute("create table x as (select x % 10000 l1, x % 1000 l2 from long_sequence(1000000));");
+                }
+
+                String query = "select count_distinct(l1), count_distinct(l2) from x;";
+                String expected = "count_distinct[BIGINT],count_distinct1[BIGINT]\n" +
+                        "10000,1000\n";
+                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
+                    sink.clear();
+                    assertResultSet(expected, sink, rs);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testServerMainParallelGroupBySmokeTest2() throws Exception {
+        // Verify that circuit breaker checks don't have weird bugs unseen in fast tests.
+        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
+            serverMain.start();
+            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
+                try (Statement statement = conn.createStatement()) {
+                    statement.execute("create table x as (select 'k' || (x % 3) k, x % 10000 l from long_sequence(1000000));");
+                }
+
+                String query = "select k, count_distinct(l) from x order by k;";
+                String expected = "k[VARCHAR],count_distinct[BIGINT]\n" +
+                        "k0,10000\n" +
+                        "k1,10000\n" +
+                        "k2,10000\n";
+                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
+                    sink.clear();
+                    assertResultSet(expected, sink, rs);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testServerMainParallelKeyedRostiGroupByLoadTest() throws Exception {
+        testServerMainParallelQueryLoadTest(
+                "CREATE TABLE tab as (" +
+                        "  select (x * 864000000)::timestamp ts, ('k' || (x % 5))::symbol key, x:: double price, x::long quantity from long_sequence(10000)" +
+                        ") timestamp (ts) PARTITION BY DAY",
+                "SELECT key, min(quantity), max(quantity) FROM tab ORDER BY key DESC",
+                "QUERY PLAN[VARCHAR]\n" +
+                        "Sort light\n" +
+                        "  keys: [key desc]\n" +
+                        "    GroupBy vectorized: true workers: 4\n" +
+                        "      keys: [key]\n" +
+                        "      values: [min(quantity),max(quantity)]\n" +
+                        "        DataFrame\n" +
+                        "            Row forward scan\n" +
+                        "            Frame forward scan on: tab\n",
+                "key[VARCHAR],min[BIGINT],max[BIGINT]\n" +
+                        "k4,4,9999\n" +
+                        "k3,3,9998\n" +
+                        "k2,2,9997\n" +
+                        "k1,1,9996\n" +
+                        "k0,5,10000\n"
+        );
+    }
+
+    @Test
+    public void testServerMainParallelNonKeyedRostiGroupByLoadTest() throws Exception {
+        testServerMainParallelQueryLoadTest(
+                "CREATE TABLE tab as (" +
+                        "  select (x * 864000000)::timestamp ts, ('k' || (x % 5))::symbol key, x:: double price, x::long quantity from long_sequence(10000)" +
+                        ") timestamp (ts) PARTITION BY DAY",
+                "SELECT min(quantity), max(quantity) FROM tab",
+                "QUERY PLAN[VARCHAR]\n" +
+                        "GroupBy vectorized: true workers: 4\n" +
+                        "  values: [min(quantity),max(quantity)]\n" +
+                        "    DataFrame\n" +
+                        "        Row forward scan\n" +
+                        "        Frame forward scan on: tab\n",
+                "min[BIGINT],max[BIGINT]\n" +
+                        "1,10000\n"
+        );
+    }
+
+    @Test
+    public void testServerMainParallelShardedGroupByLoadTest() throws Exception {
+        testServerMainParallelQueryLoadTest(
+                "CREATE TABLE tab as (" +
+                        "  select (x * 864000000)::timestamp ts, ('k' || (x % 101))::symbol key, x:: double price, x::long quantity from long_sequence(10000)" +
+                        ") timestamp (ts) PARTITION BY DAY",
+                "SELECT day_of_week(ts) day, key, vwap(price, quantity) FROM tab GROUP BY day, key ORDER BY day, key LIMIT 10",
+                "QUERY PLAN[VARCHAR]\n" +
+                        "Sort light lo: 10\n" +
+                        "  keys: [day, key]\n" +
+                        "    VirtualRecord\n" +
+                        "      functions: [day,key,vwap]\n" +
+                        "        Async Group By workers: 4\n" +
+                        "          keys: [day,key]\n" +
+                        "          values: [vwap(price,quantity)]\n" +
+                        "          filter: null\n" +
+                        "            DataFrame\n" +
+                        "                Row forward scan\n" +
+                        "                Frame forward scan on: tab\n",
+                "day[INTEGER],key[VARCHAR],vwap[DOUBLE]\n" +
+                        "1,k0,6624.171717171717\n" +
+                        "1,k1,6624.8468153184685\n" +
+                        "1,k10,6612.932687914096\n" +
+                        "1,k100,6623.496749024707\n" +
+                        "1,k11,6613.610770065386\n" +
+                        "1,k12,6281.67243296272\n" +
+                        "1,k13,6598.965586726309\n" +
+                        "1,k14,6599.64534842185\n" +
+                        "1,k15,6600.325238210883\n" +
+                        "1,k16,6601.005256016568\n"
+        );
+    }
+
+    private void testServerMainParallelQueryLoadTest(String ddl, String query, String expectedPlan, String expectedResult) throws Exception {
+        // Here we're verifying that adaptive work stealing doesn't lead to deadlocks.
+        try (final ServerMain serverMain = new ServerMain(getServerMainArgs())) {
+            serverMain.start();
+
+            try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
+                try (Statement statement = conn.createStatement()) {
+                    statement.execute(ddl);
+                }
+
+                try (ResultSet rs = conn.prepareStatement("EXPLAIN " + query).executeQuery()) {
+                    sink.clear();
+                    assertResultSet(expectedPlan, sink, rs);
+                }
+            }
+
+            final int nThreads = 8;
+            final int nIterations = 100;
+
+            final CyclicBarrier startBarrier = new CyclicBarrier(nThreads);
+            final SOCountDownLatch doneLatch = new SOCountDownLatch(nThreads);
+            final AtomicInteger errors = new AtomicInteger();
+            for (int t = 0; t < nThreads; t++) {
+                new Thread(() -> {
+                    final StringSink sink = new StringSink();
+                    try {
+                        startBarrier.await();
+
+                        try (Connection conn = DriverManager.getConnection(PG_CONNECTION_URI, PG_CONNECTION_PROPERTIES)) {
+                            for (int i = 0; i < nIterations; i++) {
+                                try (ResultSet rs = conn.prepareStatement(query).executeQuery()) {
+                                    sink.clear();
+                                    assertResultSet(expectedResult, sink, rs);
                                 }
                             }
                         }
