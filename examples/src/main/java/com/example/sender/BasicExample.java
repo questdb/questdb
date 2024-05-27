08e644af215cf68c49e55c3ca54bf28c24b6e43e
ravisinghal033 <113414376+ravisinghal033@users.noreply.github.com>
2024-05-27 16:37:00 +0530

    Update BasicExample.java

--- examples/src/main/java/com/example/sender/BasicExample.java
+++ examples/src/main/java/com/example/sender/BasicExample.java
@@ -1,20 +1,22 @@
+import java.util.*;
 package com.example.sender;
-
 import io.questdb.client.Sender;
 
 public class BasicExample {
     public static void main(String[] args) {
         try (Sender sender = Sender.fromConfig("tcp::addr=localhost:9009;")) {
             sender.table("weather_sensor")
-                    .symbol("id", "toronto1")
-                    .doubleColumn("temperature", 23.5)
-                    .doubleColumn("humidity", 0.49)
-                    .atNow();
-            sender.table("weather_sensor")
-                    .symbol("id", "dubai2")
-                    .doubleColumn("temperature", 41.2)
-                    .doubleColumn("humidity", 0.34)
-                    .atNow();
+                 .symbol("id", "dubai2")
+                .doubleColumn("humidity", 0.34)
+                .doubleColumn("temperature", 41.2)
+                
+                 .atNow();
+            sender.table("weather_sensor") 
+                .symbol("id", "toronto1")
+                .doubleColumn("humidity", 0.49)
+                .doubleColumn("temperature", 23.5)
+                .atNow();
+           
         }
     }
 }
