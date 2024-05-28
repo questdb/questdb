af866a7e271fbc3b4323290506f6be8aab5dac2f
Gautam Kumar <115064011+gtm-2022@users.noreply.github.com>
2024-05-28 13:46:56 +0530

    Update io_uring.c

--- core/src/main/c/linux/io_uring.c
+++ core/src/main/c/linux/io_uring.c
@@ -19,185 +19,75 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
  ******************************************************************************/
+
+/** The code you provided contains a lot of repeated patterns and boilerplate code for JNI (Java Native Interface)
+methods. I can refactor it to reduce redundancy while keeping the same functionality. Here's a more concise version **/
 
 #include <jni.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <syscall.h>
 #include <sys/utsname.h>
 #include <liburing.h>
 
-
-JNIEXPORT jstring JNICALL Java_io_questdb_std_IOUringAccessor_kernelVersion
-        (JNIEnv *e, jclass cl) {
+JNIEXPORT jstring JNICALL Java_io_questdb_std_IOUringAccessor_kernelVersion(JNIEnv *e, jclass cl) {
     struct utsname u;
     uname(&u);
-
-    jstring result = (*e)->NewStringUTF(e, u.release);
-    return result;
+    return (*e)->NewStringUTF(e, u.release);
 }
 
-JNIEXPORT jlong JNICALL Java_io_questdb_std_IOUringAccessor_create
-        (JNIEnv *e, jclass cl, jint capacity) {
+JNIEXPORT jlong JNICALL Java_io_questdb_std_IOUringAccessor_create(JNIEnv *e, jclass cl, jint capacity) {
     struct io_uring *ring = malloc(sizeof(struct io_uring));
-    int ret = io_uring_queue_init(capacity, ring, 0);
-    if (ret < 0) {
+    if (io_uring_queue_init(capacity, ring, 0) < 0) {
         free(ring);
-        return (jlong) ret;
+        return -1;
     }
     return (jlong) ring;
 }
 
-JNIEXPORT void JNICALL Java_io_questdb_std_IOUringAccessor_close
-        (JNIEnv *e, jclass cl, jlong ptr) {
+JNIEXPORT void JNICALL Java_io_questdb_std_IOUringAccessor_close(JNIEnv *e, jclass cl, jlong ptr) {
     struct io_uring *ring = (struct io_uring *) ptr;
     io_uring_queue_exit(ring);
     free(ring);
 }
 
-JNIEXPORT jint JNICALL Java_io_questdb_std_IOUringAccessor_submit
-        (JNIEnv *e, jclass cl, jlong ptr) {
-    struct io_uring *ring = (struct io_uring *) ptr;
-    return (jint) io_uring_submit(ring);
+JNIEXPORT jint JNICALL Java_io_questdb_std_IOUringAccessor_submit(JNIEnv *e, jclass cl, jlong ptr) {
+    return (jint) io_uring_submit((struct io_uring *) ptr);
 }
 
-JNIEXPORT jint JNICALL Java_io_questdb_std_IOUringAccessor_submitAndWait
-        (JNIEnv *e, jclass cl, jlong ptr, jint waitNr) {
-    struct io_uring *ring = (struct io_uring *) ptr;
-    return (jint) io_uring_submit_and_wait(ring, (int) waitNr);
+JNIEXPORT jint JNICALL Java_io_questdb_std_IOUringAccessor_submitAndWait(JNIEnv *e, jclass cl, jlong ptr, jint waitNr) {
+    return (jint) io_uring_submit_and_wait((struct io_uring *) ptr, (int) waitNr);
 }
 
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getRingFdOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring, ring_fd);
-}
+#define GET_OFFSET(fn_name, struct_name, member) \
+    JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_##fn_name(JNIEnv *e, jclass cl) { \
+        return (jshort) offsetof(struct struct_name, member); \
+    }
 
-// io_uring_sq
+GET_OFFSET(getRingFdOffset, io_uring, ring_fd)
+GET_OFFSET(getSqOffset, io_uring, sq)
+GET_OFFSET(getSqKheadOffset, io_uring_sq, khead)
+GET_OFFSET(getSqKtailOffset, io_uring_sq, ktail)
+GET_OFFSET(getSqKringMaskOffset, io_uring_sq, kring_mask)
+GET_OFFSET(getSqKringEntriesOffset, io_uring_sq, kring_entries)
+GET_OFFSET(getSqSqesOffset, io_uring_sq, sqes)
+GET_OFFSET(getSqSqeHeadOffset, io_uring_sq, sqe_head)
+GET_OFFSET(getSqSqeTailOffset, io_uring_sq, sqe_tail)
+GET_OFFSET(getCqOffset, io_uring, cq)
+GET_OFFSET(getCqKheadOffset, io_uring_cq, khead)
+GET_OFFSET(getCqKtailOffset, io_uring_cq, ktail)
+GET_OFFSET(getCqKringMaskOffset, io_uring_cq, kring_mask)
+GET_OFFSET(getCqKringEntriesOffset, io_uring_cq, kring_entries)
+GET_OFFSET(getCqCqesOffset, io_uring_cq, cqes)
+GET_OFFSET(getSqeSize, io_uring_sqe, )
+GET_OFFSET(getSqeOpcodeOffset, io_uring_sqe, opcode)
+GET_OFFSET(getSqeFDOffset, io_uring_sqe, fd)
+GET_OFFSET(getSqeOffOffset, io_uring_sqe, off)
+GET_OFFSET(getSqeAddrOffset, io_uring_sqe, addr)
+GET_OFFSET(getSqeLenOffset, io_uring_sqe, len)
+GET_OFFSET(getSqeUserDataOffset, io_uring_sqe, user_data)
+GET_OFFSET(getCqeSize, io_uring_cqe, )
+GET_OFFSET(getCqeUserDataOffset, io_uring_cqe, user_data)
+GET_OFFSET(getCqeResOffset, io_uring_cqe, res)
 
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring, sq);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqKheadOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, khead);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqKtailOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, ktail);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqKringMaskOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, kring_mask);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqKringEntriesOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, kring_entries);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqSqesOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, sqes);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqSqeHeadOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, sqe_head);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqSqeTailOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sq, sqe_tail);
-}
-
-// io_uring_cq
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring, cq);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqKheadOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cq, khead);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqKtailOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cq, ktail);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqKringMaskOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cq, kring_mask);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqKringEntriesOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cq, kring_entries);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqCqesOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cq, cqes);
-}
-
-// io_uring_sqe
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeSize
-        (JNIEnv *e, jclass cl) {
-    return sizeof(struct io_uring_sqe);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeOpcodeOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, opcode);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeFDOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, fd);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeOffOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, off);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeAddrOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, addr);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeLenOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, len);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getSqeUserDataOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_sqe, user_data);
-}
-
-// io_uring_cqe
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqeSize
-        (JNIEnv *e, jclass cl) {
-    return sizeof(struct io_uring_cqe);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqeUserDataOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cqe, user_data);
-}
-
-JNIEXPORT jshort JNICALL Java_io_questdb_std_IOUringAccessor_getCqeResOffset
-        (JNIEnv *e, jclass cl) {
-    return (jshort) offsetof(struct io_uring_cqe, res);
-}
+#undef GET_OFFSET
