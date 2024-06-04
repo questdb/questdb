845ada4f76f28c7f71c605288f9fefbb06d7f0a8
Steven Sklar <steven@questdb.io>
2024-06-04 14:46:54 -0400

    add more tests

--- core/src/test/java/io/questdb/test/FileWatcherTest.java
+++ core/src/test/java/io/questdb/test/FileWatcherTest.java
@@ -3,6 +3,7 @@ package io.questdb.test;
 import io.questdb.FileEventCallback;
 import io.questdb.FileWatcher;
 import io.questdb.FileWatcherFactory;
+import io.questdb.FileWatcherNativeException;
 import io.questdb.mp.SOCountDownLatch;
 import io.questdb.std.Os;
 import org.junit.Assert;
@@ -12,49 +13,78 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
+import static io.questdb.test.tools.TestUtils.assertMemoryLeak;
+
 public class FileWatcherTest extends AbstractTest {
+
+    @Test
+    public void testEmptyFilename() throws Exception {
+        assertMemoryLeak(() -> {
+            Assert.assertThrows(
+                    IllegalArgumentException.class,
+                    () -> FileWatcherFactory.getFileWatcher("", Assert::fail)
+            );
+        });
+    }
 
     @Test
     public void testFileAppend() throws Exception {
 
-        final File targetFile = temp.newFile();
-        SOCountDownLatch threadLatch = new SOCountDownLatch(1);
+        assertMemoryLeak(() -> {
+            final File targetFile = temp.newFile();
+            SOCountDownLatch threadLatch = new SOCountDownLatch(1);
 
-        try (final FileWatcher fw = FileWatcherFactory.getFileWatcher(
-                targetFile.toString(),
-                new FileChangedCallback(threadLatch))) {
+            try (final FileWatcher fw = FileWatcherFactory.getFileWatcher(
+                    targetFile.toString(),
+                    new FileChangedCallback(threadLatch))) {
 
-            fw.watch();
-            // todo: synchronize the start of the watch here, so we don't write before the watch is set up
-            Thread.sleep(1000);
-            try (PrintWriter writer = new PrintWriter(targetFile.getAbsolutePath(), StandardCharsets.UTF_8)) {
-                writer.println("hello");
+                fw.watch();
+                // todo: synchronize the start of the watch here, so we don't write before the watch is set up
+                Thread.sleep(1000);
+                try (PrintWriter writer = new PrintWriter(targetFile.getAbsolutePath(), StandardCharsets.UTF_8)) {
+                    writer.println("hello");
+                }
+                threadLatch.await();
             }
-            threadLatch.await();
-        }
+        });
+
     }
 
     @Test
     public void testFileCopyOnWrite() throws Exception {
+        assertMemoryLeak(() -> {
+            final File targetFile = temp.newFile();
+            SOCountDownLatch threadLatch = new SOCountDownLatch(1);
 
-        final File targetFile = temp.newFile();
-        SOCountDownLatch threadLatch = new SOCountDownLatch(1);
+            try (final FileWatcher fw = FileWatcherFactory.getFileWatcher(
+                    targetFile.toString(),
+                    new FileChangedCallback(threadLatch))) {
 
-        try (final FileWatcher fw = FileWatcherFactory.getFileWatcher(
-                targetFile.toString(),
-                new FileChangedCallback(threadLatch))) {
-
-            fw.watch();
-            // todo: synchronize the start of the watch here, so we don't write before the watch is set up
-            Thread.sleep(1000);
-            Assert.assertTrue(targetFile.delete());
-            try (PrintWriter writer = new PrintWriter(targetFile.getAbsolutePath(), StandardCharsets.UTF_8)) {
-                writer.println("hello");
+                fw.watch();
+                // todo: synchronize the start of the watch here, so we don't write before the watch is set up
+                Thread.sleep(1000);
+                Assert.assertTrue(targetFile.delete());
+                try (PrintWriter writer = new PrintWriter(targetFile.getAbsolutePath(), StandardCharsets.UTF_8)) {
+                    writer.println("hello");
+                }
+                threadLatch.await();
             }
-            threadLatch.await();
-        }
+        });
+
     }
 
+    @Test
+    public void testFileDoesNotExist() throws Exception {
+        assertMemoryLeak(() -> {
+            Assert.assertThrows(
+                    FileWatcherNativeException.class,
+                    () -> FileWatcherFactory.getFileWatcher(
+                            "/hello/i/dont/exist",
+                            Assert::fail)
+            );
+        });
+
+    }
 
     static class FileChangedCallback implements FileEventCallback {
 
