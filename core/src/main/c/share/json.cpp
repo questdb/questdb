4b161456d4b56c94fb07cf491627bd941bd5f88e
Adam Cimarosti <cimarosti@gmail.com>
2024-06-20 16:46:06 +0100

    another potential work-around

--- core/src/main/c/share/json.cpp
+++ core/src/main/c/share/json.cpp
@@ -23,6 +23,7 @@
  ******************************************************************************/
 
 #include <jni.h>
+#define SIMDJSON_THREADS_ENABLED 0
 #include <simdjson.h>
 #include <limits>
 #include <cmath>
