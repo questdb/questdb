24047e08476d3a3739334ca6b822ca0d3d9b9d94
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2024-06-27 18:04:46 +0300

    a few tests for convert partitions

---
+++ core/src/test/java/io/questdb/test/griffin/AlterTableConvertPartitionTest.java
@@ -0,0 +1,133 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2024 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.test.griffin;
+
+import io.questdb.cairo.ColumnType;
+import io.questdb.cairo.PartitionBy;
+import io.questdb.std.str.Path;
+import io.questdb.test.AbstractCairoTest;
+import io.questdb.test.cairo.TableModel;
+import io.questdb.test.std.TestFilesFacadeImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AlterTableConvertPartitionTest extends AbstractCairoTest {
+
+    @Test
+    public void testConvertListPartitions() throws Exception {
+        assertMemoryLeak(TestFilesFacadeImpl.INSTANCE, () -> {
+                    final String tableName = testName.getMethodName();
+                    createTable(tableName,
+                            "insert into " + tableName + " values(1, '2024-06-10T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(2, '2024-06-11T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(3, '2024-06-12T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(4, '2024-06-12T00:00:01.000000Z')",
+                            "insert into " + tableName + " values(5, '2024-06-15T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(6, '2024-06-12T00:00:02.000000Z')");
+
+                    ddl("alter table " + tableName + " convert partition list '2024-06-10', '2024-06-11', '2024-06-12', '2024-06-15'");
+
+                    assertPartitionExists(tableName, "2024-06-10");
+                    assertPartitionExists(tableName, "2024-06-11.0");
+                    assertPartitionExists(tableName, "2024-06-12.1");
+                    assertPartitionExists(tableName, "2024-06-15.3");
+                }
+        );
+    }
+
+    @Test
+    public void testConvertTimestampPartitions() throws Exception {
+        assertMemoryLeak(TestFilesFacadeImpl.INSTANCE, () -> {
+                    final String tableName = testName.getMethodName();
+                    createTable(tableName,
+                            "insert into " + tableName + " values(1, '2024-06-10T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(2, '2024-06-11T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(3, '2024-06-12T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(4, '2024-06-12T00:00:01.000000Z')",
+                            "insert into " + tableName + " values(5, '2024-06-15T00:00:00.000000Z')",
+                            "insert into " + tableName + " values(6, '2024-06-12T00:00:02.000000Z')");
+
+                    ddl("alter table " + tableName + " convert partition where timestamp = to_timestamp('2024-06-12', 'yyyy-MM-dd')");
+
+                    assertPartitionDoesntExists(tableName, "2024-06-10");
+                    assertPartitionDoesntExists(tableName, "2024-06-11.0");
+                    assertPartitionExists(tableName, "2024-06-12.1");
+                    assertPartitionDoesntExists(tableName, "2024-06-15.3");
+                }
+        );
+    }
+    @Test
+    public void testConvertAllPartitions() throws Exception {
+        assertMemoryLeak(TestFilesFacadeImpl.INSTANCE, () -> {
+                final String tableName = testName.getMethodName();
+                createTable(tableName,
+                    "insert into " + tableName + " values(1, '2024-06-10T00:00:00.000000Z')",
+                    "insert into " + tableName + " values(2, '2024-06-11T00:00:00.000000Z')",
+                    "insert into " + tableName + " values(3, '2024-06-12T00:00:00.000000Z')",
+                    "insert into " + tableName + " values(4, '2024-06-12T00:00:01.000000Z')",
+                    "insert into " + tableName + " values(5, '2024-06-15T00:00:00.000000Z')",
+                    "insert into " + tableName + " values(6, '2024-06-12T00:00:02.000000Z')");
+
+                ddl("alter table " + tableName + " convert partition where timestamp > 0");
+
+                assertPartitionExists(tableName, "2024-06-10");
+                assertPartitionExists(tableName, "2024-06-11.0");
+                assertPartitionExists(tableName, "2024-06-12.1");
+                assertPartitionExists(tableName, "2024-06-15.3");
+            }
+        );
+    }
+
+
+    private void assertPartitionExists(String tableName, String partition) {
+       assertPartitionExists0(tableName, false, partition);
+    }
+
+    private void assertPartitionDoesntExists(String tableName, String partition) {
+        assertPartitionExists0(tableName, true, partition);
+    }
+
+    private void assertPartitionExists0(String tableName, boolean rev, String partition) {
+        Path path = Path.getThreadLocal(configuration.getRoot());
+        path.concat(engine.getTableTokenIfExists(tableName).getDirName());
+        int tablePathLen = path.size();
+
+        path.trimTo(tablePathLen);
+        path.concat(partition).put(".parquet").$();
+        if (rev) {
+            Assert.assertFalse(ff.exists(path));
+        } else {
+            Assert.assertTrue(ff.exists(path));
+        }
+    }
+
+    private void createTable(String tableName, String... inserts) throws Exception {
+        TableModel model = new TableModel(configuration, tableName, PartitionBy.DAY).col("id", ColumnType.INT).timestamp();
+        AbstractCairoTest.create(model);
+        for (int i = 0, n = inserts.length; i < n; i++) {
+            insert(inserts[i]);
+        }
+    }
+}
