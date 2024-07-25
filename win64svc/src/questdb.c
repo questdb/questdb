5e21fee1c4dc6382575385d5cfd72e2a50a86f1a
Alex Pelagenko <2159629+ideoma@users.noreply.github.com>
2024-07-25 10:01:14 +0000

    rebuild Windows service supply -Dbin parameter

--- win64svc/src/questdb.c
+++ win64svc/src/questdb.c
@@ -97,7 +97,7 @@ void buildJavaArgs(CONFIG *config) {
 
     // put together command line, dir is x2 because we're including the path to `hs_err_pid`
     // 512 is extra for the constant strings
-    char *args = malloc((strlen(javaOpts) + strlen(classpath) + strlen(mainClass) + strlen(config->dir)*3 + 512 + 35) *
+    char *args = malloc((strlen(javaOpts) + strlen(classpath) + strlen(mainClass) + strlen(config->dir)*3 + 512 + 6) *
                         sizeof(char));
     strcpy(args, javaOpts);
     // quote the directory in case it contains spaces
@@ -110,9 +110,7 @@ void buildJavaArgs(CONFIG *config) {
         strcat(args, classpath);
         strcat(args, "\"");
     }
-    strcat(args, " -Dout=\"");
-    strcat(args, config->dir);
-    strcat(args, "\\conf\\standalone_log.conf\"");
+    strcat(args, " -Dbin");
     strcat(args, " -m ");
     strcat(args, mainClass);
     strcat(args, " -d \"");
