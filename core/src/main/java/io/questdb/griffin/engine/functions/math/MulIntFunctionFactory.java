ab6d97849892f1f40eeeb372c2b86826217e0f17
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-07-26 16:36:47 +0200

    the behaviour change is now more localized

--- core/src/main/java/io/questdb/griffin/engine/functions/math/MulIntFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/math/MulIntFunctionFactory.java
@@ -72,17 +72,6 @@ public class MulIntFunctionFactory implements FunctionFactory {
         }
 
         @Override
-        public long getLong(Record rec) {
-            final int l = left.getInt(rec);
-            final int r = right.getInt(rec);
-
-            if (l == Numbers.INT_NULL || r == Numbers.INT_NULL) {
-                return Numbers.LONG_NULL;
-            }
-            return (long) l * r;
-        }
-
-        @Override
         public String getName() {
             return "*";
         }
@@ -90,6 +79,17 @@ public class MulIntFunctionFactory implements FunctionFactory {
         @Override
         public Function getRight() {
             return right;
+        }
+
+        @Override
+        public long getTimestamp(Record rec) {
+            final int l = left.getInt(rec);
+            final int r = right.getInt(rec);
+
+            if (l == Numbers.INT_NULL || r == Numbers.INT_NULL) {
+                return Numbers.LONG_NULL;
+            }
+            return (long) l * r;
         }
 
         @Override
