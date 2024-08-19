bc86c6df2da36248ea6c3b2f7d6dfb112b738417
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2024-08-19 14:18:38 +0300

    expose update partition API

--- core/rust/qdbr/src/parquet_write/jni.rs
+++ core/rust/qdbr/src/parquet_write/jni.rs
@@ -240,3 +240,170 @@ fn compression_from_i64(value: i64) -> Result<CompressionOptions, ParquetError> 
         }
     })
 }
+
+#[no_mangle]
+pub extern "system" fn Java_io_questdb_griffin_engine_table_parquet_PartitionEncoder_updatePartition(
+    mut env: JNIEnv,
+    _class: JClass,
+    row_group_id: jshort,
+    col_count: jint,
+    col_names: *const u8,
+    col_names_len: i32,
+    col_name_sizes_ptr: *const i32,
+    col_types_ptr: *const i32,
+    col_ids_ptr: *const i32,
+    _timestamp_index: jint,
+    col_tops_ptr: *const i64,
+    primary_col_addrs_ptr: *const *const u8,
+    primary_col_sizes_ptr: *const i64,
+    secondary_col_addrs_ptr: *const *const u8,
+    secondary_col_sizes_ptr: *const i64,
+    symbol_offsets_addrs_ptr: *const *const u64,
+    symbol_offsets_sizes_ptr: *const i64,
+    row_count: jlong,
+    dest_path: *const u8,
+    dest_path_len: i32,
+) {
+    let update = || -> anyhow::Result<()> {
+        let row_count = row_count as usize;
+        let dest_path = unsafe {
+            std::str::from_utf8_unchecked(slice::from_raw_parts(dest_path, dest_path_len as usize))
+        };
+
+        let col_count = col_count as usize;
+        let col_names = read_utf8_encoded_string_list(
+            col_count,
+            col_names,
+            col_names_len as usize,
+            col_name_sizes_ptr,
+        );
+        let col_types = unsafe { slice::from_raw_parts(col_types_ptr, col_count) };
+        let col_tops = unsafe { slice::from_raw_parts(col_tops_ptr, col_count) };
+        let col_ids = unsafe { slice::from_raw_parts(col_ids_ptr, col_count) };
+
+        let primary_col_addrs_slice =
+            unsafe { slice::from_raw_parts(primary_col_addrs_ptr, col_count) };
+        let primary_col_sizes_slice =
+            unsafe { slice::from_raw_parts(primary_col_sizes_ptr, col_count) };
+
+        let secondary_col_addrs_slice =
+            unsafe { slice::from_raw_parts(secondary_col_addrs_ptr, col_count) };
+        let secondary_col_sizes_slice =
+            unsafe { slice::from_raw_parts(secondary_col_sizes_ptr, col_count) };
+
+        let symbol_offsets_addrs_slice =
+            unsafe { slice::from_raw_parts(symbol_offsets_addrs_ptr, col_count) };
+        let symbol_offsets_sizes_slice =
+            unsafe { slice::from_raw_parts(symbol_offsets_sizes_ptr, col_count) };
+
+        let mut columns = vec![];
+        for i in 0..col_count {
+            let col_id = col_ids[i];
+            let col_name = col_names[i];
+            let col_type = col_types[i];
+            let col_top = col_tops[i];
+
+            let primary_col_addr = primary_col_addrs_slice[i];
+            let primary_col_size = primary_col_sizes_slice[i];
+
+            let secondary_col_addr = secondary_col_addrs_slice[i];
+            let secondary_col_size = secondary_col_sizes_slice[i];
+
+            let symbol_offsets_addr = symbol_offsets_addrs_slice[i];
+            let symbol_offsets_size = symbol_offsets_sizes_slice[i];
+
+            let column = Column::from_raw_data(
+                col_id,
+                col_name,
+                col_type,
+                col_top,
+                row_count,
+                primary_col_addr,
+                primary_col_size as usize,
+                secondary_col_addr,
+                secondary_col_size as usize,
+                symbol_offsets_addr,
+                symbol_offsets_size as usize,
+            )?;
+            columns.push(column);
+        }
+
+        let table = "update".to_string();
+        let partition = Partition { table, columns };
+        //TODO: validate schema
+
+        let dest_path = Path::new(&dest_path);
+        let file_size = fs::metadata(dest_path)?.len();
+        let mut file = File::options()
+            .read(true)
+            .write(true)
+            .open(dest_path)
+            .with_context(|| {
+                format!(
+                    "Could not open for update parquet file [path={}]",
+                    dest_path.display()
+                )
+            })?;
+
+        let metadata = read_metadata(&mut file)?;
+
+        let version = Version::V1;
+        let created_by = metadata.created_by.clone();
+        let schema = metadata.schema_descr.clone();
+
+        let row_group_options = WriteOptions {
+            write_statistics: true,
+            compression: CompressionOptions::Uncompressed,
+            version,
+            row_group_size: None,
+            data_page_size: None,
+        };
+
+        let new_row_group = create_row_group(
+            &partition,
+            0,
+            row_count,
+            metadata.schema_descr.fields(),
+            &to_encodings(&partition),
+            row_group_options,
+            false,
+        )?;
+
+        let options = write::WriteOptions { write_statistics: true, version: Version::V1 };
+        let mut parquet_file = ParquetFile::new_updater(
+            &mut file,
+            file_size,
+            schema,
+            options,
+            created_by,
+            None,
+            metadata.into_thrift(),
+        );
+        parquet_file.replace(new_row_group, Some(row_group_id))?;
+        parquet_file.end(None)?;
+
+        Ok(())
+    };
+
+    match update() {
+        Ok(_) => (),
+        Err(err) => {
+            if let Some(jni_err) = err.downcast_ref::<jni::errors::Error>() {
+                match jni_err {
+                    jni::errors::Error::JavaException => {
+                        // Already thrown.
+                    }
+                    _ => {
+                        let msg = format!("Failed to update partition: {:?}", jni_err);
+                        env.throw_new("java/lang/RuntimeException", msg)
+                            .expect("failed to throw exception");
+                    }
+                }
+            } else {
+                let msg = format!("Failed to update partition: {:?}", err);
+                env.throw_new("java/lang/RuntimeException", msg)
+                    .expect("failed to throw exception");
+            }
+        }
+    }
+}
