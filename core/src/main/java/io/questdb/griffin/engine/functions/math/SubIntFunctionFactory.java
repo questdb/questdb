8be0bd26b795da4f26d178b83de8044877438590
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-09-02 12:51:55 +0200

    fix(sql): better overflow behaviour when converting timezones (#4824)

--- core/src/main/java/io/questdb/griffin/engine/functions/math/SubIntFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/math/SubIntFunctionFactory.java
@@ -75,6 +75,18 @@ public class SubIntFunctionFactory implements FunctionFactory {
         }
 
         @Override
+        public long getLong(Record rec) {
+            int l = left.getInt(rec);
+            int r = right.getInt(rec);
+
+            if (l != Numbers.INT_NULL && r != Numbers.INT_NULL) {
+                return ((long) l) - r;
+            }
+
+            return Numbers.INT_NULL;
+        }
+
+        @Override
         public Function getRight() {
             return right;
         }
