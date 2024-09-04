1a9b5e1f362848b6575345f3b82198952c750f29
Jaromir Hamala <jaromir.hamala@gmail.com>
2024-09-04 12:55:26 +0200

    more fuzzing

--- core/src/test/java/io/questdb/test/griffin/AsOfJoinFuzzTest.java
+++ core/src/test/java/io/questdb/test/griffin/AsOfJoinFuzzTest.java
@@ -44,6 +44,26 @@ public class AsOfJoinFuzzTest extends AbstractCairoTest {
         testFuzz(0);
     }
 
+    @Test
+    public void testFuzzPartitionByNoneManyDuplicates() throws Exception {
+        testFuzzPartitionByNone(50);
+    }
+
+    @Test
+    public void testFuzzPartitionByNoneNoDuplicates() throws Exception {
+        testFuzzPartitionByNone(0);
+    }
+
+    @Test
+    public void testFuzzPartitionByNoneSomeDuplicates() throws Exception {
+        testFuzzPartitionByNone(10);
+    }
+
+    @Test
+    public void testFuzzSomeDuplicates() throws Exception {
+        testFuzz(10);
+    }
+
     private void assertResultSetsMatch(String leftTable, String rightTable) throws Exception {
         final StringSink expectedSink = new StringSink();
         // equivalent of the below query, but uses slow factory
@@ -73,6 +93,38 @@ public class AsOfJoinFuzzTest extends AbstractCairoTest {
             }
 
             ddl("CREATE TABLE t2 (ts TIMESTAMP, i INT, s SYMBOL) timestamp(ts) partition by day bypass wal");
+            ts = TimestampFormatUtils.parseTimestamp("2000-01-01T00:00:00.000Z");
+            ts += Timestamps.HOUR_MICROS * rnd.nextLong(48);
+            for (int i = 0; i < table2Size; i++) {
+                if (rnd.nextInt(100) >= tsDuplicatePercentage) {
+                    ts += Timestamps.HOUR_MICROS * rnd.nextLong(24);
+                }
+                String symbol = "s_" + rnd.nextInt(10);
+                insert("INSERT INTO t2 values (" + ts + ", " + i + ", '" + symbol + "');");
+            }
+
+            assertResultSetsMatch("t1", "t2");
+        });
+    }
+
+    private void testFuzzPartitionByNone(int tsDuplicatePercentage) throws Exception {
+        final Rnd rnd = TestUtils.generateRandom(LOG);
+        assertMemoryLeak(() -> {
+            final int table1Size = rnd.nextPositiveInt() % 1000;
+            final int table2Size = rnd.nextPositiveInt() % 1000;
+
+            ddl("CREATE TABLE t1 (ts TIMESTAMP, i INT, s SYMBOL) timestamp(ts)");
+            long ts = TimestampFormatUtils.parseTimestamp("2000-01-01T00:00:00.000Z");
+            ts += Timestamps.HOUR_MICROS * (rnd.nextLong() % 48);
+            for (int i = 0; i < table1Size; i++) {
+                if (rnd.nextInt(100) >= tsDuplicatePercentage) {
+                    ts += Timestamps.HOUR_MICROS * rnd.nextLong(24);
+                }
+                String symbol = "s_" + rnd.nextInt(10);
+                insert("INSERT INTO t1 values (" + ts + ", " + i + ", '" + symbol + "');");
+            }
+
+            ddl("CREATE TABLE t2 (ts TIMESTAMP, i INT, s SYMBOL) timestamp(ts)");
             ts = TimestampFormatUtils.parseTimestamp("2000-01-01T00:00:00.000Z");
             ts += Timestamps.HOUR_MICROS * rnd.nextLong(48);
             for (int i = 0; i < table2Size; i++) {
