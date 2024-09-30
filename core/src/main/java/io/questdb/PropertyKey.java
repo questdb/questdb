c546189ac4f6a9a1abfe2d50a69dec1a1fc1e99d
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-09-30 13:06:25 +0100

    Sketchy conflict merge

--- core/src/main/java/io/questdb/PropertyKey.java
+++ core/src/main/java/io/questdb/PropertyKey.java
@@ -484,7 +484,7 @@ public enum PropertyKey implements ConfigPropertyKey {
     POSTHOG_API_KEY("posthog.api.key"),
     POSTHOG_ENABLED("posthog.enabled"),
     LOG_LEVEL_VERBOSE("log.level.verbose"),
-    LOG_SQL_QUERY_PROGRESS_EXE("log.sql.query.progress.exe");
+    LOG_SQL_QUERY_PROGRESS_EXE("log.sql.query.progress.exe"),
     CAIRO_PARTITION_ENCODER_PARQUET_VERSION("cairo.partition.encoder.parquet.version"),
     CAIRO_PARTITION_ENCODER_PARQUET_STATISTICS_ENABLED("cairo.partition.encoder.parquet.statistics.enabled"),
     CAIRO_PARTITION_ENCODER_PARQUET_COMPRESSION_CODEC("cairo.partition.encoder.parquet.compression.codec"),
