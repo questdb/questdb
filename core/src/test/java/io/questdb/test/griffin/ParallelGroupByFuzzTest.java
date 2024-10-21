2899a028b444ffd11a6795f0f36f2e4635f1828b
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-10-21 23:55:32 +0100

    perf(sql): parallelise sample and population covariance (#5074)

--- core/src/test/java/io/questdb/test/griffin/ParallelGroupByFuzzTest.java
+++ core/src/test/java/io/questdb/test/griffin/ParallelGroupByFuzzTest.java
@@ -702,6 +702,15 @@ public class ParallelGroupByFuzzTest extends AbstractCairoTest {
     }
 
     @Test
+    public void testParallelGroupByCovariance() throws Exception {
+        Assume.assumeTrue(enableParallelGroupBy);
+        testParallelGroupByAllTypes("SELECT round(covar_samp(adouble, along), 14) FROM tab", "round\n" +
+                        "-92233.72036854776\n",
+                "SELECT round(covar_pop(adouble, along), 13) FROM tab", "round\n" +
+                        "-922337.2036854776\n");
+    }
+
+    @Test
     public void testParallelGroupByStdDev() throws Exception {
         Assume.assumeTrue(enableParallelGroupBy);
         testParallelGroupByAllTypes("SELECT round(stddev_samp(adouble), 14) FROM tab", "round\n" +
