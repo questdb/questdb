21b4497d001e34aef0c639ea343280ed6980f66d
Andrei Pechkurov <apechkurov@gmail.com>
2024-10-21 21:45:46 +0300

    The actual fix

--- core/src/test/java/io/questdb/test/cairo/TxnScoreboardTest.java
+++ core/src/test/java/io/questdb/test/cairo/TxnScoreboardTest.java
@@ -39,7 +39,6 @@ import io.questdb.test.tools.TestUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -339,8 +338,6 @@ public class TxnScoreboardTest extends AbstractCairoTest {
         for (int i = 0; i < 10000; i++) {
             LOG.info().$("iteration: " + i).$();
             testLimits();
-            // Make sure to delete all files from root, so that we don't start with some garbage in txn scoreboards.
-            wipeFiles(new File(root));
         }
     }
 
@@ -592,20 +589,6 @@ public class TxnScoreboardTest extends AbstractCairoTest {
                 scoreboard.acquireTxn(900992);
             }
         });
-    }
-
-    private static void wipeFiles(File dir) {
-        File[] files = dir.listFiles();
-        if (files == null) {
-            return;
-        }
-        for (File f : files) {
-            if (f.isDirectory()) {
-                wipeFiles(f);
-            } else if (f.isFile()) {
-                Assert.assertTrue(f.delete());
-            }
-        }
     }
 
     @SuppressWarnings("SameParameterValue")
