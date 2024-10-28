ac2a7bb5de740bb212181c19497a8755e51532db
Marko Topolnik <marko.topolnik@gmail.com>
2024-10-28 12:41:31 +0000

    Multiple parameterized benchmark runs

--- benchmarks/src/main/java/org/questdb/PGWireInsertSelectBenchmark.java
+++ benchmarks/src/main/java/org/questdb/PGWireInsertSelectBenchmark.java
@@ -40,15 +40,76 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLongArray;
 import java.util.stream.IntStream;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class PGWireInsertSelectBenchmark {
-    private static final int INSERT_BATCH_SIZE = 1000;
-    private static final int N_INSERTERS = 2;
-    private static final int N_SELECTORS = 2;
+    public static final int COOLDOWN_PERIOD_SECONDS = 30;
+    private static final int INSERT_BATCH_SIZE = 500;
     private static final long RUNTIME_SECONDS = 10;
-    private static final boolean USE_ILP = true;
+    private static int N_INSERTERS = 2;
+    private static int N_SELECTORS = 2;
+    private static boolean USE_ILP = false;
 
     public static void main(String[] args) throws Exception {
+        int[][] inserterSelectorCounts = {
+                {1, 1}, {1, 2}, {2, 1}, {2, 2},
+                {3, 1}, {3, 2}, {1, 3}, {2, 3}, {3, 3},
+                {4, 1}, {4, 2}, {4, 3}, {1, 4}, {2, 4}, {3, 4}, {4, 4}
+        };
+        for (int[] inserterSelectorCount : inserterSelectorCounts) {
+            N_INSERTERS = inserterSelectorCount[0];
+            N_SELECTORS = inserterSelectorCount[1];
+            USE_ILP = false;
+            runBenchmark();
+            Thread.sleep(SECONDS.toMillis(COOLDOWN_PERIOD_SECONDS));
+            USE_ILP = true;
+            runBenchmark();
+            Thread.sleep(SECONDS.toMillis(COOLDOWN_PERIOD_SECONDS));
+        }
+    }
 
+    private static Connection createConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        Properties properties = new Properties();
+        properties.setProperty("user", "admin");
+        properties.setProperty("password", "quest");
+        properties.setProperty("sslmode", "disable");
+        properties.setProperty("binaryTransfer", Boolean.toString(true));
+        // "simple", "extended", "extendedForPrepared", "extendedCacheEverything"
+        properties.setProperty("preferQueryMode", "extended");
+//        properties.setProperty("prepareThreshold", String.valueOf(-1));
+
+        TimeZone.setDefault(TimeZone.getTimeZone("EDT"));
+        final String url = String.format("jdbc:postgresql://127.0.0.1:%d/qdb", 8812);
+        return DriverManager.getConnection(url, properties);
+    }
+
+    private static long makeTimestampMicros(long taskId, long insertId) {
+        return TimeUnit.MILLISECONDS.toMicros(10 * insertId) + taskId;
+    }
+
+    private static void reportProgress(AtomicLongArray inserterProgress, AtomicLongArray selectorProgress) {
+        long doneInserts = IntStream.range(0, inserterProgress.length()).mapToLong(inserterProgress::get).sum();
+        long doneSelects = IntStream.range(0, selectorProgress.length()).mapToLong(selectorProgress::get).sum();
+        System.out.printf("%,9d inserts %,9d selects\n", doneInserts, doneSelects);
+    }
+
+    private static void reportResult(long tookNs, AtomicLongArray inserterTotals, AtomicLongArray selectorTotals) {
+        long nsPerSecond = SECONDS.toNanos(1);
+        long doneInserts = IntStream.range(0, inserterTotals.length()).mapToLong(inserterTotals::get).sum();
+        long doneSelects = IntStream.range(0, selectorTotals.length()).mapToLong(selectorTotals::get).sum();
+        long insertsPerSecond = doneInserts * nsPerSecond / tookNs;
+        long selectsPerSecond = doneSelects * nsPerSecond / tookNs;
+
+        System.out.printf("\n%d %s inserters, %d selectors combined performance:\n" +
+                        "%,d inserts per second, %,d selects per second\n" +
+                        "Per-thread performance:\n" +
+                        "%,d inserts per second, %,d selects per second\n\n",
+                N_INSERTERS, USE_ILP ? "ILP" : "JDBC", N_SELECTORS, insertsPerSecond, selectsPerSecond,
+                insertsPerSecond / N_INSERTERS, selectsPerSecond / N_SELECTORS);
+    }
+
+    static void runBenchmark() {
         AtomicLongArray inserterProgress = new AtomicLongArray(N_INSERTERS);
         AtomicLongArray selectorProgress = new AtomicLongArray(N_SELECTORS);
         ExecutorService pool = Executors.newFixedThreadPool(N_INSERTERS + N_SELECTORS);
@@ -57,29 +118,25 @@ public class PGWireInsertSelectBenchmark {
                  Statement ddlStatement = connection.createStatement()
             ) {
                 ddlStatement.execute("drop table if exists tango");
-                ddlStatement.execute("create table tango (" +
-                        "ts timestamp, " +
-                        "n long" +
-                        ") timestamp(ts) partition by hour");
+                ddlStatement.execute("create table tango (ts timestamp, n long) timestamp(ts) partition by hour");
             }
             long start = System.nanoTime();
-            long deadline = start + TimeUnit.SECONDS.toNanos(RUNTIME_SECONDS);
+            long deadline = start + SECONDS.toNanos(RUNTIME_SECONDS);
             for (int taskid = 0; taskid < N_INSERTERS; taskid++) {
                 final int taskId = taskid;
                 pool.submit(() -> {
                     if (USE_ILP) {
                         try (Sender sender = Sender.fromConfig("http::addr=localhost:9000;auto_flush=off;")) {
                             for (long i = 1; ; i++) {
-                                sender.table("tango").longColumn("n", 0).at(10 * i + taskId, ChronoUnit.MILLIS);
-                                boolean batchComplete = i % INSERT_BATCH_SIZE == 0;
-                                if (batchComplete) {
+                                sender.table("tango")
+                                        .longColumn("n", 0)
+                                        .at(makeTimestampMicros(taskId, i), ChronoUnit.MICROS);
+                                if (i % INSERT_BATCH_SIZE == 0) {
                                     sender.flush();
                                     inserterProgress.lazySet(taskId, i);
                                 }
                                 if (System.nanoTime() > deadline) {
-                                    if (!batchComplete) {
-                                        inserterProgress.set(taskId, i);
-                                    }
+                                    inserterProgress.set(taskId, i);
                                     break;
                                 }
                             }
@@ -91,7 +148,7 @@ public class PGWireInsertSelectBenchmark {
                              PreparedStatement st = connection.prepareStatement("insert into tango values (?, ?)")
                         ) {
                             for (long i = 1; ; i++) {
-                                st.setLong(1, TimeUnit.MILLISECONDS.toMicros(10 * i) + taskId);
+                                st.setLong(1, makeTimestampMicros(taskId, i));
                                 st.setLong(2, 0);
                                 st.addBatch();
                                 boolean batchComplete = i % INSERT_BATCH_SIZE == 0;
@@ -102,8 +159,8 @@ public class PGWireInsertSelectBenchmark {
                                 if (System.nanoTime() > deadline) {
                                     if (!batchComplete) {
                                         st.executeBatch();
-                                        inserterProgress.set(taskId, i);
                                     }
+                                    inserterProgress.set(taskId, i);
                                     break;
                                 }
                             }
@@ -136,7 +193,7 @@ public class PGWireInsertSelectBenchmark {
             }
             pool.shutdown();
             while (true) {
-                boolean done = pool.awaitTermination(1, TimeUnit.SECONDS);
+                boolean done = pool.awaitTermination(1, SECONDS);
                 reportProgress(inserterProgress, selectorProgress);
                 if (done) break;
             }
@@ -145,38 +202,5 @@ public class PGWireInsertSelectBenchmark {
         } catch (Exception e) {
             e.printStackTrace(System.err);
         }
-    }
-
-    private static Connection createConnection() throws Exception {
-        Class.forName("org.postgresql.Driver");
-        Properties properties = new Properties();
-        properties.setProperty("user", "admin");
-        properties.setProperty("password", "quest");
-        properties.setProperty("sslmode", "disable");
-        properties.setProperty("binaryTransfer", Boolean.toString(true));
-        // "simple", "extended", "extendedForPrepared", "extendedCacheEverything"
-        properties.setProperty("preferQueryMode", "extended");
-//        properties.setProperty("prepareThreshold", String.valueOf(-1));
-
-        TimeZone.setDefault(TimeZone.getTimeZone("EDT"));
-        final String url = String.format("jdbc:postgresql://127.0.0.1:%d/qdb", 8812);
-        return DriverManager.getConnection(url, properties);
-    }
-
-    private static void reportProgress(AtomicLongArray inserterProgress, AtomicLongArray selectorProgress) {
-        long doneInserts = IntStream.range(0, inserterProgress.length()).mapToLong(inserterProgress::get).sum();
-        long doneSelects = IntStream.range(0, selectorProgress.length()).mapToLong(selectorProgress::get).sum();
-        System.out.printf("%,9d inserts %,9d selects\n", doneInserts, doneSelects);
-    }
-
-    private static void reportResult(long tookNs, AtomicLongArray inserterTotals, AtomicLongArray selectorTotals) {
-        long nsPerSecond = TimeUnit.SECONDS.toNanos(1);
-        long doneInserts = IntStream.range(0, inserterTotals.length()).mapToLong(inserterTotals::get).sum();
-        long doneSelects = IntStream.range(0, selectorTotals.length()).mapToLong(selectorTotals::get).sum();
-        long insertsPerSecond = doneInserts * nsPerSecond / tookNs;
-        long selectsPerSecond = doneSelects * nsPerSecond / tookNs;
-
-        System.out.printf("\nBenchmark result:\n" +
-                "%,d inserts per second, %,d selects per second\n", insertsPerSecond, selectsPerSecond);
     }
 }
