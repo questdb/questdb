3c2c1040021678a4767feb0c7d638df0ab36d21f
Marko Topolnik <marko.topolnik@gmail.com>
2024-10-30 18:46:04 +0000

    Fix propagating partition drop probability

--- core/src/test/java/io/questdb/test/griffin/wal/FuzzRunner.java
+++ core/src/test/java/io/questdb/test/griffin/wal/FuzzRunner.java
@@ -399,7 +399,7 @@ public class FuzzRunner {
                 colRenameProb,
                 colTypeChangeProb,
                 truncateProb,
-                0.0,
+                partitionDropProb,
                 dataAddProb,
                 equalTsRowsProb,
                 strLen,
