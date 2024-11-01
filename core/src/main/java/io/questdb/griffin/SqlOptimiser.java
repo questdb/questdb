ec62c2091e3e5ddab6efc8915dec05c7b3b4e725
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-11-01 13:58:38 +0000

    cleanup

--- core/src/main/java/io/questdb/griffin/SqlOptimiser.java
+++ core/src/main/java/io/questdb/griffin/SqlOptimiser.java
@@ -6085,8 +6085,6 @@ public class SqlOptimiser implements Mutable {
                 throw SqlException.$(position, "Invalid table name or alias");
             }
 
-            // todo: NW, this fails, no aliases, no columns on the model
-            // ((select-virtual timestamp, symbol, min + max + last / 3 typical_price, volume from (select-group-by timestamp, symbol, min(price) min, last(price) last, max(price) max, sum(amount) volume from (trades2 timestamp (timestamp)) sample by 1d align to calendar with offset '00:00'))) trades2
             if (joinModels.getQuick(index).getAliasToColumnMap().excludes(columnName, dot + 1, columnName.length())) {
                 throw SqlException.invalidColumn(position, columnName);
             }
