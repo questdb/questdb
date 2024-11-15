4b7269896bef1d238a6ee8c8fc4fc2d757983c55
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-11-15 11:49:23 +0000

    refactor of function and associated tests

--- core/src/main/java/io/questdb/griffin/engine/functions/date/TimestampAddFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/date/TimestampAddFunctionFactory.java
@@ -52,26 +52,26 @@ public class TimestampAddFunctionFactory implements FunctionFactory {
 
     @Override
     public Function newInstance(int position, ObjList<Function> args, IntList argPositions, CairoConfiguration configuration, SqlExecutionContext sqlExecutionContext) {
-
-        Function period = args.getQuick(0);
-        Function interval = args.getQuick(1);
-        if (period.isConstant()) {
-            char periodValue = period.getChar(null);
-            if (periodValue < addFunctionsMax) {
-                LongAddIntFunction func = addFunctions.getQuick(periodValue);
-                if (func != null) {
-                    if (interval.isConstant()) {
-                        if (interval.getInt(null) != Numbers.INT_NULL) {
-                            return new AddLongIntVarConstFunction(args.getQuick(2), interval.getInt(null), func, periodValue);
+        Function periodFunc = args.getQuick(0);
+        Function strideFunc = args.getQuick(1);
+        Function timestampFunc = args.getQuick(2);
+        if (periodFunc.isConstant()) {
+            char period = periodFunc.getChar(null);
+            if (period < addFunctionsMax) {
+                LongAddIntFunction periodAddFunc = addFunctions.getQuick(period);
+                if (periodAddFunc != null) {
+                    if (strideFunc.isConstant()) {
+                        if (strideFunc.getInt(null) != Numbers.INT_NULL) {
+                            return new TimestampAddConstConstVar(period, periodAddFunc, strideFunc.getInt(null), timestampFunc);
                         }
                         return TimestampConstant.NULL;
                     }
-                    return new AddLongIntVarVarFunction(args.getQuick(2), args.getQuick(1), func, periodValue);
+                    return new TimestampAddConstVarVar(period, periodAddFunc, strideFunc, timestampFunc);
                 }
             }
             return TimestampConstant.NULL;
         }
-        return new DateAddFunc(args.getQuick(2), args.getQuick(0), args.getQuick(1));
+        return new TimestampAddFunc(periodFunc, strideFunc, timestampFunc);
     }
 
     @FunctionalInterface
@@ -79,97 +79,97 @@ public class TimestampAddFunctionFactory implements FunctionFactory {
         long add(long a, int b);
     }
 
-    private static class AddLongIntVarConstFunction extends TimestampFunction implements UnaryFunction {
-        private final Function arg;
-        private final LongAddIntFunction func;
-        private final int interval;
-        private final char periodSymbol;
+    private static class TimestampAddConstConstVar extends TimestampFunction implements UnaryFunction {
+        private final char period;
+        private final LongAddIntFunction periodAddFunction;
+        private final int stride;
+        private final Function timestampFunc;
 
-        public AddLongIntVarConstFunction(Function left, int right, LongAddIntFunction func, char periodSymbol) {
-            this.arg = left;
-            this.interval = right;
-            this.func = func;
-            this.periodSymbol = periodSymbol;
+        public TimestampAddConstConstVar(char period, LongAddIntFunction periodAddFunction, int stride, Function timestampFunc) {
+            this.period = period;
+            this.periodAddFunction = periodAddFunction;
+            this.stride = stride;
+            this.timestampFunc = timestampFunc;
         }
 
         @Override
         public Function getArg() {
-            return arg;
+            return timestampFunc;
         }
 
         @Override
         public long getTimestamp(Record rec) {
-            final long l = arg.getTimestamp(rec);
-            if (l == Numbers.LONG_NULL) {
+            final long timestamp = timestampFunc.getTimestamp(rec);
+            if (timestamp == Numbers.LONG_NULL) {
                 return Numbers.LONG_NULL;
             }
-            return func.add(l, interval);
+            return periodAddFunction.add(timestamp, stride);
         }
 
         @Override
         public void toPlan(PlanSink sink) {
-            sink.val("dateadd('").val(periodSymbol).val("',").val(interval).val(',').val(arg).val(')');
+            sink.val("dateadd('").val(period).val("',").val(stride).val(',').val(timestampFunc).val(')');
         }
     }
 
-    private static class AddLongIntVarVarFunction extends TimestampFunction implements BinaryFunction {
-        private final LongAddIntFunction func;
-        private final Function left;
-        private final char periodSymbol;
-        private final Function right;
+    private static class TimestampAddConstVarVar extends TimestampFunction implements BinaryFunction {
+        private final char period;
+        private final LongAddIntFunction periodAddFunc;
+        private final Function strideFunc;
+        private final Function timestampFunc;
 
-        public AddLongIntVarVarFunction(Function left, Function right, LongAddIntFunction func, char periodSymbol) {
-            this.left = left;
-            this.right = right;
-            this.func = func;
-            this.periodSymbol = periodSymbol;
+        public TimestampAddConstVarVar(char period, LongAddIntFunction periodAddFunc, Function strideFunc, Function timestampFunc) {
+            this.period = period;
+            this.periodAddFunc = periodAddFunc;
+            this.strideFunc = strideFunc;
+            this.timestampFunc = timestampFunc;
         }
 
         @Override
         public Function getLeft() {
-            return left;
+            return timestampFunc;
         }
 
         @Override
         public Function getRight() {
-            return right;
+            return strideFunc;
         }
 
         @Override
         public long getTimestamp(Record rec) {
-            final long l = left.getTimestamp(rec);
-            final int r = right.getInt(rec);
-            if (l == Numbers.LONG_NULL || r == Numbers.INT_NULL) {
+            final int stride = strideFunc.getInt(rec);
+            final long timestamp = timestampFunc.getTimestamp(rec);
+            if (timestamp == Numbers.LONG_NULL || stride == Numbers.INT_NULL) {
                 return Numbers.LONG_NULL;
             }
-            return func.add(l, r);
+            return periodAddFunc.add(timestamp, stride);
         }
 
         @Override
         public void toPlan(PlanSink sink) {
-            sink.val("dateadd('").val(periodSymbol).val("',").val(left).val(',').val(right).val(')');
+            sink.val("dateadd('").val(period).val("',").val(strideFunc).val(',').val(timestampFunc).val(')');
         }
     }
 
-    private static class DateAddFunc extends TimestampFunction implements TernaryFunction {
-        final Function center;
-        final Function left;
-        final Function right;
+    private static class TimestampAddFunc extends TimestampFunction implements TernaryFunction {
+        final Function periodFunc;
+        final Function strideFunc;
+        final Function timestampFunc;
 
-        public DateAddFunc(Function left, Function center, Function right) {
-            this.left = left;
-            this.center = center;
-            this.right = right;
+        public TimestampAddFunc(Function periodFunc, Function strideFunc, Function timestampFunc) {
+            this.periodFunc = periodFunc;
+            this.strideFunc = strideFunc;
+            this.timestampFunc = timestampFunc;
         }
 
         @Override
         public Function getCenter() {
-            return center;
+            return strideFunc;
         }
 
         @Override
         public Function getLeft() {
-            return left;
+            return periodFunc;
         }
 
         @Override
@@ -179,18 +179,23 @@ public class TimestampAddFunctionFactory implements FunctionFactory {
 
         @Override
         public Function getRight() {
-            return right;
+            return timestampFunc;
         }
 
         @Override
         public long getTimestamp(Record rec) {
-            final long l = left.getTimestamp(rec);
-            final char c = center.getChar(rec);
-            final int r = right.getInt(rec);
-            if (l == Numbers.LONG_NULL || r == Numbers.INT_NULL) {
+            final char period = periodFunc.getChar(rec);
+            final int stride = strideFunc.getInt(rec);
+            final long timestamp = timestampFunc.getTimestamp(rec);
+            if (timestamp == Numbers.LONG_NULL || stride == Numbers.INT_NULL) {
                 return Numbers.LONG_NULL;
             }
-            return Timestamps.addPeriod(l, c, r);
+            return Timestamps.addPeriod(timestamp, period, stride);
+        }
+
+        @Override
+        public void toPlan(PlanSink sink) {
+            sink.val("dateadd('").val(periodFunc).val("',").val(strideFunc).val(',').val(timestampFunc).val(')');
         }
     }
 
