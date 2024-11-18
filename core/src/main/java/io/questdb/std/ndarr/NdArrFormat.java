96db7a8155919ceb6b8d41dd5a878dd8cce5aafe
Adam Cimarosti <cimarosti@gmail.com>
2024-11-18 12:22:21 +0000

    completed parsing api stub, next up dummy values and plugging it in

---
+++ core/src/main/java/io/questdb/std/ndarr/NdArrFormat.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2024 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.std.ndarr;
+
+/**
+ * Tag indicating how the array is represented.
+ * * https://en.wikipedia.org/wiki/Matrix_representation
+ * * https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_(CSR,_CRS_or_Yale_format)
+ */
+public interface NdArrFormat {
+    /**
+     * Dense, row-major
+     */
+    int RM = 0;
+
+    /**
+     * Sparse, CSR
+     * 32-bit int indices
+     */
+    int CSR = RM + 1;  // = 1
+
+    /**
+     * Sparse, CSC
+     * 32-bit int indices
+     */
+    int CSC = CSR + 1;  // = 2
+}
--- core/src/main/java/io/questdb/std/ndarr/NdArrLiteralParser.java
+++ core/src/main/java/io/questdb/std/ndarr/NdArrLiteralParser.java
@@ -27,8 +27,6 @@ package io.questdb.std.ndarr;
 import io.questdb.cairo.ColumnType;
 import io.questdb.std.IntList;
 import io.questdb.std.QuietCloseable;
-import io.questdb.std.str.DirectUtf8Sequence;
-import io.questdb.std.str.Utf8s;
 
 /**
  * Parse N-dimensional array literals.
@@ -39,8 +37,6 @@ import io.questdb.std.str.Utf8s;
  * <p>
  * Semantically equivalent to NULL is an empty array:
  * {}
- * <p>
- * <p>
  * <p>
  * A 1-D array of longs.
  * {1, 2, 3}
@@ -61,7 +57,7 @@ import io.questdb.std.str.Utf8s;
  *  {5,0,0,0,6},
  *  {0,7,0,0,0},
  *  {0,0,0,0,8}}
- * "R" implies CSR and "C" implies CSC.
+ * "R" tags CSR and "C" tags CSC.
  * The three following arrays of numbers indicate:
  *   * {row_pointers/col_pointers}
  *   * {column_indices/row_indices}
@@ -70,22 +66,20 @@ import io.questdb.std.str.Utf8s;
  * NOTE: The element type is inferred, but can also be fixed during parsing.
  */
 public class NdArrLiteralParser implements QuietCloseable {
-
-    /** Number of bytes per element. Since we only handle DOUBLE and LONG, this is always the same. */
-    private static final long ELEM_SIZE = 8;
-
     /**
-     * The current number of elements in the current dimension.
+     * The current number of elements in the current dimension (for `NdArrFormat.RM` parsing).
      */
     private int currDimLen = 0;
 
     /**
-     * The dimension index we're currently parsing.
+     * The dimension index we're currently parsing (for `NdArrFormat.RM` parsing).
      */
     private int dimIndex = -1;
 
     /**
      * Stack-like state used to parse the dimensions.
+     * <p>
+     * When `NdArrFormat.RM`:
      * Each dimension is stored as a level.
      * Each dimension can be either known, or unknown.
      * Initially during parsing we don't know how many elements we will have:
@@ -93,18 +87,11 @@ public class NdArrLiteralParser implements QuietCloseable {
      * At some point during parsing, once a `}` closing brace is encountered, the dimension is locked and its sign
      * is flipped to positive.
      * In short, negative (uncertain) dimensions are bumped, positive (determined) dimensions validate future data.
+     * <p>
+     * When format is `NdArrFormat.CSR` or `NdArrFormat.CSC`, this is set after parsing.
      */
     private final IntList dims = new IntList(8);
 
-    /**
-     * Column type detected during parsing.
-     * This can hold one of three values:
-     *   * UNDEFINED (not inferred yet)
-     *   * DOUBLE
-     *   * LONG
-     *
-     * While UNDEFINED, the buffer is held as LONG and converted just-in-time.
-     */
     private int elementType = ColumnType.UNDEFINED;
 
     @Override
@@ -113,16 +100,17 @@ public class NdArrLiteralParser implements QuietCloseable {
     }
 
     /**
-     * Get the inferred column type.
-     * If a column type invariant was specified at `.of` construction, this will be that same type.
+     * Get the inferred element ColumnType.
+     * One of UNDEFINED, DOUBLE or LONG.
+     * If a column type invariant was specified during parsing, this will be that same type.
      * If the parsing is ambiguous, e.g. for literals such as `{}` or `{1, 2, 3}`:
-     *   * Returns UNDEFINED
+     *   * Returns UNDEFINED.
      *   * The buffer holds LONG numbers.
      * <p>
      * N.B.: This method should only be called once parsing is complete.
      */
-    public int getInferredColType() {
-        throw new UnsupportedOperationException("nyi");
+    public int getElementType() {
+        return elementType;
     }
 
     /**
@@ -139,40 +127,85 @@ public class NdArrLiteralParser implements QuietCloseable {
     }
 
     /**
-     * Get the parsed data in row-major order.
+     * Get the address of the {row_pointers/col_pointers} vector for the CSR/CSC sparse array.
+     * Returns a nullptr if `getFormat()` is dense (i.e. `RM`).
+     * The returned buffer contains 32-bit integers.
+     */
+    public long getSparsePointers() {
+        throw new UnsupportedOperationException("nyi");
+    }
+
+    /**
+     * Get the count of numbers present in the vector returned by `getSparsePointers()`.
+     * Call `getSparsePointersSize()` to get the byte size.
+     */
+    public int getSparsePointersCount() {
+        throw new UnsupportedOperationException("nyi");
+    }
+
+    /**
+     * Number of bytes in the vector returned by `getSparsePointers()`.
+     */
+    public int getSparsePointersSize() {
+        return getSparsePointersCount() * Integer.BYTES;
+    }
+
+    /**
+     * Get the {column_indices/row_indices} vector for the CSR/CSC sparse array.
+     * Returns a nullptr if `getFormat()` is dense (i.e. `RM`).
+     * The returned buffer contains 32-bit integers.
+     */
+    public long getSparseIndices() {
+        throw new UnsupportedOperationException("nyi");
+    }
+
+    /**
+     * Get the count of numbers present in the vector returned by `getSparseIndices()`.
+     * Call `getSparseIndicesSize()` to get the byte size.
+     */
+    public int getSparseIndicesCount() {
+        throw new UnsupportedOperationException("nyi");
+    }
+
+    /**
+     * Number of bytes in the vector returned by `getSparseIndices()`.
+     */
+    public int getSparseIndicesSize() {
+        return getSparseIndicesCount() * Integer.BYTES;
+    }
+
+    /**
+     * Get the ptr address to the flattened values buffer.
+     * Its content depends on the format (see `getFormat()`).
      * <p>
+     * When the format is `NdArrFormat.RM` (dense, row-major) it's a
+     * flattened array of all the elements.
+     *
      * For example, for the 4x3x2 matrix: {
      *     {{1, 2}, {3, 4}, {5, 6}},
      *     {{7, 8}, {9, 0}, {1, 2}},
      *     {{3, 4}, {5, 6}, {7, 8}},
      *     {{9, 0}, {1, 2}, {3, 4}}
      * }
-     * The buffer would contain a flat vector of longs or doubles (see getInferredColType)
-     *
+     * The buffer would contain a flat vector of elements (see getElementType)
+     * with the numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
+     * <p>
+     * When
      */
-    public long getBuffer() {
+    public long getElements() {
         throw new UnsupportedOperationException("nyi");
     }
 
     /**
      * Number of bytes in the buffer.
-     * This returns 8 (bytes) * total_element_count.
      */
-    public long getBufferSize() {
-        return ELEM_SIZE * getFlatElementsCount();
+    public int getElementsSize() {
+        return ColumnType.sizeOf(getElementType()) * getElementsCount();
     }
 
-    public long getFlatElementsCount() {
-        long total = 1;
-        for (int dimIndex = 0; dimIndex < dims.size(); ++dimIndex) {
-            final int dim = dims.get(dimIndex);
-            if (dim < 0) {
-                throw new UnsupportedOperationException("getFlatElementsCount() called prematurely");
-            }
-            total *= dim;
-            total *= dim;
-        }
-        return total;
+    /** Number of elements returned by `getElements()` */
+    public int getElementsCount() {
+        throw new UnsupportedOperationException("nyi");
     }
 
     private void reset(int targetType) {
