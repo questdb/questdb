4ec5360a8743889c824a9322d4fbe8dbad37bbd9
Andrei Pechkurov <apechkurov@gmail.com>
2024-11-26 12:21:51 +0200

    Add missing props

--- core/src/main/java/io/questdb/DynamicPropServerConfiguration.java
+++ core/src/main/java/io/questdb/DynamicPropServerConfiguration.java
@@ -79,8 +79,10 @@ public class DynamicPropServerConfiguration implements DynamicServerConfiguratio
             PropertyKey.PG_RO_USER,
             PropertyKey.PG_RO_PASSWORD,
             PropertyKey.PG_NAMED_STATEMENT_LIMIT,
-            PropertyKey.PG_RECV_BUFFER_SIZE,
-            PropertyKey.PG_SEND_BUFFER_SIZE
+            PropertyKey.PG_RECV_BUFFER_SIZE, // deprecated, but someone may be using it
+            PropertyKey.PG_NET_CONNECTION_RCVBUF,
+            PropertyKey.PG_SEND_BUFFER_SIZE, // deprecated, but someone may be using it
+            PropertyKey.PG_NET_CONNECTION_SNDBUF
     ));
     private final BuildInformation buildInformation;
     private final CairoConfigurationImpl cairoConfig;
