6c0536a649d05fd9951764a46f86147a2b35263a
hankyulkim-dc <hankyul.kim@datacrunchglobal.com>
2024-11-27 10:25:57 +0900

    loading VM version of columns completed

--- core/src/main/java/io/questdb/CallTablesMemory.java
+++ core/src/main/java/io/questdb/CallTablesMemory.java
@@ -15,7 +15,7 @@ import io.questdb.log.LogFactory;
 import io.questdb.mp.SynchronizedJob;
 import io.questdb.std.str.DirectString;
 import io.questdb.std.ObjHashSet;
-
+import io.questdb.std.Misc;
 
 import java.io.Closeable;
 import java.util.Timer;
@@ -30,27 +30,28 @@ public class CallTablesMemory extends SynchronizedJob implements Closeable {
 
             for (int t = 0, n = tables.size(); t < n; t++) {
                 TableToken tableToken = tables.get(t);
-                //LOG.info().$("[EDIT] Reading [table=").$(tableToken).I$();
+                LOG.info().$("[EDIT] Reading [table=").$(tableToken).I$();
                 TableReader reader = null;
                 // Since at initialization there's no other worker scanning the table
                 reader = engine.getReaderWithRepair(tableToken);
                 int partitionCount = reader.getPartitionCount();
-                
+
                 for (int partitionIndex = 0; partitionIndex < partitionCount; partitionIndex++) {
                     //LOG.info().$("TESTING").$(partitionCount).I$();
                     long rowCount = reader.openPartition(partitionIndex);
-
                     if (rowCount > 1){
                         for (int columnIndex = 0; columnIndex < reader.getColumnCount(); columnIndex++) {
                             int absoluteIndex = reader.getPrimaryColumnIndex(reader.getColumnBase(partitionIndex), columnIndex);
-                            MemoryR columnData = reader.getColumn(absoluteIndex);
-                            LOG.info().$("[EDIT]").$(columnData);
-                            /* In case, we want to manipulate the tuples through another data structure / debugging purposes*/
-                            /*LOG.info().$("[EDIT] Reading [table=").$(readEntireColumn(columnData, reader.getMetadata().getColumnType(columnIndex), rowCount)).I$();*/
-                        }   
-                    }   
+                            LOG.info().$("[EDIT]").$(reader.getColumn(absoluteIndex)).I$();
+                        }
+                    }
                 }
-            }
+
+                Misc.free(reader);
+
+
+
+                }
         } catch (Throwable th) {
             close();
             throw th;
@@ -98,6 +99,8 @@ public class CallTablesMemory extends SynchronizedJob implements Closeable {
 
     @Override
     public void close() {
+        //this.halt();
+        //LOG.info().$("Background worker stopped").$();
     }
 
     @Override
