68732c4a31dbb931b3913045489d6df26ffbbbbc
glasstiger <imre@questdb.io>
2024-11-28 01:40:56 +0000

    INSERT AS SELECT to return the number of rows inserted

--- core/src/main/java/io/questdb/cutlass/pgwire/modern/PGPipelineEntry.java
+++ core/src/main/java/io/questdb/cutlass/pgwire/modern/PGPipelineEntry.java
@@ -2528,6 +2528,7 @@ public class PGPipelineEntry implements QuietCloseable, Mutable {
                 break;
             case CompiledQuery.INSERT_AS_SELECT:
                 sqlTag = TAG_INSERT_AS_SELECT;
+                sqlAffectedRowCount = cq.getAffectedRowsCount();
                 break;
             case CompiledQuery.SET:
                 sqlTag = TAG_SET;
