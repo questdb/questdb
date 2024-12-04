dd926738fc8e7c1c383bd407915ead84e712dd50
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2024-12-04 20:24:34 +0200

    dedup some code

--- core/rust/qdbr/src/parquet_read/slicer/rle.rs
+++ core/rust/qdbr/src/parquet_read/slicer/rle.rs
@@ -21,11 +21,10 @@ impl RleIterator<'_> {
     }
 }
 
-pub struct RleDictionarySlicer<'a, 'b, T: DictDecoder> {
+struct SlicerInner<'a, 'b> {
     decoder: Option<Decoder<'a>>,
     data: RleIterator<'a>,
     sliced_row_count: usize,
-    dict: T,
     error: Option<ParquetError>,
 
     // TODO(amunra): Clean this up -- non-idiomatic Rust code.
@@ -33,40 +32,108 @@ pub struct RleDictionarySlicer<'a, 'b, T: DictDecoder> {
     error_value: &'b [u8],
 }
 
+impl<'a, 'b> SlicerInner<'a, 'b> {
+    fn new(
+        decoder: Option<Decoder<'a>>,
+        iter: RleIterator<'a>,
+        sliced_row_count: usize,
+        error_value: &'b [u8],
+    ) -> Self {
+        Self {
+            decoder,
+            data: iter,
+            sliced_row_count,
+            error: None,
+            error_value,
+        }
+    }
+
+    fn decode(&mut self) -> ParquetResult<()> {
+        let iter_err = || Err(fmt_err!(Layout, "Unexpected end of rle iterator"));
+
+        let Some(ref mut decoder) = self.decoder else {
+            return iter_err();
+        };
+
+        let Some(run) = decoder.next() else {
+            return iter_err();
+        };
+
+        let num_bits = decoder.num_bits();
+        match run? {
+            HybridEncoded::Bitpacked(values) => {
+                let count = values.len() * 8 / num_bits;
+                let inner_decoder = bitpacked::Decoder::<u32>::try_new(values, num_bits, count)?;
+                self.data = RleIterator::Bitpacked(inner_decoder)
+            }
+            HybridEncoded::Rle(pack, repeat) => {
+                let mut bytes = [0u8; std::mem::size_of::<u32>()];
+                pack.iter().zip(bytes.iter_mut()).for_each(|(src, dst)| {
+                    *dst = *src;
+                });
+                let value = u32::from_le_bytes(bytes);
+                let iterator = std::iter::repeat(value).take(repeat);
+                self.data = RleIterator::Rle(iterator);
+            }
+        }
+        Ok(())
+    }
+
+    fn next_slice(&mut self, _count: usize) -> Option<&[u8]> {
+        // Not supported
+        None
+    }
+
+    fn count(&self) -> usize {
+        self.sliced_row_count
+    }
+
+    fn result(&self) -> ParquetResult<()> {
+        match &self.error {
+            Some(err) => Err(err.clone()),
+            None => Ok(()),
+        }
+    }
+}
+
+pub struct RleDictionarySlicer<'a, 'b, T: DictDecoder> {
+    dict: T,
+    inner: SlicerInner<'a, 'b>,
+}
+
 impl<T: DictDecoder> DataPageSlicer for RleDictionarySlicer<'_, '_, T> {
     // TODO(amunra): Clean this up -- non-idiomatic Rust code -- Should this just be a
     //               fn next(&mut self) -> Option<Result<&[u8], ParquetReadError>> ?
     fn next(&mut self) -> &[u8] {
-        if self.error.is_some() {
-            return self.error_value;
+        if self.inner.error.is_some() {
+            return self.inner.error_value;
         }
 
-        if let Some(idx) = self.data.next() {
+        if let Some(idx) = self.inner.data.next() {
             if idx < self.dict.len() {
                 self.dict.get_dict_value(idx)
             } else {
-                self.error = Some(fmt_err!(
+                self.inner.error = Some(fmt_err!(
                     Layout,
                     "index {} is out of dict bounds {}",
                     idx,
                     self.dict.len()
                 ));
-                self.error_value
+                self.inner.error_value
             }
         } else {
             match self.decode() {
                 Ok(()) => self.next(),
                 Err(err) => {
-                    self.error = Some(err);
-                    self.error_value
+                    self.inner.error = Some(err);
+                    self.inner.error_value
                 }
             }
         }
     }
 
     fn next_slice(&mut self, _count: usize) -> Option<&[u8]> {
-        // Not supported
-        None
+        self.inner.next_slice(_count)
     }
 
     fn skip(&mut self, count: usize) {
@@ -76,18 +143,15 @@ impl<T: DictDecoder> DataPageSlicer for RleDictionarySlicer<'_, '_, T> {
     }
 
     fn count(&self) -> usize {
-        self.sliced_row_count
+        self.inner.count()
     }
 
     fn data_size(&self) -> usize {
-        (self.sliced_row_count as f32 * self.dict.avg_key_len()) as usize
+        (self.inner.sliced_row_count as f32 * self.dict.avg_key_len()) as usize
     }
 
     fn result(&self) -> ParquetResult<()> {
-        match &self.error {
-            Some(err) => Err(err.clone()),
-            None => Ok(()),
-        }
+        self.inner.result()
     }
 }
 
@@ -104,53 +168,31 @@ impl<'a, 'b, T: DictDecoder> RleDictionarySlicer<'a, 'b, T> {
             buffer = &buffer[1..];
             let decoder = Decoder::new(buffer, num_bits as usize);
             let mut res = Self {
-                decoder: Some(decoder),
-                data: RleIterator::Rle(std::iter::repeat(0).take(0)),
-                sliced_row_count,
                 dict,
-                error: None,
-                error_value,
+                inner: SlicerInner::new(
+                    Some(decoder),
+                    RleIterator::Rle(std::iter::repeat(0).take(0)),
+                    sliced_row_count,
+                    error_value,
+                ),
             };
             res.decode()?;
             Ok(res)
         } else {
             Ok(Self {
-                decoder: None,
-                data: RleIterator::Rle(std::iter::repeat(0).take(row_count)),
-                sliced_row_count,
                 dict,
-                error: None,
-                error_value,
+                inner: SlicerInner::new(
+                    None,
+                    RleIterator::Rle(std::iter::repeat(0).take(row_count)),
+                    sliced_row_count,
+                    error_value,
+                ),
             })
         }
     }
 
     fn decode(&mut self) -> ParquetResult<()> {
-        if let Some(ref mut decoder) = self.decoder {
-            if let Some(run) = decoder.next() {
-                let encoded = run?;
-                if let HybridEncoded::Bitpacked(values) = encoded {
-                    let count = values.len() * 8 / decoder.num_bits();
-                    let inner_decoder =
-                        bitpacked::Decoder::<u32>::try_new(values, decoder.num_bits(), count)?;
-                    self.data = RleIterator::Bitpacked(inner_decoder);
-                    return Ok(());
-                } else if let HybridEncoded::Rle(pack, repeat) = encoded {
-                    let mut bytes = [0u8; std::mem::size_of::<u32>()];
-                    pack.iter().zip(bytes.iter_mut()).for_each(|(src, dst)| {
-                        *dst = *src;
-                    });
-                    let value = u32::from_le_bytes(bytes);
-                    let iterator = std::iter::repeat(value).take(repeat);
-                    self.data = RleIterator::Rle(iterator);
-                    return Ok(());
-                } else {
-                    return Err(fmt_err!(Unsupported, "encoding not supported: {encoded:?}"));
-                }
-            }
-        }
-        // TODO(amunra): Not a layout error. This is just an iterator pattern gone wrong, needs cleaning up.
-        Err(fmt_err!(Layout, "Unexpected end of rle iterator"))
+        self.inner.decode()
     }
 }
 
@@ -160,41 +202,32 @@ impl<'a, 'b, T: DictDecoder> RleDictionarySlicer<'a, 'b, T> {
 /// This is an optimisation for specially-encoded columns that skips mapping the parquet
 /// symbol keys to the global keys.
 pub struct RleLocalIsGlobalSymbolDecoder<'a, 'b> {
-    decoder: Option<Decoder<'a>>,
-    data: RleIterator<'a>,
     next_value: [u8; 4],
-    sliced_row_count: usize,
-    error: Option<ParquetError>,
-
-    // TODO(amunra): Clean this up -- non-idiomatic Rust code.
-    //               Use the type system instead of magic values.
-    error_value: &'b [u8],
+    inner: SlicerInner<'a, 'b>,
 }
 
 impl DataPageSlicer for RleLocalIsGlobalSymbolDecoder<'_, '_> {
-    // TODO(amunra): Deduplicate this code.
     fn next(&mut self) -> &[u8] {
-        if self.error.is_some() {
-            return self.error_value;
+        if self.inner.error.is_some() {
+            return self.inner.error_value;
         }
 
-        if let Some(idx) = self.data.next() {
+        if let Some(idx) = self.inner.data.next() {
             self.next_value.copy_from_slice(&idx.to_le_bytes());
             &self.next_value
         } else {
             match self.decode() {
                 Ok(()) => self.next(),
                 Err(err) => {
-                    self.error = Some(err);
-                    self.error_value
+                    self.inner.error = Some(err);
+                    self.inner.error_value
                 }
             }
         }
     }
 
     fn next_slice(&mut self, _count: usize) -> Option<&[u8]> {
-        // Not supported
-        None
+        self.inner.next_slice(_count)
     }
 
     fn skip(&mut self, count: usize) {
@@ -204,18 +237,15 @@ impl DataPageSlicer for RleLocalIsGlobalSymbolDecoder<'_, '_> {
     }
 
     fn count(&self) -> usize {
-        self.sliced_row_count
+        self.inner.count()
     }
 
     fn data_size(&self) -> usize {
-        self.sliced_row_count * std::mem::size_of::<u32>()
+        self.inner.sliced_row_count * size_of::<u32>()
     }
 
     fn result(&self) -> ParquetResult<()> {
-        match &self.error {
-            Some(err) => Err(err.clone()),
-            None => Ok(()),
-        }
+        self.inner.result()
     }
 }
 
@@ -226,63 +256,35 @@ impl<'a, 'b> RleLocalIsGlobalSymbolDecoder<'a, 'b> {
         sliced_row_count: usize,
         error_value: &'b [u8],
     ) -> ParquetResult<Self> {
-        // TODO(amunra): Deduplicate this code.
         let num_bits = buffer[0];
         if num_bits > 0 {
             buffer = &buffer[1..];
             let decoder = Decoder::new(buffer, num_bits as usize);
             let mut res = Self {
-                decoder: Some(decoder),
-                data: RleIterator::Rle(std::iter::repeat(0).take(0)),
                 next_value: [0; 4],
-                sliced_row_count,
-                error: None,
-                error_value,
+                inner: SlicerInner::new(
+                    Some(decoder),
+                    RleIterator::Rle(std::iter::repeat(0).take(0)),
+                    sliced_row_count,
+                    error_value,
+                ),
             };
             res.decode()?;
             Ok(res)
         } else {
             Ok(Self {
-                decoder: None,
-                data: RleIterator::Rle(std::iter::repeat(0).take(row_count)),
                 next_value: [0; 4],
-                sliced_row_count,
-                error: None,
-                error_value,
+                inner: SlicerInner::new(
+                    None,
+                    RleIterator::Rle(std::iter::repeat(0).take(row_count)),
+                    sliced_row_count,
+                    error_value,
+                ),
             })
         }
     }
 
     fn decode(&mut self) -> ParquetResult<()> {
-        // TODO(amunra): Deduplicate this code.
-        let iter_err = || Err(fmt_err!(Layout, "Unexpected end of rle iterator"));
-
-        let Some(ref mut decoder) = self.decoder else {
-            return iter_err();
-        };
-
-        let Some(run) = decoder.next() else {
-            return iter_err();
-        };
-
-        match run? {
-            HybridEncoded::Bitpacked(values) => {
-                let count = values.len() * 8 / decoder.num_bits();
-                let inner_decoder =
-                    bitpacked::Decoder::<u32>::try_new(values, decoder.num_bits(), count)?;
-                self.data = RleIterator::Bitpacked(inner_decoder);
-                Ok(())
-            }
-            HybridEncoded::Rle(pack, repeat) => {
-                let mut bytes = [0u8; std::mem::size_of::<u32>()];
-                pack.iter().zip(bytes.iter_mut()).for_each(|(src, dst)| {
-                    *dst = *src;
-                });
-                let value = u32::from_le_bytes(bytes);
-                let iterator = std::iter::repeat(value).take(repeat);
-                self.data = RleIterator::Rle(iterator);
-                Ok(())
-            }
-        }
+        self.inner.decode()
     }
 }
