0ad62a70fc157e8c77b83d355cf208268cec3726
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-12-04 17:07:59 +0000

    fix test

--- core/src/main/java/io/questdb/griffin/OperatorExpression.java
+++ core/src/main/java/io/questdb/griffin/OperatorExpression.java
@@ -26,12 +26,14 @@ package io.questdb.griffin;
 
 import io.questdb.std.ObjList;
 
+// Removing the type breaks JDK 8
+@SuppressWarnings("Convert2Diamond")
 public final class OperatorExpression {
     public static final int BINARY = 2;
     public static final int SET = 3;
     public static final int UNARY = 1;
     private static final OperatorRegistry legacyRegistry = new OperatorRegistry(
-            new ObjList<>() {{
+            new ObjList<OperatorExpression>() {{
                 add(new OperatorExpression(Operator.UnaryMinus, 3, false, UNARY));
                 add(new OperatorExpression(Operator.UnaryComplement, 3, false, UNARY));
                 add(new OperatorExpression(Operator.UnarySetNegation, 11, false, UNARY));
@@ -77,7 +79,7 @@ public final class OperatorExpression {
                 add(new OperatorExpression(Operator.BinaryOr, 11, true, BINARY, false));
             }});
     private static final OperatorRegistry registry = new OperatorRegistry(
-            new ObjList<>() {{
+            new ObjList<OperatorExpression>() {{
                 add(new OperatorExpression(Operator.DeclareVariableAssignment, 100, false, BINARY));
                 add(new OperatorExpression(Operator.UnaryMinus, 3, false, UNARY));
                 add(new OperatorExpression(Operator.UnaryComplement, 3, false, UNARY));
