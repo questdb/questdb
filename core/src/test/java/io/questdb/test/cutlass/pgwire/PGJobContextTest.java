04aebc4ee1acb0db5cf2658967733fd1bd2a4ea3
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2024-12-04 16:07:23 +0000

    fix test

--- core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
+++ core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
@@ -7328,7 +7328,7 @@ nodejs code:
                 ps1.executeQuery();
                 Assert.fail("PSQLException should be thrown");
             } catch (PSQLException e) {
-                assertContains(e.getMessage(), "there is no matching operator`!=` with the argument types: BOOLEAN != STRING");
+                assertContains(e.getMessage(), "there is no matching operator `!=` with the argument types: BOOLEAN != STRING");
             }
 
             try (PreparedStatement s = connection.prepareStatement("select 2 a,2 b from long_sequence(1) where x > 0 and x < 10")) {
