9e8b96e5501e23337709f867c40ad08a8c6773e1
vicgao <vicgao@tencent.com>
2024-12-14 01:14:48 +0800

    adding tests.

--- core/src/main/java/io/questdb/griffin/engine/functions/window/AvgDoubleWindowFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/window/AvgDoubleWindowFunctionFactory.java
@@ -1316,7 +1316,7 @@ public class AvgDoubleWindowFunctionFactory extends AbstractWindowFunctionFactor
             sink.val(" over (");
             sink.val("partition by ");
             sink.val(partitionByRecord.getFunctions());
-            sink.val(" rows between unbounded preceding and current row )");
+            sink.val(" rows between unbounded preceding and current row)");
         }
     }
 
