17622b5840ee3286aaa731ad88a46cd056508967
glasstiger <imre@questdb.io>
2025-01-03 17:30:56 +0000

    ilp over http soft connection limit test

--- core/src/test/java/io/questdb/test/cutlass/http/HttpConnectionCountTest.java
+++ core/src/test/java/io/questdb/test/cutlass/http/HttpConnectionCountTest.java
@@ -39,6 +39,8 @@ import io.questdb.cutlass.http.client.HttpClient;
 import io.questdb.cutlass.http.client.HttpClientFactory;
 import io.questdb.cutlass.http.client.Response;
 import io.questdb.cutlass.http.processors.JsonQueryProcessor;
+import io.questdb.cutlass.http.processors.LineHttpProcessor;
+import io.questdb.cutlass.line.LineSenderException;
 import io.questdb.mp.SOCountDownLatch;
 import io.questdb.mp.WorkerPool;
 import io.questdb.network.PeerDisconnectedException;
@@ -49,7 +51,6 @@ import io.questdb.std.str.Utf8s;
 import io.questdb.test.AbstractBootstrapTest;
 import io.questdb.test.tools.TestUtils;
 import org.jetbrains.annotations.Nullable;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.BrokenBarrierException;
@@ -57,32 +58,31 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.questdb.PropertyKey.*;
+import static io.questdb.test.tools.TestUtils.*;
 import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class HttpConnectionCountTest extends AbstractBootstrapTest {
+    private static final String EXEC_TEST_URI = "/exec-test";
     private static final String EXEC_URI = "/exec";
-    private static final String TEST_URI = "/exec-test";
-
-    @Before
-    public void setUp() {
-        super.setUp();
-        TestUtils.unchecked(() -> createDummyConfiguration(
-                METRICS_ENABLED + "=true",
-                HTTP_WORKER_COUNT + "=6",
-                HTTP_QUERY_CONNECTION_LIMIT + "=4"
-        ));
-        dbPath.parent().$();
-    }
+    private static final String ILP_PATH = "/write";
+    private static final String ILP_TEST_PATH = "/write-test";
 
     @Test
-    public void testQueryConnectionLimit() throws Exception {
+    public void testIlpConnectionLimit() throws Exception {
+        unchecked(() -> createDummyConfiguration(
+                METRICS_ENABLED + "=true",
+                HTTP_WORKER_COUNT + "=6",
+                HTTP_ILP_CONNECTION_LIMIT + "=4"
+        ));
+
         final int numOfThreads = 4;
         final CyclicBarrier limitBarrier = new CyclicBarrier(numOfThreads + 1);
         final CyclicBarrier endBarrier = new CyclicBarrier(numOfThreads + 1);
 
-        TestUtils.assertMemoryLeak(() -> {
+        assertMemoryLeak(() -> {
             try (final ServerMain serverMain = new ServerMain(getServerMainArgs()) {
                 @Override
                 protected Services services() {
@@ -94,7 +94,120 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
                                 server.bind(new HttpRequestProcessorFactory() {
                                     @Override
                                     public String getUrl() {
-                                        return TEST_URI;
+                                        return ILP_TEST_PATH;
+                                    }
+
+                                    @Override
+                                    public HttpRequestProcessor newInstance() {
+                                        return new LineHttpProcessor(
+                                                cairoEngine,
+                                                configuration.getHttpServerConfiguration().getRecvBufferSize(),
+                                                configuration.getHttpServerConfiguration().getSendBufferSize(),
+                                                configuration.getHttpServerConfiguration().getLineHttpProcessorConfiguration()
+                                        ) {
+                                            @Override
+                                            public void onRequestComplete(
+                                                    HttpConnectionContext context
+                                            ) throws PeerDisconnectedException, PeerIsSlowToReadException {
+                                                super.onRequestComplete(context);
+
+                                                await(limitBarrier);
+                                                await(endBarrier);
+                                            }
+                                        };
+                                    }
+                                });
+                            }
+                            return server;
+                        }
+                    };
+                }
+            }) {
+                serverMain.start();
+
+                final AtomicInteger errorCount = new AtomicInteger();
+                final SOCountDownLatch end = new SOCountDownLatch(numOfThreads);
+                for (int i = 0; i < numOfThreads; i++) {
+                    final int threadIndex = i;
+                    new Thread(() -> {
+                        try (Sender sender = Sender.builder(Sender.Transport.HTTP)
+                                .address("localhost:" + serverMain.getHttpServerPort())
+                                .httpPath(ILP_TEST_PATH)
+                                .build()
+                        ) {
+                            sender.table("tab").longColumn("col", 1).atNow();
+                        } catch (Throwable e) {
+                            errorCount.incrementAndGet();
+                            LOG.error().$("Error while ingesting [thread=").$(threadIndex).$(", error=").$(e.getMessage()).$(']').$();
+                        }
+                        end.countDown();
+                    }).start();
+                }
+
+                limitBarrier.await();
+                // at this point limit has been reached
+
+                // ilp over http, sender should fail with soft limit breach
+                try (Sender sender = Sender.builder(Sender.Transport.HTTP)
+                        .address("localhost:" + serverMain.getHttpServerPort())
+                        .httpPath(ILP_PATH)
+                        .build()
+                ) {
+                    sender.table("tab").longColumn("col", 2).atNow();
+                    try {
+                        sender.flush();
+                        fail("sender should fail with soft limit breach");
+                    } catch (LineSenderException e) {
+                        assertContains(e.getMessage(), "Could not flush buffer: exceeded HTTP connection soft limit [name=line_http_connections]");
+                    }
+                } catch (Throwable e) {
+                    errorCount.incrementAndGet();
+                    LOG.error().$("Error while ingesting [error=").$(e.getMessage()).$(']').$();
+                }
+
+                // http query, should be able to connect
+                try (final HttpClient httpClient = HttpClientFactory.newPlainTextInstance(new DefaultHttpClientConfiguration())) {
+                    assertExecRequest(httpClient, EXEC_URI, "select 1", HTTP_OK,
+                            "{\"query\":\"select 1\",\"columns\":[{\"name\":\"1\",\"type\":\"INT\"}],\"timestamp\":-1,\"dataset\":[[1]],\"count\":1}"
+                    );
+                } catch (Throwable e) {
+                    errorCount.incrementAndGet();
+                    LOG.error().$("Error executing query [error=").$(e.getMessage()).$(']').$();
+                }
+
+                // let threads finish
+                endBarrier.await();
+                end.await();
+                assertEquals(0, errorCount.get());
+            }
+        });
+    }
+
+    @Test
+    public void testQueryConnectionLimit() throws Exception {
+        unchecked(() -> createDummyConfiguration(
+                METRICS_ENABLED + "=true",
+                HTTP_WORKER_COUNT + "=6",
+                HTTP_QUERY_CONNECTION_LIMIT + "=4"
+        ));
+
+        final int numOfThreads = 4;
+        final CyclicBarrier limitBarrier = new CyclicBarrier(numOfThreads + 1);
+        final CyclicBarrier endBarrier = new CyclicBarrier(numOfThreads + 1);
+
+        assertMemoryLeak(() -> {
+            try (final ServerMain serverMain = new ServerMain(getServerMainArgs()) {
+                @Override
+                protected Services services() {
+                    return new Services() {
+                        @Override
+                        public @Nullable HttpServer createHttpServer(ServerConfiguration configuration, CairoEngine cairoEngine, WorkerPool workerPool, int sharedWorkerCount) {
+                            HttpServer server = super.createHttpServer(configuration, cairoEngine, workerPool, sharedWorkerCount);
+                            if (server != null) {
+                                server.bind(new HttpRequestProcessorFactory() {
+                                    @Override
+                                    public String getUrl() {
+                                        return EXEC_TEST_URI;
                                     }
 
                                     @Override
@@ -131,7 +244,7 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
                     final int threadIndex = i;
                     new Thread(() -> {
                         try (final HttpClient httpClient = HttpClientFactory.newPlainTextInstance(new DefaultHttpClientConfiguration())) {
-                            assertTestRequest(httpClient, "select 1", HTTP_OK,
+                            assertExecRequest(httpClient, EXEC_TEST_URI, "select 1", HTTP_OK,
                                     "{\"query\":\"select 1\",\"columns\":[{\"name\":\"1\",\"type\":\"INT\"}],\"timestamp\":-1,\"dataset\":[[1]],\"count\":1}"
                             );
                         } catch (Throwable e) {
@@ -145,9 +258,9 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
                 limitBarrier.await();
                 // at this point limit has been reached
 
-                // http query should fail with soft limit breach
+                // http query, should fail with soft limit breach
                 try (final HttpClient httpClient = HttpClientFactory.newPlainTextInstance(new DefaultHttpClientConfiguration())) {
-                    assertExecRequest(httpClient, "select 2", HTTP_BAD_REQUEST,
+                    assertExecRequest(httpClient, EXEC_URI, "select 2", HTTP_BAD_REQUEST,
                             "exceeded HTTP connection soft limit [name=json_http_connections]\r\n"
                     );
                 } catch (Throwable e) {
@@ -161,6 +274,10 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
                         .build()
                 ) {
                     sender.table("tab").longColumn("col", 1).atNow();
+                    sender.flush();
+                } catch (Throwable e) {
+                    errorCount.incrementAndGet();
+                    LOG.error().$("Sender error [error=").$(e.getMessage()).$(']').$();
                 }
 
                 // let threads finish
@@ -180,15 +297,6 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
     }
 
     private void assertExecRequest(
-            HttpClient httpClient,
-            String sql,
-            int expectedHttpStatusCode,
-            String expectedHttpResponse
-    ) {
-        assertRequest(httpClient, EXEC_URI, sql, expectedHttpStatusCode, expectedHttpResponse);
-    }
-
-    private void assertRequest(
             HttpClient httpClient,
             String uri,
             String sql,
@@ -213,14 +321,5 @@ public class HttpConnectionCountTest extends AbstractBootstrapTest {
             TestUtils.assertEquals(expectedHttpResponse, sink.toString());
             sink.clear();
         }
-    }
-
-    private void assertTestRequest(
-            HttpClient httpClient,
-            String sql,
-            int expectedHttpStatusCode,
-            String expectedHttpResponse
-    ) {
-        assertRequest(httpClient, TEST_URI, sql, expectedHttpStatusCode, expectedHttpResponse);
     }
 }
