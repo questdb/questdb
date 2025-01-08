4cbc88d86b04d90d103cefe5c9a256900ca377ec
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2025-01-08 20:31:47 +0000

    chore(sql): shift ParallelGroupByFuzzTest out of `griffin` (#5298)

--- core/src/test/java/io/questdb/test/cairo/fuzz/ParallelGroupByFuzzTest.java
+++ core/src/test/java/io/questdb/test/cairo/fuzz/ParallelGroupByFuzzTest.java
@@ -22,7 +22,7 @@
  *
  ******************************************************************************/
 
-package io.questdb.test.griffin;
+package io.questdb.test.cairo.fuzz;
 
 import io.questdb.PropertyKey;
 import io.questdb.cairo.CairoEngine;
