5086fab890d61154e43a94ce69df5c7a85397e99
Vlad Ilyushchenko <bluestreak01@users.noreply.github.com>
2025-01-10 11:49:45 +0000

    chore(pgwire): followup for 5296 with a better fix (#5305)

--- core/src/main/java/io/questdb/cutlass/pgwire/modern/PGConnectionContextModern.java
+++ core/src/main/java/io/questdb/cutlass/pgwire/modern/PGConnectionContextModern.java
@@ -884,8 +884,10 @@ public class PGConnectionContextModern extends IOContext<PGConnectionContextMode
             if (pipelineCurrentEntry == null) {
                 pipelineCurrentEntry = entryPool.next();
             }
-        } else {
-            releaseToPoolIfAbandoned(pipelineCurrentEntry);
+            // we are liable to look up the current entry, depending on how protocol is used
+            // if this the case, we should not attempt to save the current entry prematurely
+        } else if (lookedUpPipelineEntry != pipelineCurrentEntry) {
+            addPipelineEntry();
             pipelineCurrentEntry = lookedUpPipelineEntry;
         }
 
