80b2e293201d33d72417db5d0b4bdbfdb499298a
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-01-20 16:49:54 +0100

    test adjusted

--- core/src/test/java/io/questdb/test/griffin/DBeaverTest.java
+++ core/src/test/java/io/questdb/test/griffin/DBeaverTest.java
@@ -118,8 +118,8 @@ public class DBeaverTest extends AbstractCairoTest {
 
             assertQueryNoLeakCheck(
                     "relname\tattrelid\tattname\tattnum\tatttypid\tattnotnull\tatttypmod\tattlen\tattidentity\tattisdropped\tatthasdef\tdef_value\tdescription\n" +
-                            "xyz\t1\ta\t1\t23\tfalse\t0\t4\t\tfalse\ttrue\t\t\n" +
-                            "xyz\t1\tt\t2\t1114\tfalse\t0\t-1\t\tfalse\ttrue\t\t\n",
+                            "xyz\t1\ta\t1\t23\tfalse\t-1\t4\t\tfalse\ttrue\t\t\n" +
+                            "xyz\t1\tt\t2\t1114\tfalse\t-1\t8\t\tfalse\ttrue\t\t\n",
                     "SELECT \n" +
                             "    c.relname,\n" +
                             "    a.*,\n" +
