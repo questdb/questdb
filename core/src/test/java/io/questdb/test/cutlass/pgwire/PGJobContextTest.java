f129f739dd93e67849cdb34bc67f10cc25552727
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-01-28 15:54:46 +0100

    test(core): flapping ParquetTest.testAllTypesColTopMiddlePartition() fixed (#5338)

--- core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
+++ core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
@@ -11944,7 +11944,6 @@ create table tab as (
                 ts.setNanos((int) ((micros % 1_000_000) * 1000));
                 statement.setTimestamp(1, ts);
                 statement.setTimestamp(2, ts);
-                statement.executeQuery();
                 rs = statement.executeQuery();
 
                 long finalMicros = micros;
