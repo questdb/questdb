a19527f201c632fcf7fbd4b3cd699ea206a4bae7
Marko Topolnik <marko.topolnik@gmail.com>
2025-02-04 11:44:07 +0100

    Auto-reorder code in SqlKeywords

--- core/src/main/java/io/questdb/griffin/SqlKeywords.java
+++ core/src/main/java/io/questdb/griffin/SqlKeywords.java
@@ -758,6 +758,16 @@ public class SqlKeywords {
                 && (tok.charAt(1) | 32) == 'f';
     }
 
+    public static boolean isIgnoreWord(CharSequence tok) {
+        return tok.length() == 6
+                && (tok.charAt(0) | 32) == 'i'
+                && (tok.charAt(1) | 32) == 'g'
+                && (tok.charAt(2) | 32) == 'n'
+                && (tok.charAt(3) | 32) == 'o'
+                && (tok.charAt(4) | 32) == 'r'
+                && (tok.charAt(5) | 32) == 'e';
+    }
+
     public static boolean isInKeyword(CharSequence tok) {
         return tok.length() == 2
                 && (tok.charAt(0) | 32) == 'i'
@@ -1227,6 +1237,15 @@ public class SqlKeywords {
                 && (tok.byteAt(3) | 32) == 'l';
     }
 
+    public static boolean isNullsWord(CharSequence tok) {
+        return tok.length() == 5
+                && (tok.charAt(0) | 32) == 'n'
+                && (tok.charAt(1) | 32) == 'u'
+                && (tok.charAt(2) | 32) == 'l'
+                && (tok.charAt(3) | 32) == 'l'
+                && (tok.charAt(4) | 32) == 's';
+    }
+
     public static boolean isO3MaxLagKeyword(CharSequence tok) {
         return tok.length() == 8
                 && (tok.charAt(0) | 32) == 'o'
@@ -1465,6 +1484,17 @@ public class SqlKeywords {
                 && (tok.charAt(3) | 32) == 'a'
                 && (tok.charAt(4) | 32) == 'm'
                 && (tok.charAt(5) | 32) == 'e';
+    }
+
+    public static boolean isRespectWord(CharSequence tok) {
+        return tok.length() == 7
+                && (tok.charAt(0) | 32) == 'r'
+                && (tok.charAt(1) | 32) == 'e'
+                && (tok.charAt(2) | 32) == 's'
+                && (tok.charAt(3) | 32) == 'p'
+                && (tok.charAt(4) | 32) == 'e'
+                && (tok.charAt(5) | 32) == 'c'
+                && (tok.charAt(6) | 32) == 't';
     }
 
     public static boolean isResumeKeyword(CharSequence tok) {
@@ -1971,36 +2001,6 @@ public class SqlKeywords {
                 && (tok.charAt(1) | 32) == 'o'
                 && (tok.charAt(2) | 32) == 'n'
                 && (tok.charAt(3) | 32) == 'e';
-    }
-
-    public static boolean isRespectWord(CharSequence tok) {
-        return tok.length() == 7
-                && (tok.charAt(0) | 32) == 'r'
-                && (tok.charAt(1) | 32) == 'e'
-                && (tok.charAt(2) | 32) == 's'
-                && (tok.charAt(3) | 32) == 'p'
-                && (tok.charAt(4) | 32) == 'e'
-                && (tok.charAt(5) | 32) == 'c'
-                && (tok.charAt(6) | 32) == 't';
-    }
-
-    public static boolean isIgnoreWord(CharSequence tok) {
-        return tok.length() == 6
-                && (tok.charAt(0) | 32) == 'i'
-                && (tok.charAt(1) | 32) == 'g'
-                && (tok.charAt(2) | 32) == 'n'
-                && (tok.charAt(3) | 32) == 'o'
-                && (tok.charAt(4) | 32) == 'r'
-                && (tok.charAt(5) | 32) == 'e';
-    }
-
-    public static boolean isNullsWord(CharSequence tok) {
-        return tok.length() == 5
-                && (tok.charAt(0) | 32) == 'n'
-                && (tok.charAt(1) | 32) == 'u'
-                && (tok.charAt(2) | 32) == 'l'
-                && (tok.charAt(3) | 32) == 'l'
-                && (tok.charAt(4) | 32) == 's';
     }
 
     public static boolean startsWithGeoHashKeyword(CharSequence tok) {
