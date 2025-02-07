0f52227075a4b8de3e89bf6bb6d104258f37b313
Marko Topolnik <marko.topolnik@gmail.com>
2025-02-07 16:32:42 +0100

    Fix test assertion

--- core/src/test/java/io/questdb/test/griffin/AlterTableAddColumnTest.java
+++ core/src/test/java/io/questdb/test/griffin/AlterTableAddColumnTest.java
@@ -703,7 +703,7 @@ public class AlterTableAddColumnTest extends AbstractCairoTest {
                             "\tm BINARY,\n" +
                             "\tn STRING,\n" +
                             "\tarr INT[]\n" + // <-- array should be present
-                            ") timestamp(timestamp) BYPASS WAL\n" +
+                            ") timestamp(timestamp) PARTITION BY NONE BYPASS WAL\n" +
                             "WITH maxUncommittedRows=1000, o3MaxLag=300000000us;\n",
                     "show create table x;");
         });
