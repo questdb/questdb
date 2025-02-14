6c6db47beae21677e1caaf84a6a65d102aae2459
Marko Topolnik <marko.topolnik@gmail.com>
2025-02-14 11:15:04 +0100

    getExtendedOps() == null instead of instanceof ScalarFunction

--- core/src/main/java/io/questdb/griffin/FunctionParser.java
+++ core/src/main/java/io/questdb/griffin/FunctionParser.java
@@ -329,7 +329,7 @@ public class FunctionParser implements PostOrderTreeTraversalAlgo.Visitor, Mutab
             final Function function = functionStack.poll();
             positionStack.pop();
             assert positionStack.size() == functionStack.size();
-            if (function != null && function.isConstant() && (function instanceof ScalarFunction)) {
+            if (function != null && function.isConstant() && function.getExtendedOps() == null) {
                 return functionToConstant(function);
             }
             return function;
