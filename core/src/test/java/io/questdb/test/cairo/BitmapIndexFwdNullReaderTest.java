540aee427a955da3159d13eff00c463440cf58b4
Andrei Pechkurov <apechkurov@gmail.com>
2025-02-28 19:39:54 +0200

    Improve null reader tests

--- core/src/test/java/io/questdb/test/cairo/BitmapIndexFwdNullReaderTest.java
+++ core/src/test/java/io/questdb/test/cairo/BitmapIndexFwdNullReaderTest.java
@@ -31,7 +31,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BitmapIndexFwdNullReaderTest {
-
     private static final BitmapIndexFwdNullReader reader = new BitmapIndexFwdNullReader();
 
     @Test
@@ -43,14 +42,18 @@ public class BitmapIndexFwdNullReaderTest {
     public void testCursor() {
         final Rnd rnd = new Rnd();
         for (int i = 0; i < 10; i++) {
-            int n = rnd.nextPositiveInt() % 1024;
+            final int n = rnd.nextPositiveInt() % 1024;
+
             int m = 0;
             RowCursor cursor = reader.getCursor(true, 0, 0, n);
             while (cursor.hasNext()) {
                 Assert.assertEquals(m++, cursor.next());
             }
-
             Assert.assertEquals(n + 1, m);
+
+            // non-null key
+            cursor = reader.getCursor(true, 42, 0, n);
+            Assert.assertFalse(cursor.hasNext());
         }
     }
 
@@ -69,5 +72,4 @@ public class BitmapIndexFwdNullReaderTest {
         Assert.assertEquals(0, reader.getUnIndexedNullCount());
         Assert.assertEquals(0, reader.getValueBlockCapacity());
     }
-
 }
