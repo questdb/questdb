28758747b9612e5f1a33a3d8d5dea9772a47ad53
glasstiger <imre@questdb.io>
2025-03-03 18:26:00 +0000

    fix OperationCodes

--- core/src/main/java/io/questdb/cairo/OperationCodes.java
+++ core/src/main/java/io/questdb/cairo/OperationCodes.java
@@ -1,10 +1,10 @@
 package io.questdb.cairo;
 
 public class OperationCodes {
-    public static final int CREATE_MAT_VIEW = 4;
     public static final int CREATE_TABLE = 1;
-    public static final int DROP_ALL = 3;
-    public static final int DROP_MAT_VIEW = 5;
-    public static final int DROP_TABLE = 2;
-    public static final int MAX = CREATE_MAT_VIEW;
+    public static final int DROP_TABLE = CREATE_TABLE + 1;
+    public static final int DROP_ALL = DROP_TABLE + 1;
+    public static final int CREATE_MAT_VIEW = DROP_ALL + 1;
+    public static final int DROP_MAT_VIEW = CREATE_MAT_VIEW + 1;
+    public static final int MAX = DROP_MAT_VIEW;
 }
