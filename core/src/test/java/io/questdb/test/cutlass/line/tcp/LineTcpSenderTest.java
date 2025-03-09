6cc7a1c04224fbef2bec11e7fa4b10509e14e68a
Marko Topolnik <marko.topolnik@gmail.com>
2025-03-09 14:11:04 +0100

    Auto-rearrange methods

--- core/src/test/java/io/questdb/test/cutlass/line/tcp/LineTcpSenderTest.java
+++ core/src/test/java/io/questdb/test/cutlass/line/tcp/LineTcpSenderTest.java
@@ -272,6 +272,47 @@ public class LineTcpSenderTest extends AbstractLineTcpReceiverTest {
     }
 
     @Test
+    public void testDoubleArrayTest() throws Exception {
+        runInContext(r -> {
+            try (Sender sender = Sender.builder(Sender.Transport.TCP)
+                    .address("127.0.0.1")
+                    .port(bindPort)
+                    .build()) {
+                String table = "nd_test";
+                long ts = IntervalUtils.parseFloorPartialTimestamp("2025-02-22");
+                sender.table(table)
+                        .symbol("x", "42i")
+                        .symbol("y", "[6f1.0,2.5,3.0,4.5,5.0]")  // ensuring no array parsing for symbol
+                        .longColumn("l1", 23452345)
+                        .doubleArray("a1", (double[]) createDoubleArray(new int[]{5}))
+                        .doubleArray("a2", (double[][]) createDoubleArray(new int[]{2, 3}))
+                        .doubleArray("a3", (double[][][]) createDoubleArray(new int[]{1, 2, 3}))
+                        .doubleArray("a4", DoubleArray.create(4.0, 1, 1, 2, 1))
+                        .doubleArray("a5", DoubleArray.create(5.0, 3, 2, 1, 4, 1))
+                        .doubleArray("a6", DoubleArray.create(6.0, 1, 3, 4, 2, 1, 1))
+                        .doubleArray("a7", DoubleArray.create(7.0, 2, 2, 2, 1, 1, 1, 2))
+                        .doubleArray("a8", DoubleArray.create(8.0, 1, 1, 2, 1, 1, 1, 2, 1))
+                        .doubleArray("a9", DoubleArray.create(9.0, 1, 2, 1, 2, 1, 1, 2, 1, 1))
+                        .doubleArray("a10", DoubleArray.create(10.0, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2))
+                        .doubleArray("a11", DoubleArray.create(11.0, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2))
+                        .doubleArray("a12", DoubleArray.create(12.0, 1, 2, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1))
+                        .doubleArray("a13", DoubleArray.create(13.0, 1, 1, 2, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1))
+                        .doubleArray("a14", DoubleArray.create(14.0, 1, 1, 3, 1, 1, 1, 1, 1, 2, 1, 1, 2, 2, 1))
+                        .doubleArray("a15", DoubleArray.create(15.0, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 3, 1, 1, 1))
+                        .doubleArray("a16", DoubleArray.create(16.0, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+                        .at(ts, ChronoUnit.MICROS);
+                sender.flush();
+
+                assertTableSizeEventually(engine, table, 1);
+                // @todo getArray support in TestTableReadCursor
+/*                try (TableReader reader = getReader(table)) {
+                    TestUtils.assertReader("", reader, new StringSink());
+                }*/
+            }
+        });
+    }
+
+    @Test
     public void testDouble_edgeValues() throws Exception {
         runInContext(r -> {
             try (Sender sender = Sender.builder(Sender.Transport.TCP)
@@ -486,62 +527,7 @@ public class LineTcpSenderTest extends AbstractLineTcpReceiverTest {
     }
 
     @Test
-    public void testMinBufferSizeWhenAuth() throws Exception {
-        authKeyId = AUTH_KEY_ID1;
-        int tinyCapacity = 42;
-        runInContext(r -> {
-            try (LineTcpSender sender = LineTcpSender.newSender(HOST, bindPort, tinyCapacity)) {
-                sender.authenticate(AUTH_KEY_ID1, AUTH_PRIVATE_KEY1);
-                fail();
-            } catch (LineSenderException e) {
-                assertContains(e.getMessage(), "challenge did not fit into buffer");
-            }
-        });
-    }
-
-    @Test
-    public void testNDDoubleArrayTest() throws Exception {
-        runInContext(r -> {
-            try (Sender sender = Sender.builder(Sender.Transport.TCP)
-                    .address("127.0.0.1")
-                    .port(bindPort)
-                    .build()) {
-                String table = "nd_test";
-                long ts = IntervalUtils.parseFloorPartialTimestamp("2025-02-22");
-                sender.table(table)
-                        .symbol("x", "42i")
-                        .symbol("y", "[6f1.0,2.5,3.0,4.5,5.0]")  // ensuring no array parsing for symbol
-                        .longColumn("l1", 23452345)
-                        .doubleArray("a1", (double[]) createDoubleArray(new int[]{5}))
-                        .doubleArray("a2", (double[][]) createDoubleArray(new int[]{2, 3}))
-                        .doubleArray("a3", (double[][][]) createDoubleArray(new int[]{1, 2, 3}))
-                        .doubleArray("a4", DoubleArray.create(4.0, 1, 1, 2, 1))
-                        .doubleArray("a5", DoubleArray.create(5.0, 3, 2, 1, 4, 1))
-                        .doubleArray("a6", DoubleArray.create(6.0, 1, 3, 4, 2, 1, 1))
-                        .doubleArray("a7", DoubleArray.create(7.0, 2, 2, 2, 1, 1, 1, 2))
-                        .doubleArray("a8", DoubleArray.create(8.0, 1, 1, 2, 1, 1, 1, 2, 1))
-                        .doubleArray("a9", DoubleArray.create(9.0, 1, 2, 1, 2, 1, 1, 2, 1, 1))
-                        .doubleArray("a10", DoubleArray.create(10.0, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2))
-                        .doubleArray("a11", DoubleArray.create(11.0, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2))
-                        .doubleArray("a12", DoubleArray.create(12.0, 1, 2, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1))
-                        .doubleArray("a13", DoubleArray.create(13.0, 1, 1, 2, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1))
-                        .doubleArray("a14", DoubleArray.create(14.0, 1, 1, 3, 1, 1, 1, 1, 1, 2, 1, 1, 2, 2, 1))
-                        .doubleArray("a15", DoubleArray.create(15.0, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 3, 1, 1, 1))
-                        .doubleArray("a16", DoubleArray.create(16.0, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
-                        .at(ts, ChronoUnit.MICROS);
-                sender.flush();
-
-                assertTableSizeEventually(engine, table, 1);
-                // @todo getArray support in TestTableReadCursor
-/*                try (TableReader reader = getReader(table)) {
-                    TestUtils.assertReader("", reader, new StringSink());
-                }*/
-            }
-        });
-    }
-
-    @Test
-    public void testNDLongArrayTest() throws Exception {
+    public void testLongArrayTest() throws Exception {
         runInContext(r -> {
             try (Sender sender = Sender.builder(Sender.Transport.TCP)
                     .address("127.0.0.1")
@@ -577,6 +563,20 @@ public class LineTcpSenderTest extends AbstractLineTcpReceiverTest {
 /*                try (TableReader reader = getReader(table)) {
                     TestUtils.assertReader("", reader, new StringSink());
                 }*/
+            }
+        });
+    }
+
+    @Test
+    public void testMinBufferSizeWhenAuth() throws Exception {
+        authKeyId = AUTH_KEY_ID1;
+        int tinyCapacity = 42;
+        runInContext(r -> {
+            try (LineTcpSender sender = LineTcpSender.newSender(HOST, bindPort, tinyCapacity)) {
+                sender.authenticate(AUTH_KEY_ID1, AUTH_PRIVATE_KEY1);
+                fail();
+            } catch (LineSenderException e) {
+                assertContains(e.getMessage(), "challenge did not fit into buffer");
             }
         });
     }
