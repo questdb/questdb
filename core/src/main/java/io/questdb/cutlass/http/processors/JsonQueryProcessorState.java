2f15645be4aa025db7c60d82cef33bc9d1d8ba03
Andrei Pechkurov <apechkurov@gmail.com>
2025-03-19 18:25:11 +0200

    Improve comment

--- core/src/main/java/io/questdb/cutlass/http/processors/JsonQueryProcessorState.java
+++ core/src/main/java/io/questdb/cutlass/http/processors/JsonQueryProcessorState.java
@@ -946,7 +946,7 @@ public class JsonQueryProcessorState implements Mutable, Closeable {
         this.queryJitCompiled = factory.usesCompiledFilter();
         // Enable column pre-touch in REST API only when LIMIT K,N is not specified since when limit is defined
         // we do a no-op loop over the cursor to calculate the total row count and pre-touch only slows things down.
-        // We use override flag to avoid affecting the explain plan.
+        // Make sure to use the override flag to avoid affecting the explain plan.
         sqlExecutionContext.setColumnPreTouchEnabledOverride(stop == Long.MAX_VALUE);
         this.circuitBreaker = sqlExecutionContext.getCircuitBreaker();
         final RecordMetadata metadata = factory.getMetadata();
