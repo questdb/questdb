c7767ecb9cf77ceb7ac77f3316d0028daf8b2e74
Eugene Lysiuchenko <79919431+eugenels@users.noreply.github.com>
2025-04-02 14:49:19 +0300

    reset wal directory policy

--- core/src/test/java/io/questdb/test/cairo/wal/WalWriterTest.java
+++ core/src/test/java/io/questdb/test/cairo/wal/WalWriterTest.java
@@ -3580,50 +3580,55 @@ public class WalWriterTest extends AbstractCairoTest {
 
             assertTableExistence(true, tableToken);
 
-            engine.setWalDirectoryPolicy(new WalDirectoryPolicy() {
-                @Override
-                public void initDirectory(Path dirPath) {
-                    final File segmentDirFile = new File(dirPath.toString());
-                    final File customInitFile = new File(segmentDirFile, "customInitFile");
-                    try {
-                        //noinspection ResultOfMethodCallIgnored
-                        customInitFile.createNewFile();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+            WalDirectoryPolicy oldPolicy = engine.getWalDirectoryPolicy();
+            try {
+                engine.setWalDirectoryPolicy(new WalDirectoryPolicy() {
+                    @Override
+                    public void initDirectory(Path dirPath) {
+                        final File segmentDirFile = new File(dirPath.toString());
+                        final File customInitFile = new File(segmentDirFile, "customInitFile");
+                        try {
+                            //noinspection ResultOfMethodCallIgnored
+                            customInitFile.createNewFile();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
+
+                    @Override
+                    public boolean isInUse(Path path) {
+                        return false;
+                    }
+
+                    @Override
+                    public void rollbackDirectory(Path path) {
+                        // do nothing
+                    }
+
+                    @Override
+                    public boolean truncateFilesOnClose() {
+                        return true;
+                    }
+                });
+
+                try (WalWriter walWriter = engine.getWalWriter(tableToken)) {
+                    for (int i = 0; i < 10; i++) {
+                        TableWriter.Row row = walWriter.newRow(0);
+                        row.putByte(0, (byte) i);
+                        row.append();
+                    }
+
+                    walWriter.commit();
                 }
 
-                @Override
-                public boolean isInUse(Path path) {
-                    return false;
-                }
+                assertWalExistence(true, tableToken, 1);
+                File segmentDir = assertSegmentExistence(true, tableToken, 1, 0);
 
-                @Override
-                public void rollbackDirectory(Path path) {
-                    // do nothing
-                }
-
-                @Override
-                public boolean truncateFilesOnClose() {
-                    return true;
-                }
-            });
-
-            try (WalWriter walWriter = engine.getWalWriter(tableToken)) {
-                for (int i = 0; i < 10; i++) {
-                    TableWriter.Row row = walWriter.newRow(0);
-                    row.putByte(0, (byte) i);
-                    row.append();
-                }
-
-                walWriter.commit();
+                final File customInitFile = new File(segmentDir, "customInitFile");
+                assertTrue(customInitFile.exists());
+            } finally {
+                engine.setWalDirectoryPolicy(oldPolicy);
             }
-
-            assertWalExistence(true, tableToken, 1);
-            File segmentDir = assertSegmentExistence(true, tableToken, 1, 0);
-
-            final File customInitFile = new File(segmentDir, "customInitFile");
-            assertTrue(customInitFile.exists());
         });
     }
 
@@ -3640,62 +3645,67 @@ public class WalWriterTest extends AbstractCairoTest {
 
             assertTableExistence(true, tableToken);
             String pending = "custom.pending";
-            engine.setWalDirectoryPolicy(new WalDirectoryPolicy() {
-                @Override
-                public void initDirectory(Path dirPath) {
-                    final File segmentDirFile = new File(dirPath.toString());
-                    final File customInitFile = new File(segmentDirFile, pending);
-                    try {
-                        //noinspection ResultOfMethodCallIgnored
-                        customInitFile.createNewFile();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+            WalDirectoryPolicy oldPolicy = engine.getWalDirectoryPolicy();
+            try {
+                engine.setWalDirectoryPolicy(new WalDirectoryPolicy() {
+                    @Override
+                    public void initDirectory(Path dirPath) {
+                        final File segmentDirFile = new File(dirPath.toString());
+                        final File customInitFile = new File(segmentDirFile, pending);
+                        try {
+                            //noinspection ResultOfMethodCallIgnored
+                            customInitFile.createNewFile();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
+
+                    @Override
+                    public boolean isInUse(Path path) {
+                        return true;
+                    }
+
+                    @Override
+                    public void rollbackDirectory(Path path) {
+                        final File segmentDirFile = new File(path.toString());
+                        final File customInitFile = new File(segmentDirFile, pending);
+                        customInitFile.delete();
+                    }
+
+                    @Override
+                    public boolean truncateFilesOnClose() {
+                        return true;
+                    }
+                });
+
+                try (WalWriter wal1 = engine.getWalWriter(tableToken);
+                     WalWriter wal2 = engine.getWalWriter(tableToken)
+                ) {
+                    for (int i = 0; i < 10; i++) {
+                        TableWriter.Row row = wal1.newRow(0);
+                        row.putByte(0, (byte) i);
+                        row.append();
+                    }
+                    wal1.commit();
+
+                    // wal2 without commits
+                    wal2.truncateSoft();
                 }
 
-                @Override
-                public boolean isInUse(Path path) {
-                    return true;
-                }
+                assertWalExistence(true, tableToken, 1);
+                File segmentDir = assertSegmentExistence(true, tableToken, 1, 0);
 
-                @Override
-                public void rollbackDirectory(Path path) {
-                    final File segmentDirFile = new File(path.toString());
-                    final File customInitFile = new File(segmentDirFile, pending);
-                    customInitFile.delete();
-                }
+                final File pendingFile = new File(segmentDir, pending);
+                assertTrue(pendingFile.exists());
 
-                @Override
-                public boolean truncateFilesOnClose() {
-                    return true;
-                }
-            });
+                assertWalExistence(true, tableToken, 2);
+                File segmentDir2 = assertSegmentExistence(true, tableToken, 2, 0);
 
-            try (WalWriter wal1 = engine.getWalWriter(tableToken);
-                 WalWriter wal2 = engine.getWalWriter(tableToken)
-            ) {
-                for (int i = 0; i < 10; i++) {
-                    TableWriter.Row row = wal1.newRow(0);
-                    row.putByte(0, (byte) i);
-                    row.append();
-                }
-                wal1.commit();
-
-                // wal2 without commits
-                wal2.truncateSoft();
+                final File pendingFile2 = new File(segmentDir2, pending);
+                assertTrue(pendingFile2.exists());
+            } finally {
+                engine.setWalDirectoryPolicy(oldPolicy);
             }
-
-            assertWalExistence(true, tableToken, 1);
-            File segmentDir = assertSegmentExistence(true, tableToken, 1, 0);
-
-            final File pendingFile = new File(segmentDir, pending);
-            assertTrue(pendingFile.exists());
-
-            assertWalExistence(true, tableToken, 2);
-            File segmentDir2 = assertSegmentExistence(true, tableToken, 2, 0);
-
-            final File pendingFile2 = new File(segmentDir2, pending);
-            assertTrue(pendingFile2.exists());
         });
     }
 
