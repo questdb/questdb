24b1a7ae1bdc00713d6a79fcb10d2e3f136c0371
Alex Pelagenko <2159629+ideoma@users.noreply.github.com>
2025-05-01 23:59:02 +0100

    fix(core): fix deadlock in logging when handling disk full FS error (#5634)

--- core/src/main/java/io/questdb/cairo/TableWriter.java
+++ core/src/main/java/io/questdb/cairo/TableWriter.java
@@ -2329,7 +2329,7 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
     }
 
     @Override
-    public TableToken getTableToken() {
+    public @NotNull TableToken getTableToken() {
         return tableToken;
     }
 
@@ -4062,7 +4062,7 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
                 }
             } else if (noOpRowCount > 0) {
                 LOG.critical()
-                        .$("o3 ignoring write on read-only partition [table=").utf8(tableToken.getTableName())
+                        .$("o3 ignoring write on read-only partition [table=").$(tableToken)
                         .$(", timestamp=").$ts(lastOpenPartitionTs)
                         .$(", numRows=").$(noOpRowCount)
                         .$();
@@ -9172,7 +9172,12 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
     }
 
     private void throwDistressException(Throwable cause) {
-        LOG.critical().$("writer error [table=").utf8(tableToken.getTableName()).$(", e=").$((Sinkable) cause).I$();
+        try {
+            Sinkable sinkable = (Sinkable) cause;
+            LOG.critical().$("writer error [table=").$(tableToken).$(", e=").$(sinkable).I$();
+        } catch (Throwable th) {
+            LOG.critical().$("writer error [table=").$(tableToken).$(", e=").$(cause).I$();
+        }
         distressed = true;
         throw new CairoError(cause);
     }
