33ba169000a2dc228d7a4a7ef84da3eda26f6917
Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2025-05-08 16:21:08 +0300

    feat(sql): ALTER MATERIALIZED VIEW SET REFRESH LIMIT and SET TTL (#5638)

--- core/src/main/java/io/questdb/cairo/TableWriter.java
+++ core/src/main/java/io/questdb/cairo/TableWriter.java
@@ -2057,7 +2057,7 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
 
     public void enforceTtl() {
         partitionRemoveCandidates.clear();
-        int ttl = metadata.getTtlHoursOrMonths();
+        final int ttl = metadata.getTtlHoursOrMonths();
         if (ttl == 0) {
             return;
         }
@@ -3091,6 +3091,13 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
     }
 
     @Override
+    public void setMetaMatViewRefreshLimit(int limitHoursOrMonths) {
+        commit();
+        metadata.setMatViewRefreshLimitHoursOrMonths(limitHoursOrMonths);
+        writeMetadataToDisk();
+    }
+
+    @Override
     public void setMetaMaxUncommittedRows(int maxUncommittedRows) {
         commit();
         metadata.setMaxUncommittedRows(maxUncommittedRows);
@@ -3105,9 +3112,9 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
     }
 
     @Override
-    public void setMetaTtlHoursOrMonths(int metaTtlHoursOrMonths) {
+    public void setMetaTtl(int ttlHoursOrMonths) {
         commit();
-        metadata.setTtlHoursOrMonths(metaTtlHoursOrMonths);
+        metadata.setTtlHoursOrMonths(ttlHoursOrMonths);
         writeMetadataToDisk();
     }
 
@@ -8648,6 +8655,7 @@ public class TableWriter implements TableWriterAPI, MetadataService, Closeable {
             ddlMem.putBool(metadata.isWalEnabled());
             ddlMem.putInt(TableUtils.calculateMetaFormatMinorVersionField(version, columnCount));
             ddlMem.putInt(metadata.getTtlHoursOrMonths());
+            ddlMem.putInt(metadata.getMatViewRefreshLimitHoursOrMonths());
 
             ddlMem.jumpTo(META_OFFSET_COLUMN_TYPES);
             for (int i = 0; i < columnCount; i++) {
