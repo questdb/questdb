0947600f22dfb86c88b28e2533c2b9e365c24878
bluestreak <bluestreak@gmail.com>
2025-05-19 21:43:51 +0100

    export new code

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -122,8 +122,8 @@ open module io.questdb {
     exports io.questdb.griffin.engine.table.parquet;
     exports io.questdb.cairo.mv;
     exports io.questdb.cutlass.pgwire.modern;
+    exports io.questdb.cairo.file;
     exports io.questdb.cairo.arr;
     exports io.questdb.griffin.engine.functions.array;
-    exports io.questdb.cairo.file;
     exports io.questdb.cutlass.line.array;
 }
