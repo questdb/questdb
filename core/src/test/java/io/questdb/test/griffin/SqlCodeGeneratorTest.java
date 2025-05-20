543544a965812885a498286738eedf779bdc527e
bluestreak <bluestreak@gmail.com>
2025-05-20 14:20:18 +0100

    fix(sql): fix segfault when using within(geohash) with latest on and partition column is empty

--- core/src/test/java/io/questdb/test/griffin/SqlCodeGeneratorTest.java
+++ core/src/test/java/io/questdb/test/griffin/SqlCodeGeneratorTest.java
@@ -94,6 +94,65 @@ public class SqlCodeGeneratorTest extends AbstractCairoTest {
     }
 
     @Test
+    public void testABCSegfault() throws Exception {
+        String ddlTrips = "CREATE TABLE trips (\n" +
+                "pickup_datetime TIMESTAMP,\n" +
+                "pickup_geohash GEOHASH(12c)\n" +
+                ") timestamp(pickup_datetime)\n";
+
+        String dmlTrips = "INSERT INTO trips (pickup_datetime, pickup_geohash) VALUES \n" +
+                "    ('2009-01-01T00:00:00.000000Z', #dr5rsjutvshf),\n" +
+                "    ('2009-01-01T00:00:00.000000Z', #dr5ruy5ttnw1),\n" +
+                "    ('2009-01-01T00:00:02.000000Z', #dr5ruvkrr2fe),\n" +
+                "    ('2009-01-01T00:00:04.000000Z', #dr5reff6hu5e),\n" +
+                "    ('2009-01-01T00:00:07.000000Z', #dr5rshd6guzu),\n" +
+                "    ('2009-01-01T00:00:09.000000Z', #dr5rugk21jew),\n" +
+                "    ('2009-01-01T00:00:10.000000Z', #dr5ru7bhxxr7),\n" +
+                "    ('2009-01-01T00:00:13.000000Z', #dr5ruhv90nt3),\n" +
+                "    ('2009-01-01T00:00:15.000000Z', #dr5ru43qt65b),\n" +
+                "    ('2009-01-01T00:00:16.000000Z', #dr5rt6qwr9s8),\n" +
+                "    ('2009-01-01T00:00:17.000000Z', #dr5rvn6ccrhq),\n" +
+                "    ('2009-01-01T00:00:21.000000Z', #dr5rgcj30u1n),\n" +
+                "    ('2009-01-01T00:00:23.000000Z', #dr5rgbk02dxk),\n" +
+                "    ('2009-01-01T00:00:25.000000Z', #000000000000),\n" +
+                "    ('2009-01-01T00:00:25.000000Z', #s00000000000),\n" +
+                "    ('2009-01-01T00:00:27.000000Z', #dr5rutj2ty38),\n" +
+                "    ('2009-01-01T00:00:27.000000Z', #dr5rsncg1he5),\n" +
+                "    ('2009-01-01T00:00:27.000000Z', #dr5x1p1t86gx),\n" +
+                "    ('2009-01-01T00:00:28.000000Z', #dr5ruswebtne),\n" +
+                "    ('2009-01-01T00:00:29.000000Z', #dr5rsqhc82jq),\n" +
+                "    ('2009-01-01T00:00:29.000000Z', #dr5rzjybb9g4),\n" +
+                "    ('2009-01-01T00:00:29.000000Z', #dr7grdqzppp0),\n" +
+                "    ('2009-01-01T00:00:29.000000Z', #dr5rgbw8fxz6),\n" +
+                "    ('2009-01-01T00:00:30.000000Z', #dr5rsqqnccnz),\n" +
+                "    ('2009-01-01T00:00:30.000000Z', #dr5rtnj79c20),\n" +
+                "    ('2009-01-01T00:00:33.000000Z', #dr5rzjy9r094),\n" +
+                "    ('2009-01-01T00:00:36.000000Z', #dr5rvhfgw67v),\n" +
+                "    ('2009-01-01T00:00:37.000000Z', #dr5rsnpw997n),\n" +
+                "    ('2009-01-01T00:00:39.000000Z', #dr72h8hkt556),\n" +
+                "    ('2009-01-01T00:00:43.000000Z', #000000000000)\n";
+
+        assertMemoryLeak(() -> {
+            execute(ddlTrips);
+            execute(dmlTrips);
+            execute("ALTER TABLE trips ADD COLUMN sym SYMBOL;");
+            execute("ALTER TABLE trips ALTER COLUMN sym ADD INDEX;");
+            drainWalQueue();
+
+            String query = "trips WHERE pickup_geohash WITHIN (#dr5) LATEST ON pickup_datetime PARTITION BY sym;";
+
+            // below will segfault
+            assertQuery("pickup_datetime\tpickup_geohash\tsym\n",
+                    query,
+                    null,
+                    "pickup_datetime",
+                    true,
+                    true
+            );
+        });
+    }
+
+    @Test
     public void testAliasedColumnFollowedByWildcard() throws Exception {
         assertQuery(
                 "k1\ta\tk\n" +
