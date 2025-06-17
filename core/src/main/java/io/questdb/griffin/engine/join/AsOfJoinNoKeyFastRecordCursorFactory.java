d40cdab7c7f5304c5971a659c88aeca352365dcf
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-06-17 15:09:38 +0200

    style

--- core/src/main/java/io/questdb/griffin/engine/join/AsOfJoinNoKeyFastRecordCursorFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/join/AsOfJoinNoKeyFastRecordCursorFactory.java
@@ -138,9 +138,7 @@ public class AsOfJoinNoKeyFastRecordCursorFactory extends AbstractJoinRecordCurs
                 nextSlave(masterTimestamp);
                 if (toleranceInterval != Numbers.LONG_NULL && record.hasSlave()) {
                     slaveTimestamp = slaveRecB.getTimestamp(slaveTimestampIndex);
-                    if (slaveTimestamp < masterTimestamp - toleranceInterval) {
-                        record.hasSlave(false);
-                    }
+                    record.hasSlave(slaveTimestamp >= masterTimestamp - toleranceInterval);
                 }
                 isMasterHasNextPending = true;
                 return true;
