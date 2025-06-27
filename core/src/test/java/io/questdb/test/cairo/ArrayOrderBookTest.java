b4993ecb3e927baf86f0253ef1a921f04e349253
Marko Topolnik <marko.topolnik@gmail.com>
2025-06-27 08:44:43 +0200

    One more use case test

--- core/src/test/java/io/questdb/test/cairo/ArrayOrderBookTest.java
+++ core/src/test/java/io/questdb/test/cairo/ArrayOrderBookTest.java
@@ -41,7 +41,7 @@ public class ArrayOrderBookTest extends AbstractCairoTest {
         assertMemoryLeak(() -> {
             execute("INSERT INTO order_book VALUES " +
                     "(0, ARRAY[ [0.0,0,0,0], [10.0, 15, 13, 12] ], ARRAY[ [0.0,0,0,0], [20.0, 25, 23, 22] ]), " +
-                    "(0, ARRAY[ [0.0,0,0,0], [15.0,  2, 20, 23] ], ARRAY[ [0.0,0,0,0], [14.0, 45, 22,  5] ])"
+                    "(1, ARRAY[ [0.0,0,0,0], [15.0,  2, 20, 23] ], ARRAY[ [0.0,0,0,0], [14.0, 45, 22,  5] ])"
             );
             assertSql("ask_vol\tbid_vol\tratio\n" +
                             "38.0\t68.0\t1.7894736842105263\n" +
@@ -59,7 +59,7 @@ public class ArrayOrderBookTest extends AbstractCairoTest {
         assertMemoryLeak(() -> {
             execute("INSERT INTO order_book VALUES " +
                     "(0, ARRAY[ [10.0, 10.02, 10.04, 10.10, 10.12, 10.14], [10.0, 15, 13, 12, 18, 20] ], NULL), " +
-                    "(0, ARRAY[ [10.0, 10.02, 10.04, 10.10, 10.12, 10.14], [10.0,  5,  3, 12, 18, 20] ], NULL)"
+                    "(1, ARRAY[ [10.0, 10.02, 10.04, 10.10, 10.12, 10.14], [10.0,  5,  3, 12, 18, 20] ], NULL)"
             );
             assertSql("cum_volumes\ttarget_level\tprice\n" +
                             "[10.0,25.0,38.0,50.0,68.0,88.0]\t3\t10.04\n" +
@@ -68,6 +68,25 @@ public class ArrayOrderBookTest extends AbstractCairoTest {
                             "q2 AS (SELECT asks, cum_volumes, insertion_point(cum_volumes, 30.0, true) target_level FROM q1) " +
                             "SELECT cum_volumes, target_level, asks[1, target_level] price " +
                             "FROM q2");
+        });
+    }
+
+    @Test
+    public void testPriceWeightedVolumeImbalance() throws Exception {
+        assertMemoryLeak(() -> {
+            execute("INSERT INTO order_book VALUES " +
+                    "(0, ARRAY[ [6.0, 6.1], [15.0, 25] ], ARRAY[ [5.0, 5.1], [10.0, 20] ]), " +
+                    "(1, ARRAY[ [6.2, 6.4], [20.0,  9] ], ARRAY[ [5.1, 5.2], [20.0, 25] ])"
+            );
+            assertSql("mid_price\tweighted_ask_pressure\tweighted_bid_pressure\n" +
+                            "5.5\t[7.5,14.999999999999991]\t[5.0,8.000000000000007]\n" +
+                            "5.65\t[10.999999999999996,6.75]\t[11.000000000000014,11.250000000000004]\n",
+                    "WITH q1 AS (SELECT *, round((asks[1][1] + bids[1][1]) / 2, 2) mid_price FROM order_book) " +
+                            "SELECT " +
+                            "mid_price, " +
+                            "(asks[1] - mid_price) * asks[2] weighted_ask_pressure, " +
+                            "(mid_price - bids[1]) * bids[2] weighted_bid_pressure " +
+                            "FROM q1");
         });
     }
 
@@ -99,7 +118,7 @@ public class ArrayOrderBookTest extends AbstractCairoTest {
         assertMemoryLeak(() -> {
             execute("INSERT INTO order_book VALUES " +
                     "(0, ARRAY[ [10.0, 10.02, 10.04, 10.10, 10.12, 10.14], [10.0, 15, 13, 12, 18, 20] ], NULL), " +
-                    "(0, ARRAY[ [10.0, 10.10, 10.12, 10.14, 10.16, 10.18], [1.0, 5, 3, 2, 8, 10] ], NULL)"
+                    "(1, ARRAY[ [10.0, 10.10, 10.12, 10.14, 10.16, 10.18], [1.0, 5, 3, 2, 8, 10] ], NULL)"
             );
             assertSql("inflated_top\n10.1\n10.1\n",
                     "SELECT asks[1,1] + 0.1 inflated_top FROM order_book");
@@ -116,7 +135,7 @@ public class ArrayOrderBookTest extends AbstractCairoTest {
         assertMemoryLeak(() -> {
             execute("INSERT INTO order_book VALUES " +
                     "(0, ARRAY[ [10.0, 10.02, 10.04, 10.1, 10.12, 10.14], [10.0, 15, 13, 12, 18, 20] ], NULL), " +
-                    "(0, ARRAY[ [20.0, 20.02, 20.04, 20.1, 20.12, 20.14], [1.0, 5, 3, 2, 8, 10] ], NULL)"
+                    "(1, ARRAY[ [20.0, 20.02, 20.04, 20.1, 20.12, 20.14], [1.0, 5, 3, 2, 8, 10] ], NULL)"
             );
             assertSql("inflated_top\n10.1\n20.2\n",
                     "SELECT 1.01 * asks[1,1] inflated_top FROM order_book");
