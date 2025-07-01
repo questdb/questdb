6e914e759f2a05976ca5954381e78463862b16ba
Marko Topolnik <marko.topolnik@gmail.com>
2025-07-01 16:39:59 +0200

    Address a TODO

--- core/src/main/java/io/questdb/griffin/engine/functions/cast/CastDoubleArrayToDoubleArrayFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/cast/CastDoubleArrayToDoubleArrayFunctionFactory.java
@@ -64,8 +64,7 @@ public final class CastDoubleArrayToDoubleArrayFunctionFactory implements Functi
                     .put(" (").put(ColumnType.decodeArrayDimensionality(fromType)).put("D)")
                     .put(", to=").put(ColumnType.nameOf(toType))
                     .put(" (").put(ColumnType.decodeArrayDimensionality(toType)).put("D)")
-                    // todo: make sure flattening exists and has this syntax
-                    .put("]. Use array flattening operation (e.g. 'arr[:]' or 'flatten_array(arr)') instead");
+                    .put("]. Use array flattening operation (e.g. 'flatten(arr)') instead");
         }
         if (dimsToAdd == 0) {
             // nothing to do
