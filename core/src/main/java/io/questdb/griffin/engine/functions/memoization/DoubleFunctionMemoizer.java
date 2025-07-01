3df21a2e63d51465e9f08e5be6f742c313e73878
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-07-01 10:10:23 +0200

    double memoization

---
+++ core/src/main/java/io/questdb/griffin/engine/functions/memoization/DoubleFunctionMemoizer.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2024 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.griffin.engine.functions.memoization;
+
+import io.questdb.cairo.sql.Function;
+import io.questdb.cairo.sql.Record;
+import io.questdb.cairo.sql.SymbolTableSource;
+import io.questdb.griffin.SqlException;
+import io.questdb.griffin.SqlExecutionContext;
+import io.questdb.griffin.engine.functions.DoubleFunction;
+import io.questdb.griffin.engine.functions.UnaryFunction;
+
+public final class DoubleFunctionMemoizer extends DoubleFunction implements UnaryFunction {
+    private final Function fn;
+    private boolean memoized;
+    private double value;
+
+    public DoubleFunctionMemoizer(Function fn) {
+        assert fn.shouldMemoize();
+        this.fn = fn;
+    }
+
+    @Override
+    public boolean shouldMemoize() {
+        return true;
+    }
+
+    @Override
+    public Function getArg() {
+        return fn;
+    }
+
+    @Override
+    public double getDouble(Record rec) {
+        return memoized ? value : fn.getDouble(rec);
+    }
+
+    @Override
+    public void init(SymbolTableSource symbolTableSource, SqlExecutionContext executionContext) throws SqlException {
+        memoized = false;
+        UnaryFunction.super.init(symbolTableSource, executionContext);
+    }
+
+    @Override
+    public void memoize(Record record) {
+        value = fn.getDouble(record);
+        memoized = true;
+    }
+
+    @Override
+    public boolean supportsRandomAccess() {
+        return fn.supportsRandomAccess();
+    }
+}
