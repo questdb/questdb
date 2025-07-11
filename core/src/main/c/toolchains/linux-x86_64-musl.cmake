9c2cfea5a0e4769e04be96d269719ca5ebe74175
Raphael DALMON <raphael.dalmon@gmail.com>
2025-07-11 09:41:25 +0200

    fix(toolchain): add MUSL flag to linux-x86_64-musl.cmake

--- core/src/main/c/toolchains/linux-x86_64-musl.cmake
+++ core/src/main/c/toolchains/linux-x86_64-musl.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_SYSTEM_LIBC musl)
+set(MUSL True)
 
 set(CMAKE_C_FLAGS "-I/usr/lib/jvm/java-17-openjdk/include -I/usr/lib/jvm/java-17-openjdk/include/linux -std=gnu99 -D_GNU_SOURCE -ftls-model=global-dynamic -fno-gnu-tm -Wno-int-conversion" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS "-I/usr/lib/jvm/java-17-openjdk/include -I/usr/lib/jvm/java-17-openjdk/include/linux -fno-exceptions -ftls-model=global-dynamic -fno-gnu-tm -Wno-int-conversion" CACHE INTERNAL "")
