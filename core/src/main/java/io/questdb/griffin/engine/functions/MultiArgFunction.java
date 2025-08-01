185e0c2820738797119f40a675c12068a72589fa
nmarasoiu <dumitru.nicolae.marasoiu@outlook.com>
2025-08-01 13:14:12 +0100

    WIP initial loop iterations unroll on some multi-arg functions (concat const) and some methods common between multi-arg functions

--- core/src/main/java/io/questdb/griffin/engine/functions/MultiArgFunction.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/MultiArgFunction.java
@@ -49,8 +49,27 @@ public interface MultiArgFunction extends Function {
 
     @Override
     default boolean isConstant() {
-        ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
+        final ObjList<Function> args = getArgs();
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return true;
+            case 1:
+                return args.getQuick(0).isConstant();
+            case 2:
+                return args.getQuick(0).isConstant()
+                        && args.getQuick(1).isConstant();
+            case 3:
+                return args.getQuick(0).isConstant()
+                        && (args.getQuick(1).isConstant()
+                        && args.getQuick(2).isConstant());
+            case 4:
+                return args.getQuick(0).isConstant()
+                        && (args.getQuick(1).isConstant()
+                        && (args.getQuick(2).isConstant()
+                        && args.getQuick(3).isConstant()));
+        }
+        for (int i = 0; i < n; i++) {
             if (!args.getQuick(i).isConstant()) {
                 return false;
             }
@@ -61,9 +80,57 @@ public interface MultiArgFunction extends Function {
     @Override
     default boolean isNonDeterministic() {
         final ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
-            final Function function = args.getQuick(i);
-            if (function.isNonDeterministic()) {
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return false;
+            case 1:
+                return args.getQuick(0).isNonDeterministic();
+            case 2:
+                return args.getQuick(0).isNonDeterministic()
+                        || args.getQuick(1).isNonDeterministic();
+            case 3:
+                return args.getQuick(0).isNonDeterministic()
+                        || args.getQuick(1).isNonDeterministic()
+                        || args.getQuick(2).isNonDeterministic();
+            case 4:
+                return args.getQuick(0).isNonDeterministic()
+                        || args.getQuick(1).isNonDeterministic()
+                        || args.getQuick(2).isNonDeterministic()
+                        || args.getQuick(3).isNonDeterministic();
+        }
+        for (int i = 0; i < n; i++) {
+            if (args.getQuick(i).isNonDeterministic()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //todo delete after code review
+    default boolean isRandom1() {
+        final ObjList<Function> args = getArgs();
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return false;
+            case 1:
+                return args.getQuick(0).isRandom();
+            case 2:
+                return args.getQuick(0).isRandom()
+                        || args.getQuick(1).isRandom();
+            case 3:
+                return args.getQuick(0).isRandom()
+                        || args.getQuick(1).isRandom()
+                        || args.getQuick(2).isRandom();
+            case 4:
+                return args.getQuick(0).isRandom()
+                        || args.getQuick(1).isRandom()
+                        || args.getQuick(2).isRandom()
+                        || args.getQuick(3).isRandom();
+        }
+        for (int i = 0; i < n; i++) {
+            if (args.getQuick(i).isRandom()) {
                 return true;
             }
         }
@@ -73,9 +140,28 @@ public interface MultiArgFunction extends Function {
     @Override
     default boolean isRandom() {
         final ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
-            final Function function = args.getQuick(i);
-            if (function.isRandom()) {
+        final int n = args.size();
+        int i = 0;
+        switch (n) {
+            case 0:
+                return false;
+            case 1:
+                return args.getQuick(i).isRandom();
+            case 2:
+                return args.getQuick(i++).isRandom()
+                        || args.getQuick(i).isRandom();
+            case 3:
+                return args.getQuick(i++).isRandom()
+                        || args.getQuick(i++).isRandom()
+                        || args.getQuick(i).isRandom();
+            case 4:
+                return args.getQuick(i++).isRandom()
+                        || args.getQuick(i++).isRandom()
+                        || args.getQuick(i++).isRandom()
+                        || args.getQuick(i).isRandom();
+        }
+        for (; i < n; i++) {
+            if (args.getQuick(i).isRandom()) {
                 return true;
             }
         }
@@ -85,7 +171,15 @@ public interface MultiArgFunction extends Function {
     @Override
     default boolean isRuntimeConstant() {
         final ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return true;
+            case 1:
+                final Function function = args.getQuick(0);
+                return function.isRuntimeConstant() || function.isConstant();
+        }
+        for (int i = 0; i < n; i++) {
             final Function function = args.getQuick(i);
             if (!function.isRuntimeConstant() && !function.isConstant()) {
                 return false;
@@ -97,7 +191,34 @@ public interface MultiArgFunction extends Function {
     @Override
     default boolean isThreadSafe() {
         final ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return true;
+            case 1:
+                return args.getQuick(0).isThreadSafe();
+            case 2:
+                return args.getQuick(0).isThreadSafe()
+                        && args.getQuick(1).isThreadSafe();
+            case 3:
+                return args.getQuick(0).isThreadSafe()
+                        && (args.getQuick(1).isThreadSafe()
+                        && args.getQuick(2).isThreadSafe());
+            default:
+                if (!args.getQuick(0).isThreadSafe()) {
+                    return false;
+                }
+                if (!args.getQuick(1).isThreadSafe()) {
+                    return false;
+                }
+                if (!args.getQuick(2).isThreadSafe()) {
+                    return false;
+                }
+                if (!args.getQuick(3).isThreadSafe()) {
+                    return false;
+                }
+        }
+        for (int i = 4; i < n; i++) {
             final Function function = args.getQuick(i);
             if (!function.isThreadSafe()) {
                 return false;
@@ -134,7 +255,34 @@ public interface MultiArgFunction extends Function {
     @Override
     default boolean supportsParallelism() {
         final ObjList<Function> args = getArgs();
-        for (int i = 0, n = args.size(); i < n; i++) {
+        final int n = args.size();
+        switch (n) {
+            case 0:
+                return true;
+            case 1:
+                return args.getQuick(0).supportsParallelism();
+            case 2:
+                return args.getQuick(0).supportsParallelism()
+                        && args.getQuick(1).supportsParallelism();
+            case 3:
+                return args.getQuick(0).supportsParallelism()
+                        && (args.getQuick(1).supportsParallelism()
+                        && args.getQuick(2).supportsParallelism());
+            default:
+                if (!args.getQuick(0).supportsParallelism()) {
+                    return false;
+                }
+                if (!args.getQuick(1).supportsParallelism()) {
+                    return false;
+                }
+                if (!args.getQuick(2).supportsParallelism()) {
+                    return false;
+                }
+                if (!args.getQuick(3).supportsParallelism()) {
+                    return false;
+                }
+        }
+        for (int i = 4; i < n; i++) {
             final Function function = args.getQuick(i);
             if (!function.supportsParallelism()) {
                 return false;
