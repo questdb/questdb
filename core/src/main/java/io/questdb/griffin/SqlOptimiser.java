d61928185601ad93dee4d8d0cd701e4e7ce8e2ff
siddharth0815 <agarwal.sid619@gmail.com>
2025-08-17 03:20:48 +0530

    add test for union and join query

--- core/src/main/java/io/questdb/griffin/SqlOptimiser.java
+++ core/src/main/java/io/questdb/griffin/SqlOptimiser.java
@@ -7241,7 +7241,7 @@ public class SqlOptimiser implements Mutable {
             propagateTopDownColumns(rewrittenModel, rewrittenModel.allowsColumnsChange());
             rewriteMultipleTermLimitedOrderByPart2(rewrittenModel);
             authorizeColumnAccess(sqlExecutionContext, rewrittenModel);
-            System.out.println("****" + rewrittenModel.toString0() + "****");
+//            System.out.println("****" + rewrittenModel.toString0() + "****");
             return rewrittenModel;
         } catch (Throwable th) {
             // at this point, models may have functions than need to be freed
