41ce1235e5a3a6df647cb5d6b86b31c03992cdb1
Marko Topolnik <marko.topolnik@gmail.com>
2025-08-18 09:03:02 +0200

    Revert "Remove AND mask on timestamp type"
    This reverts commit 63819182ff3922851d3abdecb462943224a2f1d8.

--- core/src/main/java/io/questdb/cairo/ColumnType.java
+++ core/src/main/java/io/questdb/cairo/ColumnType.java
@@ -465,11 +465,11 @@ public final class ColumnType {
     }
 
     public static boolean isTimestampMicro(int timestampType) {
-        return timestampType == TIMESTAMP_MICRO;
+        return (timestampType & TIMESTAMP_TYPE_MASK) == TIMESTAMP_MICRO;
     }
 
     public static boolean isTimestampNano(int timestampType) {
-        return timestampType == TIMESTAMP_NANO;
+        return (timestampType & TIMESTAMP_TYPE_MASK) == TIMESTAMP_NANO;
     }
 
     public static boolean isToSameOrWider(int fromType, int toType) {
@@ -587,7 +587,7 @@ public final class ColumnType {
 
     private static int getTimestampTypePriority(int timestampType) {
         assert tagOf(timestampType) == TIMESTAMP;
-        switch (timestampType) {
+        switch (timestampType & TIMESTAMP_TYPE_MASK) {
             case TIMESTAMP_MICRO:
                 return 1;
             case TIMESTAMP_NANO:
