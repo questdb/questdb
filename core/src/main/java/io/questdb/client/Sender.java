337d68d08df102f6a82ce4a24523497fb8a62c27
Nick Woolmer <29717167+nwoolmer@users.noreply.github.com>
2025-08-22 18:10:14 +0100

    linter

--- core/src/main/java/io/questdb/client/Sender.java
+++ core/src/main/java/io/questdb/client/Sender.java
@@ -685,8 +685,7 @@ public interface Sender extends Closeable, ArraySender<Sender> {
             if (hosts.size() != 1 || ports.size() != 1) {
                 throw new LineSenderException("only a single address (host:port) is supported for TCP transport");
             }
-            
-            assert hosts.size() == 1 && ports.size() == 1;
+
             LineChannel channel = new PlainTcpLineChannel(nf, hosts.getQuick(0), ports.getQuick(0), bufferCapacity * 2);
             AbstractLineTcpSender sender;
             if (tlsEnabled) {
