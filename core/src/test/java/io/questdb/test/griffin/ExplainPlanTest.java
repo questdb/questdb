e1e9c473440115e22aa6a5a2d39b51a5530d0ecd
siddharth0815 <agarwal.sid619@gmail.com>
2025-08-28 03:26:08 +0530

    fix test

--- core/src/test/java/io/questdb/test/griffin/ExplainPlanTest.java
+++ core/src/test/java/io/questdb/test/griffin/ExplainPlanTest.java
@@ -1232,7 +1232,7 @@ public class ExplainPlanTest extends AbstractCairoTest {
                             "          filter: bid_price=189.4\n" +
                             "            Radix sort light\n" +
                             "              keys: [timestamp]\n" +
-                            "                Async Top K lo: 6 workers: 1\n" +
+                            "                Async JIT Top K lo: 6 workers: 1\n" +
                             "                  filter: price=184.0\n" +
                             "                  keys: [size, price]\n" +
                             "                    PageFrame\n" +
