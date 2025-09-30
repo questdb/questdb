a6a1559a029e0e9e4105397154804d0c0cc65404
Andrei Pechkurov <apechkurov@gmail.com>
2025-09-30 13:08:32 +0300

    test(sql): fix flaky ParallelLatestByTest

--- core/src/test/java/io/questdb/test/AbstractCairoTest.java
+++ core/src/test/java/io/questdb/test/AbstractCairoTest.java
@@ -639,7 +639,6 @@ public abstract class AbstractCairoTest extends AbstractTest {
         ParanoiaState.FD_PARANOIA_MODE = new Rnd(System.nanoTime(), System.currentTimeMillis()).nextInt(100) > 70;
         engine.getMetrics().clear();
         engine.getMatViewStateStore().clear();
-        SqlCodeGenerator.ALLOW_FUNCTION_MEMOIZATION = false;
     }
 
     public void tearDown(boolean removeDir) {
@@ -665,7 +664,6 @@ public abstract class AbstractCairoTest extends AbstractTest {
         tearDown(true);
         super.tearDown();
         spinLockTimeout = DEFAULT_SPIN_LOCK_TIMEOUT;
-        SqlCodeGenerator.ALLOW_FUNCTION_MEMOIZATION = false;
     }
 
     private static void assertCalculateSize(RecordCursorFactory factory) throws SqlException {
