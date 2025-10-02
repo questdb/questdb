092346e06d0056db381ba1e89571a1938d6afb6c
Mircea Cadariu <cadariu.mircea@gmail.com>
2025-10-02 20:08:43 +0100

    port StringAggGroupByFunction to use GroupByCharSink

--- core/src/main/java/io/questdb/griffin/engine/functions/groupby/StringAggGroupByFunction.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/groupby/StringAggGroupByFunction.java
@@ -34,100 +34,132 @@ import io.questdb.griffin.PlanSink;
 import io.questdb.griffin.engine.functions.GroupByFunction;
 import io.questdb.griffin.engine.functions.StrFunction;
 import io.questdb.griffin.engine.functions.UnaryFunction;
-import io.questdb.std.Misc;
-import io.questdb.std.ObjList;
-import io.questdb.std.str.DirectUtf16Sink;
+import io.questdb.griffin.engine.groupby.GroupByAllocator;
+import io.questdb.griffin.engine.groupby.GroupByCharSink;
 
 class StringAggGroupByFunction extends StrFunction implements UnaryFunction, GroupByFunction {
-    // Cleared function retains up to INITIAL_SINK_CAPACITY * LIST_CLEAR_THRESHOLD bytes.
-    private static final int INITIAL_SINK_CAPACITY = 512;
-    private static final int LIST_CLEAR_THRESHOLD = 64;
     private final Function arg;
     private final char delimiter;
     private final int functionPosition;
     private final int maxBytes;
-    private final ObjList<DirectUtf16Sink> sinks = new ObjList<>();
-    private int sinkIndex = 0;
-    private int touchedMemorySize;
+
+    private final GroupByCharSink sinkA = new GroupByCharSink();
+    private final GroupByCharSink sinkB = new GroupByCharSink();
+
     private int valueIndex;
+    private int totalMemoryUsed;
 
     public StringAggGroupByFunction(Function arg, int functionPosition, char delimiter, int maxBytes) {
         this.arg = arg;
         this.delimiter = delimiter;
-        this.maxBytes = maxBytes;
         this.functionPosition = functionPosition;
+        this.maxBytes = maxBytes;
     }
 
     @Override
     public void clear() {
-        // Free extra sinks.
-        touchedMemorySize = 0;
-        if (sinks.size() > LIST_CLEAR_THRESHOLD) {
-            for (int i = sinks.size() - 1; i > LIST_CLEAR_THRESHOLD - 1; i--) {
-                Misc.free(sinks.getQuick(i));
-                sinks.remove(i);
-            }
-        }
-        // Reset capacity on the remaining ones.
-        for (int i = 0, n = sinks.size(); i < n; i++) {
-            DirectUtf16Sink sink = sinks.getQuick(i);
-            if (sink != null) {
-                sink.resetCapacity();
-                touchedMemorySize += sink.size();
-            }
-        }
-        sinkIndex = 0;
+        sinkA.of(0);
+        sinkB.of(0);
+        totalMemoryUsed = 0;
     }
 
     @Override
     public void close() {
-        Misc.freeObjListAndClear(sinks);
+        sinkA.clear();
+        sinkB.clear();
     }
 
     @Override
     public void computeFirst(MapValue mapValue, Record record, long rowId) {
-        final DirectUtf16Sink sink;
-        if (sinks.size() <= sinkIndex) {
-            sinks.extendAndSet(sinkIndex, sink = new DirectUtf16Sink(INITIAL_SINK_CAPACITY));
-        } else {
-            sink = sinks.getQuick(sinkIndex);
-            sink.clear();
-        }
+        final var str = arg.getStrA(record);
 
-        final CharSequence str = arg.getStrA(record);
         if (str != null) {
-            sink.put(str);
+            //Non-null value: initialize a new sink
+            sinkA.of(0);
+            sinkA.put(str);
+
+            totalMemoryUsed += sinkA.length() * 2;  // UTF-16: 2 bytes per char
+            assertSizeCompliance();
+
+            // Store the sink pointer in the map at valueIndex
+            mapValue.putLong(valueIndex, sinkA.ptr());
+            // Store false at valueIndex+1 to indicate group is not null
             mapValue.putBool(valueIndex + 1, false);
         } else {
+            // First value is null: mark group as null
+            mapValue.putLong(valueIndex, 0);
             mapValue.putBool(valueIndex + 1, true);
         }
-        mapValue.putInt(valueIndex, sinkIndex++);
-        touchedMemorySize += sink.size();
-        assertTouchedMemoryCompliance();
     }
 
     @Override
     public void computeNext(MapValue mapValue, Record record, long rowId) {
-        final DirectUtf16Sink sink = sinks.getQuick(mapValue.getInt(valueIndex));
-        final CharSequence str = arg.getStrA(record);
-        if (str != null) {
-            final int hi = sink.size();
-            final boolean nullValue = mapValue.getBool(valueIndex + 1);
-            if (!nullValue) {
-                sink.putAscii(delimiter);
-            }
-            sink.put(str);
+        final var str = arg.getStrA(record);
+
+        if (str == null) {
+            return;
+        }
+
+        final var ptr = mapValue.getLong(valueIndex);
+        final var wasNull = mapValue.getBool(valueIndex + 1);
+
+        if (wasNull || ptr == 0) {
+            // First non-null value for this group
+            sinkA.of(0);
+            sinkA.put(str);
+
+            totalMemoryUsed += sinkA.length() * 2;
+            assertSizeCompliance();
+
+            mapValue.putLong(valueIndex, sinkA.ptr());
             mapValue.putBool(valueIndex + 1, false);
-            touchedMemorySize += sink.size() - hi;
-            assertTouchedMemoryCompliance();
+        } else {
+            // Group already has data, append with delimiter
+            sinkA.of(ptr);
+
+            int oldLen = sinkA.length();
+
+            sinkA.putAscii(delimiter);
+            sinkA.put(str);
+
+            int newLen = sinkA.length();
+            int addedChars = newLen - oldLen;
+            totalMemoryUsed += addedChars * 2;
+            assertSizeCompliance();
+
+            mapValue.putLong(valueIndex, sinkA.ptr());
         }
     }
 
-    private void assertTouchedMemoryCompliance() {
-        if (touchedMemorySize > maxBytes) {
-            throw CairoException.nonCritical().position(functionPosition)
-                    .put("string_agg() result exceeds max size of ").put(maxBytes).put(" bytes");
+    @Override
+    public void merge(MapValue destValue, MapValue srcValue) {
+        final long srcPtr = srcValue.getLong(valueIndex);
+        final boolean isSrcNull = srcValue.getBool(valueIndex + 1);
+
+        if (isSrcNull || srcPtr == 0) {
+            // nothing to do
+            return;
         }
+
+        final long destPtr = destValue.getLong(valueIndex);
+        final boolean isDestNull = destValue.getBool(valueIndex + 1);
+
+        if (isDestNull || destPtr == 0) {
+            // just copy source pointer
+            destValue.putLong(valueIndex, srcPtr);
+            destValue.putBool(valueIndex + 1, false);
+            return;
+        }
+
+        // If we got here, both groups have data - need to merge them
+        sinkA.of(destPtr);
+        sinkB.of(srcPtr);
+
+        sinkA.putAscii(delimiter);
+        sinkA.put(sinkB);
+
+        // Update destination with merged result pointer
+        destValue.putLong(valueIndex, sinkA.ptr());
     }
 
     @Override
@@ -137,16 +169,26 @@ class StringAggGroupByFunction extends StrFunction implements UnaryFunction, Gro
 
     @Override
     public CharSequence getStrA(Record rec) {
-        final boolean nullValue = rec.getBool(valueIndex + 1);
-        if (nullValue) {
-            return null;
+        final boolean isNull = rec.getBool(valueIndex + 1);
+        final long ptr = rec.getLong(valueIndex);
+
+        if (isNull || ptr == 0) {
+            return "";
         }
-        return sinks.getQuick(rec.getInt(valueIndex));
+
+        return sinkA.of(ptr);
     }
 
     @Override
     public CharSequence getStrB(Record rec) {
-        return getStrA(rec);
+        final boolean isNull = rec.getBool(valueIndex + 1);
+        final long ptr = rec.getLong(valueIndex);
+
+        if (isNull || ptr == 0) {
+            return "";
+        }
+
+        return sinkB.of(ptr);
     }
 
     @Override
@@ -162,8 +204,10 @@ class StringAggGroupByFunction extends StrFunction implements UnaryFunction, Gro
     @Override
     public void initValueTypes(ArrayColumnTypes columnTypes) {
         this.valueIndex = columnTypes.getColumnCount();
-        columnTypes.add(ColumnType.INT); // sink index
-        columnTypes.add(ColumnType.BOOLEAN); // null flag
+        // Reserve slot at valueIndex for sink pointer
+        columnTypes.add(ColumnType.LONG);
+        // Reserve slot at valueIndex+1 for null flag
+        columnTypes.add(ColumnType.BOOLEAN);
     }
 
     @Override
@@ -182,8 +226,35 @@ class StringAggGroupByFunction extends StrFunction implements UnaryFunction, Gro
     }
 
     @Override
+    public void setEmpty(MapValue mapValue) {
+        mapValue.putLong(valueIndex, 0);
+        mapValue.putBool(valueIndex + 1, true);
+    }
+
+    @Override
     public boolean supportsParallelism() {
-        return false;
+        return true;
+    }
+
+    @Override
+    public boolean isThreadSafe() {
+        return UnaryFunction.super.isThreadSafe();
+    }
+
+    @Override
+    public void setAllocator(GroupByAllocator allocator) {
+        sinkA.setAllocator(allocator);
+        sinkB.setAllocator(allocator);
+    }
+
+    private void assertSizeCompliance() {
+        if (totalMemoryUsed > maxBytes) {
+            throw CairoException.nonCritical()
+                    .position(functionPosition)
+                    .put("string_agg() result exceeds max size of ")
+                    .put(maxBytes)
+                    .put(" bytes");
+        }
     }
 
     @Override
@@ -194,6 +265,5 @@ class StringAggGroupByFunction extends StrFunction implements UnaryFunction, Gro
     @Override
     public void toTop() {
         UnaryFunction.super.toTop();
-        sinkIndex = 0;
     }
 }
