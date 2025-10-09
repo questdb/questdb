8ca493ba7b58bf49b5d8ef0d40312c4da144ff2d
Andrei Pechkurov <apechkurov@gmail.com>
2025-10-09 15:19:12 +0300

    Add clarifying comment

--- core/src/main/java/io/questdb/cairo/pool/AbstractPool.java
+++ core/src/main/java/io/questdb/cairo/pool/AbstractPool.java
@@ -44,6 +44,7 @@ public abstract class AbstractPool implements Closeable {
     private final long inactiveTtlUs;
     @SuppressWarnings({"FieldCanBeLocal", "FieldMayBeFinal"})
     private volatile int closed = FALSE;
+    // keep volatile to avoid races around listener clean up in tests
     private volatile PoolListener eventListener;
 
     public AbstractPool(CairoConfiguration configuration, long inactiveTtlMillis) {
