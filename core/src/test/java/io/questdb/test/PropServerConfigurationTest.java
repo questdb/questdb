62cca92209f7638ee4465783f02e0cd7fd4720e5
Vlad Ilyushchenko <bluestreak@gmail.com>
2025-10-10 14:16:12 +0100

    fix tests

--- core/src/test/java/io/questdb/test/PropServerConfigurationTest.java
+++ core/src/test/java/io/questdb/test/PropServerConfigurationTest.java
@@ -483,7 +483,7 @@ public class PropServerConfigurationTest {
         Assert.assertTrue(configuration.getWalApplyPoolConfiguration().isEnabled());
         Assert.assertFalse(configuration.getWalApplyPoolConfiguration().haltOnError());
         Assert.assertEquals("wal-apply", configuration.getWalApplyPoolConfiguration().getPoolName());
-        Assert.assertEquals(2, configuration.getWalApplyPoolConfiguration().getWorkerCount());
+        Assert.assertEquals(4, configuration.getWalApplyPoolConfiguration().getWorkerCount());
         Assert.assertEquals(10, configuration.getWalApplyPoolConfiguration().getSleepTimeout());
         Assert.assertEquals(7_000, configuration.getWalApplyPoolConfiguration().getNapThreshold());
         Assert.assertEquals(10_000, configuration.getWalApplyPoolConfiguration().getSleepThreshold());
