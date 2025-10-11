21eb4152fcfcca379f9b041d8d525207e6ae9c40
Andrei Pechkurov <apechkurov@gmail.com>
2025-10-11 15:32:26 +0300

    perf(sql): lazy merge in parallel count_distinct functions

---
+++ core/src/test/java/io/questdb/test/griffin/engine/groupby/GroupByLongListTest.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ *     ___                  _   ____  ____
+ *    / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *   | | | | | | |/ _ \/ __| __| | | |  _ \
+ *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *    \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ *  Copyright (c) 2014-2019 Appsicle
+ *  Copyright (c) 2019-2024 QuestDB
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
+
+package io.questdb.test.griffin.engine.groupby;
+
+import io.questdb.griffin.engine.groupby.FastGroupByAllocator;
+import io.questdb.griffin.engine.groupby.GroupByAllocator;
+import io.questdb.griffin.engine.groupby.GroupByLongList;
+import io.questdb.std.Numbers;
+import io.questdb.test.AbstractCairoTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GroupByLongListTest extends AbstractCairoTest {
+
+    @Test
+    public void testSmoke() throws Exception {
+        assertMemoryLeak(() -> {
+            try (GroupByAllocator allocator = new FastGroupByAllocator(64, Numbers.SIZE_1GB)) {
+                GroupByLongList list = new GroupByLongList(16);
+                list.setAllocator(allocator);
+                list.of(0);
+
+                final int N = 1000;
+
+                for (int i = 0; i < N; i++) {
+                    list.add(i);
+                }
+                Assert.assertEquals(N, list.size());
+                Assert.assertTrue(list.capacity() >= N);
+                for (int i = 0; i < N; i++) {
+                    Assert.assertEquals(i, list.get(i));
+                }
+            }
+        });
+    }
+}
