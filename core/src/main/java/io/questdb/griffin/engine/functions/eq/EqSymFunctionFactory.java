a74ff2b87e4dbd32a2de91e05f40295f7bc8c423
Andrei Pechkurov <apechkurov@gmail.com>
2025-10-28 12:14:51 +0200

    tmp

--- core/src/main/java/io/questdb/griffin/engine/functions/eq/EqSymFunctionFactory.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/eq/EqSymFunctionFactory.java
@@ -28,11 +28,16 @@ import io.questdb.cairo.CairoConfiguration;
 import io.questdb.cairo.sql.Function;
 import io.questdb.cairo.sql.Record;
 import io.questdb.cairo.sql.StaticSymbolTable;
+import io.questdb.cairo.sql.SymbolTableSource;
 import io.questdb.griffin.FunctionFactory;
+import io.questdb.griffin.SqlException;
 import io.questdb.griffin.SqlExecutionContext;
 import io.questdb.griffin.engine.functions.SymbolFunction;
 import io.questdb.std.Chars;
+import io.questdb.std.DirectIntIntHashMap;
 import io.questdb.std.IntList;
+import io.questdb.std.MemoryTag;
+import io.questdb.std.Misc;
 import io.questdb.std.ObjList;
 import io.questdb.std.Transient;
 
@@ -58,25 +63,42 @@ public class EqSymFunctionFactory implements FunctionFactory {
     ) {
         final Function leftFunc = args.getQuick(0);
         final Function rightFunc = args.getQuick(1);
-        if (getStaticSymbolTable(leftFunc) != null && getStaticSymbolTable(rightFunc) != null) {
-            return new Func(leftFunc, rightFunc);
+        if (isSymbolTableStatic(leftFunc) && isSymbolTableStatic(rightFunc)) {
+            return new Func(configuration, leftFunc, rightFunc);
         }
         // SYMBOL cannot be constant, so we can't use any of the half-constant functions
         return new EqSymStrFunctionFactory.Func(leftFunc, rightFunc);
     }
 
-    private static StaticSymbolTable getStaticSymbolTable(Function symFunc) {
-        return ((SymbolFunction) symFunc).getStaticSymbolTable();
+    private static boolean isSymbolTableStatic(Function symFunc) {
+        return ((SymbolFunction) symFunc).isSymbolTableStatic();
     }
 
     private static class Func extends AbstractEqBinaryFunction {
-        private final StaticSymbolTable leftTable;
-        private final StaticSymbolTable rightTable;
+        private final DirectIntIntHashMap lookupCache;
+        private StaticSymbolTable leftTable;
+        private StaticSymbolTable rightTable;
 
-        public Func(Function left, Function right) {
+        public Func(CairoConfiguration configuration, Function left, Function right) {
             super(left, right);
-            this.leftTable = getStaticSymbolTable(left);
-            this.rightTable = getStaticSymbolTable(right);
+            this.lookupCache = new DirectIntIntHashMap(
+                    configuration.getSqlSmallMapKeyCapacity(),
+                    configuration.getSqlFastMapLoadFactor(),
+                    0,
+                    MemoryTag.NATIVE_UNORDERED_MAP
+            );
+        }
+
+        @Override
+        public void close() {
+            super.close();
+            Misc.free(lookupCache);
+        }
+
+        @Override
+        public void cursorClosed() {
+            super.cursorClosed();
+            lookupCache.restoreInitialCapacity();
         }
 
         @Override
@@ -91,6 +113,18 @@ public class EqSymFunctionFactory implements FunctionFactory {
                 return negated != (b == null);
             }
             return negated != Chars.equalsNc(a, b);
+        }
+
+        @Override
+        public void init(SymbolTableSource symbolTableSource, SqlExecutionContext executionContext) throws SqlException {
+            super.init(symbolTableSource, executionContext);
+            this.leftTable = ((SymbolFunction) left).getStaticSymbolTable();
+            this.rightTable = ((SymbolFunction) right).getStaticSymbolTable();
+        }
+
+        @Override
+        public boolean isThreadSafe() {
+            return false;
         }
     }
 }
