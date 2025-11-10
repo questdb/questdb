c78ee31f8de1b8821a0e4e0a4e31529b29f45af3
Mircea Cadariu <cadariu.mircea@gmail.com>
2025-11-10 10:35:02 +0000

    Implement last_not_null(array)

--- core/src/main/java/io/questdb/griffin/engine/functions/groupby/LastArrayGroupByFunction.java
+++ core/src/main/java/io/questdb/griffin/engine/functions/groupby/LastArrayGroupByFunction.java
@@ -40,9 +40,9 @@ import io.questdb.std.Numbers;
 import org.jetbrains.annotations.NotNull;
 
 public class LastArrayGroupByFunction extends ArrayFunction implements GroupByFunction, UnaryFunction {
-    private final Function arg;
-    private final GroupByArraySink sink;
-    private int valueIndex;
+    protected final Function arg;
+    protected final GroupByArraySink sink;
+    protected int valueIndex;
 
     public LastArrayGroupByFunction(@NotNull Function arg) {
         this.arg = arg;
