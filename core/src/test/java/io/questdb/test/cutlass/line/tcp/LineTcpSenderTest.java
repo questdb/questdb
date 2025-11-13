96b46a0fe2affd6d8c325aeb12edf251a7e562b8
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-11-13 10:20:24 +0100

    test(ilp): fix race condition in testSymbolCapacityReloadFuzz()
    Easy to reproduce spurious failure. It's sufficient to inject
    randomized pauses to Sender threads:
    ```java
    for (int i = 0; i < N; i++) {
        sender1.table("mytable")
                .symbol("sym1", rnd1.nextString(10))
                .symbol("sym2", rnd1.nextString(2))
                .doubleColumn("dd", rnd1.nextDouble())
                .atNow();
        if (i % 1000 == 0) {
            Os.sleep(ThreadLocalRandom.current().nextInt(10));
        }
    }
    sender1.flush();
    ```
    With this extra sleep the test fails consistently on my local.
    
    Root cause: The test uses ILP@TCP - so sender.flush() only guarantees
    ILP lines are successfully written to TCP send buffer. There can be
    an arbitrary long pause between the flush and when the just
    flushed lines are observed by TCP receiver, let alone WAL apply job.
    
    The fix acknowledges this delay and relaxes the assertion. It also
    runs the WalApply job even after the main WAL Apply thread is done,
    becasue the WAL Apply Thread might finish before it had a chance
    to observe all lines.

--- core/src/test/java/io/questdb/test/cutlass/line/tcp/LineTcpSenderTest.java
+++ core/src/test/java/io/questdb/test/cutlass/line/tcp/LineTcpSenderTest.java
@@ -74,6 +74,7 @@ import java.util.function.Consumer;
 import static io.questdb.client.Sender.*;
 import static io.questdb.test.cutlass.http.line.LineHttpSenderTest.createDoubleArray;
 import static io.questdb.test.tools.TestUtils.assertContains;
+import static io.questdb.test.tools.TestUtils.assertEventually;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.*;
 
@@ -1579,15 +1580,6 @@ public class LineTcpSenderTest extends AbstractLineTcpReceiverTest {
                     Sender sender1 = Sender.fromConfig(confString);
                     Sender sender2 = Sender.fromConfig(confString)
             ) {
-                SOCountDownLatch writerIsBack = new SOCountDownLatch(1);
-                engine.setPoolListener(
-                        (factoryType, thread, tableToken, event, segment, position) -> {
-                            if (factoryType == PoolListener.SRC_WRITER && event == PoolListener.EV_RETURN) {
-                                writerIsBack.countDown();
-                            }
-                        }
-                );
-
                 AtomicBoolean walRunning = new AtomicBoolean(true);
                 SOCountDownLatch doneAll = new SOCountDownLatch(3);
                 SOCountDownLatch doneILP = new SOCountDownLatch(2);
@@ -1661,17 +1653,18 @@ public class LineTcpSenderTest extends AbstractLineTcpReceiverTest {
 
                 Assert.assertEquals(0, errorCount.get());
 
-                // make sure to assert before closing the Sender
-                // since the Sender will always flush on close
-                writerIsBack.await();
-                TestUtils.assertSql(
-                        engine,
-                        sqlExecutionContext,
-                        "select count() from mytable",
-                        sink,
-                        "count\n" +
-                                N * 2 + "\n"
-                );
+
+                assertEventually(() -> {
+                    drainWalQueue();
+                    TestUtils.assertSql(
+                            engine,
+                            sqlExecutionContext,
+                            "select count() from mytable",
+                            sink,
+                            "count\n" +
+                                    N * 2 + "\n"
+                    );
+                });
             }
         });
     }
