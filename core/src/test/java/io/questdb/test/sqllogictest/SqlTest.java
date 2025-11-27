f173d7f7caa8c7d120246c8c981e7287fe32d8ce
Marko Topolnik <marko.topolnik@gmail.com>
2025-11-27 14:19:45 +0100

    Remove temporary code

--- core/src/test/java/io/questdb/test/sqllogictest/SqlTest.java
+++ core/src/test/java/io/questdb/test/sqllogictest/SqlTest.java
@@ -26,7 +26,6 @@ package io.questdb.test.sqllogictest;
 
 import org.junit.runners.Parameterized;
 
-import java.util.Arrays;
 import java.util.Collection;
 
 /**
@@ -39,7 +38,6 @@ public class SqlTest extends AbstractSqllogicTestRunner {
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> files() {
-        return Arrays.asList(new Object[][]{{"/sql/test_stddev.test"}});
-//        return files("sql");
+        return files("sql");
     }
 }
