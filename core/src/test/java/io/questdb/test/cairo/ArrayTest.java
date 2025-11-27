3a3a938a3804b4ca499a7b3646db8e29252935f3
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-11-27 13:23:37 +0100

    syntax fix

--- core/src/test/java/io/questdb/test/cairo/ArrayTest.java
+++ core/src/test/java/io/questdb/test/cairo/ArrayTest.java
@@ -882,8 +882,7 @@ public class ArrayTest extends AbstractCairoTest {
                        [ [ 1,  2,  3], [ 4,  5,  6], [ 7,  8,  9] ],
                        [ [10, 11, 12], [13, 14, 15], [16, 17, 18] ],
                        [ [19, 20, 21], [22, 23, 24], [25, 26, 27] ]
-                    ] arr from long_sequence(1)););
-                    """);
+                    ] arr from long_sequence(1));""");
             assertExceptionNoLeakCheck("SELECT arr[1, 3.0] subarr FROM tango;",
                     14, "invalid type for array access [type=DOUBLE]"
             );
