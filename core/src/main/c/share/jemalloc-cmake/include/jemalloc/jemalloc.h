7a1b834dd4202e701ee18bf8717fccbee64a31b4
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-11-29 21:19:00 +0100

    no dot prefix jemalloc

--- core/src/main/c/share/jemalloc-cmake/include/jemalloc/jemalloc.h
+++ core/src/main/c/share/jemalloc-cmake/include/jemalloc/jemalloc.h
@@ -70,32 +70,32 @@ extern "C" {
  * these macro definitions.
  */
 #ifndef JEMALLOC_NO_RENAME
-#  define je_aligned_alloc je_aligned_alloc
-#  define je_calloc je_calloc
-#  define je_dallocx je_dallocx
-#  define je_free je_free
-#  define je_free_sized je_free_sized
-#  define je_free_aligned_sized je_free_aligned_sized
-#  define je_mallctl je_mallctl
-#  define je_mallctlbymib je_mallctlbymib
-#  define je_mallctlnametomib je_mallctlnametomib
-#  define je_malloc je_malloc
-#  define je_malloc_conf je_malloc_conf
-#  define je_malloc_conf_2_conf_harder je_malloc_conf_2_conf_harder
-#  define je_malloc_message je_malloc_message
-#  define je_malloc_stats_print je_malloc_stats_print
-#  define je_malloc_usable_size je_malloc_usable_size
-#  define je_mallocx je_mallocx
-#  define je_smallocx_8d8379da443f46dc976252b968cb9ca8e63ec974 je_smallocx_8d8379da443f46dc976252b968cb9ca8e63ec974
-#  define je_nallocx je_nallocx
-#  define je_posix_memalign je_posix_memalign
-#  define je_rallocx je_rallocx
-#  define je_realloc je_realloc
-#  define je_sallocx je_sallocx
-#  define je_sdallocx je_sdallocx
-#  define je_xallocx je_xallocx
-#  define je_valloc je_valloc
-#  define je_malloc_size je_malloc_size
+#  define je_aligned_alloc aligned_alloc
+#  define je_calloc calloc
+#  define je_dallocx dallocx
+#  define je_free free
+#  define je_free_sized free_sized
+#  define je_free_aligned_sized free_aligned_sized
+#  define je_mallctl mallctl
+#  define je_mallctlbymib mallctlbymib
+#  define je_mallctlnametomib mallctlnametomib
+#  define je_malloc malloc
+#  define je_malloc_conf malloc_conf
+#  define je_malloc_conf_2_conf_harder malloc_conf_2_conf_harder
+#  define je_malloc_message malloc_message
+#  define je_malloc_stats_print malloc_stats_print
+#  define je_malloc_usable_size malloc_usable_size
+#  define je_mallocx mallocx
+#  define je_smallocx_8d8379da443f46dc976252b968cb9ca8e63ec974 smallocx_8d8379da443f46dc976252b968cb9ca8e63ec974
+#  define je_nallocx nallocx
+#  define je_posix_memalign posix_memalign
+#  define je_rallocx rallocx
+#  define je_realloc realloc
+#  define je_sallocx sallocx
+#  define je_sdallocx sdallocx
+#  define je_xallocx xallocx
+#  define je_valloc valloc
+#  define je_malloc_size malloc_size
 #endif
 
 #include <stdlib.h>
