5e3a0e9eb070f7de363b24a74f43977522e856b8
Andrei Pechkurov <apechkurov@gmail.com>
2026-01-19 10:15:54 +0200

    Make close in AsyncTopKRecordCursor and AsyncFilteredNegativeLimitRecordCursor consistent with other cursors

--- core/src/main/java/io/questdb/griffin/engine/table/AsyncFilteredRecordCursor.java
+++ core/src/main/java/io/questdb/griffin/engine/table/AsyncFilteredRecordCursor.java
@@ -409,17 +409,17 @@ class AsyncFilteredRecordCursor implements RecordCursor {
     }
 
     void of(PageFrameSequence<?> frameSequence, long rowsRemaining) {
-        isOpen = true;
+        this.isOpen = true;
         this.frameSequence = frameSequence;
         this.rowsRemaining = rowsRemaining;
-        ogRowsRemaining = rowsRemaining;
+        this.ogRowsRemaining = rowsRemaining;
         // put a cap the number of in-flight page frame tasks in case of LIMIT N query
-        dispatchLimit = rowsRemaining != Long.MAX_VALUE ? defaultDispatchLimit : Integer.MAX_VALUE;
-        frameIndex = -1;
-        frameLimit = -1;
-        frameRowIndex = -1;
-        frameRowCount = -1;
-        allFramesActive = true;
+        this.dispatchLimit = rowsRemaining != Long.MAX_VALUE ? defaultDispatchLimit : Integer.MAX_VALUE;
+        this.frameIndex = -1;
+        this.frameLimit = -1;
+        this.frameRowIndex = -1;
+        this.frameRowCount = -1;
+        this.allFramesActive = true;
         frameMemoryPool.of(frameSequence.getPageFrameAddressCache());
         record.of(frameSequence.getSymbolTableSource());
         if (recordB != null) {
