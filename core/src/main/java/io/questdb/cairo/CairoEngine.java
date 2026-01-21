3a7fcea23d5537583d546d99f3537261ab68149a
glasstiger <imre@questdb.io>
2026-01-21 10:01:50 +0000

    chore(core): cairo engine refactoring to enable custom table name registry and view graphs

--- core/src/main/java/io/questdb/cairo/CairoEngine.java
+++ core/src/main/java/io/questdb/cairo/CairoEngine.java
@@ -231,8 +231,8 @@ public class CairoEngine implements Closeable, WriterSource {
             this.queryRegistry = new QueryRegistry(configuration);
             this.rootExecutionContext = createRootExecutionContext();
             this.matViewTimerQueue = createMatViewTimerQueue();
-            this.matViewGraph = new MatViewGraph();
-            this.viewGraph = new ViewGraph();
+            this.matViewGraph = createMatViewGraph();
+            this.viewGraph = createViewGraph();
             this.frameFactory = new FrameFactory(configuration);
             this.dataID = DataID.open(configuration);
 
@@ -244,9 +244,7 @@ public class CairoEngine implements Closeable, WriterSource {
 
             // Migrate database files.
             EngineMigration.migrateEngineTo(this, ColumnType.VERSION, ColumnType.MIGRATION_VERSION, false);
-            tableNameRegistry = configuration.isReadOnlyInstance()
-                    ? new TableNameRegistryRO(this, tableFlagResolver)
-                    : new TableNameRegistryRW(this, tableFlagResolver);
+            tableNameRegistry = createTableNameRegistry(configuration, tableFlagResolver);
             tableNameRegistry.reload();
 
             this.sqlCompilerPool = new SqlCompilerPool(this);
@@ -604,7 +602,6 @@ public class CairoEngine implements Closeable, WriterSource {
     ) {
         return createTable(securityContext, mem, path, ifNotExists, struct, keepLock, false, TableUtils.TABLE_KIND_REGULAR_TABLE);
     }
-
 
     public @NotNull TableToken createTable(
             SecurityContext securityContext,
@@ -2172,6 +2169,10 @@ public class CairoEngine implements Closeable, WriterSource {
         return token;
     }
 
+    protected @NotNull MatViewGraph createMatViewGraph() {
+        return new MatViewGraph();
+    }
+
     // used in ent
     protected MatViewStateStore createMatViewStateStore() {
         return configuration.isMatViewEnabled() ? new MatViewStateStoreImpl(this) : NoOpMatViewStateStore.INSTANCE;
@@ -2186,11 +2187,21 @@ public class CairoEngine implements Closeable, WriterSource {
         return new SqlExecutionContextImpl(this, 0).with(AllowAllSecurityContext.INSTANCE);
     }
 
+    protected @NotNull TableNameRegistry createTableNameRegistry(CairoConfiguration configuration, TableFlagResolver tableFlagResolver) {
+        return configuration.isReadOnlyInstance()
+                ? new TableNameRegistryRO(this, tableFlagResolver)
+                : new TableNameRegistryRW(this, tableFlagResolver);
+    }
+
     protected @NotNull <T extends AbstractTelemetryTask> Telemetry<T> createTelemetry(
             Telemetry.TelemetryTypeBuilder<T> builder,
             CairoConfiguration configuration
     ) {
         return new Telemetry<>(builder, configuration);
+    }
+
+    protected @NotNull ViewGraph createViewGraph() {
+        return new ViewGraph();
     }
 
     protected Iterable<FunctionFactory> getFunctionFactories() {
