abd283c57b09d907d0052e16c77f74f78fb7ca07
glasstiger <imre@questdb.io>
2026-02-23 15:03:35 +0000

    fixing tests

--- core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
+++ core/src/test/java/io/questdb/test/cutlass/pgwire/PGJobContextTest.java
@@ -131,10 +131,9 @@ import java.util.stream.Stream;
 import static io.questdb.PropertyKey.CAIRO_WRITER_ALTER_BUSY_WAIT_TIMEOUT;
 import static io.questdb.PropertyKey.CAIRO_WRITER_ALTER_MAX_WAIT_TIMEOUT;
 import static io.questdb.cairo.sql.SqlExecutionCircuitBreaker.TIMEOUT_FAIL_ON_FIRST_CHECK;
-import static io.questdb.test.tools.TestUtils.*;
-import static io.questdb.test.tools.TestUtils.assertEquals;
+import static io.questdb.test.tools.TestUtils.assertContains;
+import static io.questdb.test.tools.TestUtils.createTimestamp;
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 /**
  * This class contains tests which replay PGWIRE traffic.
@@ -3602,8 +3601,8 @@ if __name__ == "__main__":
                 {"drop table exists doesnt", "ERROR: table and column names that are SQL keywords have to be enclosed in double quotes, such as \"exists\""},
                 {"drop table if exists", "ERROR: table name expected"},
                 {"drop table if exists;", "ERROR: table name expected"},
-                {"drop all table if exists;", "ERROR: ';' or 'tables' expected"},
-                {"drop all tables if exists;", "ERROR: ';' or 'tables' expected"},
+                {"drop all table if exists;", "ERROR: unexpected token [table]"},
+                {"drop all tables if exists;", "ERROR: unexpected token [if]"},
                 {"drop database ;", "ERROR: 'table' or 'view' or 'materialized view' or 'all' expected"}
         };
         assertWithPgServer(CONN_AWARE_ALL, (connection, binary, mode, port) -> {
@@ -7248,7 +7247,7 @@ nodejs code:
                     final PreparedStatement copy = connection.prepareStatement("copy x from '/test-numeric-headers.csv' with header true");
                     final ResultSet ignore = copy.executeQuery()
             ) {
-                assertEventually(() -> {
+                TestUtils.assertEventually(() -> {
                     try (
                             final PreparedStatement select = connection.prepareStatement("select * from x");
                             final ResultSet rs = select.executeQuery()
@@ -13296,7 +13295,7 @@ create table tab as (
                         stmt.executeQuery();
                         Assert.fail("Exception is not thrown");
                     } catch (PSQLException ex) {
-                        ex.printStackTrace();
+                        ex.printStackTrace(System.out);
                         // expected
                         Assert.assertNotNull(ex);
                     }
