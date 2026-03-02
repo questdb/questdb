b8ead62cc93920da9278dbecd8b55ce655191f90
Marko Topolnik <marko.topolnik@gmail.com>
2026-03-02 16:59:46 +0100

    Lint fix

--- core/src/main/java/io/questdb/cutlass/qwp/server/QwpTudCache.java
+++ core/src/main/java/io/questdb/cutlass/qwp/server/QwpTudCache.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/******************************************************************************
  *     ___                  _   ____  ____
  *    / _ \ _   _  ___  ___| |_|  _ \| __ )
  *   | | | | | | |/ _ \/ __| __| | | |  _ \
@@ -6,7 +6,7 @@
  *    \__\_\\__,_|\___||___/\__|____/|____/
  *
  *  Copyright (c) 2014-2019 Appsicle
- *  Copyright (c) 2019-2024 QuestDB
+ *  Copyright (c) 2019-2026 QuestDB
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
