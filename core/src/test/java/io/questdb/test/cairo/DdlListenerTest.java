281a674b58e9f673db119701df763686e3acfcca
glasstiger <imre@questdb.io>
2026-03-09 00:37:35 +0000

    improve DROP ALL test assertion

--- core/src/test/java/io/questdb/test/cairo/DdlListenerTest.java
+++ core/src/test/java/io/questdb/test/cairo/DdlListenerTest.java
@@ -43,6 +43,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static io.questdb.cairo.TableUtils.TABLE_DOES_NOT_EXIST;
+import static io.questdb.test.tools.TestUtils.assertContains;
 import static io.questdb.test.tools.TestUtils.assertEquals;
 
 public class DdlListenerTest extends AbstractCairoTest {
@@ -922,14 +923,10 @@ public class DdlListenerTest extends AbstractCairoTest {
                 Assert.fail("expected CairoException for listener failures");
             } catch (Exception e) {
                 // DROP ALL collects listener failures and reports them
-                Assert.assertEquals(
-                        "[-1] Failures while dropping tables, views and materialized views [" +
-                                "'tab3': listener error on tab3, " +
-                                "'tab2': listener error on tab2, " +
-                                "'tab1': listener error on tab1" +
-                                "]",
-                        e.getMessage()
-                );
+                assertContains(e.getMessage(), "Failures while dropping tables, views and materialized views [");
+                assertContains(e.getMessage(), "'tab1': listener error on tab1");
+                assertContains(e.getMessage(), "'tab2': listener error on tab2");
+                assertContains(e.getMessage(), "'tab3': listener error on tab3");
             }
 
             // All 3 tables must have been dropped despite the listener throwing on each one
