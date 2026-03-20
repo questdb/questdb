9025b6b5ddbaa23d671826fb59b7fe5024afaa4c
glasstiger <imre@questdb.io>
2026-03-20 00:10:58 +0000

    model toSink() to include storage policy

--- core/src/main/java/io/questdb/griffin/engine/ops/CreateTableOperationBuilderImpl.java
+++ core/src/main/java/io/questdb/griffin/engine/ops/CreateTableOperationBuilderImpl.java
@@ -31,6 +31,7 @@ import io.questdb.cairo.TableUtils;
 import io.questdb.griffin.SqlCompiler;
 import io.questdb.griffin.SqlException;
 import io.questdb.griffin.SqlExecutionContext;
+import io.questdb.griffin.engine.table.ShowCreateTableRecordCursorFactory;
 import io.questdb.griffin.model.CreateTableColumnModel;
 import io.questdb.griffin.model.ExpressionNode;
 import io.questdb.griffin.model.QueryModel;
@@ -41,10 +42,9 @@ import io.questdb.std.LowerCaseCharSequenceObjHashMap;
 import io.questdb.std.Mutable;
 import io.questdb.std.ObjList;
 import io.questdb.std.str.CharSink;
+import io.questdb.std.str.Sinkable;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import static io.questdb.griffin.engine.table.ShowCreateTableRecordCursorFactory.ttlToSink;
 
 public class CreateTableOperationBuilderImpl implements CreateTableOperationBuilder, Mutable {
     private static final IntList castGroups = new IntList();
@@ -59,6 +59,7 @@ public class CreateTableOperationBuilderImpl implements CreateTableOperationBuil
     private int maxUncommittedRows;
     private long o3MaxLag = -1;
     private ExpressionNode partitionByExpr;
+    private Sinkable ttlToSinkOverride;
     // transient field, unoptimized AS SELECT model, used in toSink()
     private QueryModel selectModel;
     private CharSequence selectText;
@@ -166,6 +167,7 @@ public class CreateTableOperationBuilderImpl implements CreateTableOperationBuil
         maxUncommittedRows = 0;
         o3MaxLag = -1;
         partitionByExpr = null;
+        ttlToSinkOverride = null;
         tableNameExpr = null;
         timestampExpr = null;
         selectText = null;
@@ -242,6 +244,14 @@ public class CreateTableOperationBuilderImpl implements CreateTableOperationBuil
         return walEnabled;
     }
 
+    void ttlToSink(CharSink<?> sink) {
+        if (ttlToSinkOverride != null) {
+            ttlToSinkOverride.toSink(sink);
+        } else {
+            ShowCreateTableRecordCursorFactory.ttlToSink(ttlHoursOrMonths, sink);
+        }
+    }
+
     public void setBatchO3MaxLag(long batchO3MaxLag) {
         this.batchO3MaxLag = batchO3MaxLag;
     }
@@ -272,6 +282,10 @@ public class CreateTableOperationBuilderImpl implements CreateTableOperationBuil
 
     public void setPartitionByExpr(ExpressionNode partitionByExpr) {
         this.partitionByExpr = partitionByExpr;
+    }
+
+    public void setTtlToSinkOverride(Sinkable ttlToSinkOverride) {
+        this.ttlToSinkOverride = ttlToSinkOverride;
     }
 
     @Override
@@ -382,11 +396,11 @@ public class CreateTableOperationBuilderImpl implements CreateTableOperationBuil
         }
         if (partitionByExpr != null) {
             sink.putAscii(" partition by ").put(partitionByExpr.token);
+            ttlToSink(sink);
             if (walEnabled) {
                 sink.putAscii(" wal");
             }
         }
-        ttlToSink(ttlHoursOrMonths, sink);
         if (volumeAlias != null) {
             sink.putAscii(" in volume '").put(volumeAlias).putAscii('\'');
         }
