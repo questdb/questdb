83358160ce3c0aa47e065ee7c6329ea250b3382d
Jaromir Hamala <jaromir.hamala@gmail.com>
2026-05-19 14:23:31 +0200

    test to override all Record methods

--- core/src/test/java/io/questdb/test/cairo/sql/DelegatingRecordTest.java
+++ core/src/test/java/io/questdb/test/cairo/sql/DelegatingRecordTest.java
@@ -26,58 +26,23 @@ package io.questdb.test.cairo.sql;
 
 import io.questdb.cairo.sql.DelegatingRecord;
 import io.questdb.cairo.sql.Record;
-import io.questdb.std.str.StringSink;
-import io.questdb.std.str.Utf16Sink;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 public class DelegatingRecordTest {
 
     @Test
-    public void testDelegatesArrayDouble1d2d() {
-        final DelegatingRecord record = new DelegatingRecord();
-        record.of(new Record() {
-            @Override
-            public double getArrayDouble1d2d(int col, int columnType, int idx0, int idx1) {
-                Assert.assertEquals(1, col);
-                Assert.assertEquals(2, columnType);
-                Assert.assertEquals(3, idx0);
-                Assert.assertEquals(4, idx1);
-                return 42.5;
+    public void testOverridesAllRecordMethods() throws Exception {
+        for (Method method : Record.class.getMethods()) {
+            if (method.getDeclaringClass() != Record.class || method.isSynthetic() || Modifier.isStatic(method.getModifiers())) {
+                continue;
             }
-        });
+            final Method delegatingMethod = DelegatingRecord.class.getMethod(method.getName(), method.getParameterTypes());
 
-        Assert.assertEquals(42.5, record.getArrayDouble1d2d(1, 2, 3, 4), 0.0);
-    }
-
-    @Test
-    public void testDelegatesLongIPv4() {
-        final DelegatingRecord record = new DelegatingRecord();
-        record.of(new Record() {
-            @Override
-            public long getLongIPv4(int col) {
-                Assert.assertEquals(1, col);
-                return 0x01020304L;
-            }
-        });
-
-        Assert.assertEquals(0x01020304L, record.getLongIPv4(1));
-    }
-
-    @Test
-    public void testDelegatesVarcharUtf16Sink() {
-        final DelegatingRecord record = new DelegatingRecord();
-        record.of(new Record() {
-            @Override
-            public void getVarchar(int col, Utf16Sink utf16Sink) {
-                Assert.assertEquals(1, col);
-                utf16Sink.put("fast");
-            }
-        });
-        final StringSink sink = new StringSink();
-
-        record.getVarchar(1, sink);
-
-        Assert.assertEquals("fast", sink.toString());
+            Assert.assertNotEquals(method.toGenericString(), Record.class, delegatingMethod.getDeclaringClass());
+        }
     }
 }
