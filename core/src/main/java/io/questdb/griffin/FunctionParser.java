25b390d1bc37bb7d74951e9b9cc973b768353f1d
Raphael DALMON <raphael.dalmon@gmail.com>
2026-08-03 15:30:43 +0200

    feat(core): add decimal casting support in FunctionParser

--- core/src/main/java/io/questdb/griffin/FunctionParser.java
+++ core/src/main/java/io/questdb/griffin/FunctionParser.java
@@ -1307,6 +1307,8 @@ public class FunctionParser implements PostOrderTreeTraversalAlgo.Visitor, Mutab
             case ColumnType.CHAR:
                 if (toType == ColumnType.SYMBOL) {
                     return new CastCharToSymbolFunctionFactory.Func(function);
+                } else if (ColumnType.isDecimal(toType)) {
+                    return CastStrToDecimalFunctionFactory.newInstance(sqlExecutionContext.getDecimal256(), position, toType, function);
                 }
                 break;
             case ColumnType.IPv4:
