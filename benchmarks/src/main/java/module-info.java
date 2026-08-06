e645dd3211046b557239dbb562b03bdeaff64eba
Jaromir Hamala <jaromir.hamala@gmail.com>
2026-08-06 18:25:53 +0200

    chore(build): prepare for 10.0.1

--- benchmarks/src/main/java/module-info.java
+++ benchmarks/src/main/java/module-info.java
@@ -30,7 +30,7 @@ module io.questdb.benchmarks {
     requires simpleclient.common;
     requires org.jetbrains.annotations;
     requires java.sql;
-    requires questdb.client;
+    requires io.questdb.client;
 
     exports org.questdb.jmh_generated;
 }
--- utils/src/main/java/module-info.java
+++ utils/src/main/java/module-info.java
@@ -2,7 +2,7 @@
 open module io.questdb.cliutil {
     uses io.questdb.griffin.FunctionFactory;
     requires transitive io.questdb;
-    requires static questdb.client;
+    requires static io.questdb.client;
     requires com.google.gson;
     requires java.sql;
     exports io.questdb.cliutil;
